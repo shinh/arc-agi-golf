214cd09e5539fa7dd76ea46613a13058ced13951
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-04 05:00:15 +0900

    Add solution for task 397

---
+++ logic/task397.py
@@ -0,0 +1,10 @@
+def p(g):
+ o=[r[:]for r in g];h=len(g)
+ for y in range(h-1):
+  for x in range(len(g[0])-1):
+   a=g[y][x:x+2]+g[y+1][x:x+2]
+   if 0 not in a:
+    for i in range(len(set(a))):
+     r=y+2+i
+     if r<h:o[r][x]=o[r][x+1]=3
+ return o
