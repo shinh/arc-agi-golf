9def999a5473c62c07d0ef73a865e6e2ff3780cc
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-03 14:36:24 +0900

    Initial commit

---
+++ code_golf_utils.py
@@ -0,0 +1,255 @@
+# Copyright 2025 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Module containing utilities for the 2025 Google Code Golf Championship."""
+
+import copy
+import importlib.util
+import json
+import os
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+#code_golf_dir = "/kaggle/input/google-code-golf-2025/"
+code_golf_dir = "./tasks/"
+libraries = ["collections", "itertools", "math", "operator", "re", "string",
+             "struct"]
+colors = [
+    (0, 0, 0),
+    (30, 147, 255),
+    (250, 61, 49),
+    (78, 204, 48),
+    (255, 221, 0),
+    (153, 153, 153),
+    (229, 59, 163),
+    (255, 133, 28),
+    (136, 216, 241),
+    (147, 17, 49),
+]
+task_zero = {
+    "train": [{
+        "input": [
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+            [5, 1, 1, 1, 1, 1, 1, 5, 5, 5],
+            [5, 1, 1, 1, 1, 1, 1, 5, 5, 5],
+            [5, 1, 1, 1, 1, 1, 1, 5, 5, 5],
+            [5, 1, 1, 1, 1, 1, 1, 5, 5, 5],
+            [5, 1, 1, 1, 1, 1, 1, 5, 5, 5],
+            [5, 1, 1, 1, 1, 1, 1, 5, 5, 5],
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+        ],
+        "output": [
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+            [5, 1, 1, 1, 1, 1, 1, 5, 5, 5],
+            [5, 1, 1, 1, 1, 1, 1, 0, 5, 5],
+            [5, 1, 1, 1, 1, 1, 1, 0, 5, 5],
+            [5, 1, 1, 1, 1, 1, 1, 0, 5, 5],
+            [5, 1, 1, 1, 1, 1, 1, 0, 5, 5],
+            [5, 1, 1, 1, 1, 1, 1, 0, 5, 5],
+            [5, 5, 0, 0, 0, 0, 0, 0, 5, 5],
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+        ],
+    }],
+    "test": [{
+        "input": [
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+            [5, 5, 4, 4, 4, 4, 4, 4, 5, 5],
+            [5, 5, 4, 4, 4, 4, 4, 4, 5, 5],
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+            [5, 5, 4, 4, 4, 4, 4, 5, 5, 5],
+            [5, 5, 4, 5, 5, 5, 4, 5, 5, 5],
+            [5, 5, 4, 5, 5, 5, 4, 5, 5, 5],
+            [5, 5, 4, 4, 4, 4, 4, 5, 5, 5],
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+        ],
+        "output": [
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+            [5, 5, 4, 4, 4, 4, 4, 4, 5, 5],
+            [5, 5, 4, 4, 4, 4, 4, 4, 0, 5],
+            [5, 5, 5, 0, 0, 0, 0, 0, 0, 5],
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+            [5, 5, 4, 4, 4, 4, 4, 5, 5, 5],
+            [5, 5, 4, 0, 0, 0, 4, 0, 5, 5],
+            [5, 5, 4, 0, 5, 5, 4, 0, 5, 5],
+            [5, 5, 4, 4, 4, 4, 4, 0, 5, 5],
+            [5, 5, 5, 0, 0, 0, 0, 0, 5, 5],
+        ],
+    }],
+    "arc-gen": [{
+        "input": [
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+            [5, 5, 2, 2, 2, 2, 2, 2, 5, 5],
+            [5, 5, 2, 5, 5, 5, 5, 2, 5, 5],
+            [5, 5, 2, 5, 5, 5, 5, 2, 5, 5],
+            [5, 5, 2, 5, 5, 5, 5, 2, 5, 5],
+            [5, 5, 2, 5, 5, 5, 5, 2, 5, 5],
+            [5, 5, 2, 2, 2, 2, 2, 2, 5, 5],
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+        ],
+        "output": [
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+            [5, 5, 2, 2, 2, 2, 2, 2, 5, 5],
+            [5, 5, 2, 0, 0, 0, 0, 2, 0, 5],
+            [5, 5, 2, 0, 5, 5, 5, 2, 0, 5],
+            [5, 5, 2, 0, 5, 5, 5, 2, 0, 5],
+            [5, 5, 2, 0, 5, 5, 5, 2, 0, 5],
+            [5, 5, 2, 2, 2, 2, 2, 2, 0, 5],
+            [5, 5, 5, 0, 0, 0, 0, 0, 0, 5],
+            [5, 5, 5, 5, 5, 5, 5, 5, 5, 5],
+        ],
+    }],
+}
+
+
+def load_examples(task_num):
+  """Loads relevant data from ARC-AGI and ARC-GEN."""
+  if not task_num:
+    return task_zero
+  with open(code_golf_dir + f"task{task_num:03d}.json.gz") as f:
+    examples = json.load(f)
+  return examples
+
+
+def show_legend():
+  image = [[(255, 255, 255) for _ in range(21)] for _ in range(3)]
+  for idx, color in enumerate(colors):
+    image[1][2 * idx + 1] = color
+  fig = plt.figure(figsize=(10, 5))
+  ax = fig.add_axes([0, 0, 1, 1])
+  ax.imshow(np.array(image))
+  for idx, _ in enumerate(colors):
+    color = "white" if idx in [0, 9] else "black"
+    ax.text(2 * idx + 0.9, 1.1, str(idx), color=color)
+  ax.set_xticks([])
+  ax.set_yticks([])
+
+
+def show_examples(examples, bgcolor=(255, 255, 255)):
+  # Determine the dimensions of the image to be rendered.
+  width, height, offset = 0, 0, 1
+  for example in examples:
+    grid, output = example["input"], example["output"]
+    width += len(grid[0]) + 1 + len(output[0]) + 4
+    height = max(height, max(len(grid), len(output)) + 4)
+  # Determine the contents of the image.
+  image = [[bgcolor for _ in range(width)] for _ in range(height)]
+  for example in examples:
+    grid, output = example["input"], example["output"]
+    grid_width, output_width = len(grid[0]), len(output[0])
+    for r, row in enumerate(grid):
+      for c, cell in enumerate(row):
+        image[r + 2][offset + c + 1] = colors[cell]
+    offset += grid_width + 1
+    for r, row in enumerate(output):
+      for c, cell in enumerate(row):
+        image[r + 2][offset + c + 1] = colors[cell]
+    offset += output_width + 4
+  # Draw the image.
+  fig = plt.figure(figsize=(10, 5))
+  ax = fig.add_axes([0, 0, 1, 1])
+  ax.imshow(np.array(image))
+  # Draw the horizontal and vertical lines.
+  offset = 1
+  for example in examples:
+    grid, output = example["input"], example["output"]
+    grid_width, grid_height = len(grid[0]), len(grid)
+    output_width, output_height = len(output[0]), len(output)
+    ax.hlines([r + 1.5 for r in range(grid_height+1)],
+              xmin=offset+0.5, xmax=offset+grid_width+0.5, color="black")
+    ax.vlines([offset + c + 0.5 for c in range(grid_width+1)],
+              ymin=1.5, ymax=grid_height+1.5, color="black")
+    offset += grid_width + 1
+    ax.hlines([r + 1.5 for r in range(output_height+1)],
+              xmin=offset+0.5, xmax=offset+output_width+0.5, color="black")
+    ax.vlines([offset + c + 0.5 for c in range(output_width+1)],
+              ymin=1.5, ymax=output_height+1.5, color="black")
+    offset += output_width + 2
+    ax.vlines([offset+0.5], ymin=-0.5, ymax=height-0.5, color="black")
+    offset += 2
+  ax.set_xticks([])
+  ax.set_yticks([])
+
+
+def verify_program(task_num, examples):
+  task_name, task_path = "task_with_imports", "/kaggle/working/task.py"
+  module_path = "/kaggle/working/task_with_imports.py"
+  with open(task_path, "r") as file:
+    file_content = file.read()
+    if "import" in file_content:
+      print("Error: Imports are not permitted")
+      return
+  with open(module_path, "w") as file:
+    for library in libraries:
+      file.write(f"from {library} import *\n")
+    file.write(file_content)
+  spec = importlib.util.spec_from_file_location(task_name, module_path)
+  if spec is None:
+    print("Error: Unable to import task.py.")
+    return
+  module = sys.modules[task_name] = importlib.util.module_from_spec(spec)
+  spec.loader.exec_module(module)
+  if not hasattr(module, "p"):
+    print("Error: Unable to locate function p() in task.py.")
+    return
+  program = getattr(module, "p")
+  if not callable(program):
+    print("Error: Function p() in task.py is not callable.")
+    return
+  print()
+  def verify(example_subset):
+    right, wrong, expected = 0, 0, None
+    for example in example_subset:
+      example_copy = copy.deepcopy(example)
+      try:
+        if program(example_copy["input"]) == example_copy["output"]:
+          right += 1
+        else:
+          expected = copy.deepcopy(example)
+          wrong += 1
+      except:
+        wrong += 1
+    return right, wrong, expected
+  arc_agi_right, arc_agi_wrong, arc_agi_expected = verify(examples["train"] + examples["test"])
+  arc_gen_right, arc_gen_wrong, arc_gen_expected = verify(examples["arc-gen"])
+  print(f"Results on ARC-AGI exaples: {arc_agi_right} pass, {arc_agi_wrong} fail")
+  print(f"Results on ARC-GEN exaples: {arc_gen_right} pass, {arc_gen_wrong} fail")
+  print()
+  if arc_agi_wrong + arc_gen_wrong == 0:
+    task_length = os.path.getsize(task_path)
+    print("Your code IS READY for submission!")
+    print("Its length appears to be " + str(task_length) + " bytes.")
+    print("Next steps:")
+    print(" * Copy it into a file named task{:03d}.py on your local machine.".format(task_num))
+    print(" * Create a zip file containing that program along with all others.")
+    print(" * Submit that zip file to the Kaggle competition so that it can be officially scored.")
+  else:
+    print("Your code IS NOT ready for submission.")
+    expected = arc_agi_expected if arc_agi_expected else arc_gen_expected
+    if not expected: return
+    actual = {}
+    actual["input"] = expected["input"]
+    actual["output"] = program(copy.deepcopy(expected["input"]))
+    print("The expected result is shown in green; your actual result is shown in red.")
+    show_examples([expected], bgcolor=(200, 255, 200))
+    show_examples([actual], bgcolor=(255, 200, 200))
---
+++ describe.py
@@ -0,0 +1,81 @@
+import argparse
+import gzip
+import json
+import os
+
+import matplotlib.pyplot as plt
+
+import code_golf_utils
+
+
+def save(task_id, out_dir, skip_savefig):
+    task_id = "%03d" % task_id
+    js = json.load(gzip.open(os.path.join("tasks", "task" + task_id + ".json.gz")))
+    data = []
+    for kind in ["train", "test", "arc-gen"]:
+        data.extend(js[kind])
+
+    input_hs = []
+    input_ws = []
+    output_hs = []
+    output_ws = []
+    io_ratio_hs = []
+    io_ratio_ws = []
+    for d in data:
+        ib = d["input"]
+        ob = d["output"]
+        input_hs.append(len(ib))
+        input_ws.append(len(ib[0]))
+        output_hs.append(len(ob))
+        output_ws.append(len(ob[0]))
+        io_ratio_hs.append(len(ob) / len(ib))
+        io_ratio_ws.append(len(ob[0]) / len(ib[0]))
+
+    info = []
+    if len(set(input_hs)) == 1 and len(set(input_ws)) == 1:
+        info.append(f"static input size {input_ws[0]}x{input_hs[0]}")
+    if len(set(output_hs)) == 1 and len(set(output_ws)) == 1:
+        info.append(f"static output size {output_ws[0]}x{output_hs[0]}")
+
+    if not info:
+        if len(set(io_ratio_hs)) == 1 and len(set(io_ratio_ws)) == 1:
+            info.append(f"static output/input ratio {io_ratio_ws[0]}x{io_ratio_hs[0]}")
+
+    comment = ""
+    if os.path.exists(os.path.join(out_dir, f"{task_id}.txt")):
+        with open(os.path.join(out_dir, f"{task_id}.txt")) as f:
+            comment = f.read().strip()
+
+    print(f"Task {task_id}: {', '.join(info)} {comment}")
+
+    html = "<h3>Task %s</h3>" % task_id
+    html += "<div>" + ", ".join(info) + "</div>"
+    html += "<div>" + comment + "</div>"
+    html += f"<div><img src='{task_id}-train.png'></div>"
+    html += f"<div><img src='{task_id}-arc-gen.png'></div>"
+    open(os.path.join(out_dir, f"{task_id}.html"), "w").write(html)
+
+    if not skip_savefig:
+        examples = code_golf_utils.load_examples(int(task_id))
+        code_golf_utils.show_examples(examples['train'][0:5])
+        plt.savefig(os.path.join(out_dir, f"{task_id}-train.png"))
+        code_golf_utils.show_examples(examples['arc-gen'][0:5])
+        plt.savefig(os.path.join(out_dir, f"{task_id}-arc-gen.png"))
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Show code golf examples.")
+    parser.add_argument("task_id")
+    parser.add_argument("out")
+    parser.add_argument("--skip_savefig", action="store_true")
+    args = parser.parse_args()
+
+    if args.task_id == "all":
+        for task_id in range(1, 401):
+            save(task_id, f"{args.out}", args.skip_savefig)
+    else:
+        save(int(args.task_id), args.out, args.skip_savefig)
+
+
+if __name__ == "__main__":
+    main()
