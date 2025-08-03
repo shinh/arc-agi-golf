db8e68685429a1cf39b4db4d5bd0ffae19be639e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-04 08:53:44 +0900

    Add a script to extract dsls

---
+++ extract_dsl.py
@@ -0,0 +1,80 @@
+import re
+
+
+def reindent(code):
+    lines = []
+    cur_indent = 0
+    prev_indent = 0
+    indents = {}
+    for line in code.splitlines():
+        line = re.sub(r"#.*", "", line)
+        line = line.rstrip()
+        if not line:
+            continue
+
+        n = len(line) - len(line.lstrip())
+        if n > prev_indent:
+            cur_indent += 1
+        elif n < prev_indent:
+            cur_indent = indents[n]
+        prev_indent = n
+        indents[n] = cur_indent
+
+        lines.append(" " * cur_indent + line.lstrip())
+    return "\n".join(lines)
+
+
+def concat(name, defs, reqs_map, seen):
+    code = ""
+    for r in reqs_map[name]:
+        if r not in defs:
+            continue
+        if r in seen:
+            continue
+        seen.add(r)
+        code += concat(r, defs, reqs_map, seen)
+    code += defs[name]
+    return code
+
+
+def main():
+    defs = {}
+    cur_func = None
+    for line in open("dsl.py").readlines():
+        if m := re.match(r"^([A-Z_]+) = (.*)", line):
+            defs[m.group(1)] = m.group(0)
+        elif m := re.match(r"^def (\w+)\(", line):
+            cur_func = m.group(1)
+            defs[cur_func] = line
+        elif line.strip() == "":
+            cur_func = None
+        elif cur_func:
+            defs[cur_func] += line
+
+    for name, func in defs.items():
+        func, _ = re.subn(r": [A-Z]\w+", "", func)
+        func, _  = re.subn(r"\) -> [A-Z]\w+", ")", func)
+        func = reindent(func)
+        defs[name] = func
+
+    reqs_map = {}
+    for name, func in defs.items():
+        reqs = []
+        for m in re.findall(r"([A-Z_]+|[a-z]+\()", func):
+            if m.endswith("("):
+                m = m[:-1]
+            if m != name:
+                reqs.append(m)
+        reqs_map[name] = set(reqs)
+
+    for task_id in range(1, 401):
+        task_id_str = f"{task_id:03d}"
+        name = "verify_task" + task_id_str
+
+        func = concat(name, defs, reqs_map, set())
+
+        open(f"dsls/task{task_id_str}.py", "w").write(func)
+
+
+if __name__ == "__main__":
+    main()
