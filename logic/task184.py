9eba555bd6cda18af514cce3da13aca499f85896
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-04 05:14:04 +0900

    Implement cluster color extraction for task184

--- logic/task184.py
+++ logic/task184.py
@@ -1,13 +1,15 @@
 def p(g):
-    o=[];s=1
-    for r in g:
-        if all(v==0 for v in r):s=1
-        elif s:
-            u=[];p0=0
-            for v in r:
-                if v:
-                    if p0==0 or v!=p0:u.append(v)
-                    p0=v
-                else:p0=0
-            o.append(u);s=0
+    h=len(g);w=len(g[0])
+    R=[-1]+[i for i,r in enumerate(g)if not any(r)]+[h]
+    C=[-1]+[i for i in range(w)if not any(r[i]for r in g)]+[w]
+    o=[];m=len(C)-1
+    for i in range(len(R)-1):
+        x=[]
+        for j in range(m):
+            u=[]
+            for r in g[R[i]+1:R[i+1]]:
+                for v in r[C[j]+1:C[j+1]]:
+                    if v and v not in u:u+=v,
+            x+=u if m==1 else [u[0]]
+        o+=x,
     return o
