c78b40f1067640e663cd9df868d548d382ae59b0
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-03 19:57:24 +0900

    Add solutions for tasks 287-289

---
+++ logic/task287.py
@@ -0,0 +1,8 @@
+def p(g):
+    n=len(g);w=len(g[0]);o=[r[:] for r in g]
+    for i in range(n//2):
+        j=n-1-i;r1=g[i];r2=g[j]
+        if r1==r1[::-1]:o[i]=o[j]=r1
+        elif r2==r2[::-1]:o[i]=o[j]=r2
+        else:s=r1[:w//2];o[i]=o[j]=s+s[::-1]
+    return o
