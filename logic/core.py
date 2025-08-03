865b2de181f0508c8cd76ff1e05c657bd4ef7cbe
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-04 04:10:19 +0900

    Move debug.py

--- logic/core.py
+++ logic/core.py
@@ -1,9 +1,2 @@
 def create(rows, cols):
     return [[0 for _ in range(cols)] for _ in range(rows)]
-
-def show(g):
-    for r in g:
-        a=[]
-        for c in r:
-            a.append(str(c))
-        print("".join(a))
---
+++ logic/debug.py
@@ -0,0 +1,6 @@
+def show(g):
+    for r in g:
+        a=[]
+        for c in r:
+            a.append(str(c))
+        print("".join(a))
