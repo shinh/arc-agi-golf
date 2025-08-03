e6dcf0518a04a680713e0fead73fbbbb04476524
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-04 03:32:42 +0900

    Add solutions for tasks 236, 237, 239

---
+++ logic/task237.py
@@ -0,0 +1,9 @@
+def p(g):
+    h=len(g);w=len(g[0])
+    o=[r[:]for r in g]
+    for y,r in enumerate(g):
+        for x,c in enumerate(r):
+            if c:
+                for i in range(x,w):o[y][i]=c
+                for j in range(y,h):o[j][w-1]=c
+    return o
