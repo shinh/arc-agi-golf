ca3580dc56fba0a2c7de2f436e7ed69ec08a79db
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-04 04:04:41 +0900

    Show TODO

--- submit.py
+++ submit.py
@@ -4,11 +4,19 @@ import os
 import code_golf_utils
 
 
-def submit(task_id):
+def submit(task_id, skip_verify=False):
+    if skip_verify:
+        return
+
     print("Submitting task", task_id)
 
+    core = open("logic/core.py").read()
+
+    if not os.path.exists(f"logic/task{task_id:03d}.py"):
+        open(f"reports/task{task_id:03d}.txt", "w").write("TODO")
+        return
+
     try:
-        core = open("logic/core.py").read()
         logic = open(f"logic/task{task_id:03d}.py").read()
         code = core + "\n" + logic
         task_path = f"submissions/task{task_id:03d}.py"
@@ -23,14 +31,37 @@ def submit(task_id):
         open(f"reports/task{task_id:03d}.txt", "w").write("ERROR")
 
 
+def report():
+    score = 0
+    fail_tests = []
+    error_tests = []
+    todo_tests = []
+    for task_id in range(1, 401):
+        result = open(f"reports/task{task_id:03d}.txt").read()
+        if result == "FAIL":
+            fail_tests.append(task_id)
+        elif result == "ERROR":
+            error_tests.append(task_id)
+        elif result == "TODO":
+            todo_tests.append(task_id)
+        else:
+            score += int(result)
+    print(f"Failed tests: {len(fail_tests)}")
+    print(f"Error tests: {len(error_tests)}")
+    print(f"TODO tests: {len(todo_tests)}")
+    print(f"Total score: {score}")
+
+
 def main():
     parser = argparse.ArgumentParser(description="Submit code golf.")
     parser.add_argument("task_id")
+    parser.add_argument("--skip_verify", action="store_true")
     args = parser.parse_args()
 
     if args.task_id == "all":
         for task_id in range(1, 401):
-            submit(task_id)
+            submit(task_id, args.skip_verify)
+        report()
     else:
         submit(int(args.task_id))
 
