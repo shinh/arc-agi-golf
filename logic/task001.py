3b4bb8b13604e31f414f9dd56673d7b01f43206a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-04 22:48:39 +0900

    Golf task001

--- logic/task001.py
+++ logic/task001.py
@@ -1,8 +1,2 @@
 def p(g):
-    o=create(9,9)
-    for y in range(9):
-        for x in range(9):
-            c=g[y//3][x//3]
-            if c:
-                o[y][x]=g[y%3][x%3]
-    return o
+    return[[(g[r%3][c%3]if g[r//3][c//3]else 0)for c in range(9)]for r in range(9)]
