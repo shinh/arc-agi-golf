077cd8c5bcefd3ffd18908a5dccc40456cc11ce2
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-04 10:53:36 +0900

    Implement tasks 284 and 321

--- logic/task284.py
+++ logic/task284.py
@@ -1,2 +1,15 @@
 def p(g):
+    pts=[(i,j,v)for i,r in enumerate(g)for j,v in enumerate(r)if v]
+    (r1,c1,v1),(r2,c2,v2)=pts
+    t=abs(r1-r2)<abs(c1-c2)
+    if t:
+        g=[list(r)for r in zip(*g)];r1,c1,r2,c2=c1,r1,c2,r2
+    if r1>r2:r1,c1,v1,r2,c2,v2=r2,c2,v2,r1,c1,v1
+    m=(r1+r2)//2;c=c1
+    for i in range(r1,r2+1):g[i][c]=v2
+    for i in range(r1,m+1):g[i][c]=v1
+    for d in(-2,2):g[m][c+d]=v1;g[m+1][c+d]=v2
+    for j in range(c-2,c+3):g[m-1][j]=v1;g[m+2][j]=v2
+    g[m][c]=g[m+1][c]=0
+    if t:g=[list(r)for r in zip(*g)]
     return g
--- logic/task321.py
+++ logic/task321.py
@@ -1,2 +1,12 @@
 def p(g):
-    return g
+    a=[r[:4]for r in g];b=[r[5:9]for r in g];c=[r[10:]for r in g]
+    s=lambda x:set(sum(x,[]))
+    t=(s(a)&s(b)&s(c)).pop()
+    u=[(s(a)-{t}).pop(),(s(b)-{t}).pop(),(s(c)-{t}).pop()]
+    o=[[t]*4 for _ in range(4)]
+    for i in range(4):
+        for j in range(4):
+            if c[i][j]==u[2]:o[i][j]=u[2]
+            if b[i][j]==u[1]:o[i][j]=u[1]
+            if a[i][j]==u[0]:o[i][j]=u[0]
+    return o
