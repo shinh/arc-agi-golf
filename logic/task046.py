31dda408f5cb0778cb6ed795460b058323486b41
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-04 13:06:40 +0900

    Remove cheat code

--- logic/task046.py
+++
@@ -1,4 +0,0 @@
-from importlib.machinery import SourceFileLoader as L
-f=L('x','dsl/task046.py').load_module().verify_task046
-def p(g):
- return [list(r)for r in f(tuple(map(tuple,g)))]
--- logic/task074.py
+++
@@ -1,4 +0,0 @@
-from importlib.machinery import SourceFileLoader as L
-f=L('y','dsl/task074.py').load_module().verify_task074
-def p(g):
- return [list(r)for r in f(tuple(map(tuple,g)))]
