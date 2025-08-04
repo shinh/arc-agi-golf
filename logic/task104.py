2a6e078ad3585fecc42ef0ec8c168bc387bb8a75
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-04 14:30:31 +0900

    refactor task104 solution

--- logic/task104.py
+++ logic/task104.py
@@ -1,11 +1,6 @@
 def p(g):
-    a=b=0
-    for i,r in enumerate(g):
-        for j,v in enumerate(r):
-            if v==2:a,b=i,j
-    d=1-(a<2 and g[a+1][b]==3);e=1-(b<2 and g[a][b+1]==3)
-    r=5*(1-d);c=5*(1-e)
-    o=[[0]*9 for _ in range(9)]
-    for x,y in((r,c),(r+(4 if d else -4),c+(4 if e else -4))):
-        for i in range(x,x+4):o[i][y:y+4]=[3]*4
+    a=1-g[0][1]//3;b=1-g[1][0]//3
+    c=5*b+a*(4-8*b);s=[3]*4;o=[[0]*9 for _ in[0]*9]
+    for x,y in((a,c),(a+4,c+4-8*(a^b))):
+        for i in range(4):o[x+i][y:y+4]=s
     return o
