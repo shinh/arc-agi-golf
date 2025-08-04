5a6f69673d97a741562980181a69c25d1cb3fb6a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-04 13:40:12 +0900

    Fix task77 bounding box fill

--- logic/task077.py
+++ logic/task077.py
@@ -1,6 +1,17 @@
 def p(g):
-    for y,r in enumerate(g):
-        for x,v in enumerate(r):
-            if v!=2 and any(0<=y+dy<len(g) and 0<=x+dx<len(r) and g[y+dy][x+dx]==2 for dy in(-1,0,1) for dx in(-1,0,1) if dx or dy):
-                if v in {1,3,8}:g[y][x]=4
+    c=min({v for r in g for v in r},key=lambda v:sum(r.count(v)for r in g))
+    s={(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==c}
+    o=s.copy();u=set()
+    while s:
+        q=[s.pop()];t=set(q)
+        while q:
+            y,x=q.pop()
+            for Y in range(y-2,y+3):
+                for X in range(x-2,x+3):
+                    p=(Y,X)
+                    if p in s:s.remove(p);q.append(p);t.add(p)
+        ys,xs=zip(*t)
+        u|={(y,x)for y in range(min(ys),max(ys)+1)for x in range(min(xs),max(xs)+1)}
+    for y,x in u:g[y][x]=4
+    for y,x in o:g[y][x]=c
     return g
