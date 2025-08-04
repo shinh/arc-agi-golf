cfd0eb28521376b8792d34bd0a72feb5b7175259
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-04 10:53:30 +0900

    Implement tasks 377 and 378

--- logic/task377.py
+++ logic/task377.py
@@ -1,2 +1,7 @@
 def p(g):
+ u=lambda a:[r for i,r in enumerate(a)if r not in a[:i]]
+ g=u(g);g=list(map(list,zip(*g)))
+ g=u(g);g=list(map(list,zip(*g)))
+ g+=g[-2::-1];g=list(map(list,zip(*g)))
+ g+=g[-2::-1]
  return g
