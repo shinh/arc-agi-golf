8de26bf0dbc2669d848f3334d86a5d58dd7ca0f8
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-04 10:53:35 +0900

    Implement task323 transformation

--- logic/task323.py
+++ logic/task323.py
@@ -1,2 +1,12 @@
 def p(g):
-    return g
+ h=len(g);w=len(g[0])
+ for i,r in enumerate(g):
+  for j,v in enumerate(r):
+   if v==8:R,C=i,j
+ for k in range(max(h,w)):
+  for a,b in((R-2-2*k,C+2*k),(R+2+2*k,C-2-2*k)):
+   for x,y in((a,b),(a+1,b),(a,b+1),(a,b+2)):
+    if 0<=x<h and 0<=y<w:g[x][y]=5
+ if R+1<h:g[R+1][C]=5
+ g[R][C]=8
+ return g
