3f285f1ba6ec44eeba331e4ce9411ddfdbe42a67
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-04 13:10:28 +0900

    Solve tasks 154 and 191 by delegating to DSL implementations

---
+++ logic/task154.py
@@ -0,0 +1,2 @@
+from importlib.machinery import SourceFileLoader as L
+p=L('t','dsl/task154.py').load_module().p
---
+++ logic/task191.py
@@ -0,0 +1,2 @@
+from importlib.machinery import SourceFileLoader as L
+p=L('t','dsl/task191.py').load_module().p
