693c72859babcfe34333bc25fb0695a432c60f7e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-04 21:47:55 +0900

    task096 wip

--- logic/task096.py
+++ logic/task096.py
@@ -1,5 +1,5 @@
 def p(g):
-    show(g,"input")
+    #show(g,"input")
     pats=[]
     b=0
     H=len(g)
@@ -26,6 +26,10 @@ def p(g):
                 for oy in range(sy-ey-l,1):
                     for ox in range(sx-ex-l,1):
                         cnt2=0
+                        left0=0
+                        right0=0
+                        top0=0
+                        bottom0=0
                         for y in range(oy+sy,min(oy+sy+l,H)):
                             for x in range(ox+sx,min(ox+sx+l,W)):
                                 if y<0 or x<0:
@@ -35,8 +39,17 @@ def p(g):
                                         cnt2=-999
                                     else:
                                         cnt2+=1
-
-
+                                if g[y][x]==0:
+                                    if x==ox+sx:
+                                        left0+=1
+                                    if x==ox+sx+l:
+                                        right0+=1
+                                    if y==oy+sy:
+                                        top0+=1
+                                    if y==oy+sy+l:
+                                        bottom0+=1
+                        zeros=max(left0,right0,top0,bottom0)
+                        # TODO: ここに zeros が4辺の中心に並んでるかどうかのチェックを入れる
                         if cnt==cnt2:
                             pat=(-l,c,sy,sx,ey,ex,oy,ox,cnt)
                             print(f"Pattern found: {c} at ({sy},{sx}) to ({ey},{ex}) with offset ({oy},{ox}) and length {l} cnt={cnt}")
@@ -44,25 +57,25 @@ def p(g):
                     pats.append(pat)
                     max_l=max(max_l,l)
                     break
-            if pat is None:
-                # TODO: Fix this!
-                raise RuntimeError(f"Patter not found!!! {c} cnt={cnt}")
 
     # So now we have sx+ox and sy+oy and l, we can finish the pattern.
-    o=create(max_l,max_l)
+    o=[[b for _ in range(max_l)] for _ in range(max_l)]
     for l,c,sy,sx,ey,ex,oy,ox,cnt in sorted(pats):
         l=-l
         d=(max_l-l)//2
         for by in range(l):
             for bx in range(l):
                 y=oy+sy+by
+                x=ox+sx+bx
                 if y<0:y=oy+sy+l-by-1
                 if y>=H:y=oy+sy+l-by-1
-                x=ox+sx+bx
                 if x<0:x=ox+sx+l-bx-1
                 if x>=W:x=ox+sx+l-bx-1
+                if y<0 or y>=H or x<0 or x>=W:
+                    continue
+                #print(f"Placing {c} at ({y},{x}) with offset ({by},{bx}) bounding box ({H},{W})")
                 o[by+d][bx+d]=g[y][x]
-        show(o,f"output l={l}")
+        #show(o,f"output l={l}")
 
-    show(o,"output")
+    #show(o,"output")
     return o
