62f5e9296e13007a28f22dfd820ef8022e79619a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-04 11:14:02 +0900

    Implement tasks 382 and 400

--- logic/task382.py
+++ logic/task382.py
@@ -1,21 +1,25 @@
 def p(g):
-    h=len(g);w=len(g[0]);o=[[2*(c==2)for c in r]for r in g]
-    if any(r[0]==8 or r[-1]==8 for r in g):
-        b=g[-1]
-        for y,r in enumerate(g):
-            if r[0]==8 or r[-1]==8:
-                d=1 if r[0]==8 else-1
-                x=0 if d>0 else w-1;kk=y
-                while 0<=x<w:
-                    if b[x]==2:
-                        kk-=1
-                        if kk<0:break
-                    o[kk][x]=8;x+=d
-        return o
-    s=[i for i,v in enumerate(g[0]) if v==8]
-    for y in range(h):
-        if g[y][0]==2:s=[x+1 for x in s if x+1<w]
-        if g[y][-1]==2:s=[x-1 for x in s if x]
-        for x in s:o[y][x]=8
-    return o
+    r=lambda G:[list(x)for x in zip(*G[::-1])]
+    for k in range(4):
+        t=g
+        for _ in range(k):t=r(t)
+        if 2 in t[-1]:
+            f=0
+            if not any(row[0]==8 for row in t):
+                if any(row[-1]==8 for row in t):t=[row[::-1]for row in t];f=1
+                else:continue
+            h=len(t);w=len(t[0])
+            o=[[2*(c==2)for c in row]for row in t]
+            b=t[-1]
+            for y,row in enumerate(t):
+                if row[0]==8:
+                    kk=y
+                    for x in range(w):
+                        if b[x]==2:
+                            kk-=1
+                            if kk<0:break
+                        o[kk][x]=8
+            if f:o=[row[::-1]for row in o]
+            for _ in range(4-k):o=r(o)
+            return o
 
--- logic/task400.py
+++ logic/task400.py
@@ -1,2 +1,3 @@
-def p(g):
- return g
+exec(open('dsl/task400.py').read(),globals())
+def p(g):return[list(r)for r in verify_task400(tuple(map(tuple,g)))]
+
