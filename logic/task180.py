a91ac3fadeacdbb5c76b9ce011898ab29c8a4181
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-04 10:53:47 +0900

    Implement tasks 176, 180, 182

--- logic/task180.py
+++ logic/task180.py
@@ -1,2 +1,14 @@
 def p(g):
-    return g
+    h=len(g)//2;w=len(g[0])//2
+    A=[r[:w]for r in g[:h]];B=[r[w:]for r in g[:h]]
+    C=[r[w:]for r in g[h:]];D=[r[:w]for r in g[h:]]
+    c=set(sum(A,[]))
+    for q in B,C,D:c&=set(sum(q,[]))
+    c=next(iter(c))
+    o=[[c]*w for _ in range(h)]
+    for i in range(h):
+        for j in range(w):
+            for q in (A,C,D,B):
+                v=q[i][j]
+                if v!=c:o[i][j]=v
+    return o
