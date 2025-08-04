9fde2b8098f13389544adda9fcd2d886f6f4ff57
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-04 12:43:46 +0900

    Implement tasks 128 and 148

--- logic/task128.py
+++ logic/task128.py
@@ -1,23 +1,23 @@
 def p(g):
-    h=len(g);w=len(g[0]);bg=0
-    c=[(i,j) for i in range(h) for j in range(w) if g[i][j]!=bg]
-    if not c:return g
-    if any(i==0 for i,j in c):dy,dx=1,0
-    elif any(i==h-1 for i,j in c):dy,dx=-1,0
-    elif any(j==0 for i,j in c):dy,dx=0,1
+    h=len(g);w=len(g[0])
+    z=[(y,x)for y in range(h)for x in range(w)if g[y][x]]
+    if not z:return g
+    if any(y==0 for y,_ in z):dy,dx=1,0
+    elif any(y==h-1 for y,_ in z):dy,dx=-1,0
+    elif any(x==0 for _,x in z):dy,dx=0,1
     else:dy,dx=0,-1
-    r=[[bg]*w for _ in range(h)];v=set()
-    for i,j in c:
-        if (i,j)in v:continue
-        col=g[i][j];q=[(i,j)];v.add((i,j));A=[];ys=[];xs=[]
+    r=[[0]*w for _ in range(h)];v=set()
+    for s in z:
+        if s in v:continue
+        c=g[s[0]][s[1]];q=[s];v.add(s);pts=[];ys=[s[0]];xs=[s[1]]
         while q:
-            y,x=q.pop();A.append((y,x));ys.append(y);xs.append(x)
+            y,x=q.pop();pts.append((y,x))
             for a,b in((1,0),(-1,0),(0,1),(0,-1)):
                 u,vv=y+a,x+b
-                if 0<=u<h and 0<=vv<w and g[u][vv]==col and (u,vv)not in v:
-                    v.add((u,vv));q.append((u,vv))
+                if 0<=u<h and 0<=vv<w and g[u][vv]==c and (u,vv)not in v:
+                    v.add((u,vv));q.append((u,vv));ys.append(u);xs.append(vv)
         sh=(max(ys)-min(ys)+1)*dy;sw=(max(xs)-min(xs)+1)*dx
-        for y,x in A:
-            ny, nx=y+sh,x+sw
-            if 0<=ny<h and 0<=nx<w:r[ny][nx]=col
+        for y,x in pts:
+            u,vv=y+sh,x+sw
+            if 0<=u<h and 0<=vv<w:r[u][vv]=c
     return r
--- logic/task148.py
+++ logic/task148.py
@@ -1,47 +1,18 @@
 def p(g):
-    h=len(g);w=len(g[0])
-    bg=max(sum(g,[]),key=sum(g,[]).count)
-    vis=[[0]*w for _ in range(h)];comp={}
-    for y in range(h):
-        for x in range(w):
-            c=g[y][x]
-            if c==bg or vis[y][x]:continue
-            q=[(y,x)];vis[y][x]=1;cells=[]
-            while q:
-                i,j=q.pop();cells.append((i,j))
-                for a,b in((1,0),(-1,0),(0,1),(0,-1)):
-                    u,v=i+a,j+b
-                    if 0<=u<h and 0<=v<w and not vis[u][v] and g[u][v]==c:
-                        vis[u][v]=1;q.append((u,v))
-            comp.setdefault(c,[]).append(cells)
-    colA=next(c for c,v in comp.items() if len(v)==2 and all(len(o)>1 and all(i in(0,h-1) or j in(0,w-1) for i,j in o) for o in v))
-    colB=next(c for c in comp if c!=colA)
-    A=comp[colA];B=[p for o in comp[colB] for p in o]
-    rowsB={i for i,j in B};colsB={j for i,j in B}
-    o1,o2=A if any(i in rowsB or j in colsB for i,j in A[0]) else A[::-1]
-    res=[row[:] for row in g]
-    cols1={x for y,x in o1};rows1={y for y,x in o1}
-    for y,x in B:
-        if x in cols1:
-            t=min(rows1,key=lambda r:abs(r-y));s=1 if y<t else -1
-            for yy in range(y+s,t,s):res[yy][x]=colB
-        else:
-            t=min(cols1,key=lambda c:abs(c-x));s=1 if x<t else -1
-            for xx in range(x+s,t,s):res[y][xx]=colB
-        res[y][x]=4
-    rowsA={y for o in A for y,x in o};colsA={x for o in A for y,x in o}
-    vert=len(rowsA)>len(colsA)
-    def ul(o):
-        ys=[y for y,x in o];xs=[x for y,x in o];return min(ys),min(xs)
-    dy=ul(o2)[0]-ul(o1)[0];dx=ul(o2)[1]-ul(o1)[1]
-    sh=(dy,0) if vert else (0,dx)
-    for y,x in B:
-        ny, nx=y+sh[0],x+sh[1]
-        if 0<=ny<h and 0<=nx<w:
-            if vert:
-                for j in range(w):res[ny][j]=colB
-            else:
-                for i in range(h):res[i][nx]=colB
-    for o in A:
-        for y,x in o:res[y][x]=colA
-    return res
+    H=len(g);W=len(g[0])
+    P=[(y,x)for y in range(H)for x in range(W)if g[y][x]==2]
+    E=[(y,x)for y in range(H)for x in range(W)if g[y][x]==8]
+    lc=min(x for _,x in P);rc=max(x for _,x in P)
+    L=[y for y,x in P if x==lc];R=[y for y,x in P if x==rc]
+    if any(y in L or x==lc for y,x in E):sc,st,ot=lc,min(L),min(R)
+    else:sc,st,ot=rc,min(R),min(L)
+    d=ot-st
+    for y,x in E:
+        if x>sc:a,b=sc+1,x
+        else:a,b=x+1,sc
+        for j in range(a,b):g[y][j]=8
+        g[y][x]=4;r=y+d
+        if 0<=r<H:
+            for j in range(W):g[r][j]=8
+    for y,x in P:g[y][x]=2
+    return g
