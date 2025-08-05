7a3953acf89be69b2dcfbeaf4be0db18ff5f9184
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 19:43:08 +0900

    Shrink task143 logic

--- logic/task143.py
+++ logic/task143.py
@@ -1,14 +1,14 @@
 def p(g):
- G=[r[:]for r in g];O=[];L=()
- for y in range(10):
-  for x in range(10):
+ G=[*map(list,g)];O=[];L=();R=range
+ for y in R(10):
+  for x in R(10):
    if c:=G[y][x]:
     G[y][x]=0;o=q=[(y,x)];u=w=y;v=z=x
     for i,j in q:
      for A,B in((i+1,j),(i-1,j),(i,j+1),(i,j-1)):
-      if 9>=A>=0<=B<=9 and G[A][B]==c:G[A][B]=0;q+=(A,B),;u=min(u,A);v=min(v,B);w=max(w,A);z=max(z,B)
-    t=(c,o,u,v,w,z,{(y-u,x-v)for y,x in o});O+=t,
-    if not L and len(o)==w-u+z-v+1 and not u*v*(9-w)*(9-z) and len({d for i in range(u,w+1)for d in g[i][v:z+1]})==3:L=t
- c,o,u,v,w,z,_=L;sh=next(t[6] for t in O if t[1]!=o and u<=t[2]<=w>=t[4] and v<=t[3]<=z>=t[5])
- for y,x in next(t for t in O if t[6]==sh and not(u<=t[2]<=w>=t[4] and v<=t[3]<=z>=t[5]))[1]:g[y][x]=c
+      if -1<A<10>B>-1 and G[A][B]==c:G[A][B]=0;q+=(A,B),;u=min(u,A);v=min(v,B);w=max(w,A);z=max(z,B)
+    q=o,u,v,w,z,{(y-u,x-v)for y,x in o};O+=q,
+    if not L and len(o)==w-u+z-v+1 and u*v*(9-w)*(9-z)<1 and len({d for i in R(u,w+1)for d in g[i][v:z+1]})>2:L=q;C=c
+ o,u,v,w,z,_=L;B=lambda t:u<=t[1]<=w>=t[3] and v<=t[2]<=z>=t[4];sh=next(t[5] for t in O if t!=L and B(t))
+ for y,x in next(t for t in O if t[5]==sh and not B(t))[0]:g[y][x]=C
  return g
