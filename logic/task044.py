3a7038a0f31212878c8bcaf34fca34ed5beaa3d1
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 17:22:38 +0900

    refactor task044

--- logic/task044.py
+++ logic/task044.py
@@ -1,47 +1,38 @@
 def p(g):
-    h=w=10;D=[(1,0),(-1,0),(0,1),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]
-    z=[];v=set()
-    for y in range(h):
-        for x in range(w):
-            if g[y][x]==0 and (y,x) not in v:
-                s=[(y,x)];c=[];e=1
-                while s:
-                    y1,x1=s.pop()
-                    if (y1,x1) in v or g[y1][x1]!=0:continue
-                    v.add((y1,x1));c.append((y1,x1))
-                    for dy,dx in D:
-                        ny=y1+dy;nx=x1+dx
-                        if 0<=ny<h and 0<=nx<w:
-                            t=g[ny][nx]
-                            if t==0:s.append((ny,nx))
-                            elif t!=5:e=0
-                        else:e=0
-                if e:z.append(c)
-    o={};v=set();cnt=[0]*10
-    for y in range(h):
-        for x in range(w):
-            c=g[y][x]
-            if c and c!=5 and (y,x) not in v:
-                s=[(y,x)];r=[]
-                while s:
-                    y1,x1=s.pop()
-                    if (y1,x1) in v or g[y1][x1]!=c:continue
-                    v.add((y1,x1));r.append((y1,x1))
-                    for dy,dx in D:
-                        ny=y1+dy;nx=x1+dx
-                        if 0<=ny<h and 0<=nx<w:s.append((ny,nx))
-                cnt[c]+=1
-                m=min(y for y,x in r);n=min(x for y,x in r)
-                k=tuple(sorted((y-m,x-n) for y,x in r))
-                o.setdefault(k,[]).append((r,c))
-    for c in z:
-        m=min(y for y,x in c);n=min(x for y,x in c)
-        k=tuple(sorted((y-m,x-n) for y,x in c))
-        if k in o:
-            a=o[k]
-            j=next((i for i,(r,col) in enumerate(a) if cnt[col]==1),-1)
-            if j+1:
-                r,col=a.pop(j)
-                for y,x in r:g[y][x]=0
-                for y,x in c:g[y][x]=col
-    return g
+    d=[(i//3-1,i%3-1)for i in range(9)if i-4]
+    z=[];o={};c=[0]*10
+    def f(y,x):
+        t=g[y][x]%10;s=[(y,x)];r=[];e=not t
+        while s:
+            y,x=s.pop()
+            v=g[y][x]
+            if v>9 or v%10!=t:continue
+            g[y][x]=v+10;r.append((y,x))
+            for dy,dx in d:
+                Y,X=y+dy,x+dx
+                if 0<=Y<10 and 0<=X<10:
+                    v=g[Y][X]
+                    if v<10 and v%10==t:s.append((Y,X))
+                    elif not t and v%10 and v%10-5:e=0
+                elif not t:e=0
+        return r,e
+    for y in range(10):
+        for x in range(10):
+            if g[y][x]<10:
+                r,e=f(y,x);t=g[y][x]%10
+                if t and t!=5:
+                    c[t]+=1;r.sort();Y,X=r[0];k=tuple((y-Y)*10+x-X for y,x in r)
+                    o.setdefault(k,[]).append((r,t))
+                elif not t and e:
+                    r.sort();z.append(r)
+    for r in z:
+        Y,X=r[0];k=tuple((y-Y)*10+x-X for y,x in r)
+        a=o.get(k)
+        if a:
+            i=next((i for i,(q,t) in enumerate(a) if c[t]==1),-1)
+            if i+1:
+                q,t=a.pop(i)
+                for y,x in q:g[y][x]=0
+                for y,x in r:g[y][x]=t
+    return[[x%10 for x in r]for r in g]
+
