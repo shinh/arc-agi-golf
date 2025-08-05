f8a01d98a1e9aecadfdeeec56cd0ad34221cd539
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 10:43:34 +0900

    Replace mostcolor with zero in task117

--- logic/task117.py
+++ logic/task117.py
@@ -2,9 +2,9 @@ def p(g):
     h=len(g);w=len(g[0]);cnt={}
     for r in g:
         for c in r:cnt[c]=cnt.get(c,0)+1
-    bg=max(cnt,key=cnt.get)
+    bg=0
     for v in cnt:
-        if v!=bg and cnt[v]==5:
+        if v and cnt[v]==5:
             pts=[(y,x)for y,row in enumerate(g) for x,c in enumerate(row) if c==v]
             ys=[y for y,_ in pts];xs=[x for _,x in pts]
             if max(ys)-min(ys)==max(xs)-min(xs)==2:
@@ -14,7 +14,7 @@ def p(g):
                     r=[r[:]for r in g]
                     for y,row in enumerate(g):
                         for x,c in enumerate(row):
-                            if c!=bg:
+                            if c:
                                 for Y,X in((y,x),(s-y,x),(y,t-x),(s-y,t-x)):
                                     if 0<=Y<h and 0<=X<w:r[Y][X]=c
                     return r
