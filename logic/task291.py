3b4c9bd2ef43cf207c11d1183ad5211de4eeb9fc
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 11:01:27 +0900

    golf 291

--- logic/task291.py
+++ logic/task291.py
@@ -1,6 +1,4 @@
 def p(g):
-    d=set(sum(g,[]))
-    for c in d:
-        if c:
-            y,x=zip(*[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==c])
-            if len(y)!=(max(y)-min(y)+1)*(max(x)-min(x)+1):return [[c]]
+    for c in set(sum(g,[]))-{0}:
+        y,x=zip(*[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==c])
+        if len(y)!=(max(y)-min(y)+1)*(max(x)-min(x)+1):return [[c]]
