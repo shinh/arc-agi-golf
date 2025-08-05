18153e0ad89d952bb64842a4db02a90436ebaedc
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 19:38:51 +0900

    Further shrink task366

--- logic/task366.py
+++ logic/task366.py
@@ -6,37 +6,35 @@ def p(g):
  else:a=[R[:w//2]for R in g];b=[R[(w+1)//2:]for R in g]
  if len({*sum(a,[])})>len({*sum(b,[])}) :a,b=b,a
  B=[R[:]for R in a];O=b
- fb=sum(B,[]);bc=max(fb,key=fb.count)
- fo=sum(O,[]);bg=max(fo,key=fo.count);fo=[v for v in fo if v-bg]
- if not fo:return B
- oc=max(fo,key=fo.count)
- H,W=len(O),len(O[0]);S=set();objs=[]
- for i in range(H):
-  for j in range(W):
-   if O[i][j]-bg and(i,j)not in S:
-    q=[(i,j)];S.add((i,j));o=[]
+ d=max(F:=sum(B,[]),key=F.count)
+ e=max(G:=sum(O,[]),key=G.count);G=[v for v in G if v-e]
+ if not G:return B
+ f=max(G,key=G.count)
+ H,W=len(B),len(B[0]);P=[[d]*(W+2)for _ in range(H+2)]
+ for i,R in enumerate(B):P[i+1][1:-1]=R
+ h,w=len(O),len(O[0]);s=set()
+ for i in range(h):
+  for j in range(w):
+   if O[i][j]-e and(i,j)not in s:
+    q=[(i,j)];s.add((i,j));o=[]
     while q:
      x,y=q.pop();o.append((O[x][y],(x,y)))
      for u,v in((x+1,y),(x-1,y),(x,y+1),(x,y-1)):
-      if 0<=u<H and 0<=v<W and O[u][v]-bg and(u,v)not in S:S.add((u,v));q.append((u,v))
-    objs.append(o)
- H,W=len(B),len(B[0]);pad=[[bc]*(W+2)for _ in range(H+2)]
- for i,R in enumerate(B):pad[i+1][1:-1]=R
- for o in objs:
-  cs=[p for _,p in o];xs,ys=zip(*cs);mi=min(xs);mj=min(ys);ma=max(xs);mb=max(ys)
-  pat=[[ (v,(i,j)),(bc,(i,j)) ][v==oc]for v,(i,j) in o]
-  for i in range(mi-1,ma+2):pat+=[(bc,(i,mj-1)),(bc,(i,mb+1))]
-  for j in range(mj-1,mb+2):pat+=[(bc,(mi-1,j)),(bc,(ma+1,j))]
-  pi=min(i for _,(i,j) in pat);pj=min(j for _,(i,j) in pat)
-  pat=[(v,(i-pi,j-pj))for v,(i,j) in pat]
-  ph=max(i for _,(i,j) in pat)+1;pw=max(j for _,(i,j) in pat)+1
-  occ=set()
-  for si in range(len(pad)-ph+1):
-   for sj in range(len(pad[0])-pw+1):
-    if all(pad[si+a][sj+b]==v for v,(a,b) in pat):occ.add((si-1,sj-1))
-  if occ:
-   di,dj=next(iter(occ));di-=pi;dj-=pj
-   for v,(i,j) in o:
-    ii=i+di;jj=j+dj
-    if 0<=ii<H and 0<=jj<W:B[ii][jj]=v
+      if 0<=u<h and 0<=v<w and O[u][v]-e and(u,v)not in s:s.add((u,v));q.append((u,v))
+    X,Y=zip(*(p for _,p in o));mi,ma=min(X),max(X);mj,mb=min(Y),max(Y)
+    p=[[(v,(i,j)),(d,(i,j))][v==f]for v,(i,j) in o]
+    for x in range(mi-1,ma+2):p+=[(d,(x,mj-1)),(d,(x,mb+1))]
+    for y in range(mj-1,mb+2):p+=[(d,(mi-1,y)),(d,(ma+1,y))]
+    pi=min(i for _,(i,j) in p);pj=min(j for _,(i,j) in p)
+    p=[(v,(i-pi,j-pj))for v,(i,j) in p]
+    ph=max(i for _,(i,j) in p)+1;pw=max(j for _,(i,j) in p)+1
+    t=set()
+    for si in range(len(P)-ph+1):
+     for sj in range(len(P[0])-pw+1):
+      if all(P[si+a][sj+b]==v for v,(a,b) in p):t.add((si-1,sj-1))
+    if t:
+     di,dj=next(iter(t));di-=pi;dj-=pj
+     for v,(x,y) in o:
+      x+=di;y+=dj
+      if 0<=x<H and 0<=y<W:B[x][y]=v
  return B
