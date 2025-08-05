04a815b1e5fa5e19a87a3141f41fe6145ee08e80
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 17:48:52 +0900

    refactor task080

--- logic/task080.py
+++ logic/task080.py
@@ -1,52 +1,38 @@
 def p(g):
- h=len(g);w=len(g[0])
- sr=next(i for i in range(1,h)if len(set(g[i]))==1)+1
- sc=next(j for j in range(1,w)if len({g[k][j]for k in range(h)})==1)+1
- B=[r[::sc]for r in g[::sr]];H=len(B);W=len(B[0])
- D=(1,0,-1,0,1);S=set();objs=[]
- for i in range(H):
-  for j in range(W):
-   if(i,j)in S or B[i][j]==0:continue
-   q=[(i,j)];S.add((i,j));o=[]
-   while q:
-    y,x=q.pop();o.append((y,x))
-    for k in range(4):
-     ny=y+D[k];nx=x+D[k+1]
-     if 0<=ny<H and 0<=nx<W and(ny,nx)not in S and B[ny][nx]:S.add((ny,nx));q.append((ny,nx))
-   objs.append(o)
- if not objs:return g
- obj=max(objs,key=lambda o:len({B[y][x]for y,x in o}))
- oth=[o for o in objs if o is not obj]
- cols=[B[y][x]for y,x in obj]
- t=oth and B[oth[0][0][0]][oth[0][0][1]]
- c=t if t in cols else min(set(cols),key=cols.count)
- mn=min(y for y,_ in obj);ml=min(x for _,x in obj)
- pat=[(B[y][x],y-mn,x-ml)for y,x in obj]
- ay,ax=min((y,x)for col,y,x in pat if col==c)
- pat=[(col,y-ay,x-ax)for col,y,x in pat]
- S=set();tgt=[]
- for i in range(H):
-  for j in range(W):
-   if(i,j)in S or B[i][j]!=c:continue
-   q=[(i,j)];S.add((i,j));my=i;mx=j
-   while q:
-    y,x=q.pop()
-    for k in range(4):
-     ny=y+D[k];nx=x+D[k+1]
-     if 0<=ny<H and 0<=nx<W and(ny,nx)not in S and B[ny][nx]==c:
-      S.add((ny,nx));q.append((ny,nx))
-      if ny<my:my=ny
-      if nx<mx:mx=nx
-   tgt.append((my,mx))
- for ty,tx in tgt:
-  for col,dy,dx in pat:
-   y,x=ty+dy,tx+dx
-   if 0<=y<H and 0<=x<W:B[y][x]=col
- out=[r[:]for r in g]
- for bi in range(H):
-  for bj in range(W):
-   v=B[bi][bj];r0=bi*sr;c0=bj*sc
-   for i in range(sr-1):
-    for j in range(sc-1):out[r0+i][c0+j]=v
- return out
+ m=len(g);n=len(g[0])
+ a=next(i for i in range(1,m)if len(set(g[i]))==1)+1
+ b=next(j for j in range(1,n)if len({r[j]for r in g})==1)+1
+ B=[r[::b]for r in g[::a]];h=len(B);w=len(B[0])
+ d=(1,0,-1,0,1);s=set();f=0
+ for i in range(h):
+  for j in range(w):
+   if B[i][j]and(i,j)not in s:
+    q=[(i,j)];s.add((i,j));o=[];col=[]
+    while q:
+     y,x=q.pop();o.append((y,x));col.append(B[y][x])
+     for k in range(4):
+      ny=y+d[k];nx=x+d[k+1]
+      if 0<=ny<h and 0<=nx<w and B[ny][nx]and(ny,nx)not in s:s.add((ny,nx));q.append((ny,nx))
+    if len(set(col))>1:obj=o;f=1;break
+  if f:break
+ t=0;f=0
+ for i in range(h):
+  for j in range(w):
+   if B[i][j]and(i,j)not in s:t=B[i][j];f=1;break
+  if f:break
+ c=t if t in col else min(col,key=col.count)
+ mn=min(y for y,_ in obj);mx=min(x for _,x in obj)
+ pat=[(B[y][x],y-mn,x-mx)for y,x in obj]
+ ay,ax=min((y,x)for k,y,x in pat if k==c)
+ pat=[(k,y-ay,x-ax)for k,y,x in pat]
+ for y in range(h):
+  for x in range(w):
+   if B[y][x]==c and(y==0 or B[y-1][x]!=c)and(x==0 or B[y][x-1]!=c):
+    for k,dy,dx in pat:
+     Y=y+dy;X=x+dx
+     if 0<=Y<h and 0<=X<w:B[Y][X]=k
+ for i in range(m):
+  for j in range(n):
+   if i%a<a-1 and j%b<b-1:g[i][j]=B[i//a][j//b]
+ return g
 
