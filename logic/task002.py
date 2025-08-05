4ffb296ce32aded094e3ec007f1c773d88dcc6bf
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 15:22:55 +0900

    golf 2 more

--- logic/task002.py
+++ logic/task002.py
@@ -7,7 +7,7 @@ def p(g):
                 s=[(y,x)];r=[];e=1
                 while s:
                     y1,x1=s.pop()
-                    if (y1,x1) in v or g[y1][x1]!=0: continue
+                    if (y1,x1) in v or g[y1][x1]: continue
                     v[(y1,x1)]=1;r.append((y1,x1))
                     for dy,dx in ((1,0),(-1,0),(0,1),(0,-1)):
                         ny,nx=y1+dy,x1+dx
@@ -15,8 +15,8 @@ def p(g):
                             e=0
                         else:
                             c=g[ny][nx]
-                            if c==0: s.append((ny,nx))
-                            elif c!=3: e=0
+                            if c==0:s+=[(ny,nx)]
+                            elif c!=3:e=0
                 if e:
                     for y1,x1 in r: g[y1][x1]=4
     return g
