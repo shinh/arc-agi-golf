b41ea19f413d646f91271283267e7c8e23b606e4
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 19:07:58 +0900

    golf task170 more

--- logic/task170.py
+++ logic/task170.py
@@ -1,25 +1,25 @@
 def p(g):
-    H,W=len(g),len(g[0]);b=0
-    s=set();o=[];d=[(1,0),(-1,0),(0,1),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]
-    for i in range(H):
-        for j in range(W):
-            if g[i][j] and(i,j)not in s:
-                q=[(i,j)];s.add((i,j));pts=[];pal=set()
-                while q:
-                    x,y=q.pop();pts.append((x,y));pal.add(g[x][y])
-                    for dx,dy in d:
-                        nx,ny=x+dx,y+dy
-                        if 0<=nx<H and 0<=ny<W and g[nx][ny] and(nx,ny)not in s:
-                            s.add((nx,ny));q.append((nx,ny))
-                o.append((pts,pal))
-    p1,_=max(o,key=lambda x:len(x[1]));p2,_=min(o,key=lambda x:len(x[1]));c=g[p2[0][0]][p2[0][1]]
-    xs=[x for x,_ in p2];ys=[y for _,y in p2];mi,ma=min(xs),max(xs);mj,mz=min(ys),max(ys)
-    h2=ma-mi+1;w2=mz-mj+1;bg=[[0]*w2 for _ in range(h2)]
-    for x,y in p2:bg[x-mi][y-mj]=g[x][y]
-    xs1=[x for x,_ in p1];ys1=[y for _,y in p1];n1,miny=min(xs1),min(ys1)
-    h1=max(xs1)-n1+1;w1=max(ys1)-miny+1;si=h2//h1;sj=w2//w1
-    out=[[bg[i*si][j*sj] for j in range(w1)] for i in range(h1)]
-    for x,y in p1:
-        i=x-n1;j=y-miny
-        if out[i][j]==c:out[i][j]=g[x][y]
-    return out
+    A=[r[:]for r in g];h=len(g);w=len(g[0]);p=s=[];S=0;T=1e9
+    for i in range(h):
+     for j in range(w):
+      if g[i][j]:
+       q=[(i,j)];g[i][j]=0;P={A[i][j]};R=[]
+       while q:
+        x,y=q.pop();R+=[(x,y)]
+        for a in-1,0,1:
+         for b in-1,0,1:
+          u,v=x+a,y+b
+          if a|b and 0<=u<h and 0<=v<w and g[u][v]:
+           g[u][v]=0;q+=[(u,v)];P|={A[u][v]}
+       l=len(P)
+       if l>S:S=l;p=R
+       if l<T:T=l;s=R
+    m=A[s[0][0]][s[0][1]]
+    x,y=zip(*s);a,b=min(x),max(x);c,d=min(y),max(y)
+    B=[r[c:d+1]for r in A[a:b+1]]
+    x,y=zip(*p);a,c=min(x),min(y);b=max(x)-a+1;d=max(y)-c+1;I=len(B)//b;J=len(B[0])//d
+    o=[[B[i*I][j*J]for j in range(d)]for i in range(b)]
+    for x,y in p:
+     i=x-a;j=y-c
+     if o[i][j]==m:o[i][j]=A[x][y]
+    return o
