2c15e7c9c21ee841c37a4e6bfa1b237144a17aa3
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 14:27:35 +0900

    Refine task118 solution

--- logic/task118.py
+++ logic/task118.py
@@ -1,46 +1,44 @@
 def p(g):
-    h=len(g);w=len(g[0])
-    cs={c for r in g for c in r}
-    c=min(cs,key=lambda k:sum(r.count(k)for r in g))
-    o={(i,j)for i in range(h)for j in range(w)if g[i][j]==c}
-    s=set(o);ch=1
-    while ch:
-        ch=0
-        for i in range(h):
-            xs=[j for j in range(w)if(i,j)in s]
+    H=len(g);W=len(g[0])
+    C=min({v for r in g for v in r},key=lambda k:sum(r.count(k)for r in g))
+    O={(i,j)for i in range(H)for j in range(W)if g[i][j]==C}
+    S=set(O);f=1
+    while f:
+        f=0
+        for i in range(H):
+            xs=[j for j in range(W)if(i,j)in S]
             for a,b in zip(xs,xs[1:]):
                 if b-a<5:
                     for k in range(a+1,b):
-                        if(i,k)not in s:s.add((i,k));ch=1
-        for j in range(w):
-            ys=[i for i in range(h)if(i,j)in s]
+                        if(i,k)not in S:S.add((i,k));f=1
+        for j in range(W):
+            ys=[i for i in range(H)if(i,j)in S]
             for a,b in zip(ys,ys[1:]):
                 if b-a<5:
                     for k in range(a+1,b):
-                        if(k,j)not in s:s.add((k,j));ch=1
-    cts={(i,j)for i,j in s if((i,j-1)in s or(i,j+1)in s)and((i-1,j)in s or(i+1,j)in s)}
-    rem=set(s);info=[]
-    while rem:
-        stack=[rem.pop()];comp=[]
-        while stack:
-            i,j=stack.pop();comp.append((i,j))
+                        if(k,j)not in S:S.add((k,j));f=1
+    T={(i,j)for i,j in S if((i,j-1)in S or(i,j+1)in S)and((i-1,j)in S or(i+1,j)in S)}
+    R=set(S);I=[];L=0
+    while R:
+        st=[R.pop()];comp=[]
+        while st:
+            i,j=st.pop();comp.append((i,j))
             for di in(-1,0,1):
                 for dj in(-1,0,1):
                     if di or dj:
                         n=(i+di,j+dj)
-                        if n in rem:rem.remove(n);stack.append(n)
+                        if n in R:R.remove(n);st.append(n)
         ys=[i for i,_ in comp];xs=[j for _,j in comp]
-        h0=max(ys)-min(ys)+1;w0=max(xs)-min(xs)+1
-        info.append((comp,min(ys),min(xs),h0,w0,max(h0,w0)))
-    L=max(d for *_,d in info)
-    for comp,y0,x0,h0,w0,d in info:
-        if len(comp)==L and (h0==1 or w0==1):cts.add((y0+h0//2,x0+w0//2))
+        h=max(ys)-min(ys)+1;w=max(xs)-min(xs)+1;d=max(h,w);L=max(L,d)
+        I.append((comp,min(ys),min(xs),h,w,d))
+    for comp,y,x,h,w,d in I:
+        if d==L and (h==1 or w==1):T.add((y+h//2,x+w//2))
     m=L//2
-    for i,j in cts:
+    for i,j in T:
         for d in range(-m,m+1):
-            if 0<=i+d<h:s.add((i+d,j))
-            if 0<=j+d<w:s.add((i,j+d))
+            if 0<=i+d<H:S.add((i+d,j))
+            if 0<=j+d<W:S.add((i,j+d))
     r=[r[:]for r in g]
-    for i,j in s:r[i][j]=8
-    for i,j in o:r[i][j]=c
+    for i,j in S:r[i][j]=8
+    for i,j in O:r[i][j]=C
     return r
