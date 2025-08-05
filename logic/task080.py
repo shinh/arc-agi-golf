e03f2e2f2e458b316d984eb9a7655974845ec381
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 18:48:57 +0900

    refactor task080

--- logic/task080.py
+++ logic/task080.py
@@ -2,37 +2,29 @@ def p(g):
  m=len(g);n=len(g[0])
  a=next(i for i in range(1,m)if len(set(g[i]))==1)+1
  b=next(j for j in range(1,n)if len({r[j]for r in g})==1)+1
- B=[r[::b]for r in g[::a]];h=len(B);w=len(B[0])
- d=(1,0,-1,0,1);s=set();f=0
- for i in range(h):
-  for j in range(w):
-   if B[i][j]and(i,j)not in s:
-    q=[(i,j)];s.add((i,j));o=[];col=[]
-    while q:
-     y,x=q.pop();o.append((y,x));col.append(B[y][x])
-     for k in range(4):
-      ny=y+d[k];nx=x+d[k+1]
-      if 0<=ny<h and 0<=nx<w and B[ny][nx]and(ny,nx)not in s:s.add((ny,nx));q.append((ny,nx))
-    if len(set(col))>1:obj=o;f=1;break
-  if f:break
- t=0;f=0
- for i in range(h):
-  for j in range(w):
-   if B[i][j]and(i,j)not in s:t=B[i][j];f=1;break
-  if f:break
- c=t if t in col else min(col,key=col.count)
- mn=min(y for y,_ in obj);mx=min(x for _,x in obj)
- pat=[(B[y][x],y-mn,x-mx)for y,x in obj]
- ay,ax=min((y,x)for k,y,x in pat if k==c)
- pat=[(k,y-ay,x-ax)for k,y,x in pat]
+ B=[r[::b]for r in g[::a]];h=len(B);w=len(B[0]);d=(1,0,-1,0,1)
  for y in range(h):
   for x in range(w):
-   if B[y][x]==c and(y==0 or B[y-1][x]!=c)and(x==0 or B[y][x-1]!=c):
-    for k,dy,dx in pat:
+   if B[y][x]:
+    q=[(y,x)];o={(y,x)};l=[B[y][x]]
+    while q:
+     y,x=q.pop()
+     for k in range(4):
+      Y=y+d[k];X=x+d[k+1]
+      if 0<=Y<h and 0<=X<w and B[Y][X]and(Y,X)not in o:o.add((Y,X));q.append((Y,X));l.append(B[Y][X])
+    if len(set(l))>1:break
+  else:continue
+  break
+ t=next((B[y][x]for y in range(h)for x in range(w)if B[y][x]and(y,x)not in o),0)
+ c=t if t in l else min(l,key=l.count)
+ mn=min(y for y,_ in o);mx=min(x for _,x in o)
+ ay,ax=min((y-mn,x-mx)for y,x in o if B[y][x]==c)
+ P=[(B[y][x],y-mn-ay,x-mx-ax)for y,x in o]
+ for y in range(h):
+  for x in range(w):
+   if B[y][x]==c and(y<1 or B[y-1][x]!=c)and(x<1 or B[y][x-1]!=c):
+    for k,dy,dx in P:
      Y=y+dy;X=x+dx
      if 0<=Y<h and 0<=X<w:B[Y][X]=k
- for i in range(m):
-  for j in range(n):
-   if i%a<a-1 and j%b<b-1:g[i][j]=B[i//a][j//b]
- return g
+ return[[B[i//a][j//b]if i%a<a-1 and j%b<b-1 else g[i][j]for j in range(n)]for i in range(m)]
 
