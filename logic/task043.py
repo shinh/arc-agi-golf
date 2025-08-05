2dc452023d6aa0ddee553445d77c71bd175a3241
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 10:00:35 +0900

    Golf task043

--- logic/task043.py
+++ logic/task043.py
@@ -1,5 +1,6 @@
 def p(g):
-    r=[i for i,v in enumerate(g[0])if v==5];w=len(g[0])
+    w=len(g[0])
+    r=[i for i in range(w)if g[0][i]>4]
     for row in g[1:]:
         if row[-1]==5:row[:]=[2*(i in r)for i in range(w)];row[-1]=5
     return g
