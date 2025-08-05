303d7d5ad42d99aef124805527a7c09d455eee5e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 10:58:55 +0900

    mostcolor 234

--- logic/task234.py
+++ logic/task234.py
@@ -1,8 +1,6 @@
 def p(g):
-    from collections import Counter
     h=len(g);w=len(g[0])
-    bg=Counter(sum(g,[])).most_common(1)[0][0]
-    cols=[c for c in {v for r in g for v in r} if c!=bg]
+    cols=[c for c in {v for r in g for v in r} if c]
     def box(c):
         P=[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==c]
         xs,ys=zip(*P)
@@ -13,9 +11,9 @@ def p(g):
     mc=[c for c in cols if c!=rc][0]
     rect=set()
     for i,j in B[mc][5]:
-        L=g[i][j-1] if j>0 else bg;R=g[i][j+1] if j<w-1 else bg
-        U=g[i-1][j] if i>0 else bg;D=g[i+1][j] if i<h-1 else bg
-        if (L==bg and R==bg) or (U==bg and D==bg):
+        L=g[i][j-1] if j>0 else 0;R=g[i][j+1] if j<w-1 else 0
+        U=g[i-1][j] if i>0 else 0;D=g[i+1][j] if i<h-1 else 0
+        if (L==0 and R==0) or (U==0 and D==0):
             continue
         rect.add((i,j))
     st=min(i for i,j in rect);sb=max(i for i,j in rect)
@@ -25,6 +23,6 @@ def p(g):
         di=db+1-st if st>db else dt-1-sb;dj=0
     else:
         dj=dr+1-sl if sl>dr else dl-1-sr;di=0
-    res=[[bg if v==mc else v for v in r] for r in g]
+    res=[[0if v==mc else v for v in r] for r in g]
     for i,j in rect:res[i+di][j+dj]=mc
     return res
