5721c800dc9309b75bf78351c24cfeff386e0868
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 10:15:17 +0900

    Golf task019

--- logic/task019.py
+++ logic/task019.py
@@ -1,10 +1,9 @@
 def p(g):
     h=len(g);w=len(g[0])
-    f=sum(g,[]);b=max(set(f),key=f.count)
     o=[[g[y%h][x%w] for x in range(w*2)]for y in range(h*2)]
-    c=[(y,x)for y in range(h*2)for x in range(w*2)if o[y][x]!=b]
+    c=[(y,x)for y in range(h*2)for x in range(w*2)if o[y][x]]
     for y,x in c:
         for dy,dx in(-1,-1),(-1,1),(1,-1),(1,1):
             Y=y+dy;X=x+dx
-            if 0<=Y<h*2 and 0<=X<w*2 and o[Y][X]==b:o[Y][X]=8
+            if 0<=Y<h*2 and 0<=X<w*2 and o[Y][X]==0:o[Y][X]=8
     return o
