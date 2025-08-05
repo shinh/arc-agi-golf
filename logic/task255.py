263ef66ae63bd725ef57e431a8b07d53e0abcb1e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 09:43:51 +0900

    Remove continue

--- logic/task255.py
+++ logic/task255.py
@@ -5,19 +5,19 @@ def p(g):
         for o in range(4):
             for sx in range(-1,30):
                 for ex in range(sx+3,32):
-                    if any(g[0][max(sx,0):ex]):continue
-                    connected=0
-                    for ey in range(30):
-                        if any(fg==c for c in g[ey][max(sx,0):ex]):
-                            ey -= 1
-                            break
-                        if all(c==3 for c in g[ey][max(sx,0):ex]):
-                            connected=1
-                            break
-                        ey+=1
-                    if max_size<(ex-sx)*ey+ey and(t<2 or connected):
-                        max_size=(ex-sx)*ey+ey
-                        best_sx,best_ex,best_y,best_o=sx,ex,ey,o
+                    if not any(g[0][max(sx,0):ex]):
+                        connected=0
+                        for ey in range(30):
+                            if any(fg==c for c in g[ey][max(sx,0):ex]):
+                                ey -= 1
+                                break
+                            if all(c==3 for c in g[ey][max(sx,0):ex]):
+                                connected=1
+                                break
+                            ey+=1
+                        if max_size<(ex-sx)*ey+ey and(t<2 or connected):
+                            max_size=(ex-sx)*ey+ey
+                            best_sx,best_ex,best_y,best_o=sx,ex,ey,o
             g=[list(r)for r in zip(*g[::-1])]
         for o in range(4):
             if best_o==o and best_y>3:
