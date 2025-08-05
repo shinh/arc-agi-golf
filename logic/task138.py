6dcf45ffedd28cc75d6f97d1c0af8c92c9df5ca3
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 19:16:05 +0900

    Further shorten task138 implementation

--- logic/task138.py
+++ logic/task138.py
@@ -1,26 +1,18 @@
 def p(g):
- g=[r[:]for r in g];h=len(g);w=len(g[0])
- v=set();u=[]
- for i in range(h):
-  for j in range(w):
-   if not g[i][j] and(i,j)not in v:
-    q=[(i,j)];s=[];b=0;v.add((i,j))
-    while q:
-     x,y=q.pop();s+=[(x,y)];b|=x in(0,h-1)or y in(0,w-1)
-     for dx,dy in((1,0),(-1,0),(0,1),(0,-1)):
-      nx,ny=x+dx,y+dy
-      if 0<=nx<h and 0<=ny<w and not g[nx][ny] and(nx,ny)not in v:v.add((nx,ny));q+=[(nx,ny)]
-    if not b:u+=s
- a=min(i for i,_ in u)-1;b=min(j for _,j in u)-1
- d=max(i for i,_ in u)+2;e=max(j for _,j in u)+2
+ R=range;m=min;M=max;E=enumerate;g=[r[:]for r in g];h=len(g);w=len(g[0]);v=set()
+ f=lambda x,y:0<=x<h and 0<=y<w and not g[x][y]and(x,y)not in v and(v.add((x,y))or f(x+1,y)or f(x-1,y)or f(x,y+1)or f(x,y-1))
+ for i in R(h):f(i,0);f(i,w-1)
+ for j in R(w):f(0,j);f(h-1,j)
+ u=[(i,j)for i in R(h)for j in R(w)if not g[i][j]and(i,j)not in v]
+ x,y=zip(*u);a=m(x)-1;b=m(y)-1;d=M(x)+2;e=M(y)+2
  G=[r[b:e]for r in g[a:d]];T=[r[1:-1]for r in G[1:-1]]
- k=min({x for r in T for x in r},key=lambda x:x==0)
- I={(i+1,j+1)for i,r in enumerate(T)for j,x in enumerate(r)if x==k}
- B={(i,j)for i,r in enumerate(G)for j,x in enumerate(r)if x==k}-I
+ k=m({x for r in T for x in r},key=lambda x:x<1)
+ I={(i+1,j+1)for i,r in E(T)for j,x in E(r)if x==k}
+ B={(i,j)for i,r in E(G)for j,x in E(r)if x==k}-I
  for a in I:
   for b in B:
    if a[0]==b[0]:
-    for j in range(min(a[1],b[1]),max(a[1],b[1])+1):G[a[0]][j]=k
-   elif a[1]==b[1]:
-    for i in range(min(a[0],b[0]),max(a[0],b[0])+1):G[i][a[1]]=k
+    for j in R(m(a[1],b[1]),M(a[1],b[1])+1):G[a[0]][j]=k
+   if a[1]==b[1]:
+    for i in R(m(a[0],b[0]),M(a[0],b[0])+1):G[i][a[1]]=k
  return G
