3a24d0b1af57694569b5b0b2923fe30f89fd7701
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 19:40:19 +0900

    Shrink task080 solution further

--- logic/task080.py
+++ logic/task080.py
@@ -6,24 +6,24 @@ def p(g):
  for y in R(h):
   for x in R(w):
    if B[y][x]:
-    q=[(y,x)];o={(y,x)};l=[B[y][x]]
-    while q:
-     y,x=q.pop()
+    q=[(y,x)];l=B[y][x],;i=0
+    while i<len(q):
+     y,x=q[i];i+=1
      for k in R(4):
       Y,X=y+d[k],x+d[k+1]
-      if 0<=Y<h and 0<=X<w and B[Y][X]and(Y,X)not in o:o.add((Y,X));q+=[(Y,X)];l+=B[Y][X],
+      if 0<=Y<h and 0<=X<w and B[Y][X]and(Y,X)not in q:q+=[(Y,X)];l+=B[Y][X],
     if len({*l})>1:break
   else:continue
   break
- c=next((B[y][x]for y in R(h)for x in R(w)if B[y][x]in l and(y,x)not in o),min(l,key=l.count))
- mn=min(y for y,_ in o);mx=min(x for _,x in o)
- ay,ax=min((y-mn,x-mx)for y,x in o if B[y][x]==c)
- P=[(B[y][x],y-mn-ay,x-mx-ax)for y,x in o]
+ c=min(l,key=l.count)
+ mn,mx=map(min,zip(*q))
+ ay,ax=min((y-mn,x-mx)for y,x in q if B[y][x]==c)
+ P=[(B[y][x],y-mn-ay,x-mx-ax)for y,x in q]
  for y in R(h):
   for x in R(w):
    if B[y][x]==c and(y<1 or B[y-1][x]!=c)and(x<1 or B[y][x-1]!=c):
     for k,dy,dx in P:
      Y,X=y+dy,x+dx
      if 0<=Y<h and 0<=X<w:B[Y][X]=k
- return[[B[i//a][j//b]if(i+1)%a and(j+1)%b else g[i][j]for j in R(n)]for i in R(m)]
+ return[[B[i//a][j//b]if(-~i%a)*(-~j%b) else g[i][j]for j in R(n)]for i in R(m)]
 
