5e081ecbbf7972b65646331f628e49c1f130865d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 16:55:28 +0900

    Reimplement 046

--- logic/task046.py
+++ logic/task046.py
@@ -1,32 +1,33 @@
 def p(g):
- h=len(g);w=len(g[0])
- f=sum(g,[])
- d={(i,j):v for i,r in enumerate(g) for j,v in enumerate(r)if v};o=[]
- while d:
-  q=[next(iter(d))];u=[]
-  for x,y in q:
-   if(x,y)in d:
-    v=d.pop((x,y));u+=[(v,x,y)]
-    q+=[(x+1,y),(x-1,y),(x,y+1),(x,y-1)]
-  o+=u,
- s=[c for c in set(f)if all(1<=sum(v==c for v,_,_ in u)<=2 for u in o)]
- sp=min(s,key=f.count)
- o.sort(key=lambda u:min(y for _,_,y in u))
- r=[];a=None
- for u in o:
-  spc=[(i,j)for v,i,j in u if v==sp];oth=[(v,i,j)for v,i,j in u if v!=sp]
-  col=[v for v,_,_ in oth];m=max(col+[0],key=col.count)
-  if a==None:r+=oth+[(m,i,j)for i,j in spc];a=max(spc);continue
-  ac=min(spc,key=lambda p:(abs(p[0]-a[0])+abs(p[1]-a[1]),p[1],p[0]))
-  di=a[0]-ac[0];dj=a[1]-ac[1]+1
-  spc=[(i+di,j+dj)for i,j in spc];oth=[(v,i+di,j+dj)for v,i,j in oth];ac=(ac[0]+di,ac[1]+dj)
-  r+=oth+[(m,i,j)for i,j in spc]
-  spc=[p for p in spc if p!=ac];a=max(spc)if spc else ac
- if not r:return g
- mi=min(j for _,_,j in r);ma=max(j for _,_,j in r);w=ma-mi+1
- out=[[0]*w for _ in g]
- for v,i,j in r:
-  j-=mi
-  if 0<=i<h and 0<=j<w:out[i][j]=v
- return out
+    grays=[]
+    colors=[0]
+    seps=[-1]
+    for x in range(len(g[0])):
+        l=[g[y][x]for y in range(3)]
+        if not any(l):
+            colors+=[0]
+            seps+=[x]
+        for y in range(3):
+            if g[y][x]==5:
+                grays+=[y]
+            elif g[y][x]>0:
+                colors[-1]=g[y][x]
+    seps+=[len(g[0])]
+    grays+=[0]
 
+    #show(g, "c")
+    #print(grays)
+
+    out=[[]for _ in g]
+    prev_offset=0
+    for i in range(len(colors)-(colors[-1]==0)):
+        #print(i)
+        offset=[0,grays[i*2-1]][i>0]
+
+        for y in range(3):
+            fy=y+offset-prev_offset
+            out[y]+=[0]*(seps[i+1]-seps[i]-1)if fy<0 or fy>=3 else[[c,colors[i]][c==5]for c in g[fy][seps[i]+1:seps[i+1]]]
+        prev_offset+=grays[i*2]-offset
+
+    #show(out, "out")
+    return out
