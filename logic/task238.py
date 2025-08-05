2b3f5aead6dc4963e4e60ad385c6805c4273dc23
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 19:39:58 +0900

    Reduce task238 solution size

--- logic/task238.py
+++ logic/task238.py
@@ -1,31 +1,30 @@
 def p(g):
     h=len(g);w=len(g[0]);D=1,0,-1,0,1
-    v=[[0]*w for _ in g];R=0,0,0,0,0
+    v=[[0]*w for _ in g];s=0
     for y in range(h):
         for x in range(w):
             if g[y][x]<1 and not v[y][x]:
-                q=[(y,x)];v[y][x]=1;n=0;a=h;c=w;b=e=0
+                q=[y,x];v[y][x]=1;n=0;a=h;c=w;b=e=0
                 while q:
-                    i,j=q.pop();n+=1;a=min(a,i);c=min(c,j);b=max(b,i);e=max(e,j)
+                    j=q.pop();i=q.pop();n+=1;a=min(a,i);c=min(c,j);b=max(b,i);e=max(e,j)
                     for k in range(4):
                         ni=i+D[k];nj=j+D[k+1]
                         if 0<=ni<h and 0<=nj<w and g[ni][nj]<1 and not v[ni][nj]:
-                            v[ni][nj]=1;q.append((ni,nj))
-                if n==(b-a+1)*(e-c+1):R=max(R,(n,a,b,c,e))
-    _,a,b,c,e=R
-    P=[r[c-1:e+2]for r in g[a-1:b+2]]
-    H=len(P);W=len(P[0]);B={}
-    for y,r in enumerate(P):
-        for x,k in enumerate(r):
-            if k:B.setdefault(k,[]).append((y,x))
-    ext=[(g[y][x],y,x)for y in range(h)for x in range(w)if g[y][x] and not(a-1<=y<=b+1 and c-1<=x<=e+1)]
-    if ext:
-        mi=min(y for _,y,_ in ext);mj=min(x for _,_,x in ext);C=[]
-        for k,y,x in ext:
-            i=y-mi+1;j=x-mj+1
-            if 0<=i<H and 0<=j<W:P[i][j]=k;C.append((i,j))
-        I=list(B.items())
-        for y,x in C:
-            ds=[min(abs(y-i)+abs(x-j)for i,j in c)for _,c in I];m=min(ds)
-            if ds.count(m)<2:P[y][x]=I[ds.index(m)][0]
+                            v[ni][nj]=1;q+=ni,nj
+                if n==(b-a+1)*(e-c+1) and n>s:s=n;A=a;B=b;C=c;E=e
+    P=[r[C-1:E+2]for r in g[A-1:B+2]]
+    H=len(P);W=len(P[0])
+    E=[(g[y][x],y,x)for y in range(h)for x in range(w)if g[y][x] and not(A-1<=y<=B+1 and C-1<=x<=E+1)]
+    C=[(k,y,x)for y,r in enumerate(P)for x,k in enumerate(r)if k]
+    if E:
+        I=min(y for _,y,_ in E);J=min(x for _,_,x in E)
+        for k,y,x in E:
+            y-=I-1;x-=J-1
+            if 0<=y<H and 0<=x<W:
+                P[y][x]=k;m=99;c=-1
+                for k,i,j in C:
+                    d=abs(y-i)+abs(x-j)
+                    if d<m:m=d;c=k
+                    elif d==m and k!=c:c=-1
+                if c+1:P[y][x]=c
     return P
