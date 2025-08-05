6456ff89b40e609bdd3cca6285c4dcbfc576fb97
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 17:15:32 +0900

    Reduce size of task191 logic

--- logic/task191.py
+++ logic/task191.py
@@ -1,5 +1,4 @@
 def p(g):
-    w=h=23
     C=set(sum(g,[]))
     def box(c):
         t=[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==c]
@@ -7,30 +6,22 @@ def p(g):
         xs,ys=zip(*t)
         return max(max(xs)-min(xs)+1,max(ys)-min(ys)+1)
     a=min(C,key=box)
-    cnt={c:sum(r.count(c)for r in g)for c in C}
-    b=0
-    d=min([c for c in C if c!=a],key=cnt.get)
-    p=[[b]*(w+2)for _ in range(h+2)]
-    for i,r in enumerate(g):
-        for j,v in enumerate(r):p[i+1][j+1]=v
-    s=[(i,j)for i in range(h+2)for j in range(w+2)if p[i][j]==a]
-    si=min(i for i,j in s);sj=min(j for i,j in s);ei=max(i for i,j in s);ej=max(j for i,j in s)
+    d=min(C-{a},key=lambda c:sum(r.count(c)for r in g))
+    p=[[0]*25]+[[0]+r+[0]for r in g]+[[0]*25]
+    s=[(i,j)for i in range(25)for j in range(25)if p[i][j]==a]
+    xs,ys=zip(*s)
+    si=min(xs);sj=min(ys);ei=max(xs);ej=max(ys)
     q=[r[sj:ej+1]for r in p[si:ei+1]]
     def rot(x):return [list(r)for r in zip(*x[::-1])]
     def fl(x):return [r[::-1]for r in x]
     T=[];r=q
     for _ in range(4):
         T+=r,fl(r);r=rot(r)
-    U=[]
-    [U.append(t)for t in T if t not in U]
-    H=W=25
-    for t in U:
-        mh=len(t);mw=len(t[0])
-        m=[[0 if v==a else d if v==d else None for v in row]for row in t]
-        for i in range(H-mh+1):
-            for j in range(W-mw+1):
-                if all(m[x][y] is None or p[i+x][j+y]==m[x][y] for x in range(mh)for y in range(mw)):
-                    for x in range(mh):
-                        for y in range(mw):
-                            p[i+x][j+y]=t[x][y]
+    for t in T:
+        h=len(t);w=len(t[0])
+        m=[[0 if v==a else d if v==d else -1 for v in row]for row in t]
+        for i in range(25-h+1):
+            for j in range(25-w+1):
+                if all(m[x][y]<0 or p[i+x][j+y]==m[x][y] for x in range(h)for y in range(w)):
+                    for x in range(h):p[i+x][j:j+w]=t[x]
     return[row[1:-1]for row in p[1:-1]]
