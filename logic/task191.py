550751dc967f2996fc913b4b1df9d51886c916dc
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 17:39:33 +0900

    Simplify task191 logic

--- logic/task191.py
+++ logic/task191.py
@@ -6,22 +6,18 @@ def p(g):
         xs,ys=zip(*t)
         return max(max(xs)-min(xs)+1,max(ys)-min(ys)+1)
     a=min(C,key=box)
-    d=min(C-{a},key=lambda c:sum(r.count(c)for r in g))
+    d=min(C-{a},key=lambda c:sum(v==c for r in g for v in r))
     p=[[0]*25]+[[0]+r+[0]for r in g]+[[0]*25]
-    s=[(i,j)for i in range(25)for j in range(25)if p[i][j]==a]
-    xs,ys=zip(*s)
+    xs,ys=zip(*((i,j)for i in range(25)for j in range(25)if p[i][j]==a))
     si=min(xs);sj=min(ys);ei=max(xs);ej=max(ys)
     q=[r[sj:ej+1]for r in p[si:ei+1]]
-    def rot(x):return [list(r)for r in zip(*x[::-1])]
-    def fl(x):return [r[::-1]for r in x]
-    T=[];r=q
+    r=q
     for _ in range(4):
-        T+=r,fl(r);r=rot(r)
-    for t in T:
-        h=len(t);w=len(t[0])
-        m=[[0 if v==a else d if v==d else -1 for v in row]for row in t]
-        for i in range(25-h+1):
-            for j in range(25-w+1):
-                if all(m[x][y]<0 or p[i+x][j+y]==m[x][y] for x in range(h)for y in range(w)):
-                    for x in range(h):p[i+x][j:j+w]=t[x]
+        for t in r,[row[::-1]for row in r]:
+            h=len(t);w=len(t[0]);m=[[d*(v==d)-(v!=a)*(v!=d)for v in row]for row in t]
+            for i in range(26-h):
+                for j in range(26-w):
+                    if all(m[x][y]<0 or p[i+x][j+y]==m[x][y] for x in range(h)for y in range(w)):
+                        for x in range(h):p[i+x][j:j+w]=t[x]
+        r=[list(s)for s in zip(*r[::-1])]
     return[row[1:-1]for row in p[1:-1]]
