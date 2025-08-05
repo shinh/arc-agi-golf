99d969b3ef37e61d7aa206e4b1b8fa0c5e4f5ed4
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 11:00:24 +0900

    mostcolor 291

--- logic/task291.py
+++ logic/task291.py
@@ -1,7 +1,6 @@
 def p(g):
-    from collections import Counter as C
-    d=C(sum(g,[]));b=max(d,key=d.get)
+    d=set(sum(g,[]))
     for c in d:
-        if c-b:
+        if c:
             y,x=zip(*[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==c])
             if len(y)!=(max(y)-min(y)+1)*(max(x)-min(x)+1):return [[c]]
