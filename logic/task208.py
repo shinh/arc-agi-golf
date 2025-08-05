8c19952b3ad696da0c9c8d917d2d2268148eb57f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 19:55:58 +0900

    golf task208 further

--- logic/task208.py
+++ logic/task208.py
@@ -1,20 +1,19 @@
 def p(g):
- n=len(g);s=[[0]*n for _ in g];d={};B={}
+ n=len(g);o=[r[:]for r in g];B=[0]*10;D=B[:]
  for i in range(n):
   for j in range(n):
-   if s[i][j]:continue
-   c=g[i][j];t=[(i,j)];s[i][j]=1;C=[];r0=r1=i;c0=c1=j
+   if (c:=g[i][j])<1:continue
+   t=[(i,j)];C=[];g[i][j]=-1;r0=r1=i;c0=c1=j
    while t:
-    x,y=t.pop();C.append((x,y));r0=min(r0,x);r1=max(r1,x);c0=min(c0,y);c1=max(c1,y)
+    x,y=t.pop();C+=x*n+y,;r0=min(r0,x);r1=max(r1,x);c0=min(c0,y);c1=max(c1,y)
     for u,v in((1,0),(0,1),(-1,0),(0,-1)):
-     nx,ny=x+u,y+v
-     if 0<=nx<n and 0<=ny<n and not s[nx][ny] and g[nx][ny]==c:s[nx][ny]=1;t.append((nx,ny))
-   if r1>r0+1 and c1>c0+1 and len(C)==2*(r1-r0+c1-c0)and all(x in(r0,r1)or y in(c0,c1)for x,y in C):d[c]=d.get(c,0)+1;B.setdefault(c,(r0,c0,r1,c1))
- C=min(d,key=lambda k:(d[k],k));r0,c0,r1,c1=B[C];h=r1-r0+1;w=c1-c0+1;iH=h-2;iW=w-2;R=[(x,y)for x in range(h)for y in range(w)if x in(0,h-1)or y in(0,w-1)];o=[r[:]for r in g]
- for i in range(n-iH+1):
-  for j in range(n-iW+1):
-   if all(g[i+x][j+y]==0 for x in range(iH)for y in range(iW)if x in(0,iH-1)or y in(0,iW-1)):
-    for x,y in R:
-     a=i-1+x;b=j-1+y
-     if 0<=a<n and 0<=b<n:o[a][b]=C
+     if n>(nx:=x+u)>=0<=(ny:=y+v)<n and g[nx][ny]==c:g[nx][ny]=-1;t+=(nx,ny),
+   if r1-r0>1 and c1-c0>1 and len(C)==2*(r1-r0+c1-c0)and all(p//n in(r0,r1)or p%n in(c0,c1)for p in C):
+    D[c]+=1;B[c]=B[c]or(r0,c0,r1,c1)
+ c=min(range(10),key=lambda k:(D[k] or 99,k));r0,c0,r1,c1=B[c];h=r1-r0+1;w=c1-c0+1
+ R=[(x,y)for x in range(h)for y in range(w) if x*y*(x-h+1)*(y-w+1)==0]
+ for i in range(1,n-h+2):
+  for j in range(1,n-w+2):
+   if all(g[i+x][j+y]==0 for x in range(h-2)for y in range(w-2) if x*y*(x-h+3)*(y-w+3)==0):
+    for x,y in R:o[i+x-1][j+y-1]=c
  return o
