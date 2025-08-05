db57b72582e6403f26baebe03652cac612f3ad9c
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 09:12:05 +0900

    Task 255

--- logic/task255.py
+++ logic/task255.py
@@ -1,236 +1,32 @@
 def p(g):
- DOWN = (1, 0)
- RIGHT = (0, 1)
- UNITY = (1, 1)
- def asindices(grid):
-  return frozenset((i, j) for i in range(len(grid)) for j in range(len(grid[0])))
- def asobject(grid):
-  return frozenset((v, (i, j)) for i, r in enumerate(grid) for j, v in enumerate(r))
- def index(grid,loc):
-  i, j = loc
-  h, w = len(grid), len(grid[0])
-  if not (0 <= i < h and 0 <= j < w):
-   return None
-  return grid[loc[0]][loc[1]]
- def toindices(patch):
-  if len(patch) == 0:
-   return frozenset()
-  if isinstance(next(iter(patch))[1], tuple):
-   return frozenset(index for value, index in patch)
-  return patch
- def lrcorner(patch):
-  return map(max, zip(*toindices(patch)))
- def ulcorner(patch):
-  return map(min, zip(*toindices(patch)))
- def backdrop(patch):
-  indices = toindices(patch)
-  si, sj = ulcorner(indices)
-  ei, ej = lrcorner(patch)
-  return frozenset((i, j) for i in range(si, ei + 1) for j in range(sj, ej + 1))
- def box(patch):
-  ai, aj = ulcorner(patch)
-  bi, bj = lrcorner(patch)
-  si, sj = min(ai, bi), min(aj, bj)
-  ei, ej = max(ai, bi), max(aj, bj)
-  vlines = {(i, sj) for i in range(si, ei + 1)} | {(i, ej) for i in range(si, ei + 1)}
-  hlines = {(si, j) for j in range(sj, ej + 1)} | {(ei, j) for j in range(sj, ej + 1)}
-  return frozenset(vlines | hlines)
- def canvas(value,dimensions):
-  return [[value for j in range(dimensions[1])]for i in range(dimensions[0])]
- def combine(a,b):
-  return type(a)((*a, *b))
- def compose(outer,inner):
-  return lambda x: outer(inner(x))
- def connect(a,b):
-  ai, aj = a
-  bi, bj = b
-  si = min(ai, bi)
-  ei = max(ai, bi) + 1
-  sj = min(aj, bj)
-  ej = max(aj, bj) + 1
-  if ai == bi:
-   return frozenset((ai, j) for j in range(sj, ej))
-  elif aj == bj:
-   return frozenset((i, aj) for i in range(si, ei))
-  elif bi - ai == bj - aj:
-   return frozenset((i, j) for i, j in zip(range(si, ei), range(sj, ej)))
-  elif bi - ai == aj - bj:
-   return frozenset((i, j) for i, j in zip(range(si, ei), range(ej - 1, sj - 1, -1)))
-  return frozenset()
- def dmirror(piece):
-  return tuple(zip(*piece))
- def dneighbors(loc):
-  return frozenset({(loc[0] - 1, loc[1]), (loc[0] + 1, loc[1]), (loc[0], loc[1] - 1), (loc[0], loc[1] + 1)})
- def fill(grid,value,patch):
-  h, w = len(grid), len(grid[0])
-  grid_filled = list(list(row) for row in grid)
-  for i, j in toindices(patch):
-   if 0 <= i < h and 0 <= j < w:
-    grid_filled[i][j] = value
-  return [list(row) for row in grid_filled]
- def fork(outer,b):
-  return lambda x: outer(x, b(x))
- def initset(value):
-  return frozenset({value})
- def insert(value,container):
-  return container.union(frozenset({value}))
- def lbind(function,fixed):
-  n = function.__code__.co_argcount
-  if n == 2:
-   return lambda y: function(fixed, y)
-  elif n == 3:
-   return lambda y, z: function(fixed, y, z)
-  else:
-   return lambda y, z, a: function(fixed, y, z, a)
- def apply(function,container):
-  return type(container)(function(e) for e in container)
- def merge(containers):
-  return type(containers)(e for c in containers for e in c)
- def mapply(function,container):
-  return merge(apply(function, container))
- def mostcolor(element):
-  values = [v for r in element for v in r]
-  return max(set(values), key=values.count)
- F = False
- T = True
- def leftmost(patch):
-  return min(j for i, j in toindices(patch))
- def shift(patch,directions):
-  if len(patch) == 0:
-   return patch
-  di, dj = directions
-  return frozenset((value, (i + di, j + dj)) for value, (i, j) in patch)
- def uppermost(patch):
-  return min(i for i, j in toindices(patch))
- def normalize(patch):
-  if len(patch) == 0:
-   return patch
-  return shift(patch, (-uppermost(patch), -leftmost(patch)))
- def occurrences(grid,obj):
-  occurrences = set()
-  normed = normalize(obj)
-  h, w = len(grid), len(grid[0])
-  for i in range(h):
-   for j in range(w):
-    occurs = True
-    for v, (a, b) in shift(normed, (i, j)):
-     if 0 <= a < h and 0 <= b < w:
-      if grid[a][b] != v:
-       occurs = False
-       break
-     else:
-      occurs = False
-      break
-    if occurs:
-     occurrences.add((i, j))
-  return frozenset(occurrences)
- def ofcolor(grid,value):
-  return frozenset((i, j) for i, r in enumerate(grid) for j, v in enumerate(r) if v == value)
- def lowermost(patch):
-  return max(i for i, j in toindices(patch))
- def rightmost(patch):
-  return max(j for i, j in toindices(patch))
- def outbox(patch):
-  ai, aj = uppermost(patch) - 1, leftmost(patch) - 1
-  bi, bj = lowermost(patch) + 1, rightmost(patch) + 1
-  si, sj = min(ai, bi), min(aj, bj)
-  ei, ej = max(ai, bi), max(aj, bj)
-  vlines = {(i, sj) for i in range(si, ei + 1)} | {(i, ej) for i in range(si, ei + 1)}
-  hlines = {(si, j) for j in range(sj, ej + 1)} | {(ei, j) for j in range(sj, ej + 1)}
-  return frozenset(vlines | hlines)
- def paint(grid,obj):
-  h, w = len(grid), len(grid[0])
-  grid_painted = list(list(row) for row in grid)
-  for value, (i, j) in obj:
-   if 0 <= i < h and 0 <= j < w:
-    grid_painted[i][j] = value
-  return tuple(tuple(row) for row in grid_painted)
- def power(function,n):
-  if n == 1:
-   return function
-  return compose(function, power(function, n - 1))
- def rbind(function,fixed):
-  return lambda x: function(x, fixed)
- def recolor(value,patch):
-  return frozenset((value, index) for index in toindices(patch))
- def remove(value,container):
-  return type(container)(e for e in container if e != value)
- def rot90(grid):
-  return tuple(row for row in zip(*grid[::-1]))
- def height(piece):
-  return len(piece)
- def width(piece):
-  return len(piece[0])
- def shape(piece):
-  return (height(piece), width(piece))
- def trim(grid):
-  return [list(r[1:-1])for r in grid[1:-1]]
- def verify_task255(I):
-  x0 = mostcolor(I)
-  H,W = shape(I)
-  x3 = [[x0 for j in range(W+2)]for i in range(H+2)]
-  x4 = asobject(I)
-  x5 = shift(x4, UNITY)
-  x6 = paint(x3, x5)
-  x9 = connect(UNITY, (1,12))
-  x10 = outbox(x9)
-  x11 = backdrop(x10)
-  x12 = recolor(x0, x11)
-  x13 = recolor(3, x9)
-  x14 = lbind(shift, x13)
-  x15 = lbind(mapply, x14)
-  x16 = rbind(occurrences, x12)
-  x17 = compose(x15, x16)
-  x18 = fork(paint, x17)
-  x19 = x18(x6)
-  x20 = ofcolor(x19, 3)
-  x21 = dmirror(x6)
-  x22 = x18(x21)
-  x23 = dmirror(x22)
-  x24 = ofcolor(x23, 3)
-  x25 = combine(x20, x24)
-  x26 = fill(x6, 3, x25)
-  x28 = dneighbors(UNITY)
-  x29 = remove((2,1), x28)
-  x30 = recolor(x0, x29)
-  x31 = initset(UNITY)
-  x32 = recolor(3, x31)
-  x33 = combine(x30, x32)
-  x34 = recolor(x0, x33)
-  x36 = initset((1,3))
-  x37 = insert((0,2), x36)
-  x38 = insert(RIGHT, x37)
-  x39 = insert(DOWN, x38)
-  x40 = recolor(x0, x39)
-  x42 = initset((1,2))
-  x43 = insert(UNITY, x42)
-  x44 = recolor(3, x43)
-  x45 = combine(x40, x44)
-  x46 = recolor(x0, x45)
-  x47 = lbind(shift, x34)
-  x48 = lbind(mapply, x47)
-  x49 = rbind(occurrences, x33)
-  x50 = compose(x48, x49)
-  x51 = fork(paint, x50)
-  x52 = lbind(shift, x46)
-  x53 = lbind(mapply, x52)
-  x54 = rbind(occurrences, x45)
-  x55 = compose(x53, x54)
-  x56 = fork(paint, x55)
-  x57 = compose(x51, x56)
-  x58 = compose(rot90, x57)
-  x59 = power(x58, 4)
-  x60 = power(x59, 2)
-  x61 = asindices(x26)
-  x62 = box(x61)
-  x63 = fill(x26, 3, x62)
-  x64 = x60(x63)
-  x65 = trim(x64)
-  h = len(x65)
-  if [sum(r[j]==3 for r in x65) for j in range(len(x65[0]))].count(h)==8:
-   for r in x65:
-    c=r.count(3)
-    if c==17:r[-1-r[::-1].index(3)]=0
-    if c==9:r[r.index(3)]=0
-  return x65
- return verify_task255(g)
+    fg=(set(g[0])-{0}).pop()
+    for t in range(30):
+        best_y=max_size=0
+        for o in range(4):
+            for sx in range(-1,30):
+                for ex in range(sx+3,32):
+                    if any(g[0][max(sx,0):ex]):continue
+                    connected=0
+                    for ey in range(30):
+                        if any(fg==c for c in g[ey][max(sx,0):ex]):
+                            ey -= 1
+                            break
+                        if all(c==3 for c in g[ey][max(sx,0):ex]):
+                            connected=1
+                            break
+                        ey+=1
+                    if max_size<(ex-sx)*ey+ey and(t<2 or connected):
+                        max_size=(ex-sx)*ey+ey
+                        best_sx,best_ex,best_y,best_o=sx,ex,ey,o
+            g=[list(r)for r in zip(*g[::-1])]
+        for o in range(4):
+            if best_o==o and best_y>3:
+                for y in range(best_y):
+                    for x in range(best_sx+1,best_ex-1):
+                        g[y][x]=3
+            g=[list(r)for r in zip(*g[::-1])]
+
+
+    #show(g,"out")
+
+    return g
