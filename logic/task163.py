336c414dde25d11c2fd6e9a99dd834352efbce3e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 16:26:57 +0900

    Reimplement task 163

--- logic/task163.py
+++ logic/task163.py
@@ -1,31 +1,8 @@
 def p(g):
- h=w=11
- F={(i,j)for i,r in enumerate(g) if len(set(r))==1 for j in range(w)}|{(i,j)for j in range(w) if len({g[i][j]for i in range(h)})==1 for i in range(h)}
- c=g[next(iter(F))[0]][next(iter(F))[1]]
- G=[[g[i][j] if (i,j)not in F else-1 for j in range(w)]for i in range(h)]
- V=[[0]*w for _ in g];O=[]
- for i in range(h):
-  for j in range(w):
-   if G[i][j]<0 or V[i][j]:continue
-   q=[(i,j)];V[i][j]=1;o=[]
-   while q:
-    y,x=q.pop();o+=[(G[y][x],y,x)]
-    for dy,dx in((1,0),(-1,0),(0,1),(0,-1)):
-     ny=y+dy;nx=x+dx
-     if 0<=ny<h and 0<=nx<w and G[ny][nx]>=0 and not V[ny][nx]:V[ny][nx]=1;q+=[(ny,nx)]
-   O+=[o]
- b=0
- o=[o for o in O if any(v==4 for v,_,_ in o)][0]
- mi=min(i for _,i,_ in o);mj=min(j for _,_,j in o)
- o=[(v,i-mi,j-mj)for v,i,j in o if v!=5]
- H=max(i for _,i,_ in o)+1;W=max(j for _,_,j in o)+1
- P=[(i,j)for v,i,j in o if v==4]
- cy=(min(i for i,_ in P)+max(i for i,_ in P))//2
- cx=(min(j for _,j in P)+max(j for _,j in P))//2
- dy=(H+1)*cy;dx=(W+1)*cx
- out=[[b]*w for _ in g]
- for i,j in F:out[i][j]=c
- for v,i,j in o:
-  y=i+dy;x=j+dx
-  if 0<=y<h and 0<=x<w:out[y][x]=v
- return out
+ for y,r in enumerate(g):
+  if 4 in r:x=r.index(4);break
+ a=y//4*4;b=x//4*4;c=y%4*4;d=x%4*4
+ o=[[5*(v==5)for v in r]for r in g]
+ for i in range(3):o[c+i][d:d+3]=g[a+i][b:b+3]
+ return o
+
