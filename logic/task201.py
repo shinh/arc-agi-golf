85945be21fb77def4f40aa7a6d6e87f689a33e35
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-05 17:38:54 +0900

    refactor task201

--- logic/task201.py
+++ logic/task201.py
@@ -1,17 +1,17 @@
 def p(g):
- D={};[D.setdefault(v,[]).append((i,j))for i,r in enumerate(g)for j,v in enumerate(r)]
- for k,v in D.items():
-  if len(v)==4 and len({x for x,_ in v})==len({y for _,y in v})==2:o=k;t=v;break
+ for o in range(10):
+  t=[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==o]
+  if len(t)==4 and len({i for i,_ in t})==len({j for _,j in t})==2:break
  r,s=zip(*t);a=min(r);b=max(r);c=min(s);d=max(s)
- s=[row[c:d+1]for row in g[a:b+1]];h=[row[:]for row in g]
- for i in range(a,b+1):h[i][c:d+1]=[0]*(d-c+1)
- T=lambda m:[*map(list,zip(*m))];m=any(len({*r})<2 for r in s)
- if m:s=T(s);h=T(h)
- z=[(v,i,j)for i,r in enumerate(h)for j,v in enumerate(r)if v]
+ s=[r[c:d+1]for r in g[a:b+1]]
+ z=[(v,i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v and not(a<=i<=b and c<=j<=d)]
+ if any(len({*r})<2 for r in s):s=[*map(list,zip(*s))];z=[(v,j,i)for v,i,j in z];m=1
+ else:m=0
  if z:x=min(i for v,i,j in z);y=min(j for v,i,j in z);z=[(v,i-x,j-y)for v,i,j in z]
- P=set(sum(s,[]))-{0,o}
- if len(P)>1:
-  p=min(P);q=max(P);L=lambda m,t:min(r.index(t)for r in m if t in r)
-  if (L(s,q)>L(s,p))!=(L(h,q)>L(h,p)):w=max(j for _,_,j in z);z=[(v,i,w-j)for v,i,j in z]
- for v,i,j in z:i+=1;j+=1;0<=i<len(s)and 0<=j<len(s[0])and s[i].__setitem__(j,v)
- return T(s)if m else s
+ P=sorted(set(sum(s,[]))-{0,o})
+ if P[1:]:
+  p,q=P[0],P[-1];L=lambda m,t:min(r.index(t)for r in m if t in r)
+  G=lambda t:min(j for v,i,j in z if v==t)
+  if (L(s,q)>L(s,p))!=(G(q)>G(p)):w=max(j for _,_,j in z);z=[(v,i,w-j)for v,i,j in z]
+ for v,i,j in z:i+=1;j+=1;len(s)>i and len(s[0])>j and s[i].__setitem__(j,v)
+ return [*map(list,zip(*s))] if m else s
