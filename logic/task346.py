cc2d265a748fd17e5450ab9c7bce8850421eadd8
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-09 00:27:03 +0900

    A bit more golf 346

--- logic/task346.py
+++ logic/task346.py
@@ -1,6 +1,6 @@
 def p(g):
- for i in range(1,len(g)-1):
-  for j in range(1,len(g[0])-1):
+ for i in range(len(g)):
+  for j in range(len(g[0])-1):
    b=g[i][j]
    a=g[i-1][j]
    if a*all(a==g[i+di][j+dj]for di in(-1,0,1) for dj in(-1,0,1) if di|dj):
