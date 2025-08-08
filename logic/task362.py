1070cb6a4dac22703c1a53d9f552331a1110733a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-08 11:25:15 +0900

    Refactor multiple tasks to remove enumerate

--- logic/task362.py
+++ logic/task362.py
@@ -1,12 +1,14 @@
 def p(g):
-    k=sum(r[-1]==5 for r in g)
-    for y,r in enumerate(g):
-        for x,v in enumerate(r):
-            if v and v-5:c=v;p=x
-        if r.count(c)==10:s=y
-    p-=k;s+=k
-    o=create(10,10)
-    for y in range(10):
-        if y==s:o[y]=[c]*10
-        else:o[y][p]=c
-    return o
+ k=sum(r[-1]==5 for r in g);y=-1
+ for r in g:
+  y+=1;x=-1
+  for v in r:
+   x+=1
+   if v and v-5:c=v;p=x
+  if r.count(c)==10:s=y
+ p-=k;s+=k
+ o=create(10,10)
+ for y in range(10):
+  if y==s:o[y]=[c]*10
+  else:o[y][p]=c
+ return o
