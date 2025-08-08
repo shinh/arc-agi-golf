be36063df2ee23ff6af7d617dceeb9d905c47492
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-09 03:45:39 +0900

    Golf 185 a bit more

--- logic/task185.py
+++ logic/task185.py
@@ -9,15 +9,4 @@ def p(g):
                 ex=max(ex,x)
                 ey=max(ey,y)
     l=(ey-sy)//3
-    o=[]
-    for y in(0,1,2):
-        r=[]
-        for x in(0,1,2):
-            c=g[sy+l*y][sx+l*x]
-            q=0
-            if c not in g[0]:
-                if c==g[sy+l*y+l][sx+l*x]==g[sy+l*y][sx+l*x+l]==g[sy+l*y+l][sx+l*x+l]:
-                    q=c
-            r.append(q)
-        o.append(r)
-    return o
+    return[[[0,g[sy+l*y][sx+l*x]][g[sy+l*y][sx+l*x]not in g[0]and g[sy+l*y][sx+l*x]==g[sy+l*y+l][sx+l*x]==g[sy+l*y][sx+l*x+l]==g[sy+l*y+l][sx+l*x+l]]for x in(0,1,2)]for y in(0,1,2)]
