2e55299aaf9a6f36808f301f7e6b4e60b701d1db
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-09 03:58:19 +0900

    A bit more golf for 185

--- logic/task185.py
+++ logic/task185.py
@@ -1,11 +1,9 @@
 def p(g):
     sx=sy=99
-    ey=-1
     for y in range(len(g)):
         for x in range(len(g[0])):
             if g[y][x]not in g[0]:
                 sx=min(sx,x)
                 sy=min(sy,y)
-                ey=max(ey,y)
-    l=(ey-sy)//3
-    return[[[0,g[sy+l*y][sx+l*x]][g[sy+l*y][sx+l*x]not in g[0]and g[sy+l*y][sx+l*x]==g[sy+l*y+l][sx+l*x]==g[sy+l*y][sx+l*x+l]==g[sy+l*y+l][sx+l*x+l]]for x in(0,1,2)]for y in(0,1,2)]
+                l=(y-sy)//3
+    return[[g[sy+l*y][sx+l*x]*(g[sy+l*y][sx+l*x]not in g[0]and g[sy+l*y][sx+l*x]==g[sy+l*y+l][sx+l*x]==g[sy+l*y][sx+l*x+l]==g[sy+l*y+l][sx+l*x+l])for x in(0,1,2)]for y in(0,1,2)]
