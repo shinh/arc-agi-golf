7f0e1d9434518d8d2b2fb657218dd18404b2a172
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-09 01:46:12 +0900

    refine task260

--- logic/task260.py
+++ logic/task260.py
@@ -1,12 +1,12 @@
 def p(g):
     for y in range(9):
         for x in range(9):
-            c=g[y][x]
-            if c==5:
-                for sy,sx in(y-1,x+1),(y+1,x-1):
-                    if g[y][sx]+g[sy][x]<1:
-                        for i in range(10-abs(sy-sx)):
-                            g[sy-min(sy,sx)+i][sx-min(sy,sx)+i]=f
+            if (c:=g[y][x])==5:
+                for d in 1,-1:
+                    a=y-d; b=x+d
+                    if g[y][b]+g[a][x]<1:
+                        for i in range(10-abs(a-b)):
+                            g[a-min(a,b)+i][b-min(a,b)+i]=f
             elif c:
                f=c
-    return[[0 if c==5 else c for c in r]for r in g]
+    return[[c*(c!=5)for c in r]for r in g]
