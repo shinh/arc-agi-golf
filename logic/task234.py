d39554872bfd01584c7411a4c7cbb6d89b53f9b1
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-08 23:44:40 +0900

    Golf 234 manually

--- logic/task234.py
+++ logic/task234.py
@@ -1,28 +1,10 @@
 def p(g):
-    h=len(g);w=len(g[0])
-    cols=[c for c in {v for r in g for v in r} if c]
-    def box(c):
-        P=[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==c]
-        xs,ys=zip(*P)
-        t=min(xs);b=max(xs);l=min(ys);r=max(ys)
-        return t,b,l,r,(b-t+1)*(r-l+1)==len(P),P
-    B={c:box(c)for c in cols}
-    rc=[c for c,v in B.items() if v[4]][0]
-    mc=[c for c in cols if c!=rc][0]
-    rect=set()
-    for i,j in B[mc][5]:
-        L=g[i][j-1] if j>0 else 0;R=g[i][j+1] if j<w-1 else 0
-        U=g[i-1][j] if i>0 else 0;D=g[i+1][j] if i<h-1 else 0
-        if (L==0 and R==0) or (U==0 and D==0):
-            continue
-        rect.add((i,j))
-    st=min(i for i,j in rect);sb=max(i for i,j in rect)
-    sl=min(j for i,j in rect);sr=max(j for i,j in rect)
-    dt,db,dl,dr=B[rc][:4]
-    if sl<=dr and dl<=sr:
-        di=db+1-st if st>db else dt-1-sb;dj=0
-    else:
-        dj=dr+1-sl if sl>dr else dl-1-sr;di=0
-    res=[[0if v==mc else v for v in r] for r in g]
-    for i,j in rect:res[i+di][j+dj]=mc
-    return res
+    # We remove only a single pixel every 4 iterations.
+    for o in range(44):
+        w=len(g[0]);y=1
+        for y in range(len(g)):
+            for x in range(w-1):
+                if g[y][x-1]+g[y][x+1]==0and g[y-1][x]!=g[y][x]and g[y][x]*g[y-1][x]:
+                    g=g[:y]+g[y+1:]+[[0]*w]
+        g=[list(r)for r in zip(*g[::-1])]
+    return g
