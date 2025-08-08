62b0abc7114b1d4dcf84044387e79198b4d55798
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-09 01:15:45 +0900

    Manual golf 260 a bit more

--- logic/task260.py
+++ logic/task260.py
@@ -1,20 +1,12 @@
 def p(g):
-    f=1
     for y in range(9):
         for x in range(9):
             c=g[y][x]
             if c==5:
-                sy=-1
-                if g[y][x+1]+g[y-1][x]<1:
-                    sy=y-1
-                    sx=x+1
-                if g[y][x-1]+g[y+1][x]<1:
-                    sy=y+1
-                    sx=x-1
-                if sy>=0:
-                    m=min(sy,sx)
-                    for i in range(10-abs(sy-sx)):
-                        g[sy-m+i][sx-m+i]=f
+                for sy,sx in(y-1,x+1),(y+1,x-1):
+                    if g[y][sx]+g[sy][x]<1:
+                        for i in range(10-abs(sy-sx)):
+                            g[sy-min(sy,sx)+i][sx-min(sy,sx)+i]=f
             elif c:
                f=c
     return[[0 if c==5 else c for c in r]for r in g]
