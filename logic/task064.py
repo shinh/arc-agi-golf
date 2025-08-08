adab4cef7bcb2436f7a4170cff10539e13299d48
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-09 03:15:04 +0900

    Golf 64

--- logic/task064.py
+++ logic/task064.py
@@ -1,19 +1,16 @@
 def p(g):
-    t=[[]for _ in range(10)]
-    for y,r in enumerate(g):
-        for x,v in enumerate(r):t[v]+=((y,x),)
-    m=-1
-    for k,v in enumerate(t):
-        if v:
-            ys=[y for y,_ in v];xs=[x for _,x in v]
-            a=min(ys);b=max(ys);c=min(xs);d=max(xs);n=len(v)
-            if (b-a+1)*(d-c+1)==n>m:m=n;rc=k;R=(a,b,c,d)
-    rare=min((len(v),k)for k,v in enumerate(t)if k!=rc and v)[1]
-    o=[r for r in g];a,b,c,d=R
-    for y,x in t[rare]:
-        if c<=x<=d:
-            for i in(range(y,a)if y<a else range(b+1,y+1)):o[i][x]=rare
-        elif a<=y<=b:
-            for j in(range(x,c)if x<c else range(d+1,x+1)):o[y][j]=rare
-    return o
-
+    bg=max(g[0],key=g[0].count)
+    for o in range(4):
+        for y in range(len(g)):
+            for x in range(len(g[0])-2):
+                c=g[y][x]
+                if c!=bg and(x<1or g[y][x-1]==bg)and(g[y][x+1]==bg or g[y][x+2]==bg):
+                    for nx in range(x,len(g[0])):
+                        u=g[y][nx]
+                        if u!=bg and u!=c:
+                            if g[y][nx+1]==u:
+                                for rx in range(x,nx):
+                                    g[y][rx]=c
+                            break
+        g=[list(r)for r in zip(*g[::-1])]
+    return g
