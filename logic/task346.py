ffd579c94533962240701584d5acb67e5f4d93ca
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-09 00:25:00 +0900

    Manual golf 346

--- logic/task346.py
+++ logic/task346.py
@@ -1,12 +1,7 @@
 def p(g):
- c=[]
- for r in g:
-  for v in r:
-   if v and v not in c:c.append(v)
- a,b=c[0],c[-1]
- h=len(g);w=len(g[0])
- for i in range(1,h-1):
-  for j in range(1,w-1):
-   if g[i][j]==b and all(g[i+di][j+dj]==a for di in(-1,0,1) for dj in(-1,0,1) if di or dj):
+ for i in range(1,len(g)-1):
+  for j in range(1,len(g[0])-1):
+   b=g[i][j]
+   a=g[i-1][j]
+   if a*all(a==g[i+di][j+dj]for di in(-1,0,1) for dj in(-1,0,1) if di|dj):
     return[[b]]
- return[[a]]
