eb0e24ec04c9b3fc5a68b347119569ed83e5d139
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-10 17:00:22 +0900

    Rely on myminifier for task016.py

--- logic/task016.py
+++ logic/task016.py
@@ -1,4 +1,2 @@
-p=lambda g:[[[0,5,6,4,3,1,2,7,9,8][x]for x in g[0]]]*3
 # p=lambda g:[[b"a\05\06\04\03\01\02\07\11\10"[x]for x in g[0]]]*3
-# 46 (optimal is 43), minifier currently mangles this but should work as is
-# p=lambda g:[[b"a	"[x]for x in g[0]]]*3
+p=lambda g:[[b"a	"[x]for x in g[0]]]*3
