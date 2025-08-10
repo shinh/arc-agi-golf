9a91b4d28e1bfa4bedf36d334a8ffa847291688d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-10 11:20:53 +0900

    Golf 201 a bit more

--- logic/task201.py
+++ logic/task201.py
@@ -1,8 +1,8 @@
 def p(g):
-    sx=sy=99
-    ex=ey=-1
-    for y in range(len(g)):
-        for x in range(len(g[0])):
+    sx=sy=sx2=sy2=99
+    ex=ey=ex2=ey2=-1
+    for y in range(13):
+        for x in range(13):
             if g[y][x]==4:
                 sx=min(sx,x)
                 sy=min(sy,y)
@@ -10,10 +10,8 @@ def p(g):
                 ey=max(ey,y)
     o=[[g[sy+y][sx+x]for x in range(ex-sx+1)]for y in range(ey-sy+1)]
 
-    sx2=sy2=99
-    ex2=ey2=-1
-    for y in range(len(g)):
-        for x in range(len(g[0])):
+    for y in range(13):
+        for x in range(13):
             if g[y][x]and(x<sx or y<sy or ex<x or ey<y):
                 sx2=min(sx2,x)
                 sy2=min(sy2,y)
