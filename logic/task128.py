60d1f9ec179fe3a14aec203340f994d23e9093ea
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 15:20:05 +0900

    Golf 128 a bit more

--- logic/task128.py
+++ logic/task128.py
@@ -1,5 +1,5 @@
 # rot90 -> shift -> rot270
-# 112 vs 72
+# 108 vs 72
 def p(g):
-    g=[[0]*(c:=r.index(0))+r[:15-c]for r in[[*r]for r in zip(*g[::-1])]]
-    return[[*r]for r in zip(*g)][::-1]
+    # How can we remove two r.index(0)?
+    return[[*r]for r in zip(*[[0]*(r.index(0))+r[:15-r.index(0)]for r in[[*r]for r in zip(*g[::-1])]])][::-1]
