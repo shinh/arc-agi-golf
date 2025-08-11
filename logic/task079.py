e9f5725c5bba6ed9dfe4dc145860148938c58a76
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 12:56:50 +0900

    Golf 79, very bad

--- logic/task079.py
+++ logic/task079.py
@@ -1,30 +1,17 @@
+# Very bad. I don't know how to check frames in a shorter manner.
 def p(g):
-    h=w=14
-    seen=set();pcs=[];mh=mw=0
-    for i in range(h):
-        for j in range(w):
-            if g[i][j]==0 or (i,j) in seen:continue
-            c=g[i][j];q=[(i,j)];seen.add((i,j));cells=[]
-            while q:
-                x,y=q.pop(0);cells.append((x,y))
-                for dx in(-1,0,1):
-                    for dy in(-1,0,1):
-                        if dx or dy:
-                            nx,ny=x+dx,y+dy
-                            if 0<=nx<h and 0<=ny<w and g[nx][ny]==c and (nx,ny)not in seen:
-                                seen.add((nx,ny));q.append((nx,ny))
-            mi=min(x for x,_ in cells);mj=min(y for _,y in cells)
-            sh=[(x-mi,y-mj)for x,y in cells]
-            pcs.append((c,sh))
-            mh=max(mh,max(x for x,_ in sh)+1)
-            mw=max(mw,max(y for _,y in sh)+1)
-    if not pcs:return [[0]]
-    freq={}
-    for c,sh in pcs:
-        k=(c,tuple(sorted(sh)))
-        freq[k]=freq.get(k,0)+1
-    c,sh=max(freq,key=freq.get)
-    o=[[0]*mw for _ in range(mh)]
-    for x,y in sh:o[x][y]=c
-    return o
-
+    m=[]
+    for y in range(12):
+        for x in range(12):
+            a=[r[x:x+3]for r in g[y:y+3]]
+            b=[]
+            if y:b+=g[y-1][max(x-1,0):x+4]
+            if y<11:b+=g[y+3][max(x-1,0):x+4]
+            if x:b+=[g[y][x-1],g[y+1][x-1],g[y+2][x-1]]
+            if x<11:b+=[g[y][x+3],g[y+1][x+3],g[y+2][x+3]]
+            s=set(sum(a,[]))-{0}
+            if len(s)==1:
+                c=[*s][0]
+                if c not in b:
+                    m.append(a)
+    return max(m,key=m.count)
