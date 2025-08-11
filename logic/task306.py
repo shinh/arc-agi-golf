494ec77000fe22f2c4035d4421ebf8e2a64e6d5d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 16:56:03 +0900

    Golf 306

--- logic/task306.py
+++ logic/task306.py
@@ -1,14 +1,9 @@
 def p(g):
-    h=len(g);w=len(g[0])
-    rs=[0]+[i+1 for i,r in enumerate(g)if all(v==4 for v in r)]
-    cs=[0]+[j+1 for j in range(w)if all(r[j]==4 for r in g)]
-    o=[r for r in g]
-    for y,row in enumerate(g):
-        for x,v in enumerate(row):
-            if v and v!=4:
-                sy=max(r for r in rs if r<=y)
-                sx=max(c for c in cs if c<=x)
-                for ry in rs:
-                    for cx in cs:
-                        o[y-sy+ry][x-sx+cx]=v
-    return o
+    for t in range(2):
+        for y in range(len(g)):
+            for x in range(len(g[0])):
+                if t:
+                    g[y][x]=g[y%10][x%10]
+                else:
+                    g[y%10][x%10]=g[y][x]
+    return g
