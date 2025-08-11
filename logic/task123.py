3c5280e17971d886191ef39e77d62b20f940116f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 19:47:50 +0900

    Golf 123

--- logic/task123.py
+++ logic/task123.py
@@ -1,12 +1,1 @@
-def p(g):
-    t=g[0][0];s=[r[0]for r in g]
-    if 0 in s:
-        a=[c for c in s if c];a=a[1:]+a[:1];s=a+[a[0]]
-    o=[[0]*10 for _ in range(10)]
-    for y in range(5):
-        for x in range(5):
-            o[y][x]=g[y][x] or t
-            o[y+5][x+5]=s[max(y,x)]
-        o[y][5:]=s
-        o[y+5][:5]=[s[y]]*5
-    return o
+p=lambda g,R=range(10):[[g[m][m]for x in R for m in[max(y,x)%(4+(g[4][4]>0))]]for y in R]
