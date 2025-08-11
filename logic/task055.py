586251845ef7f88b90d9da6c0ab3fbfd22f14415
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 20:40:34 +0900

    Golf 55

--- logic/task055.py
+++ logic/task055.py
@@ -1,9 +1,16 @@
+# [0,2,0]
+# [4,6,3]
+# [0,1,0]
 def p(g):
-    w=len(g[0]);x=[i for i in range(w) if all(r[i]==8 for r in g)];y=[i for i,r in enumerate(g) if all(c==8 for c in r)]
-    a,b=x;c,d=y
-    for i,r in enumerate(g):
-        if i<c:r[a+1:b]=[2]*(b-a-1)
-        elif i>d:r[a+1:b]=[1]*(b-a-1)
-        elif c<i<d:
-            r[:a]=[4]*a;r[a+1:b]=[6]*(b-a-1);r[b+1:]=[3]*(w-b-1)
+    dy=0
+    for y in range(len(g)):
+        dx=0
+        if all(c==8for c in g[y]):
+            dy+=1
+        else:
+            for x in range(len(g[0])):
+                if g[y][x]==8:
+                    dx+=1
+                else:
+                    g[y][x]=[0,2,0,4,6,3,0,1,0][dy*3+dx]
     return g
