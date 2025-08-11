825cfb33d5724e082d03213ccb53130732dffefe
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 16:14:44 +0900

    Golf 197

--- logic/task197.py
+++ logic/task197.py
@@ -1,13 +1,14 @@
 def p(g):
- t=None;w=len(g[0])
- for y,r in enumerate(g):
-  if not any(r):continue
-  k=w
-  while k and r[k-1]==0:k-=1
-  if k==w:t=r
-  else:
-   T=[];[T.append(x)for x in t if x not in T]
-   P=[];[P.append(x)for x in r[:k] if x not in P]
-   m={T[i]:P[i] for i in range(len(P))}
-   g[y]=[m.get(x,0) for x in t];t=g[y]
- return g
+ o=[]
+ for r in g:
+  m={}
+  nr=[]
+  for i in range(len(r)):
+   c=r[i]
+   if c:
+    m[g[1][i]]=c
+   elif m:
+    c=m[g[1][i]]
+   nr.append(c)
+  o.append(nr)
+ return o
