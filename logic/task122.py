5f5c1290fd0b09cd9b3b1f2bd39b05076e59c236
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 13:59:47 +0900

    More golf 122

--- logic/task122.py
+++ logic/task122.py
@@ -1,17 +1,12 @@
 def p(g):
-    for o in range(4):
-        if o==0 or o==3:
-            ok=0
-            for y in range(len(g)-2):
-                for x in range(len(g[0])-3):
-                    if g[y+1][x:x+4]==[2,3,2,3]:
-                        for dy in 2,1,0:
-                            for dx in 2,1,0:
-                                if dy!=1 or dx!=1:
-                                    g[y+dy][x+dx+2]=g[y+dy][x+dx]
-                                    g[y+dy][x+dx]=0
-                                    ok=1
-                    if ok:break
-                if ok:break
-        g=[list(r)for r in zip(*g[::-1])]
-    return g
+    for sy,sx in(2,0),(0,2):
+        for y in range(len(g)-2-sy):
+            for x in range(len(g[0])-2-sx):
+                if[[r[x+1]for r in g[y:y+4]],g[y+1][x:x+4]][sx//2]==[2,3,2,3]:
+                    for d in range(9):
+                        if d!=4:
+                            dy=2-d//3
+                            dx=2-d%3
+                            g[y+dy+sy][x+dx+sx]=g[y+dy][x+dx]
+                            g[y+dy][x+dx]=0
+                    return g
