bbb60fefae4609a85fb34c0202cf36abbea9b056
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 14:16:47 +0900

    Golf 122 a bit

--- logic/task122.py
+++ logic/task122.py
@@ -1,12 +1,13 @@
+# Far from the best (100)
 def p(g):
-    for sy,sx in(2,0),(0,2):
-        for y in range(len(g)-2-sy):
-            for x in range(len(g[0])-2-sx):
-                if[[r[x+1]for r in g[y:y+4]],g[y+1][x:x+4]][sx//2]==[2,3,2,3]:
+    for sx in range(2):
+        for y in range(len(g)-2):
+            for x in range(len(g[0])-2):
+                if[[r[x+1]for r in g[y:y+4]],g[y+1][x:x+4]][sx]==[2,3,2,3]:
                     for d in range(9):
                         if d!=4:
                             dy=2-d//3
                             dx=2-d%3
-                            g[y+dy+sy][x+dx+sx]=g[y+dy][x+dx]
+                            g[y+dy+2-sx*2][x+dx+sx*2]=g[y+dy][x+dx]
                             g[y+dy][x+dx]=0
                     return g
