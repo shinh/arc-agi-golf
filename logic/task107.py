f1320665dc78f76b75d67be2c1ea22ea0f7ed35e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 16:08:49 +0900

    Golf 107

--- logic/task107.py
+++ logic/task107.py
@@ -1,12 +1,18 @@
-def p(j,u=range):
- A=len(j);c=len(j[0]);E=len({*sum(j,[])}-{0})
- j=[[j[W//E][l//E]for l in u(c*E)]for W in u(A*E)];A*=E;c*=E
- for k in u(min(A,c),0,-1):
-  for W in u(A-k+1):
-   for l in u(c-k+1):
-    J=j[W][l]
-    if J and all(r[l:l+k]==[J]*k for r in j[W:W+k]):
-     for a,C in(-1,-1),(-1,k),(k,-1),(k,k):
-      e=W+a;K=l+C
-      while-1<e<A and-1<K<c and not j[e][K]:j[e][K]=2;e+=a>0 or-1;K+=C>0 or-1
-     return j
+def p(g):
+    sx=g[1][0]<1
+    sy=g[0][1]<1
+    c=g[1][1]
+    n=len(set(sum(g,[])))-1
+    g=[[cn for c in rn for cn in[c]*n] for r in g for rn in [r]*n]
+
+    def put_red(y,x):
+        if 0<=y<len(g) and 0<=x<len(g[0]) and g[y][x]<1:
+            g[y][x]=2
+
+    for i in range(n):
+        put_red(sy*n-i-1,sx*n-i-1)
+        put_red(sy*n-i-1,sx*n+2*n+i)
+        put_red(sy*n+2*n+i,sx*n-i-1)
+        put_red(sy*n+2*n+i,sx*n+2*n+i)
+
+    return g
