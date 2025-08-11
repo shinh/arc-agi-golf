c4e90d4ef66dd73b59575f6b1b62d55d0195402a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 20:41:54 +0900

    Golf 55 a bit more

--- logic/task055.py
+++ logic/task055.py
@@ -1,16 +1,13 @@
 # [0,2,0]
 # [4,6,3]
 # [0,1,0]
-def p(g):
-    dy=0
-    for y in range(len(g)):
+def p(g,dy=0):
+    for r in g:
         dx=0
-        if all(c==8for c in g[y]):
-            dy+=1
-        else:
-            for x in range(len(g[0])):
-                if g[y][x]==8:
-                    dx+=1
-                else:
-                    g[y][x]=[0,2,0,4,6,3,0,1,0][dy*3+dx]
+        dy+=sum(r)>16
+        for x in range(len(r)):
+            if r[x]:
+                dx+=1
+            else:
+                r[x]=[0,2,0,4,6,3,0,1,0][dy*3+dx]
     return g
