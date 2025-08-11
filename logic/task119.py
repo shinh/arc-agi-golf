81275fda3d9d3277b737803b4e3a3433dd6c2bb7
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 14:28:15 +0900

    golf task119

--- logic/task119.py
+++ logic/task119.py
@@ -1,35 +1,20 @@
-# Far from best (123)
 def p(g):
-    def get(y,x):
-        if y<0 or y>11 or x<0 or x>11:
-            return 0
-        return g[y][x]
-
-    for dy,dx in ((1,1),(1,-1),(-1,1),(-1,-1)):
-        ng=[[*r]for r in g]
-        found_r=0
-        for oy in range(12):
-            for x in range(12):
-                y=oy
-                if g[y][x]&get(y-dy,x-dx)==8:
-                    y+=dy
-                    x+=dx
-                    while 0<=x<12and 0<=y<12:
-                        if g[y][x]==2:
-                            found_r=1
-                            y-=dy
-                            x-=dx
-                            if get(y+dy-1,x+dx)&get(y+dy+1,x+dx)==2:
-                                dx=-dx
-                            else:
-                                dy=-dy
-                            #print('found',y,x,dy,dx)
-                            #show(ng,'kkk')
-                            continue
-                        if g[y][x]!=0:
-                            break
-                        ng[y][x]=3
-                        y+=dy
-                        x+=dx
-        g=[g,ng][found_r]
-    return g
+ z=lambda y,x:g[y][x]if-1<x<12>y>-1 else 0
+ for a in 1,-1:
+  for b in 1,-1:
+   n=[*map(list,g)];f=0
+   for i in range(12):
+    for x in range(12):
+     y=i
+     if g[y][x]&z(y-a,x-b)==8:
+      y+=a;x+=b
+      while-1<x<12>y>-1:
+       if g[y][x]==2:
+        f=1;y-=a;x-=b
+        if z(y+a-1,x+b)&z(y+a+1,x+b)==2:b=-b
+        else:a=-a
+        continue
+       if g[y][x]:break
+       n[y][x]=3;y+=a;x+=b
+   g=[g,n][f]
+ return g
