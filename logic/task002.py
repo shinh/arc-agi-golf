bca9faf4897f4f634d3e566bd0f4c45d78d7377e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 11:42:16 +0900

    hoge

--- logic/task002.py
+++ logic/task002.py
@@ -1,7 +1,7 @@
 def p(g):
- R=range
- n=len(g);v=[[0]*n for _ in R(n)]
- def f(i,j):
-  if 0<=i<n and 0<=j<n and not v[i][j] and g[i][j]==0:v[i][j]=1;[f(i+d,j+e)for d,e in[(1,0),(-1,0),(0,1),(0,-1)]]
- [f(i,0)or f(i,n-1)or f(0,i)or f(n-1,i)for i in R(n)]
- return[[4 if g[i][j]==0 and not v[i][j]else g[i][j]for j in R(n)]for i in R(n)]
+ n=len(g)
+ for t in range(100):
+  g[0]=[[c,1][c==0]for c in g[0]]
+  g=[[[r[x],1][r[x]==0and(r*2)[x+1]==1]for x in range(len(r))]for r in g]
+  g=[*zip(*g[::-1])]
+ return[[[4,0,0,3][c]for c in r]for r in g]
