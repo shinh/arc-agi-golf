f1d84134d33b589a9288b9a0b59051a33461e2e5
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 17:17:18 +0900

    Golf 272 a bit more

--- logic/task272.py
+++ logic/task272.py
@@ -1,6 +1,3 @@
+# We can assume g has only 0 and 2. Isolated pixels must become to 1.
 def p(g):
-    for r,pr,nr in zip(g,[[0]*99]+g,g[1:]+[[0]*99]):
-        for i in range(len(r)):
-            if r[i]and[*r[1:],0][i]+[0,*r][i]+pr[i]+nr[i]<1:
-                r[i]=1
-    return g
+    return [[r[i]-(r[i]and[*r[1:],0][i]+[0,*r][i]+pr[i]+nr[i]<1)for i in range(len(r))]for r,pr,nr in zip(g,[[0]*9]+g,g[1:]+[[0]*9])]
