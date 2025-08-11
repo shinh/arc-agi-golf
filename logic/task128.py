48fd824c3c73ebfca7fc50148095846399a43730
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 15:36:11 +0900

    golf task128

--- logic/task128.py
+++ logic/task128.py
@@ -1,5 +1,1 @@
-# rot90 -> shift -> rot270
-# 108 vs 72
-def p(g):
-    # How can we remove two r.index(0)?
-    return[[*r]for r in zip(*[[0]*(r.index(0))+r[:15-r.index(0)]for r in[[*r]for r in zip(*g[::-1])]])][::-1]
+p=lambda g:[*map(list,zip(*(([0]*r.index(0)+[*r])[:15]for r in zip(*g[::-1]))))][::-1]
