a7df2970f3009bc68590f5866ce0bb5d5db0bfde
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 19:22:26 +0900

    Golf 92

--- logic/task092.py
+++ logic/task092.py
@@ -1,18 +1,10 @@
 def p(g):
-    for r in g:
-        d={}
-        for i,v in enumerate(r):
-            if v:d.setdefault(v,[]).append(i)
-        for v,l in d.items():
-            if len(l)>1:r[l[0]:l[-1]+1]=[v]*(l[-1]-l[0]+1)
-    w=len(g[0])
-    for x in range(w):
-        d={}
-        for y,r in enumerate(g):
-            v=r[x]
-            if v:d.setdefault(v,[]).append(y)
-        for v,l in d.items():
-            if len(l)>1:
-                a=l[0];b=l[-1]+1
-                for y in range(a,b):g[y][x]=v
+    for o in range(4):
+        for c in range(1,10):
+            for r in g:
+                if c in r:
+                    a=r.index(c)
+                    b=len(r)-r[::-1].index(c)-1
+                    r[a:b+1]=[c]*(b-a+1)
+        g=[*map(list,zip(*g[::-1]))]
     return g
