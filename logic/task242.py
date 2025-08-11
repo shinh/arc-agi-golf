b9d5ffc310b7ef1f0f2e74014c11386b46791af0
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 19:07:26 +0900

    wip

--- logic/task242.py
+++ logic/task242.py
@@ -2,9 +2,9 @@
 # Task242 53 bytes from theirs: 107 vs 54
 # Task351 37 bytes from theirs: 107 vs 70
 # Task400 37 bytes from theirs: 107 vs 70
-def p(g,R=range(16)):
+def p(g):
     o=[]
-    for y in R:
-        r=[g[15-y][15-x]for x in R if g[y][x]<1]
+    for r,rr in zip(g,g[::-1]):
+        r=[rc for c,rc in zip(r,rr[::-1])if c<1]
         if r:o+=r,
     return o
