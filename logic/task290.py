71ead2174dec216e9314f9f7c094380839aa9210
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 21:55:22 +0900

    Golf 290 a bit more

--- logic/task290.py
+++ logic/task290.py
@@ -1,14 +1,7 @@
-# 165
+# 127
+# I don't know how to efficiently crop non-zero a region
 def p(g):
     f=sum(g,[])
     f.sort(key=f.count)
-    a=list(set(f))[1:3]
-    o=[]
-    for r in g:
-        n=[]
-        for x in range(len(r)):
-            if r[x]:
-                n+=a[r[x]==a[0]],
-        if n:
-            o+=n,
-    return o
+    a=[*set(f)][1:]
+    return[r for r in[[[a[c==a[0]],0][c<1]for c in r if c]for r in g]if r]
