d11f29c7e6bfa8f3aec3cf8677b1a8de489f4121
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 15:04:06 +0900

    Golf 128

--- logic/task128.py
+++ logic/task128.py
@@ -1,19 +1,10 @@
 def p(g):
- r=[[0]*len(g[0])for _ in g]
- v=set()
- for i in range(len(g)):
-  for j in range(len(g[0])):
-   if g[i][j]and(i,j)not in v:
-    c,q=[(i,j)],[(i,j)]
-    v.add((i,j))
-    x=g[i][j]
-    while q:
-     a,b=q.pop()
-     for d,e in[(0,1),(1,0),(0,-1),(-1,0)]:
-      if 0<=a+d<len(g)and 0<=b+e<len(g[0])and g[a+d][b+e]==x and(a+d,b+e)not in v:
-       v.add((a+d,b+e))
-       c.append((a+d,b+e))
-       q.append((a+d,b+e))
-    h=max(a for a,b in c)-min(a for a,b in c)+1
-    for a,b in c:r[a-h][b]=x
- return r
+ for x in range(15):
+   n=0
+   while g[15-1-n][x]:
+    c=g[14-n][x]
+    g[14-n][x]=0
+    n+=1
+   for i in range(n):
+    g[14-n-i][x]=c
+ return g
