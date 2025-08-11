b64b62aafdcd5262ecfe8063815c1087647c2ea0
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-11 11:51:30 +0900

    Golf task 2

--- logic/task002.py
+++ logic/task002.py
@@ -1,7 +1,4 @@
 def p(g):
- n=len(g)
- for t in range(100):
-  g[0]=[[c,1][c==0]for c in g[0]]
-  g=[[[r[x],1][r[x]==0and(r*2)[x+1]==1]for x in range(len(r))]for r in g]
-  g=[*zip(*g[::-1])]
- return[[[4,0,0,3][c]for c in r]for r in g]
+ for t in range(80):
+  g=[*zip(*[[[c,1][c+n==1]for c,n in zip(r,[*r[1:],1])]for r in g][::-1])]
+ return[[b'\0'[c]for c in r]for r in g]
