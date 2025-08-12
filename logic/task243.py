f955ceea9faf978494ae2cb8f1d7a1b8c5bf7ffc
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-12 15:04:02 +0900

    refactor task243 flood fill

--- logic/task243.py
+++ logic/task243.py
@@ -1,9 +1,2 @@
-def p(g):
- h=len(g);w=len(g[0]);q=[]
- for y in range(h):
-  for x in range(w):q+=[(y,x)]*(g[y][x]==1)
- while q:
-  y,x=q.pop()
-  for Y,X in((y+1,x),(y-1,x),(y,x+1),(y,x-1)):
-   if-1<Y<h and-1<X<w and g[Y][X]==0:g[Y][X]=1;q+=[(Y,X)]
- return g
+# spread 1s by rotating
+p=lambda g,n=80:n and p([[a or(b==1)|0 for a,b in zip(r,r[1:]+(0,))]for r in zip(*g[::-1])],n-1)or g
