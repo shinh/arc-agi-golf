ec2ff4c3d57cfdf171974c8e7a358036c20f2e69
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-13 02:23:01 +0900

    Golf task243

--- logic/task243.py
+++ logic/task243.py
@@ -1,3 +1,6 @@
 # rotate the grid so that 1 flows in one direction
-p=lambda g,n=144:n and p([[a or b==1 for a,b in zip(r,r[1:]+(0,))]for r in zip(*g[::-1])],n-1)or g
+# p=lambda g,n=144:n and p([[a or b==1 for a,b in zip(r,r[1:]+(0,))]for r in zip(*g[::-1])],n-1)or g
+
+# Use str.replace instead
+p=lambda g,n=80:n and p([*map(list,zip(*eval(str(g).replace("1, 0","1, 1"))[::-1]))],n-1)or g
 
