e59e5aab25311ab2ef7dd9bc95b8d75de934aa4d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-12 19:10:42 +0900

    Shorten task279 flood fill

--- logic/task279.py
+++ logic/task279.py
@@ -1,8 +1,4 @@
 # flood fill using rotation like task002
 f=lambda G,n=64:n and f([[a or b==1 for a,b in zip(r,r[1:]+(2,))]for r in zip(*G[::-1])],n-1)or G
-def p(g):
-    h=len(g);w=len(g[0])
-    A=f([[2*(c<9)or(i<1 or i==h-1 or j<1 or j==w-1)for j,c in enumerate(r)]for i,r in enumerate(g)])
-    B=f([[(1,2,0)[c]for c in r]for r in A])
-    return [[[(8,1)[b<1],9][a<2]for a,b in zip(r1,r2)]for r1,r2 in zip(A,B)]
+p=lambda g:(A:=f([[2*(c<9)or i*(len(g)-1-i)<1 or j*(len(g[0])-1-j)<1 for j,c in enumerate(r)]for i,r in enumerate(g)]),[[[9,8-7*(b<1)][a>1]for a,b in zip(r1,r2)]for r1,r2 in zip(A,f([[(c+1)%3 for c in r]for r in A]))])[1]
 
