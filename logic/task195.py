f761df13be0552c50f213d529e0081e9228a5e21
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-13 01:20:22 +0900

    Golf 195

--- logic/task195.py
+++ logic/task195.py
@@ -1,7 +1,7 @@
-# 104
+# 219
 def p(g):
- for _ in range(4):
-  while set(g[0])=={0}:g=g[1:]
+ for _ in range(40):
+  if{0}==set(g[0]):g=g[1:]
   g=[*zip(*g[::-1])]
  q=[(i//3,j//3)for i in range(0,9,3)for j in range(0,9,3)if g[i][j]]
  o=[[0]*9 for _ in range(9)]
