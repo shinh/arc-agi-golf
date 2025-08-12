b4b61c3ccdc285b5f81ce8bc03bb343a89d254d4
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-13 01:44:44 +0900

    refactor task205 cropping

--- logic/task205.py
+++ logic/task205.py
@@ -1,24 +1,25 @@
+# crop big blob then stretch the rare color as a cross
+
 def p(g):
-    h=len(g);w=len(g[0]);s=set();B=[]
+    h=len(g);w=len(g[0]);v=set();B=[];C=0
     for i in range(h):
         for j in range(w):
-            if (i,j)in s:continue
-            c=g[i][j];q=[(i,j)];s.add((i,j));t=[]
+            if(i,j)in v:continue
+            c=g[i][j];q=[(i,j)];v.add((i,j));t=[]
             while q:
                 x,y=q.pop();t.append((x,y))
                 for a,b in(1,0),(-1,0),(0,1),(0,-1):
-                    u,v=x+a,y+b
-                    if 0<=u<h and 0<=v<w and g[u][v]==c and(u,v)not in s:s.add((u,v));q.append((u,v))
-            if len(t)>len(B):B=t;col=c
-    rs=[x for x,_ in B];cs=[y for _,y in B]
-    x=[r[min(cs):max(cs)+1]for r in g[min(rs):max(rs)+1]]
+                    u,vv=x+a,y+b
+                    if 0<=u<h and 0<=vv<w and g[u][vv]==c and(u,vv)not in v:v.add((u,vv));q.append((u,vv))
+            if len(t)>len(B):B=t;C=c
+    rs=[r for r,_ in B];cs=[c for _,c in B]
+    g=[row[min(cs):max(cs)+1]for row in g[min(rs):max(rs)+1]]
     for _ in range(32):
-        x=[list(r)for r in zip(*x[::-1])]
-        if len(x[0])-2>sum(v==col for v in x[0]):x=x[1:]
-    f=[v for r in x for v in r];c=min(set(f),key=f.count)
-    pts=[(i,j)for i,r in enumerate(x)for j,v in enumerate(r)if v==c]
-    H=len(x);W=len(x[0])
-    for i,j in pts:
-        for k in range(H):x[k][j]=c
-        for k in range(W):x[i][k]=c
-    return x
+        g=[list(r)for r in zip(*g[::-1])]
+        if len(g[0])-2>g[0].count(C):g=g[1:]
+    H=len(g);W=len(g[0]);f=sum(g,[]);d=min(set(f),key=f.count)
+    P=[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==d]
+    for i,j in P:
+        for k in range(H):g[k][j]=d
+        for k in range(W):g[i][k]=d
+    return g
