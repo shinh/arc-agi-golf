9c96399107091579f84fb312902bf46b100d55c5
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-13 00:50:02 +0900

    Golf 146

--- logic/task146.py
+++ logic/task146.py
@@ -1,4 +1,3 @@
-def p(g):
-    for i in range(3):
-        pce=g[i*3:i*3+3]
-        if pce!=[list(r)for r in zip(*pce)]:return pce
+def p(g,i=0):
+    pce=g[i:i+3]
+    return pce==[[*r]for r in zip(*pce)]and p(g,i+3)or pce
