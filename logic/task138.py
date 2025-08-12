fa344ffb933bc6a2022457f524fa740ccb54d52f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-12 23:59:47 +0900

    Golf 138

--- logic/task138.py
+++ logic/task138.py
@@ -1,21 +1,11 @@
 def p(g):
-    H=len(g)
-    y_borders=[]
-    for y in range(H):
-        if max(g[y],key=g[y].count):
-            y_borders.append(y)
-    x_borders=[]
-    for x in range(len(g[0])):
-        r=[g[y][x] for y in range(H)]
-        if max(r,key=r.count):
-            x_borders.append(x)
-
-    o=[[g[y][x]for x in range(x_borders[0],x_borders[-1]+1)]for y in range(y_borders[0],y_borders[-1]+1)]
-
-    for _ in range(4):
-        for r in o:
+    for t in range(4):
+        while 0in g[0]:
+            g=g[1:]
+        g=[*map(list,zip(*g[::-1]))]
+    for t in range(4):
+        for r in g:
             for x in range(r.index(r[-1]),len(r)):
                 r[x]=r[-1]
-        o=[*map(list,zip(*o[::-1]))]
-
-    return o
+        g=[*map(list,zip(*g[::-1]))]
+    return g
