759d6ee01a0a76d2347def128bbe7a7121fc1ba5
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-13 01:13:15 +0900

    Golf 207

--- logic/task207.py
+++ logic/task207.py
@@ -1,4 +1,7 @@
 def p(g):
-    l=[r[:2]for r in g];r=[r[3:]for r in g]
-    q=[tuple(map(tuple,x))for x in(l[:2],r[:2],l[3:],r[3:])]
-    return[list(r)for r in min(q,key=q.count)]
+    q=[]
+    for i in range(4):
+        y=i//2*3
+        x=i%2*3
+        q+=[g[y][x:x+2],g[y+1][x:x+2]],
+    return min(q,key=q.count)
