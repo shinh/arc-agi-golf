8b7390c60225d5c04e261822a84e727afabee6d6
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-12 13:54:46 +0900

    Use rotation flood fill for task048

--- logic/task048.py
+++ logic/task048.py
@@ -1,18 +1,13 @@
-def f(x,y,g):
-	global a;v.append((x,y))
-	for A in R(x-1,x+2):
-		for B in R(y-1,y+2):
-			if(A,B)in v:continue
-			v.append((A,B))
-			if A<0 or A>=h or B<0 or B>=w or(A,B)in[(r,c),(r+1,c),(r,c+1),(r+1,c+1)]:continue
-			if g[A][B]==2:a=8
-			if g[A][B]==8:f(A,B,g)
+# rotate grid to flow right through 8s from first 2 block
+# to detect connection to another 2 block using rotation
+
 def p(g):
-	global a,v,r,c,h,w,R;a,v,h,w,R,C=0,[],len(g),len(g[0]),range,enumerate
-	for(r,D)in C(g):
-		for(c,E)in C(D):
-			if E==2:
-				for A in R(r-1,r+3):
-					for B in R(c-1,c+3):
-						if A>=0 and A<h and B>=0 and B<w and g[A][B]==8:f(A,B,g)
-				return[[a]]
+ w=len(g[0]);s=sum(g,[]);i=s.index(2);f=0
+ for d in range(4):g[i//w+d//2][i%w+d%2]=1
+ for _ in range(64):
+  for r in g:
+   for j in range(len(r)-1):
+    if r[j]==1<r[j+1]:f|=r[j+1]<3;r[j+1]=1
+  if f:return[[8]]
+  g=[list(r)for r in zip(*g[::-1])]
+ return[[0]]
