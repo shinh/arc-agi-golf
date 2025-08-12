70326688ba39845396c5ee805c36afad176e0a98
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-13 00:58:32 +0900

    Golf 183

--- logic/task183.py
+++ logic/task183.py
@@ -1,10 +1,4 @@
-def p(j):
-	A=range;c=len(j);E=c//2-2;k=[];W=[j[0][0],j[0][-1],j[-1][0],j[-1][-1]]
-	for l in A(2,c-2):
-		J=[]
-		for a in A(2,c-2):
-			C=j[l][a]
-			if C==8:e=(l-2)//E;K=(a-2)//E;C=W[e*2+K]
-			J.append(C)
-		k.append(J)
-	return k
+def p(g):
+        H=len(g)
+        W=len(g[0])
+        return[[(g[y][x]>0)*g[-(y>=H/2)][-(x>=W/2)]for x in range(2,W-2)]for y in range(2,H-2)]
