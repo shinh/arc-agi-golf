aa4100c0e81bc09660b1f5f6e3c12eb85cfcfd58
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-12 09:01:27 +0900

    Adopt code from AI

--- logic/task002.py
+++ logic/task002.py
@@ -17,7 +17,7 @@
 # 0 1 3
 # 4 0 3
 
-p=lambda g,n=64:n and p([[[max(a,b==1),4*-~-a%11][n<2]for a,b in zip(r,[*r[1:],1])]for r in zip(*g[::-1])],n-1)or g
+# p=lambda g,n=64:n and p([[[max(a,b==1),4*-~-a%11][n<2]for a,b in zip(r,[*r[1:],1])]for r in zip(*g[::-1])],n-1)or g
 
 # possible improvements:
 #   better magic formulas for max(a,b==1) and also 4*-~-a%11
@@ -25,3 +25,6 @@ p=lambda g,n=64:n and p([[[max(a,b==1),4*-~-a%11][n<2]for a,b in zip(r,[*r[1:],1
 #       if doing that, you could have a single loop and use mod and div to get x and y
 #           this could be combined with looping for n too (would need to be a def instead of lambda
 #           maybe instead of using len(g) you could hardcode some factorial so that it goes through the list an integer number of times (factorial of largest size would work) - this could then be put into a default assignment of a parameter).
+
+# The AI found a better magic formula:
+p=lambda g,n=64:n and p([[[a or b==1,4*-~-a%11][n<2]for a,b in zip(r,r[1:]+(1,))]for r in zip(*g[::-1])],n-1)or g
