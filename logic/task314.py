01059a0a455a56fd9660715d38db95ee17544d58
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-13 13:31:20 +0900

    Golf 314

--- logic/task314.py
+++ logic/task314.py
@@ -1,29 +1,11 @@
 def p(g):
-    h=w=8
-    rows = [i for i, r in enumerate(g) if r.count(r[0]) == w]
-    cols = [j for j in range(w) if len({g[i][j] for i in range(h)}) == 1]
-    fc = g[rows[0]][0] if rows else g[0][cols[0]]
-    cnt = {}
-    for r in g:
-        for v in r:
-            if v != fc:
-                cnt[v] = cnt.get(v, 0) + 1
-    if not cnt:
-        return g
-    bg = max(cnt, key=cnt.get)
-    out = [r for r in g]
-    for c in cnt:
-        if c == bg:
-            continue
-        pts = [(i, j) for i in range(h) for j in range(w) if g[i][j] == c]
-        n = len(pts)
-        for a in range(n):
-            i1, j1 = pts[a]
-            for b in range(a + 1, n):
-                i2, j2 = pts[b]
-                if i1 == i2 and (j1 + j2) % 2 == 0:
-                    out[i1][(j1 + j2) // 2] = c
-                elif j1 == j2 and (i1 + i2) % 2 == 0:
-                    out[(i1 + i2) // 2][j1] = c
-    return out
-
+    a=[]
+    for k in range(8):
+        r=g[k]
+        c=max(r,key=r.count)
+        for i in range(2):
+            if r[i]==r[6+i]and r[i]!=c:a+=[k,3+i,r[i]],
+            if g[i][k]==g[6+i][k]and g[i][k]!=c:a+=[3+i,k,g[i][k]],
+    for y,x,c in a:
+        g[y][x]=c
+    return g
