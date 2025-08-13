60c1a1d049ec9014553b161b912dccf871bc4561
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-13 19:15:06 +0900

    Solve 174

--- logic/task174.py
+++ logic/task174.py
@@ -1,23 +1,8 @@
-def p(g):
-    w=h=10
-    seen=set()
-    for i in range(h):
-        for j in range(w):
-            if g[i][j]==0 or(i,j)in seen:continue
-            c=g[i][j];q=[(i,j)];obj=set()
-            while q:
-                x,y=q.pop()
-                if(x,y)in seen or g[x][y]!=c:continue
-                seen.add((x,y));obj.add((x,y))
-                for a in(-1,0,1):
-                    for b in(-1,0,1):
-                        if a or b:
-                            u,v=x+a,y+b
-                            if 0<=u<h and 0<=v<w:q.append((u,v))
-            mi=min(x for x,_ in obj);mj=min(y for _,y in obj)
-            ma=max(x for x,_ in obj);mb=max(y for _,y in obj)
-            w0=mb-mj+1
-            norm={(x-mi,y-mj)for x,y in obj}
-            if all((x,w0-1-y)in norm for x,y in norm):
-                return[ r[mj:mb+1] for r in g[mi:ma+1] ]
-    return g
+def p(o):
+    r=[]
+    for c in set(sum(o,[]))-{0}:
+        g=o
+        for i in range(80):
+            g=[*map(list,zip(*g[c not in g[0]:][::-1]))]
+        if all(r==r[::-1]for r in g):
+            return g
