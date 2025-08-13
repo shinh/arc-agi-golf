0340f34e5f6da5774c9bbccc899c84dc11bc58a8
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-13 10:39:58 +0900

    Golf 143 a bit more

--- logic/task143.py
+++ logic/task143.py
@@ -1,9 +1,10 @@
+# 213
 def p(g):
-    v=[[0]*(len(g)+1)]+[[0]+r for r in g]
+    v=[[0]+r for r in[[0]*99]+g]
     for c in range(10):
         for y in range(8):
             for x in range(8):
-                if(y or x)and all((v[i//4][i%4]>0)==(v[y+i//4][x+i%4]==c)for i in range(16))and c!=5:
+                if(y>4or x>4)and all((v[i//4][i%4]>0)==(v[y+i//4][x+i%4]==c)for i in range(16)):
                     for i in range(9):
                         if g[y+i//3][x+i%3]:
                             g[y+i//3][x+i%3]=5
