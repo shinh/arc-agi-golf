8e0498654de3078cbb81a65bab380d2c290b48c3
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-14 23:55:32 +0900

    Golf 31

--- logic/task031.py
+++ logic/task031.py
@@ -1,2 +1,6 @@
 # 69
-p=lambda g,n=79:-n*g or p([*map(list,zip(*g[any(g[-1])-2::-1]))],n-1)
+# p=lambda g,n=79:-n*g or p([*map(list,zip(*g[any(g[-1])-2::-1]))],n-1)
+# 68 - AI found this
+#p=lambda g,f=lambda g:zip(*filter(any,g)):[*map(list,f(f(f(f(g)))))]
+# 62 two iter is enough
+p=lambda g,f=lambda g:zip(*filter(any,g)):[*map(list,f(f(g)))]
