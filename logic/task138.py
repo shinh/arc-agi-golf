c966ce243f4a2928b0ef0fe93c92bd682483045f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-14 23:42:28 +0900

    Golf 138

--- logic/task138.py
+++ logic/task138.py
@@ -1,2 +1,2 @@
-# 120
-p=lambda g,t=80:t and p([[([*r[:r.index(r[-1])]]+[r[-1]]*99)[:len(r)],r][t>4]for r in zip(*g[0in g[0]:][::-1])],t-1)or g
+# 118
+p=lambda g,t=79:-t*g or p([[([*r[:r.index(r[-1])]]+[r[-1]]*99)[:len(r)],r][t>4]for r in zip(*g[0in g[0]:][::-1])],t-1)
