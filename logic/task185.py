4d495d520011b70368d142f87fb42b36388237c2
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-15 01:07:00 +0900

    Fix merge mistake

--- logic/task185.py
+++ logic/task185.py
@@ -1,5 +1,5 @@
-# 163
+# 158
 def p(g):
     c=max(g[0])
     for _ in 0,1:g=[r for r in zip(*g)if{*r}-{0,c}]
-    return[[[0,i][i==j==k==l!=c]for i,j,k,l in zip(r,r[1:],nr,nr[1:])]for r,nr in zip(g,g[1:])]
+    return[[i*(i==j==k==l!=c)for i,j,k,l in zip(r,r[1:],nr,nr[1:])]for r,nr in zip(g,g[1:])]
