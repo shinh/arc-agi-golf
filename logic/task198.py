04a54dc2034ba9590d3fc62ecab03518dac07bcf
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-14 22:29:56 +0900

    Golf 198 a bit

--- logic/task198.py
+++ logic/task198.py
@@ -1,5 +1,2 @@
-# 217
-def p(g):
-    for _ in range(80):
-        g=[[[a,3,4][(a in(0,3))*(1+(a==4 or b==4 or(b==c and b in[1,2,5,6,7,8,9]) or(b==d and b in[1,2,5,6,7,8,9])))]for a,b,c,d in zip(r,[*r[1:],3],[*r[2:],3,3],[3,*r])]for r in zip(*g[::-1])]
-    return g
+# 154
+p=lambda g,n=79:-n*g or p([[[a,3+(b==4 or(b>0)*(b!=3)*(b in c))][a in(0,3)]for a,b,*c in zip(r,[*r[1:],3],r[2:]+(3,3),[3,*r])]for r in zip(*g[::-1])],n-1)
