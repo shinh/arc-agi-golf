a7ec81b77a9c4757c191c66cd7c295cd07b54e0c
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-15 01:05:36 +0900

    golf task185

--- logic/task185.py
+++ logic/task185.py
@@ -1,5 +1,4 @@
 def p(g):
     c=max(g[0])
-    for i in range(2):
-        g=[[*r]for r in zip(*g)if{*r}-{0,c}]
-    return[[[0,i][i==j==k==l!=c]for i,j,k,l in zip(r,r[1:],nr,nr[1:])]for r,nr in zip(g,g[1:])]
+    for _ in 0,1:g=[r for r in zip(*g)if{*r}-{0,c}]
+    return[[i*(i==j==k==l!=c)for i,j,k,l in zip(r,r[1:],nr,nr[1:])]for r,nr in zip(g,g[1:])]
