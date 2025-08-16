18985caccc145cd2a2c3882500c399f9dc08c185
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 20:53:49 +0900

    Golf task345 solution

--- logic/task345.py
+++ logic/task345.py
@@ -1,10 +1,7 @@
 def p(g):
  # draw upward lines from bottom 2s, shifting right past 5s
  for j,x in enumerate(g[-1]):
-  if x>1:
-   y=0
-   for r in g[::-1]:
-    if r[j]>4:j+=1;g[-y][j]=2
-    r[j]=2;y+=1
+  if x:
+   for r,b in[*zip(g,g[1:])][::-1]:b[j:=j+(r[j]>4)]=2;r[j]=2
  return g
 
