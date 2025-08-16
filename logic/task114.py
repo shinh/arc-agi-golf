ff20aff6d71f67b95213376ce10b89138d9e14ea
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 13:59:59 +0900

    golf: task114

--- logic/task114.py
+++ logic/task114.py
@@ -1,7 +1,2 @@
-def p(g):
-    h=len(g);w=len(g[0])
-    o=[[0]*(w+2) for _ in range(h+2)]
-    o[0][1:-1]=g[0];o[-1][1:-1]=g[-1]
-    for i,r in enumerate(g):
-        o[i+1][1:-1]=r;o[i+1][0]=r[0];o[i+1][-1]=r[-1]
-    return o
+def p(g):#dup edge
+    return[[0]+g[0]+[0],*[v[:1]+v+v[-1:]for v in g],[0]+g[-1]+[0]]
