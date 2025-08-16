a4399d9d200f62d6c414af2359837701835cac1b
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 14:06:33 +0900

    golf task 034

--- logic/task034.py
+++ logic/task034.py
@@ -1,25 +1,24 @@
 def p(g):
-    v=set();o=[r for r in g]
+    # extend from 2s
+    o=[r[:]for r in g]
     for y in range(9):
         for x in range(9):
-            if g[y][x]==0 or (y,x) in v:continue
-            s=[(y,x)];obj=[]
+            if g[y][x]==0:continue
+            s=[(y,x)];obj=[];my=mx=9;c=0;twos=[]
             while s:
-                y1,x1=s.pop()
-                if (y1,x1) in v or g[y1][x1]==0:continue
-                v.add((y1,x1));obj.append((y1,x1))
+                y1,x1=s.pop();v=g[y1][x1]
+                if not v:continue
+                obj+=[(y1,x1)];g[y1][x1]=0
+                if v-2:c=v
+                twos+=((y1,x1),)*(v==2)
+                my=min(my,y1);mx=min(mx,x1)
                 for dy,dx in((1,0),(-1,0),(0,1),(0,-1)):
                     ny,nx=y1+dy,x1+dx
-                    if 0<=ny<9 and 0<=nx<9:s.append((ny,nx))
-            c=[g[a][b] for a,b in obj if g[a][b]!=2][0]
-            my=min(a for a,_ in obj);mx=min(b for _,b in obj)
-            d=[(-1 if a==my else 1,-1 if b==mx else 1) for a,b in obj if g[a][b]==2]
-            S=set()
-            for dy,dx in d:
+                    if 0<=ny<9>nx>=0:s+=((ny,nx),)
+            for dy,dx in((1-2*(a==my),1-2*(b==mx))for a,b in twos):
                 for a,b in obj:
                     y2,x2=a,b
-                    while 0<=y2<9 and 0<=x2<9:
-                        S.add((y2,x2));y2+=dy;x2+=dx
-            for a,b in S:o[a][b]=c
+                    while 0<=y2<9>x2>=0:
+                        o[y2][x2]=c;y2+=dy;x2+=dx
     return o
 
