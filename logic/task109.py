165f172621d39c5080e5180a05ea66d83b217ff0
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 15:46:23 +0900

    Golf task109 solution

--- logic/task109.py
+++ logic/task109.py
@@ -1,6 +1,4 @@
 def p(g):
-    n=len(g);c=g[n//2][0]
-    g=[r[:n//2]+r[n//2+1:]for i,r in enumerate(g)if i!=n//2]
-    h=len(g)//2;t=[[c*(v>0)for v in r[:h]]for r in g[:h]]
-    f=lambda a:[r[::-1]for r in a];b=t[::-1]
-    return[x+y for x,y in zip(t,f(t))]+[x+y for x,y in zip(b,f(b))]
+    # mirror TL mask
+    h=len(g)//2;g=[[v and g[h][0]for v in r[:h]]for r in g[:h]]
+    return[r+r[::-1]for r in g+g[::-1]]
