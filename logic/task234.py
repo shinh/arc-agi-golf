39532f4b610c25d81357fc49aa4d0cb95d435eae
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 17:30:30 +0900

    golf task234 solution

--- logic/task234.py
+++ logic/task234.py
@@ -1,10 +1,9 @@
 def p(g):
-    # We remove only a single pixel every 4 iterations.
-    for o in range(44):
-        w=len(g[0]);y=1
-        for y in range(len(g)):
-            for x in range(w-1):
-                if g[y][x-1]+g[y][x+1]==0and g[y-1][x]!=g[y][x]and g[y][x]*g[y-1][x]:
-                    g=g[:y]+g[y+1:]+[[0]*w]
-        g=[list(r)for r in zip(*g[::-1])]
-    return g
+ # We remove only a single pixel every 4 iterations.
+ for _ in[0]*44:
+  for y,r in enumerate(g):
+   w=len(r);R=g[y-1]
+   for x in range(w-1):
+    if r[x-1]+r[x+1]<1and(r[x]-R[x])*r[x]*R[x]:del g[y];g+=[[0]*w]
+  g=[*map(list,zip(*g[::-1]))]
+ return g
