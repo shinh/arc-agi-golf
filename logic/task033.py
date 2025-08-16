1cbffc8e1a7e5f84d023635752f3d28b9fcce38c
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 16:46:23 +0900

    golf task033

--- logic/task033.py
+++ logic/task033.py
@@ -1,8 +1,5 @@
 def p(g):
- c=g[5][0];a=[(y,x)for y in range(1,4)for x in range(1,4)if g[y][x]]
- for Y in range(3):
-  for X in range(3):
-   for y,x in a:
-    i=Y*6+y;j=X*6+x
-    if g[i][j]<1:g[i][j]=c
+ # copy pattern blocks
+ c=g[5][0];r=range(len(g))
+ [g[i].__setitem__(j,c)for i in r for j in r if g[i%6][j%6]and g[i][j]<1]
  return g
