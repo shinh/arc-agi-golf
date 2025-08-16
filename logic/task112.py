5f54b85713bdfba2d33c496df643b79b0ca19bba
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 21:59:14 +0900

    golf task112

--- logic/task112.py
+++ logic/task112.py
@@ -1,15 +1,11 @@
 def p(g):
     # mirror each color around the center of the only 2x2 block
-    h=len(g);w=len(g[0])
-    for y in range(h-1):
-        for x in range(w-1):
-            c=g[y][x]
-            if c==g[y][x+1]==g[y+1][x]==g[y+1][x+1]!=0 and sum(r.count(c)for r in g)==4:S=y*2+1;T=x*2+1
-    for y,r in enumerate(g):
-        for x,c in enumerate(r):
-            if c:
-                for Y in y,S-y:
-                    if 0<=Y<h:
-                        for X in x,T-x:
-                            if 0<=X<w:g[Y][X]=c
+    h=len(g);w=len(g[0]);f=sum(g,[])
+    for i,v in enumerate(f):
+        if v and f.count(v)==4:S=i//w*2+1;T=i%w*2+1;break
+    for i,c in enumerate(f):
+        if c:
+            for Y in i//w,S-i//w:
+                for X in i%w,T-i%w:
+                    if h>Y>-1 and w>X>-1:g[Y][X]=c
     return g
