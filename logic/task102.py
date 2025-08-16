890b8612ce3e43504d56e93fa6f7c3f134816818
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 22:34:29 +0900

    Golf task102 solution

--- logic/task102.py
+++ logic/task102.py
@@ -6,8 +6,8 @@ def p(g):
     q=[(y,x)];g[y][x]=1
     for y1,x1 in q:
      for y2,x2 in((y1-1,x1),(y1+1,x1),(y1,x1-1),(y1,x1+1)):
-      if 0<=y2<12 and 0<=x2<12 and g[y2][x2]<1:g[y2][x2]=1;q+=[(y2,x2)]
+      if-1<y2<12 and-1<x2<12 and g[y2][x2]<1:g[y2][x2]=1;q+=[(y2,x2)]
     ys,xs=zip(*q);a=min(ys);b=max(ys);c=min(xs);d=max(xs)
-    c=2 if b-a==d-c and len(q)==(b-a+1)**2 and all(g[a-1][i]==g[b+1][i]==5 for i in range(c,d+1)) and all(g[i][c-1]==g[i][d+1]==5 for i in range(a,b+1)) else 0
+    c=2 if b-a==d-c==len(q)**.5-1 and all(g[a-1][i]==g[b+1][i]==5 for i in range(c,d+1)) and all(g[i][c-1]==g[i][d+1]==5 for i in range(a,b+1)) else 0
     for y1,x1 in q:g[y1][x1]=c
  return g
