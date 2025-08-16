cd8e9125a1e1daf103785d379b1646df8521a6a0
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 23:30:45 +0900

    golf task014

--- logic/task014.py
+++ logic/task014.py
@@ -1,6 +1,2 @@
-def p(g):
- # crop rare color
- b=sum(g,[])
- c=min({*b}-{0},key=b.count)
- t=[i for r in g if c in r for i,v in enumerate(r) if v==c]
- return [r[min(t):max(t)+1]for r in g if c in r]
+def p(g):# crop rare color
+ b=sum(g,[]);c=min({*b}-{0},key=b.count);return[*map(list,zip(*[r for r in zip(*[r for r in g if c in r]) if c in r]))]
