f69762309471fc67a9ef260cd90e09fe90c9e33c
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 16:34:37 +0900

    Golf task344

--- logic/task344.py
+++ logic/task344.py
@@ -1,6 +1,7 @@
+# swap 2 next to 3 with 0 and 8
 def p(g,e=enumerate):
  for i,r in e(g):
   for j,v in e(r):
    for x,y in(i+1,j),(i-1,j),(i,j+1),(i,j-1):
-    if v==2and 0<=x<len(g)and 0<=y<len(r)and g[x][y]==3:g[i][j]=0;g[x][y]=8
+    if v==2and g[x:x+1]and g[x][y:y+1]==[3]:r[j]=0;g[x][y]=8
  return g
