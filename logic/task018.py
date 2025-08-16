07377c6694c5b674fc368a9308472a87955b163b
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 20:49:11 +0900

    golf task018

--- logic/task018.py
+++ logic/task018.py
@@ -1,27 +1,26 @@
 # flood fill 4-color shapes, paste their pattern elsewhere using majority color as wildcard
 def p(g):
     h=len(g);w=len(g[0])
-    R=[*map(list,g)];S=set();r=range
+    R=[*map(list,g)];r=range
     def F(i,j):
-        if not(0<=i<h and 0<=j<w)or g[i][j]<1 or(i,j)in S:return[]
-        S.add((i,j))
-        return[(i,j)]+F(i+1,j)+F(i-1,j)+F(i,j+1)+F(i,j-1)
+        if not(0<=i<h and 0<=j<w)or g[i][j]<1:return[]
+        u=g[i][j];g[i][j]=0
+        return[(i,j,u)]+F(i+1,j)+F(i-1,j)+F(i,j+1)+F(i,j-1)
     for i in r(h):
         for j in r(w):
-            if g[i][j]and(i,j)not in S:
-                c=F(i,j)
-                if len({g[x][y]for x,y in c})>3:
-                    xs,ys=zip(*c);mi,mx,mj,my=min(xs),max(xs),min(ys),max(ys);P=[[0]*(my-mj+1)for _ in r(mx-mi+1)];C=[]
-                    for x,y in c:R[x][y]=0;u=g[x][y];C+=u,;P[x-mi][y-mj]=u
-                    m=max(C,key=C.count)
-                    for t in P,P[::-1]:
+            if g[i][j]:
+                C=F(i,j);x,y,z=zip(*C)
+                if len({*z})>3:
+                    a,b,c,d=min(x),max(x),min(y),max(y);t=[[0]*(d-c+1)for _ in r(b-a+1)]
+                    for x,y,u in C:R[x][y]=0;t[x-a][y-c]=u
+                    for t in t,t[::-1]:
                         for _ in r(4):
-                            h2,w2=len(t),len(t[0])
-                            for i in r(h-h2+1):
-                                for j in r(w-w2+1):
-                                    if all((u:=t[a][b])in(0,m)or R[i+a][j+b]==u for a in r(h2)for b in r(w2)):
-                                        for a in r(h2):
-                                            for b in r(w2):
-                                                if(u:=t[a][b]):R[i+a][j+b]=u
+                            a,b=len(t),len(t[0])
+                            for I in r(h-a+1):
+                                for J in r(w-b+1):
+                                    if all((u:=t[x][y])in(0,max(z,key=z.count))or R[I+x][J+y]==u for x in r(a)for y in r(b)):
+                                        for x in r(a):
+                                            for y in r(b):
+                                                if(u:=t[x][y]):R[I+x][J+y]=u
                             t=[*zip(*t[::-1])]
     return R
