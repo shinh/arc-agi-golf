3f30d60776b0176a5310bd1412c381333e9da5bb
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 20:37:52 +0900

    golf: compress task176

--- logic/task176.py
+++ logic/task176.py
@@ -1,6 +1,1 @@
-def p(g):
- # set 4s
- for r,k in zip(g,(224,65,2051)):
-  for i in range(len(r)):
-   if k&1<<i%12:r[i]=4
- return g
+p=lambda g:[[k*4>>i%12&4 or v for i,v in enumerate(r)]for r,k in zip(g,(224,65,2051))]# set 4s
