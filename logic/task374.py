a0c3fde56dfbe2c0b35176f25b23289158b2448e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 15:26:08 +0900

    golf task374

--- logic/task374.py
+++ logic/task374.py
@@ -1,14 +1,11 @@
-def p(j):
- A=len(j);c=len(j[0]);E=[]
- for k in range(A):
-  for W in range(c):
-   if j[k][W]==5:
-    l=[(k,W)];j[k][W]=0;J=[]
-    while l:
-     a,C=l.pop();J+=[(a,C)]
-     for e,K in((a+1,C),(a-1,C),(a,C+1),(a,C-1)):
-      if 0<=e<A and 0<=K<c and j[e][K]==5:j[e][K]=0;l+=[(e,K)]
-    E+=J,
+def p(j):# recursive flood fill
+ A=len(j);c=len(j[0])
+ def f(a,b):
+  if 0<=a<A and 0<=b<c and j[a][b]==5:
+   j[a][b]=0
+   return[(a,b)]+f(a+1,b)+f(a-1,b)+f(a,b+1)+f(a,b-1)
+  return[]
+ E=[f(k,W)for k in range(A)for W in range(c)if j[k][W]==5]
  for J,w in zip(sorted(E,key=len),(2,4,1)):
   for a,C in J:j[a][C]=w
  return j
