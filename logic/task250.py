6fffbdcd51cfeb70bfd78a88c7e7660fd5b37ea9
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 23:36:04 +0900

    golf task250

--- logic/task250.py
+++ logic/task250.py
@@ -1,7 +1,5 @@
 def p(g):#move 5s toward 2 cluster
-    Y,X=zip(*[(y,x)for y,r in enumerate(g)for x,v in enumerate(r)if v==2])
-    y0=min(Y)-1;y1=max(Y)+1;x0=min(X)-1;x1=max(X)+1
-    o=[[v==2 and 2 for v in r]for r in g]
-    for y,x in[(y,x)for y,r in enumerate(g)for x,v in enumerate(r)if v==5]:
-        o[min(y1,max(y0,y))][min(x1,max(x0,x))]=5
-    return o
+ t=sum(g,[]);w=len(g[0]);y,x=divmod(t.index(2),w);y0,y1,x0,x1=y-1,y+2,x-1,x+2;o=[[2*(v==2)for v in r]for r in g]
+ for i,v in enumerate(t):
+  if v>4:Y,X=divmod(i,w);o[min(y1,max(y0,Y))][min(x1,max(x0,X))]=5
+ return o
