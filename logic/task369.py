57f61d86193133605ce7b267db4f4604e56e117e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 15:09:10 +0900

    Golf task369

--- logic/task369.py
+++ logic/task369.py
@@ -1,14 +1,10 @@
 def p(g):
- R=range
- v=set()
- r=[x[:]for x in g]
  def d(i,j):
-  if(i,j)in v or not(0<=i<10 and 0<=j<10)or g[i][j]:return[]
-  v.add((i,j))
-  return[(i,j)]+sum([d(i+a,j+b)for a,b in[(-1,0),(1,0),(0,-1),(0,1)]],[])
- for i in R(10):
-  for j in R(10):
-   if g[i][j]==0 and(i,j)not in v:
+  if not(-1<i<10>j>-1)or g[i][j]:return[]
+  g[i][j]=1;return[(i,j)]+[e for a,b in((1,0),(-1,0),(0,1),(0,-1))for e in d(i+a,j+b)]
+ for i in range(10):
+  for j in range(10):
+   if g[i][j]==0:
     s=d(i,j)
-    for a,b in s:r[a][b]=abs(len(s)-4)
- return r
+    for a,b in s:g[a][b]=abs(len(s)-4)
+ return g
