60d1fa4a011a4aaf2eb996d03f6c131955e13cab
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 22:07:09 +0900

    golf task196

--- logic/task196.py
+++ logic/task196.py
@@ -1,17 +1,13 @@
-def p(g):
- R=range
- h,w=len(g),len(g[0])
- v=set()
- # loops
- def d(i,j):
-  if(i,j)in v or not(-1<i<h and -1<j<w)or g[i][j]-1:return set()
-  v.add((i,j))
-  return {(i,j)}|d(i+1,j)|d(i-1,j)|d(i,j+1)|d(i,j-1)
- for i in R(h):
-  for j in R(w):
-   if g[i][j]==1 and(c:=d(i,j)):
-    a,b=zip(*c)
-    t,m,l,n=min(a),max(a),min(b),max(b)
-    if len(c)==2*(m-t+n-l)and(m-t)*(n-l)and any(g[x][y]<1for x in R(t+1,m)for y in R(l+1,n)):
-     for x,y in c:g[x][y]=3
+def p(g):# recolor rectangular loops
+ R=range;h=len(g);w=len(g[0])
+ for y in R(h):
+  for x in R(w):
+   if g[y][x]==1:
+    X=x
+    while X<w and g[y][X]==1:X+=1
+    Y=y
+    while Y<h and g[Y][x]==1:Y+=1
+    if X-x>1<Y-y and all(g[y][i]==g[Y-1][i]==1 for i in R(x,X))and all(g[i][x]==g[i][X-1]==1 for i in R(y,Y))and any(g[i][j]<1 for i in R(y+1,Y-1)for j in R(x+1,X-1)):
+     for i in R(x,X):g[y][i]=g[Y-1][i]=3
+     for i in R(y,Y):g[i][x]=g[i][X-1]=3
  return g
