af83a62d9a1dbd3f6ea6b528433550ab7f8ecd9d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 22:06:44 +0900

    golf task093

--- logic/task093.py
+++ logic/task093.py
@@ -1,15 +1,11 @@
 # expand 5 block based on surrounding colors
 def p(g):
-    t=l=14;b=r=0
-    for i in range(14):
-        for j in range(14):
-            if g[i][j]==5:t=min(t,i);b=max(b,i);l=min(l,j);r=max(r,j)
-    o=create(14,14)
-    for i in range(t,b+1):
-        left=sum(v%5>0 for v in g[i][:l]);right=sum(v%5>0 for v in g[i][r+1:])
-        o[i][l-left:r+1+right]=[5]*(r-l+1+left+right)
-    for j in range(l,r+1):
-        above=sum(r[j]%5>0 for r in g[:t]);below=sum(r[j]%5>0 for r in g[b+1:])
-        for k in range(above):o[t-1-k][j]=5
-        for k in range(below):o[b+1+k][j]=5
+    # find bounding box and extend edges by neighbor counts
+    f=sum(g,[]);a=f.index(5);d=195-f[::-1].index(5);t,l=divmod(a,14);b,r=divmod(d,14);o=create(14,14)
+    for y in range(t,b+1):
+        a=sum(v%5>0 for v in g[y][:l]);d=sum(v%5>0 for v in g[y][r+1:])
+        o[y][l-a:r+d+1]=[5]*(r-l+1+a+d)
+    for x in range(l,r+1):
+        for i in range(sum(r[x]%5>0 for r in g[:t])):o[t-1-i][x]=5
+        for i in range(sum(r[x]%5>0 for r in g[b+1:])):o[b+1+i][x]=5
     return o
