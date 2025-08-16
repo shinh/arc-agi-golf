ba068e8b05da96e3221108c63e8f2c787907aba3
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 20:00:53 +0900

    golf task226

--- logic/task226.py
+++ logic/task226.py
@@ -1,10 +1,10 @@
 # rotate the grid so flood fill can flow from one side only
 def p(g):
- m=len(g)//2
+ m=len(g)//2-1
  g[0][0]=1;g[-1][-1]=3
- for i in range(4):
-  y=m-1+i//2;x=m-1+i%2;g[y][x]=g[y][x] or 2
- for _ in range(80):
-  g=[[a or(0<b<4 and b)for a,b in zip(r,r[1:]+(5,))]for r in zip(*g[::-1])]
+ for i in m,m+1:
+  for r in g[m:m+2]:r[i]=r[i] or 2
+ for _ in[0]*12:
+  g=[[a or b*(b<4)for a,b in zip(r,(5,)+r)]for r in zip(*g[::-1])]
  return g
 
