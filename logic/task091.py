551eb3761eefdba4765e9e80b91988a38368be92
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 17:27:20 +0900

    golf task091

--- logic/task091.py
+++ logic/task091.py
@@ -1,7 +1,3 @@
 def p(g):
-    y=[];x=[]
-    for j,r in enumerate(g):
-        for i,v in enumerate(r):
-            if v==5:y+=j,;x+=i,
-    a=max(0,min(y)-1);b=min(len(g),max(y)+2);c=min(x);d=max(x)+1
-    return [r[c:d] for r in g[a:b]]
+    y,x=zip(*((j,i)for j,r in enumerate(g)for i,v in enumerate(r)if v==5))
+    return[r[min(x):max(x)+1]for r in g[max(0,min(y)-1):max(y)+2]]
