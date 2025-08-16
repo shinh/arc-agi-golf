a97864be8b5cfc05d5a53c96ffc53d7ac93c863c
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 20:56:39 +0900

    golf task186

--- logic/task186.py
+++ logic/task186.py
@@ -1,2 +1,2 @@
 # draw bar in first row and center if count is four
-p=lambda g,b=[0]*3:(n:=sum(sum(g,[])))and[([2]*n+b)[:3],(n>3)*[0,2,0]or b,b]
+p=lambda g,b=[0]*3:(n:=sum(sum(g,[])))and[([2]*n+b)[:3],[0,(n>3)*2,0],b]
