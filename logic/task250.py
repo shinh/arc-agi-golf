fadd8e631f3e68b18bc4d95f7fbe6a57e1ac78db
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 17:18:18 +0900

    add task250 report

--- logic/task250.py
+++ logic/task250.py
@@ -1,10 +1,7 @@
-def p(g):
-    a=[(y,x) for y,r in enumerate(g) for x,v in enumerate(r) if v==2]
-    y0=min(y for y,_ in a);y1=max(y for y,_ in a)
-    x0=min(x for _,x in a);x1=max(x for _,x in a)
-    o=[[0]*10 for _ in g]
-    for y,r in enumerate(g):
-        for x,v in enumerate(r):
-            if v==2:o[y][x]=2
-            if v==5:o[max(y0-1,min(y1+1,y))][max(x0-1,min(x1+1,x))]=5
+def p(g):#move 5s toward 2 cluster
+    Y,X=zip(*[(y,x)for y,r in enumerate(g)for x,v in enumerate(r)if v==2])
+    y0=min(Y)-1;y1=max(Y)+1;x0=min(X)-1;x1=max(X)+1
+    o=[[v==2 and 2 for v in r]for r in g]
+    for y,x in[(y,x)for y,r in enumerate(g)for x,v in enumerate(r)if v==5]:
+        o[min(y1,max(y0,y))][min(x1,max(x0,x))]=5
     return o
