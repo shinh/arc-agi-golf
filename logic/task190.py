17da3605d2908acc258d855818fee1aeda197f1f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 15:33:38 +0900

    golf task190

--- logic/task190.py
+++ logic/task190.py
@@ -1,16 +1,8 @@
-def p(g):
- n=10
- o=[r for r in g]
- c=next(v for r in g for v in r if v)
- for y in range(9):
-  for x in range(9):
-   if g[y][x]==c==g[y+1][x]==g[y][x+1]==g[y+1][x+1]:a,b=y,x
+def p(g):#extend rays
+ any((a:=y,b:=x,c)for y in range(9)for x in range(9)if(c:=g[y][x])==g[y+1][x]==g[y][x+1]==g[y+1][x+1]>0)
  for y in range(10):
   for x in range(10):
-   if g[y][x]==c and not(a<=y<a+2 and b<=x<b+2):
-    dy=(y>a+1)-(y<a)
-    dx=(x>b+1)-(x<b)
-    i=y+dy;j=x+dx
-    while 0<=i<10 and 0<=j<10 and o[i][j]==0:
-     o[i][j]=c;i+=dy;j+=dx
- return o
+   if g[y][x]==c and(y-a>>1 or x-b>>1):
+    d=(y>a+1)-(y<a);e=(x>b+1)-(x<b);i=y;j=x
+    while-1<(i:=i+d)<10>(j:=j+e)>-1 and g[i][j]<1:g[i][j]=c
+ return g
