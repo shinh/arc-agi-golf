e9b3c52864efd4e149410fec46a271e3ff753338
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 16:51:57 +0900

    golf task265

--- logic/task265.py
+++ logic/task265.py
@@ -1,12 +1,12 @@
 def p(g):
-    w=h=18;o=[r[:]for r in g]
-    for y in range(h-1):
-        for x in range(w-1):
-            if g[y][x]==g[y][x+1]==g[y+1][x]==g[y+1][x+1]==0 and (y<1 or g[y-1][x] or g[y-1][x+1]) and (x<1 or g[y][x-1] or g[y+1][x-1]):
-                X=x+2
-                while X<w and g[y][X]==g[y+1][X]==0:X+=1
-                Y=y+2
-                while Y<h and all(g[Y][i]==0 for i in range(x,X)):Y+=1
-                for yy in range(y,Y):o[yy][x:X]=[2]*(X-x)
+    # fill rectangles of zeros with 2
+    o=[r[:]for r in g]
+    for y in range(17):
+        for x in range(17):
+            if sum(g[y][x:x+2]+g[y+1][x:x+2])<1 and (not y or sum(g[y-1][x:x+2])) and (not x or g[y][x-1]+g[y+1][x-1]):
+                X=x+2;Y=y+2
+                while X<18 and g[y][X]+g[y+1][X]<1:X+=1
+                while Y<18 and sum(g[Y][x:X])<1:Y+=1
+                for r in o[y:Y]:r[x:X]=[2]*(X-x)
     return o
 
