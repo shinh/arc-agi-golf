a4de93a2aa5f97ed22c9a8f64e4d14ee9dfdc284
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 17:26:35 +0900

    golf task107

--- logic/task107.py
+++ logic/task107.py
@@ -1,9 +1,8 @@
-def p(g):
- sx=g[1][0]<1
- sy=g[0][1]<1
+def p(g):#scale and draw diagonals
  n=len({*sum(g,[])})-1
+ sx,sy=(g[1][0]<1)*n,(g[0][1]<1)*n
  g=[[c for c in r for _ in[0]*n]for r in g for _ in[0]*n]
- sx*=n;sy*=n;t=2*n
+ t=2*n
  for i in range(n):
   for y in sy-i-1,sy+t+i:
    for x in sx-i-1,sx+t+i:
