486cef402a6d51e5257385d956fd02c8ccfe9b32
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 13:00:39 +0900

    golf task218

--- logic/task218.py
+++ logic/task218.py
@@ -1,12 +1,4 @@
 def p(g):
-    for _ in "0000":
-        g=[r for r in g if any(r)]
-        g=[list(r)for r in zip(*g)]
-    u=[]
-    for r in g:
-        if r not in u:u+=r,
-    g=[list(r)for r in zip(*u)]
-    u=[]
-    for r in g:
-        if r not in u:u+=r,
-    return [list(r)for r in zip(*u)]
+    # drop empty & duplicate rows/cols
+    for _ in"00":g=zip(*dict.fromkeys(map(tuple,filter(any,g))))
+    return [list(r)for r in g]
