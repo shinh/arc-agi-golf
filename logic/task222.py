15f04fbb23e5dfcae042e0e9c389a4c2d847e2bd
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 23:32:40 +0900

    golf task222

--- logic/task222.py
+++ logic/task222.py
@@ -1,16 +1,12 @@
 def p(g):
     # largest uniform rect
-    B=0
+    o=create(16,16);B=0
     for y0 in range(16):
         for x0 in range(16):
-            c=g[y0][x0]
-            if c:
+            if c:=g[y0][x0]:
                 for y1 in range(y0+1,17):
                     for x1 in range(x0+1,17):
-                        if all(g[y][x]==c for y in range(y0,y1) for x in range(x0,x1)) and (a:=(y1-y0)*(x1-x0))>B:
-                            B,R=a,(y0,y1,x0,x1,c)
-    o=create(16,16)
-    if B:
-        y0,y1,x0,x1,c=R
-        for y in range(y0,y1):o[y][x0:x1]=[c]*(x1-x0)
+                        if all(r[x0:x1]==[c]*(w:=x1-x0)for r in g[y0:y1]) and (a:=(y1-y0)*w)>B:
+                            B=a;o=create(16,16)
+                            for y in range(y0,y1):o[y][x0:x1]=[c]*w
     return o
