a3edef40108ec94e5733c4afd274faa7151b0a08
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 16:18:06 +0900

    golf task321

--- logic/task321.py
+++ logic/task321.py
@@ -1,6 +1,4 @@
 def p(g):
- for r in range(4):
-  for c in range(4):
-   if g[r][c+5]>0:g[r][c+10]=g[r][c+5]
-   if g[r][c]>0:g[r][c+10]=g[r][c]
- return[R[10:]for R in g]
+ # prefer left then mid else keep
+ return[[r[c]or r[c+5]or r[c-4]for c in range(4)]for r in g]
+
