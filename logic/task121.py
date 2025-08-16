63fec10f93236a2e01b669d741aea8e7e9f8e47e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 20:05:06 +0900

    golf task121

--- logic/task121.py
+++ logic/task121.py
@@ -1,6 +1,2 @@
 def p(g):
-    for y,r in enumerate(g):
-        if 8 in r:
-            x=r.index(8);o=[g[y-1][x-1:x+2],r[x-1:x+2],g[y+1][x-1:x+2]]
-            o[1][1]=max(o[0]+o[1][:1]+o[1][2:]+o[2])
-            return o
+    y,x=divmod(sum(g,[]).index(8),len(g[0]));g=[t[x-1:x+2]for t in g[y-1:y+2]];g[1][1]=sorted(sum(g,[]))[-2];return g
