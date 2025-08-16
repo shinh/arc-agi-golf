aa0d32bd33da3526d87989594bbf4b4f26b732bd
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 17:18:20 +0900

    golf task192

--- logic/task192.py
+++ logic/task192.py
@@ -1,14 +1,8 @@
 def p(g):
-    h=len(g);w=len(g[0]);c=[0]*10
-    for r in g:
-        for v in r:c[v]+=1
-    b=max(range(1,10),key=c.__getitem__)
-    for y in range(h):
-        for x in range(w):
-            v=g[y][x]
-            if v and v!=b:
-                U=y and g[y-1][x]==b;D=y<h-1 and g[y+1][x]==b
-                L=x and g[y][x-1]==b;R=x<w-1 and g[y][x+1]==b
-                n=U+D+L+R
-                g[y][x]=b if n>2 or n==2 and not((U and D) or (L and R)) else 0
+    # keep cells touching b both axes
+    f=sum(g,[]);b=max(range(1,10),key=f.count)
+    for y,r in enumerate(g):
+        for x,v in enumerate(r):
+            if v and v-b:
+                r[x]=b*((y and g[y-1][x]==b or y<len(g)-1 and g[y+1][x]==b)*(x and r[x-1]==b or x<len(r)-1 and r[x+1]==b))
     return g
