55f4b9464fa9e89a4c906d365768414e9ac6b118
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 15:11:09 +0900

    Golf task013

--- logic/task013.py
+++ logic/task013.py
@@ -1,10 +1,10 @@
 def p(g):
- h=len(g);w=len(g[0]);a=[(y,x,g[y][x])for y in range(h)for x in range(w)if g[y][x]];y1,x1,c1=a[0];y2,x2,c2=a[1]
- dx=abs(x1-x2);dy=abs(y1-y2)
- if x1==0 or x2==0 or x1==x2 or y1 and y2 and dx<=dy:
+ # stripes
+ h=len(g);w=len(g[0]);y1,x1,c1,y2,x2,c2=sum([(y,x,g[y][x])for y in range(h)for x in range(w)if g[y][x]],())
+ if x1*x2<1 or x1==x2 or y1*y2 and abs(x1-x2)<=abs(y1-y2):
   if y1>y2:y1,x1,c1,y2,x2,c2=y2,x2,c2,y1,x1,c1
   d=y2-y1;s=[c1]+[0]*~-d+[c2]+[0]*~-d;t=(s*(h//len(s)+1))[:h-y1]
-  for i,v in enumerate(t):g[y1+i]=[v]*w
+  g[y1:]=[[v]*w for v in t]
  else:
   if x1>x2:y1,x1,c1,y2,x2,c2=y2,x2,c2,y1,x1,c1
   d=x2-x1;s=[c1]+[0]*~-d+[c2]+[0]*~-d;t=(s*(w//len(s)+1))[:w-x1]
