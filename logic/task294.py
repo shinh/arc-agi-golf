a6ea42ba42fc1c3cbb7bda3166ca4989294c4396
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 17:17:21 +0900

    golf task294

--- logic/task294.py
+++ logic/task294.py
@@ -1,2 +1,2 @@
 # rotate flood-fill to erode 5-pixels
-p=lambda g:(f:=lambda m,n=4:n and f([[a*b//5 for a,b in zip(r,r[1:]+(0,))]for r in zip(*m[::-1])],n-1)or m, [[c-3*(d>0) for c,d in zip(r,t)]for r,t in zip(g,f(g))])[1]
+p=lambda g:(m:=g,[m:=[[a&b for a,b in zip(r,r[1:]+(0,))]for r in zip(*m[::-1])]for _ in' '*4])and[[c-d*3//5 for c,d in zip(r,m)]for r,m in zip(g,m)]
