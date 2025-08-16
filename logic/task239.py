2b85e28682a7eec083e7a96fdd38fd0b476a555a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 20:00:27 +0900

    golf task239

--- logic/task239.py
+++ logic/task239.py
@@ -1,8 +1,5 @@
 def p(g):
-    d={}
-    for r in g:
-        for c in r:
-            if c:d[c]=d.get(c,0)+1
-    s=sorted(d,key=lambda k:(-d[k],k))
-    m=d[s[0]]
-    return [[(d[c]>i)*c for c in s]for i in range(m)]
+    # histogram
+    t=sum(g,[])
+    s=sorted({*t}-{0},key=lambda c:(-t.count(c),c))
+    return [[(t.count(c)>i)*c for c in s]for i in range(t.count(s[0]))]
