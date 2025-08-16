53c42dfef7de492c309ea2dc764d659c95419ec4
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 15:02:01 +0900

    golf task300

--- logic/task300.py
+++ logic/task300.py
@@ -1,7 +1,10 @@
 def p(g):
- c=[0]*10;a=[99]*10;b=[0]*10;d=[99]*10;e=[0]*10
+ # find most common color, crop its bbox
+ c=[0]*10
+ for v in sum(g,[]):c[v]+=v>0
+ v=c.index(max(c))
+ a=d=99;b=e=0
  for y,r in enumerate(g):
-  for x,v in enumerate(r):
-   if v:c[v]+=1;a[v]=min(a[v],y);b[v]=max(b[v],y);d[v]=min(d[v],x);e[v]=max(e[v],x)
- v=max(range(1,10),key=c.__getitem__)
- return[r[d[v]:e[v]+1]for r in g[a[v]:b[v]+1]]
+  for x,u in enumerate(r):
+   if u==v:a=min(a,y);b=max(b,y);d=min(d,x);e=max(e,x)
+ return[r[d:e+1]for r in g[a:b+1]]
