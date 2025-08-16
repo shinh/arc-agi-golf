dd3cdcb310d0f7352020a394ee24c84f1eeb7d61
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 22:19:36 +0900

    golf task048

--- logic/task048.py
+++ logic/task048.py
@@ -2,15 +2,14 @@
 # detect connection to another 2 block
 
 def p(g):
- w=len(g[0])+2
- s=sum(([0]+r+[0]for r in g),[0]*w)+[0]*w
- i=s.index(2);q=[]
- for d in 0,1,w,w+1:s[i+d]=1;q+=i+d,
- while q:
-  i=q.pop()
+ w=len(g[0])+2;s=[0]*w
+ for r in g:s+=0,*r,0
+ s+=[0]*w
+ q=[i:=s.index(2)];q+=i+1,i+w,i+w+1
+ for d in q:s[d]=1
+ for i in q:
   for d in-1,1,-w,w:
-   v=s[i+d]
-   if v==2:return[[8]]
-   if v==8:s[i+d]=1;q+=i+d,
+   if s[i+d]&2:return[[8]]
+   if s[i+d]&8:s[i+d]=1;q+=i+d,
  return[[0]]
 
