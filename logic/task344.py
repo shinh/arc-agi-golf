b9db2d0852ac41477fdc90aceed588fb36c29840
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 23:34:31 +0900

    Refactor task344 solution

--- logic/task344.py
+++ logic/task344.py
@@ -1,7 +1,7 @@
 # swap 2 next to 3 with 0 and 8
-def p(g,e=enumerate):
- for i,r in e(g):
-  for j,v in e(r):
-   for x,y in(i+1,j),(i-1,j),(i,j+1),(i,j-1):
-    if v==2and g[x:x+1]and g[x][y:y+1]==[3]:r[j]=0;g[x][y]=8
+def p(g):
+ for _ in'00':
+  for r in g:
+   r[:]=map(int,''.join(map(str,r)).replace('23','08').replace('32','80'))
+  g=[*map(list,zip(*g))]
  return g
