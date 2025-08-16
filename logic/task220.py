db5955107f10fcce181a4d14c0715eee08b5d3aa
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 23:20:36 +0900

    golf task220 by modifying grid in place

--- logic/task220.py
+++ logic/task220.py
@@ -1,11 +1,11 @@
 def p(g,E=enumerate):
 # color neighbors
- d={8:4,2:1,3:6};N=-1,0,1;X=[*map(list,g)]
- for r,R in E(X):
+ d={8:4,2:1,3:6}
+ for r,R in E(g):
   for c,C in E(R):
-   for i in N:
-    for j in N:
+   for i in-1,0,1:
+    for j in-1,0,1:
      if C and(i|j):
-      try:X[r+i][c+j]=d[C]
+      try:g[r+i][c+j]=d[C]
       except:0
- return X
+ return g
