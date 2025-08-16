3202dfddd386a1dff4466c7cb9d967d8911c2f47
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 17:14:09 +0900

    Golf task154

--- logic/task154.py
+++ logic/task154.py
@@ -1,12 +1,9 @@
 # Not golfed yet.
 # Note this is almost identical to task390.
+# copy left chunk to right then wipe it, rotating to cover all dirs
 def p(g):
-    for o in range(4):
-        for r in g:
-            if 2in r and 5in r:
-                s=r.index(2)
-                l=(len(r)-r[::-1].index(2)-s)//2
-                r[s+2:s+l]=r[s-l+1:s-1][::-1]
-                r[s-l+1:s-1]=[0]*(l-2)
-        g=[*map(list,zip(*g[::-1]))]
-    return g
+ for _ in[0]*4:
+  for r in g:
+   if{2,5}<=set(r):s=r.index(2);r[s+2:s+4]=r[s-3:s-1][::-1];r[s-3:s-1]=0,0
+  g=[*map(list,zip(*g[::-1]))]
+ return g
