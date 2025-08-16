3dd7ce76e67160c49a40fb5bbf6fcd4c16152e6f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 20:01:26 +0900

    golf task018 solution

--- logic/task018.py
+++ logic/task018.py
@@ -1,19 +1,19 @@
+# flood fill 4-color shapes, paste their pattern elsewhere using majority color as wildcard
 def p(g):
     h=len(g);w=len(g[0])
     R=[*map(list,g)];S=set();r=range
     def F(i,j):
         if not(0<=i<h and 0<=j<w)or g[i][j]<1 or(i,j)in S:return[]
-        S.add((i,j));s.add(g[i][j])
+        S.add((i,j))
         return[(i,j)]+F(i+1,j)+F(i-1,j)+F(i,j+1)+F(i,j-1)
     for i in r(h):
         for j in r(w):
             if g[i][j]and(i,j)not in S:
-                s=set();c=F(i,j)
-                if len(s)==4:
-                    for x,y in c:R[x][y]=0
-                    xs,ys=zip(*c);mi,mx=min(xs),max(xs);mj,my=min(ys),max(ys);P=[[0]*(my-mj+1)for _ in r(mx-mi+1)];C={}
-                    for x,y in c:u=g[x][y];C[u]=C.get(u,0)+1;P[x-mi][y-mj]=u
-                    m=max(C,key=C.get)
+                c=F(i,j)
+                if len({g[x][y]for x,y in c})>3:
+                    xs,ys=zip(*c);mi,mx,mj,my=min(xs),max(xs),min(ys),max(ys);P=[[0]*(my-mj+1)for _ in r(mx-mi+1)];C=[]
+                    for x,y in c:R[x][y]=0;u=g[x][y];C+=u,;P[x-mi][y-mj]=u
+                    m=max(C,key=C.count)
                     for t in P,P[::-1]:
                         for _ in r(4):
                             h2,w2=len(t),len(t[0])
