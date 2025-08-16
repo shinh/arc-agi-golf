f79a2089aec8ad1fc9511c41385ad2f30d5e33e8
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 16:12:26 +0900

    Golf task094 solution

--- logic/task094.py
+++ logic/task094.py
@@ -1,16 +1,10 @@
-L=len
-R=range
+L=len;R=range
+# cross 16 ones with 6
+
 def p(g):
- pr,pc=[],[]
- h,w=L(g),L(g[0])
- for r in R(h-4):
-  for c in R(w-4):
-   X=[[g[i+r][j+c]for i in R(5)]for j in R(5)]
-   X=[x for R in X for x in R]
-   X=sum([x for x in X if x==1])
-   if X==16:pr.append(r+2);pc.append(c+2)
- for r in R(h):
-  for c in R(w):
-   if r in pr or c in pc:
-    if g[r][c]!=1:g[r][c]=6
+ for r in R(L(g)-4):
+  for c in R(L(g[0])-4):
+   if sum(g[r+i][c+j]==1for i in R(5)for j in R(5))==16:
+    for q in g:q[c+2]=q[c+2]==1 or 6
+    g[r+2]=[q==1 or 6 for q in g[r+2]]
  return g
