0408aa1385d5df16aca3f3b09b31a89c7931b5ab
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 21:58:55 +0900

    golf: simplify task124

--- logic/task124.py
+++ logic/task124.py
@@ -1,10 +1,10 @@
 def p(g):
     # find shift and repeat
-    h,w=len(g),len(g[0])
-    fg=[(i,j,g[i][j])for i in range(h)for j in range(w)if g[i][j]]
-    dy,dx=max(((m,dy*dx,dy,dx)for dy in range(1,6)for dx in range(-w,w)if(m:=sum(0<=i+dy<h and 0<=j+dx<w and g[i+dy][j+dx]==v for i,j,v in fg))==(sum(0<=i+dy<h and 0<=j+dx<w for i,j,v in fg))>0),default=(0,0,1,0))[2:]
+    w=len(g[0])
+    fg=[(i,j,v)for i,r in enumerate(g)for j,v in enumerate(r)if v]
+    dy,dx=max((m,dy,dx)for dy in range(1,6)for dx in range(-w,w)if(m:=sum((i+dy,j+dx,v)in fg for i,j,v in fg)))[1:]
     out=[[0]*w for _ in range(10)]
     for i,j,v in fg:
-        for n in range(10):
-            if 9>=i+dy*n>=0<=j+dx*n<w:out[i+dy*n][j+dx*n]=v
+        y=i;x=j
+        while 0<=y<10 and 0<=x<w:out[y][x]=v;y+=dy;x+=dx
     return out
