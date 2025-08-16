0355dc6ecd9182ba37c24f184fdeceb99e46fd01
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 19:59:23 +0900

    golf task282

--- logic/task282.py
+++ logic/task282.py
@@ -1,1 +1,3 @@
-p=lambda g,R=range(1,8):(G:=[[0]*9for _ in g],[G[i+a].__setitem__(j+b,(1,5)[a*b])for i in R for j in R if g[i][j]for a in(-1,0,1)for b in(-1,0,1)if a|b])[0]
+# expand each colored cell to a star pattern
+p=lambda g,R=range(1,8):(G:=[[0]*9for _ in g],[G[i+a].__setitem__(j+b,(1,5)[a*b])for i in R for j in R for a in(-1,0,1)for b in(-1,0,1)if g[i][j]*(a|b)])[0]
+
