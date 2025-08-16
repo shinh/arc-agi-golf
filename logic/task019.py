3050e1e2764664fcf30091725268e5b8bed9ace7
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 23:35:13 +0900

    golf: optimize task019 solution

--- logic/task019.py
+++ logic/task019.py
@@ -1,11 +1,11 @@
 def p(g):
  # tile grid and add 8s diagonally
- h=len(g);w=len(g[0]);H=h*2;W=w*2;r=range;o=[g[y%h]*2 for y in r(H)]
- for y in r(H):
-  for x in r(W):
+ h=len(g);w=len(g[0]);o=[g[y%h]*2 for y in range(h*2)]
+ for y in range(h*2):
+  for x in range(w*2):
    if g[y%h][x%w]:
     for Y in y-1,y+1:
      for X in x-1,x+1:
-      if H>Y>=0<=X<W and o[Y][X]<1:o[Y][X]=8
+      if h*2>Y>-1<X<w*2>o[Y][X]<1:o[Y][X]=8
  return o
 
