e6581793bddc1b1f6d8e27ff6d4b300dffa14674
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 23:33:31 +0900

    golf task004

--- logic/task004.py
+++ logic/task004.py
@@ -1,8 +1,10 @@
 # Shift each color right except bottom and right edges.
 
-def p(g,E=enumerate):
- o=[[0]*len(r)for r in g]
- for v in{*sum(g,[])}-{0}:
-  P=[(i,j)for i,r in E(g)for j,x in E(r)if x==v];b,m=map(max,zip(*P))
-  for i,j in P:o[i][j+(i<b)*(j<m)]=v
+def p(g):
+ o=[[0]*len(r)for r in g];S={};i=len(g)
+ while i:
+  i-=1;j=len(g[i])
+  while j:
+   j-=1
+   if(v:=g[i][j]):b,m=S.setdefault(v,(i,j));o[i][j+(i<b)*(j<m)]=v
  return o
