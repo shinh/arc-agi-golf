d6ce19f67197ffca4e0d55fbfcce5a9304ef1492
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 16:31:24 +0900

    golf task065

--- logic/task065.py
+++ logic/task065.py
@@ -1,8 +1,3 @@
-def p(g):
-    s=sum(g,[]);m=len(g);n=m//2
-    for y,r in enumerate(g):
-        for x,c in enumerate(r):
-            if s.count(c)<2:
-                a=range(n)if y<n else range(n+1,m)
-                b=range(n)if x<n else range(n+1,m)
-                return[[g[i][j]for j in b]for i in a]
+def p(g):# uniq q
+ s=sum(g,[]);m=len(g);n=m>>1;d=s.index(min(s,key=s.count))
+ return[[r[n+1:],r[:n]][d%m<n]for r in[g[n+1:],g[:n]][d//m<n]]
