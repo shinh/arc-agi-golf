719d67626dab9bb74e8d03c04c91bc3fff332e7e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 15:02:15 +0900

    golf task061

--- logic/task061.py
+++ logic/task061.py
@@ -1,13 +1,14 @@
 def p(g):
+    # find tile period
     for k in range(1,19):
-        t=[[-1]*k for _ in range(k)]
+        t=[[0]*k for _ in [0]*k]
         for y in range(18):
             for x in range(18):
                 v=g[y][x]
                 if v:
                     a=t[y%k][x%k]
-                    if a<0:t[y%k][x%k]=v
-                    elif a!=v:break
+                    if a and a!=v:break
+                    t[y%k][x%k]=v
             else:continue
             break
         else:return[[t[y%k][x%k]for x in range(18)]for y in range(18)]
