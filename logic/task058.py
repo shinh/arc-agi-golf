23de7ed6ac315b1793992bb29b1999e4583e5847
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 17:14:38 +0900

    golf task058 spiral

--- logic/task058.py
+++ logic/task058.py
@@ -1,14 +1,9 @@
 def p(g):
-    n=len(g);o=create(n,n);x=y=0;o[0][0]=3
-    d=0;l=n;m=n-1;f=1
-    dx=1,0,-1,0;dy=0,1,0,-1
-    while 1:
-        for _ in range(l-1 if f else l):
-            x+=dx[d];y+=dy[d];o[y][x]=3
-        f=0;d=(d+1)%4
-        if m<1:break
-        for _ in range(m):
-            x+=dx[d];y+=dy[d];o[y][x]=3
-        d=(d+1)%4;l=m;m-=2
-        if l<1:break
-    return o
+ # spiral
+ l=len(g);m=l-1;f=1;x=y=d=0;u=1,0,-1,0;v=0,1,0,-1;g[0][0]=3
+ while l>0:
+  for _ in[0]*(l-f):x+=u[d];y+=v[d];g[y][x]=3
+  f=0;d=-~d%4
+  for _ in[0]*m:x+=u[d];y+=v[d];g[y][x]=3
+  d=-~d%4;l=m;m-=2
+ return g
