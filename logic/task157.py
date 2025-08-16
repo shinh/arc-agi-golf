469ccaf5958af93144c4db7c74ffd3129291ee8d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 16:39:52 +0900

    golf task157

--- logic/task157.py
+++ logic/task157.py
@@ -1,28 +1,26 @@
 def p(g):
-    h=len(g);w=len(g[0]);S=[]
+    # drop blocks and stack them
+    h=len(g);w=len(g[0]);S=[];M=R=[]
     for y in range(h):
         for x in range(w):
-            if g[y][x]==5:
-                q=[(y,x)];g[y][x]=0;c=[]
-                while q:
-                    i,j=q.pop();c.append((i,j))
+            if g[y][x]>4:
+                q=[(y,x)];g[y][x]=0
+                for i,j in q:
                     for u,v in (i+1,j),(i-1,j),(i,j+1),(i,j-1):
-                        if 0<=u<h and 0<=v<w and g[u][v]==5:g[u][v]=0;q.append((u,v))
-                Y,X=zip(*c);a=min(Y);b=min(X)
-                S.append(([(i-a,j-b)for i,j in c],max(Y)-a+1,max(X)-b+1))
+                        if h>u>=0<w>v>=0 and g[u][v]>4:g[u][v]=0;q+=[(u,v)]
+                Y,X=zip(*q);a=min(Y);b=min(X);S+=[([(i-a,j-b)for i,j in q],max(Y)-a+1,max(X)-b+1)]
     S.sort(key=lambda t:(-len(t[0]),-t[2]))
-    M=R=None
     def F(i):
-        nonlocal M,R,g
+        nonlocal M,R
         if i==len(S):
-            T=tuple((i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v)
-            if M is None or T<M:M,R=T,[r[:]for r in g]
+            t=[(i,j)for i in range(h)for j in range(w)if g[i][j]]
+            if not M or t<M:M,R=t,[r[:]for r in g]
             return
         T,H,W=S[i]
         for x in range(w-W+1):
             y=h-H
-            while y and all(g[y+dy-1][x+dx]==0 for dy,dx in T):y-=1
-            if all(g[y+dy][x+dx]==0 for dy,dx in T):
+            while y and all(not g[y+dy-1][x+dx] for dy,dx in T):y-=1
+            if all(not g[y+dy][x+dx] for dy,dx in T):
                 for dy,dx in T:g[y+dy][x+dx]=1
                 F(i+1)
                 for dy,dx in T:g[y+dy][x+dx]=0
