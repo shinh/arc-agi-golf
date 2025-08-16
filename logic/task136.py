63d7b206abcf23423a7f1d357bda7e2142ab5690
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 16:26:23 +0900

    golf task136

--- logic/task136.py
+++ logic/task136.py
@@ -1,13 +1,6 @@
-def p(g):
-    for y in range(10):
-        for x in range(10):
-            if g[y][x]==1:
-                while y>=0 and x>=0:g[y][x]=1;y-=1;x-=1
-                break
-        else:continue
-        break
-    for y in range(9,-1,-1):
-        for x in range(9,-1,-1):
-            if g[y][x]==2:
-                while y<9 and x<9:y+=1;x+=1;g[y][x]=2
-                return g
+def p(g):# diagonals from first 1 & last 2
+ r=sum(g,[])
+ y,x,Y,X=divmod(r.index(1),10)+divmod(99-r[::-1].index(2),10)
+ while x>-1<y:g[y][x]=1;x-=1;y-=1
+ while X<9>Y:X+=1;Y+=1;g[Y][X]=2
+ return g
