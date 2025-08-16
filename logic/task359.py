dd5177c0d2a36f00f413ff288367a49382649363
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 15:04:31 +0900

    golf task359

--- logic/task359.py
+++ logic/task359.py
@@ -1,7 +1,5 @@
 def p(g):
-    t=list(zip(*g))
-    if sum(len(set(c))for c in t)<sum(len(set(r))for r in g):
-        m=[max(c,key=c.count)for c in t]
-        return [m for _ in g]
-    w=len(g[0])
-    return [[max(r,key=r.count)]*w for r in g]
+    # pick dominant colors for rows or cols
+    if sum(len({*c})for c in zip(*g))<sum(len({*r})for r in g):
+        return[[max(c,key=c.count)for c in zip(*g)]]*len(g)
+    return[[max(r,key=r.count)]*len(r)for r in g]
