846910de9ff5c6e253604ca7687ced5244ac97e0
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 23:34:21 +0900

    golf task161

--- logic/task161.py
+++ logic/task161.py
@@ -1,5 +1,3 @@
 def p(g):
     # draw lines of rare edge color
-    T,B=g[0],g[-1]
-    c=min(({r[0]for r in g}&{r[-1]for r in g}|{*T}&{*B})-{0},key=sum(g,[]).count)
-    return [[c*(r[0]==r[-1]==c or t==b==c)for t,b in zip(T,B)]for r in g]
+    a,*_,b=zip(*g);c=min({*a}&{*b}|{*g[0]}&{*g[-1]},key=sum(g,[]).count);return[[c*(r[0]==r[-1]==c or t==b==c)for t,b in zip(g[0],g[-1])]for r in g]
