7c6c248fe66c50e645c5bf7915ccdda751f7a039
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 14:14:33 +0900

    golf task270

--- logic/task270.py
+++ logic/task270.py
@@ -1,19 +1,11 @@
-def p(g):
- d={}
- for i,r in enumerate(g):
-  for j,v in enumerate(r):d.setdefault(v,[]).append((i,j))
- def c(a):
-  is_=[i for i,_ in a];js=[j for _,j in a]
-  return(min(is_)+max(is_))//2,(min(js)+max(js))//2
- t=lambda b,p:(b[0]+(p[0]>b[0])-(p[0]<b[0]),b[1]+(p[1]>b[1])-(p[1]<b[1]))
- b=0
- o=[r for r in g]
- for v in(3,7):
-  for i,j in d.get(v,[]):o[i][j]=b
- if 1 in d:
-  b1=c(d[1])
-  for p2 in d.get(7,[]):i,j=t(b1,p2);o[i][j]=7
- if 2 in d:
-  b2=c(d[2])
-  for p2 in d.get(3,[]):i,j=t(b2,p2);o[i][j]=3
- return o
+def p(g):# move 7,3 toward centers of 1,2
+ d=[[]for _ in[0]*10]
+ for y,r in enumerate(g):
+  for x,v in enumerate(r):
+   d[v]+=((y,x),)
+   if v%4>2:g[y][x]=0
+ for v,u in(1,7),(2,3):
+  if d[v]:
+   Y,X=[(min(q)+max(q))//2for q in zip(*d[v])]
+   for a,b in d[u]:g[Y+(a>Y)-(a<Y)][X+(b>X)-(b<X)]=u
+ return g
