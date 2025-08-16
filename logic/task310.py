ba454d4b22b55fa4355845820c7d05ab405ce8cd
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 22:17:44 +0900

    golf task310

--- logic/task310.py
+++ logic/task310.py
@@ -1,8 +1,9 @@
 # crop box around least common color
 def p(m):
- a=[v for r in m for v in r if v]
- if not a:return[]
- l=min(a,key=a.count)
- y=[i for i,r in enumerate(m)if l in r];x=[i for i,c in enumerate(zip(*m))if l in c]
- return[r[x[0]:x[-1]+1]for r in m[y[0]:y[-1]+1]]
+ b=sum(m,[]);w=len(m[0]);s={*b}-{0}
+ if s:
+  l=min(s,key=b.count)
+  y,x=divmod(b.index(l),w);Y,X=divmod(len(b)+~b[::-1].index(l),w)
+  return[r[x:X+1]for r in m[y:Y+1]]
+ return[]
 
