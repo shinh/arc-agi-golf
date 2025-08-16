3bc43dfc5607286b0203885f8b652f30ca6a5119
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 16:46:03 +0900

    golf task273

--- logic/task273.py
+++ logic/task273.py
@@ -1,13 +1,9 @@
 def p(g):
-    rows=[{x for x,v in enumerate(r) if v==4} for r in g]
-    for y1 in range(10):
-        c1=rows[y1]
-        if c1:
-            for y2 in range(y1+1,10):
-                com=sorted(c1&rows[y2])
-                for i in range(len(com)):
-                    for j in range(i+1,len(com)):
-                        a,b=com[i],com[j]
-                        for y in range(y1+1,y2):
-                            for x in range(a+1,b):g[y][x]=2
+    # fill rectangles enclosed by 4s
+    rows=[{i for i in range(10)if r[i]==4}for r in g]
+    for y2 in range(10):
+        for y1 in range(y2):
+            for a in rows[y1]&rows[y2]:
+                for b in rows[y1]&rows[y2]:
+                    for r in g[y1+1:y2]:r[a+1:b]=[2]*(b-a-1)
     return g
