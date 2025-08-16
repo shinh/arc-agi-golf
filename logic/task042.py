ca4cd80ad25da80fd63117bef3e42795ea688cd1
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 13:21:55 +0900

    Golf task042

--- logic/task042.py
+++ logic/task042.py
@@ -1,24 +1,21 @@
-def p(g):
-    H=W=10;R=range
-    v=set();o=[r for r in g]
-    for y in R(H):
-        for x in R(W):
-            if g[y][x]==3 and (y,x)not in v:
-                q=[(y,x)];p=[];rs=set();cs=set()
-                while q:
-                    i,j=q.pop()
-                    if(i,j)in v:continue
-                    v.add((i,j));p.append((i,j));rs.add(i);cs.add(j)
-                    for Y in R(i-1,i+2):
-                        for X in R(j-1,j+2):
-                            if 0<=Y<H and 0<=X<W and g[Y][X]==3 and(Y,X)not in v:q.append((Y,X))
-                my,my2=min(rs),max(rs);mx,mx2=min(cs),max(cs)
-                h=my2-my+1;w=mx2-mx+1
-                for i,j in p:
-                    X=mx+mx2-j
-                    for a in(0,1):
-                        I=(i-my)*2+a+my-h//2
-                        for b in(0,1):
-                            J=(X-mx)*2+b+mx-w//2
-                            if 0<=I<H and 0<=J<W and I not in rs and J not in cs:o[I][J]=8
-    return o
+def p(g):#mirror&expand blobs
+ H=W=10;R=range
+ v=set()
+ for y in R(H):
+  for x in R(W):
+   if g[y][x]==3 and(y,x)not in v:
+    q=[(y,x)];p=[];rs=set();cs=set()
+    for i,j in q:
+     if(i,j)in v:continue
+     v.add((i,j));p+=[(i,j)];rs.add(i);cs.add(j)
+     for Y in R(i-1,i+2):
+      for X in R(j-1,j+2):
+       if-1<Y<H and-1<X<W and g[Y][X]==3:q.append((Y,X))
+    my,my2=min(rs),max(rs);mx,mx2=min(cs),max(cs);h=my2-my+1;w=mx2-mx+1
+    for i,j in p:
+     for a in 0,1:
+      I=(i-my)*2+a+my-h//2
+      for b in 0,1:
+       J=(mx2-j)*2+b+mx-w//2
+       if-1<I<H and-1<J<W and I not in rs and J not in cs:g[I][J]=8
+ return g
