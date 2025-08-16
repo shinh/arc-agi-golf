32ba7f4971d03368fa830f3385a191f3d20bc368
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 14:21:38 +0900

    golf task310

--- logic/task310.py
+++ logic/task310.py
@@ -1,15 +1,8 @@
-from collections import Counter
+# crop box around least common color
 def p(m):
- c=Counter(e for r in m for e in r if e).most_common()
- if not c:return[]
- l=c[-1][0];O=p=-1
- for i,r in enumerate(m):
-  if l in r:
-   if O<0:O=i
-   p=i
- S=U=-1
- for i in range(len(m[0])):
-  if any(m[j][i]==l for j in range(O,p+1)):
-   if S<0:S=i
-   U=i
- return[r[S:U+1]for r in m[O:p+1]]
+ a=[v for r in m for v in r if v]
+ if not a:return[]
+ l=min(a,key=a.count)
+ y=[i for i,r in enumerate(m)if l in r];x=[i for i,c in enumerate(zip(*m))if l in c]
+ return[r[x[0]:x[-1]+1]for r in m[y[0]:y[-1]+1]]
+
