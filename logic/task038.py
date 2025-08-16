672994fa4da0bdcb7b95481e2ad3882581af2340
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 20:36:16 +0900

    golf task 038

--- logic/task038.py
+++ logic/task038.py
@@ -1,4 +1,3 @@
 def p(g):
     # count 2x2 blocks of color 1
-    c=sum(a[x]*a[x+1]*b[x]*b[x+1]==1 for a,b in zip(g,g[1:]) for x in range(8))
-    return [[1]*c+[0]*(5-c)]
+    return [[1]*(c:=sum((1,)*4==y for a,b in zip(g,g[1:])for y in zip(a,a[1:],b,b[1:])))+[0]*(5-c)]
