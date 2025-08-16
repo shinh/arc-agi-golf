83bd5525db558f0f20ddb82aea0a434a5d1f4893
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 15:45:16 +0900

    golf task350

--- logic/task350.py
+++ logic/task350.py
@@ -1,18 +1,15 @@
 def p(g):
- h=len(g);w=len(g[0])
- for r in g:
-  l=None
-  for x,c in enumerate(r):
-   if c==1:
-    if l is not None:
-     for j in range(l+1,x):r[j]=8
-    l=x
- for x in range(w):
-  l=None
-  for y in range(h):
-   if g[y][x]==1:
-    if l is not None:
-     for i in range(l+1,y):g[i][x]=8
-    l=y
+ # connect 1s
+ def f(s):
+  for r in s:
+   l=-1
+   for x,v in enumerate(r):
+    if v==1:
+     if~l:r[l+1:x]=[8]*(x-l-1)
+     l=x
+ f(g)
+ t=[*map(list,zip(*g))]
+ f(t)
+ g[:]=map(list,zip(*t))
  return g
 
