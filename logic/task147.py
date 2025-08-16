d32cd99de5a84be451ad856cc8ee339ade546de2
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 15:18:33 +0900

    golf: optimize task147

--- logic/task147.py
+++ logic/task147.py
@@ -1,8 +1,7 @@
-def p(j):
-	A=[k[:]for k in j];c,E=len(j),len(j[0])
-	for k in range(c):
-		for W in range(E):
-			if j[k][W]==3:
-				for(l,J)in[(0,1),(1,0),(0,-1),(-1,0)]:
-					if 0<=k+l<c and 0<=W+J<E and j[k+l][W+J]==3:A[k][W]=8;break
-	return A
+# recolor any 3 touching another 3 to 8
+def p(g):
+        s={(y,x)for y,r in enumerate(g)for x,v in enumerate(r)if v==3}
+        for y,x in s:
+                if {(y+1,x),(y-1,x),(y,x+1),(y,x-1)}&s:g[y][x]=8
+        return g
+
