cf511b2eda34fbcc3d6d0caff0928be9d7a4c0fb
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 20:50:37 +0900

    golf task163

--- logic/task163.py
+++ logic/task163.py
@@ -1,8 +1,7 @@
 def p(g):
  # move 3x3 block marked by 4 to its 4x offset
- i=sum(g,[]).index(4);y=i//11;x=i%11
- a=y&-4;b=x&-4;y%=4;x%=4
+ y,x=divmod(sum(g,[]).index(4),11);Y=y&3;X=x&3
  o=[[5*(v==5)for v in r]for r in g]
- for k in 0,1,2:o[y*4+k][x*4:x*4+3]=g[a+k][b:b+3]
+ for k in 0,1,2:o[Y*4+k][X*4:X*4+3]=g[y-Y+k][x-X:x-X+3]
  return o
 
