8d1d134f2f484e53c803e843d878d15c221f1bd2
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 20:43:49 +0900

    chore: golf task322

--- logic/task322.py
+++ logic/task322.py
@@ -1,6 +1,5 @@
 def p(g):# propagate down
-    for x in 0,1,2:
-        v=0
-        for r in g:
-            r[x]=v=r[x] or v
-    return g
+ for x in 0,1,2:
+  v=0
+  for r in g:r[x]=v=r[x]or v
+ return g
