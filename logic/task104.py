a83155348a961835fb9ba248760a36c138820bcd
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 17:21:57 +0900

    Golf task104

--- logic/task104.py
+++ logic/task104.py
@@ -1,6 +1,4 @@
-def p(g):
-    a=1-g[0][1]//3;b=1-g[1][0]//3
-    c=5*b+a*(4-8*b);s=[3]*4;o=[[0]*9 for _ in[0]*9]
-    for x,y in((a,c),(a+4,c+4-8*(a^b))):
-        for i in range(4):o[x+i][y:y+4]=s
+def p(g):#two4x4s
+    a=g[0][1]<3;b=g[1][0]<3;c=5*b+a*(4-8*b);o=[[0]*9 for _ in[0]*9];d=c+4-8*(a^b)
+    for i in 0,1,2,3:o[a+i][c:c+4]=o[a+4+i][d:d+4]=[3]*4
     return o
