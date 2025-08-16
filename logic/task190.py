4c8332d5cdcce1981e039cfd3a0a65adf6720665
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 22:20:01 +0900

    golf task190

--- logic/task190.py
+++ logic/task190.py
@@ -1,8 +1,12 @@
-def p(g):#extend rays
- any((a:=y,b:=x,c)for y in range(9)for x in range(9)if(c:=g[y][x])==g[y+1][x]==g[y][x+1]==g[y+1][x+1]>0)
- for y in range(10):
-  for x in range(10):
-   if g[y][x]==c and(y-a>>1 or x-b>>1):
-    d=(y>a+1)-(y<a);e=(x>b+1)-(x<b);i=y;j=x
+def p(g):#ray casting from 2x2 block
+ c=max(sum(g,[]))
+ for y in range(9):
+  for x in range(9):
+   if g[y][x]==g[y][x+1]==g[y+1][x]==g[y+1][x+1]==c:a=y;b=x
+ for d in-1,1:
+  for e in-1,1:
+   i=a+d+(d>0);j=b+e+(e>0)
+   if-1<i<10 and-1<j<10 and g[i][j]==c:
     while-1<(i:=i+d)<10>(j:=j+e)>-1 and g[i][j]<1:g[i][j]=c
  return g
+
