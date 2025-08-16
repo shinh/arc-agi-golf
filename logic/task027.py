a742f21c3334b7e332d435eff0aa4320054e1496
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 14:14:51 +0900

    golf task027 by compressing symmetry search

--- logic/task027.py
+++ logic/task027.py
@@ -1,19 +1,14 @@
 def p(g):
-    a={(y,x)for y in range(10)for x in range(10)if g[y][x]==1}
+    # mirror pattern and overlay symmetrically
+    a={(y,x)for y,r in enumerate(g)for x,v in enumerate(r)if v==1}
     r={(9-x,y)for y,x in a}
-    n=lambda s:{(y-min(y for y,_ in s),x-min(x for _,x in s))for y,x in s}
-    R=lambda s:{(max(x for _,x in s)-x,y)for y,x in s}
+    n=lambda s:(Y:=min(y for y,_ in s),X:=min(x for _,x in s))and{(y-Y,x-X)for y,x in s}
+    R=lambda s:(X:=max(x for _,x in s))and{(X-x,y)for y,x in s}
     m=0;sh=a
     for dy in range(-10,10):
         for dx in range(-10,10):
             b={(y+dy,x+dx)for y,x in r}
-            if any(y<0 or y>9 or x<0 or x>9 for y,x in b):
-                continue
-            t=n(a|b)
-            if t==R(t):
-                k=len(a&b)
-                if k>m:m=k;sh=b
-    o=[r for r in g]
-    for y,x in sh-a:o[y][x]=2
-    return o
+            if all(-1<y<10 and-1<x<10 for y,x in b)and(t:=n(a|b))==R(t)and(k:=len(a&b))>m:m=k;sh=b
+    for y,x in sh-a:g[y][x]=2
+    return g
 
