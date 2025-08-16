83aa9e8890432df193d40e8ff01e31383dedcb04
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 22:16:33 +0900

    golf task076

--- logic/task076.py
+++ logic/task076.py
@@ -8,20 +8,19 @@ def p(g):
             for a in-1,0,1:
                 for b in-1,0,1:
                     if a|b and(p:=(x+a,y+b))in s:s.remove(p);q+=p,
-        if len(q)>len(m):m=q
+        m=max(m,q,key=len)
     C={v for i in range(h)for j in range(w)if(v:=g[i][j])and(i,j)not in m}
     rs,cs=zip(*m);a=min(rs);b=min(cs);H=max(rs)-a+1;W=max(cs)-b+1
     B=[(r-a,c-b,g[r][c])for r,c in m]
-    G=[*map(list,g)]
-    for _ in 0,1:
+    G=[r[:]for r in g]
+    for _ in'01':
         for _ in'0000':
             if not(S:=[b for b in B if b[2]in C]):return g
             a,b,k=S[0]
             for i,j in[(i-a,j-b)for i in range(h)for j in range(w)if g[i][j]==k]:
                 if all(0<=i+r<h>0<=j+c<w and g[i+r][j+c]==k for r,c,k in S):
                     for r,c,k in B:
-                        x,y=i+r,j+c
-                        if 0<=x<h>0<=y<w:G[x][y]=k
+                        if 0<=i+r<h>0<=j+c<w:G[i+r][j+c]=k
             B=[(c,H-1-r,k)for r,c,k in B];H,W=W,H
         B=[(r,W-1-c,k)for r,c,k in B]
     return G
