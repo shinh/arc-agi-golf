773cb10e6e59165730cb4d6ed418028b1375d6d6
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 23:21:14 +0900

    golf task270

--- logic/task270.py
+++ logic/task270.py
@@ -1,11 +1,10 @@
 def p(g):# move 7,3 toward centers of 1,2
- d=[[]for _ in[0]*10]
+ d=[[]for _ in[0]*8]
  for y,r in enumerate(g):
   for x,v in enumerate(r):
    d[v]+=((y,x),)
-   if v%4>2:g[y][x]=0
+   if v%4>2:r[x]=0
  for v,u in(1,7),(2,3):
-  if d[v]:
-   Y,X=[(min(q)+max(q))//2for q in zip(*d[v])]
-   for a,b in d[u]:g[Y+(a>Y)-(a<Y)][X+(b>X)-(b<X)]=u
+  Y,X=[sum(q)//len(q)for q in zip(*d[v])]
+  for a,b in d[u]:g[Y+(a>Y)-(a<Y)][X+(b>X)-(b<X)]=u
  return g
