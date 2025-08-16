bf1572df863893b01ef530abb4acc903c3081d99
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 23:36:43 +0900

    golf: count 2x2 blocks and draw cross

--- logic/task399.py
+++ logic/task399.py
@@ -1,6 +1,4 @@
 def p(g):
- o=create(3,3);k=0
- for r,s in zip(g,g[1:]):
-  for a,b,c,d in zip(r,r[1:],s,s[1:]):
-   if a+b+c+d==8:o[k*2//3][k*2%3]=1;k+=1
- return o
+    # draw cross for each 2x2 block of 2s
+    k=sum(sum(t)==8for r,s in zip(g,g[1:])for t in zip(r,r[1:],s,s[1:]))
+    return[[k>int(c)for c in r]for r in('051','525','354')]
