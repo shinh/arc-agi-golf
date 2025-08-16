6f91bd8732597eeb7d09fc76f42e544517478ef6
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 15:19:03 +0900

    golf task088

--- logic/task088.py
+++ logic/task088.py
@@ -1,10 +1,8 @@
 def p(g):
-    d={}
-    for y,r in enumerate(g):
-        for x,v in enumerate(r):
-            if v:d.setdefault(v,[]).append((y,x))
-    k=[c for c,s in d.items() if len(s)==4][0]
-    s=d[k];ys,xs=zip(*s)
-    a,b=min(ys)+1,max(ys)
-    c,d=min(xs)+1,max(xs)
-    return [[k*(g[y][x]>0)for x in range(c,d)]for y in range(a,b)]
+    # crop inside markers
+    for k in range(1,10):
+        y=[i for i,r in enumerate(g)if k in r]
+        if len(y)==2:
+            x=[i for i,r in enumerate(zip(*g))if k in r]
+            return[[k*(v>0)for v in r[x[0]+1:x[1]]]for r in g[y[0]+1:y[1]]]
+
