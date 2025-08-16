623554e9959562645f7472a3b3a3202b926d9173
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 23:25:03 +0900

    Golf task333 solution

--- logic/task333.py
+++ logic/task333.py
@@ -1,32 +1,12 @@
-# draw lines from each colored cell toward a nearby 3
+# extend colors to 3
 def p(g):
- for r in g:
-  for j,c in enumerate(r):
-   if c and c-3:
-    k=j+1
-    while k<10 and r[k]==0:k+=1
-    if k<10 and r[k]==3:r[j+1:k]=[c]*(k-j-1)
- g=[*map(list,zip(*g[::-1]))]
- for r in g:
-  for j,c in enumerate(r):
-   if c and c-3:
-    k=j+1
-    while k<10 and r[k]==0:k+=1
-    if k<10 and r[k]==3:r[j+1:k]=[c]*(k-j-1)
- g=[*map(list,zip(*g[::-1]))]
- for r in g:
-  for j,c in enumerate(r):
-   if c and c-3:
-    k=j+1
-    while k<10 and r[k]==0:k+=1
-    if k<10 and r[k]==3:r[j+1:k]=[c]*(k-j-1)
- g=[*map(list,zip(*g[::-1]))]
- for r in g:
-  for j,c in enumerate(r):
-   if c and c-3:
-    k=j+1
-    while k<10 and r[k]==0:k+=1
-    if k<10 and r[k]==3:r[j+1:k]=[c]*(k-j-1)
- g=[*map(list,zip(*g[::-1]))]
+ for _ in[0]*4:
+  for r in g:
+   for j,c in enumerate(r):
+    if 0<c!=3:
+     k=j+1
+     while k<10>r[k]<1:k+=1
+     if k<10>r[k]==3:r[j+1:k]=[c]*(k-j-1)
+  g=[*map(list,zip(*g[::-1]))]
  return g
 
