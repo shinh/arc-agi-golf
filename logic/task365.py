472feff483a2845b69a03a023479d42086320221
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 22:19:27 +0900

    golf task365

--- logic/task365.py
+++ logic/task365.py
@@ -1,12 +1,3 @@
 def p(j):
  #max 2s
- E=-1
- for k,r in enumerate(j):
-  for W,v in enumerate(r):
-   if v and((k and j[k-1][W])|(W and r[W-1]))<1:
-    l=J=1
-    while r[W+l:][:1]>[0]:l+=1
-    while j[k+J:]and j[k+J][W]:J+=1
-    a=[C[W:W+l]for C in j[k:k+J]]
-    if(C:=sum(z.count(2)for z in a))>E:E=C;e=a
- return e
+ return max((str(a).count('2'),a)for y,r in enumerate(j)for x,v in enumerate(r)if v*(y<1or j[y-1][x]<1)*(x<1or r[x-1]<1)for a in[[R[x:x+(*r[x:],0).index(0)]for R in j[y:y+(*[*zip(*j)][x][y:],0).index(0)]]])[1]
