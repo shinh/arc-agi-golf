1f57a27aa86514b6d69c060b3c3396e57c4d9863
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 23:31:58 +0900

    Golf task085

--- logic/task085.py
+++ logic/task085.py
@@ -2,6 +2,5 @@
 # zero tile when identical to neighbours
 def p(g):
  for r,a,b in zip(g,g[-1:]+g,g[1:]+g):
-  for x,v in enumerate(r):
-   if v==a[x]==b[x]==r[x-1]==(r+[0])[x+1]:r[x]=0
+  for x,v in enumerate(r):r[x]-=(v==a[x]==b[x]==r[x-1]==(r+[0])[x+1])*v
  return g
