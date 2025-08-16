dbf4e8a682bc6e90701bf82b401aec499b963a2e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 16:13:41 +0900

    golf: crop bounding box of rare color

--- logic/task049.py
+++ logic/task049.py
@@ -1,9 +1,7 @@
 def p(g):
- d={};y=-1
- for r in g:
-  y+=1;x=-1
-  for v in r:
-   x+=1
-   if v:d.setdefault(v,[]).append((y,x))
- ys,xs=zip(*min(d.values(),key=len))
- return [r[min(xs):max(xs)+1] for r in g[min(ys):max(ys)+1]]
+ # find rare color then crop its bbox
+ f=sum(g,[])
+ k=min({*f}-{0},key=f.count)
+ y=[i for i,r in enumerate(g) if k in r]
+ x=[i for i,c in enumerate(zip(*g)) if k in c]
+ return [r[x[0]:x[-1]+1] for r in g[y[0]:y[-1]+1]]
