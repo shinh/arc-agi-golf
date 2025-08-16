79a517b867f7ea5de8b367042b8bb6b788c46ae9
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 16:24:21 +0900

    golf: compress task081

--- logic/task081.py
+++ logic/task081.py
@@ -1,11 +1,4 @@
 def p(g):
-    h=w=7;o=[r for r in g]
-    for y in range(h):
-        for x in range(w):
-            if g[y][x]==8:
-                for dy in(-1,1):
-                    ny=y+dy
-                    if 0<=ny<h and g[ny][x]==8:
-                        if x and g[y][x-1]==0 and g[ny][x-1]==8:o[y][x-1]=1
-                        if x<6 and g[y][x+1]==0 and g[ny][x+1]==8:o[y][x+1]=1
-    return o
+ # extend 8 pairs sideways
+ for y in range(6):r,s=g[y:y+2];[7>n>=0<r[n]+s[n]==8 and g[y+(r[n]>0)].__setitem__(n,1) for x in range(7) if r[x]==s[x]==8 for n in (x-1,x+1)]
+ return g
