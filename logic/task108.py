73319085fcb2fa80993bfd12f3832cd9aba30ff3
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 16:34:51 +0900

    golf task108

--- logic/task108.py
+++ logic/task108.py
@@ -1,3 +1,1 @@
-def p(g):
-    r=[[max(g[2*i][2*j:2*j+2]+g[2*i+1][2*j:2*j+2])for j in range(5)]for i in range(5)]
-    return[[c for c in row for _ in range(4)]for row in r for _ in range(4)]
+def p(g):r=range(0,9,2);return[[max(g[i][j:j+2]+g[i+1][j:j+2])for j in r for _ in'0'*4]for i in r for _ in'0'*4]#2x2 max then 4x bigger
