512dfd9217e32e2206b95e5c206caa7d43e530f1
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 15:04:17 +0900

    golf task051

--- logic/task051.py
+++ logic/task051.py
@@ -1,12 +1,13 @@
 def p(g):
+    # extend the lone colored cell toward its neighbor
     h=len(g);w=len(g[0]);f=sum(g,[])
-    for c in range(1,10):
+    for c in f:
         if f.count(c)==1:break
     y,x=divmod(f.index(c),w)
     for dy,dx in((0,1),(1,0),(0,-1),(-1,0)):
-        y1,x1=y+dy,x+dx;y2,x2=y-dy,x-dx
-        if 0<=y1<h and 0<=x1<w and g[y1][x1] and not(0<=y2<h and 0<=x2<w and g[y2][x2]):
-            while 0<=y1<h and 0<=x1<w and g[y1][x1]:y1+=dy;x1+=dx
-            while 0<=y1<h and 0<=x1<w:g[y1][x1]=c;y1+=dy;x1+=dx
+        y1,x1=y+dy,x+dx
+        if h>y1>=0<=x1<w and g[y1][x1] and not(h>y-dy>=0<=x-dx<w and g[y-dy][x-dx]):
+            while h>y1>=0<=x1<w and g[y1][x1]:y1+=dy;x1+=dx
+            while h>y1>=0<=x1<w:g[y1][x1]=c;y1+=dy;x1+=dx
             break
     return g
