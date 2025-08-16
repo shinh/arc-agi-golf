e533ee7880ddbd277c50f14ba98943eb9645bbd1
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 20:46:37 +0900

    golf task263 solution

--- logic/task263.py
+++ logic/task263.py
@@ -1,8 +1,4 @@
 # find unique 3x3 block by zero pattern
 def p(g):
- b=[];m=[]
- for y in range(0,len(g),3):
-  for x in range(0,len(g[0]),3):
-   b+=[t:=[r[x:x+3]for r in g[y:y+3]]];m+=[[c<1for c in sum(t,[])]]
- return b[m.index(min(m,key=m.count))]
+ b,m=zip(*((t:=[r[x:x+3]for r in g[y:y+3]],[c<1for c in sum(t,[])])for y in range(0,len(g),3)for x in range(0,len(g[0]),3)));return b[m.index(min(m,key=m.count))]
 
