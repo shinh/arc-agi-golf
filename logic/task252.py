d34214cfd903cf583b7b12acbf9b014dadd4d4e4
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-16 17:26:51 +0900

    golf task252

--- logic/task252.py
+++ logic/task252.py
@@ -1,7 +1,7 @@
-def p(g,R=range):
- N=len(g)
- for i in R(N):
-  for x,y in zip(R(1,N,2),R(i+1,N,2)):
-   if g[0][i]:g[x][y]=4
-   if g[i][0]:g[y][x]=4
+def p(g):
+ # use first row/col to draw diagonals
+ for x in range(1,len(g),2):
+  for y in range(x,len(g)):
+   if g[0][y-x]:g[x][y]=4
+   if g[y-x][0]:g[y][x]=4
  return g
