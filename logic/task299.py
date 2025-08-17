3f542b70def6649ce3e13641a04573df5f7a8bfd
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-17 23:02:21 +0900

    golf task299

--- logic/task299.py
+++ logic/task299.py
@@ -1,3 +1,3 @@
 # cross row of 2 and column of 8
-# There should be a better formula
-p=lambda g:[[[0,2,8,4][(g[0][x]>0)*2+(2 in r)] for x in range(6)]for r in g]
+# better formula?
+p=lambda g:[[x+(2in r)*(2-6*x//8)for x in g[0]]for r in g]
