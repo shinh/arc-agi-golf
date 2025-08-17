65c174ba5bf5bb7d24cddc82ae6bd46fded17f52
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-17 20:09:27 +0900

    Solve 182

--- logic/task182.py
+++ logic/task182.py
@@ -1,25 +1,23 @@
 def p(g):
     # find biggest frame and recolor matching shapes
-    v=set();o=[];R=range(20)
-    for y in R:
-        for x in R:
-            if g[y][x]and(y,x)not in v:
-                c=g[y][x];q=[(y,x)];v.add((y,x))
-                for Y,X in q:
-                    for a,b in((1,0),(-1,0),(0,1),(0,-1)):
-                        u,V=Y+a,X+b
-                        if u in R and V in R and g[u][V]==c and(u,V)not in v:v.add((u,V));q+=(u,V),
-                o+=q,
-    f=[]
-    for s in o:
-        ys,xs=zip(*s);t=min(ys);b=max(ys);l=min(xs);r=max(xs)
-        if len(s)==2*(b-t+r-l):f+=((b-t)*(r-l),t,b,l,r),
-    _,t,b,l,r=max(f)
-    P=[(y,x)for y in range(t+1,b)for x in range(l+1,r)if g[y][x]]
-    y,x=P[0];c=g[y][x]
-    ys,xs=zip(*P);a=min(ys);b=min(xs);S={(y-a,x-b)for y,x in P}
-    for s in o:
-        ys,xs=zip(*s);a=min(ys);b=min(xs)
-        if {(y-a,x-b)for y,x in s}==S:
-            for y,x in s:g[y][x]=c
+    for y in range(20):
+        for x in range(20):
+            if g[y][x]==5:
+                n=0
+                for ey in range(y+1,21):
+                    if ey>19or g[ey][x]!=5:break
+                    n+=1
+                for ex in range(x+1,20):
+                    if g[ey-1][ex]!=5:break
+                    n+=1
+                if n==12:
+                    sx=x
+                    sy=y
+
+    for y in range(16):
+        for x in range(16):
+            if all((g[sy+dy+1][sx+dx+1]>0)==(g[y+dy][x+dx]>0)for dy in range(5)for dx in range(5)):
+                for dy in range(5):
+                    for dx in range(5):
+                        g[y+dy][x+dx]=g[sy+dy+1][sx+dx+1]
     return g
