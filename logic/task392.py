dc361fab7bff64388933260b2f561c0333f265d4
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-17 21:59:05 +0900

    golf task392

--- logic/task392.py
+++ logic/task392.py
@@ -1,12 +1,9 @@
-def p(g):
+def p(g):# expand a found horizontal pattern
     for o in range(4):
         for y in range(6):
             for x in range(6):
-                c=g[y+1][x]
-                if g[y][x]<1and c and g[y][x+1]and c in g[y][x+2:]:
+                if g[y][x]<1and(c:=g[y+1][x])*g[y][x+1]and(c in g[y][x+2:]):
                     l=g[y][x+2:].index(c)
-
-                    g=[[[c,5][max([y-ny-l+1,ny-y][ny>y],[x-nx-l+1,nx-x][nx>x])%(l+1)!=1]for nx in range(10)]for ny in range(10)]
-
+                    g=[[[c,5][max(ny-y,1-l-ny+y,nx-x,1-l-nx+x)%(-~l)!=1]for nx in range(10)]for ny in range(10)]
         g=[*map(list,zip(*g[::-1]))]
     return g
