0de56ae301ee600972c58da2b9f44a3a32933b0a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-18 01:04:11 +0900

    Solve 125

--- logic/task125.py
+++ logic/task125.py
@@ -1,19 +1,12 @@
 def p(g):
-    # flood each 6 region and draw padded box
-    w=15;o=[r[:]for r in g]
-    for y in range(w):
-        for x in range(w):
+    for y in range(15):
+        for x in range(15):
             if g[y][x]==6:
-                s=[(y,x)];Y=[y];X=[x];g[y][x]=0
-                while s:
-                    y,x=s.pop()
-                    for ny,nx in((y+1,x),(y-1,x),(y,x+1),(y,x-1)):
-                        if w>ny>=0<=nx<w and g[ny][nx]==6:
-                            s+=[(ny,nx)];g[ny][nx]=0;Y+=ny,;X+=nx,
-                a=max(min(Y)-1,0);b=min(max(Y)+2,w);c=max(min(X)-1,0);d=min(max(X)+2,w)
-                for y in range(a,b):
-                    for x in range(c,d):
-                        if y in(a,b-1)or x in(c,d-1):o[y][x]=3
-                        elif o[y][x]-6:o[y][x]=4
-    return o
-
+                ey,ex=y,x
+                while g[ey][x]==6:ey+=1
+                while g[y][ex]==6:ex+=1
+                for fy in range(y-1,ey+1):
+                    for fx in range(x-1,ex+1):
+                        if g[fy][fx]==8:
+                            g[fy][fx]=3+(y<fy<ey and x<fx<ex)
+    return g
