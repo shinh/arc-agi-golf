0575beeb0b043238049a737afb633d4397cad831
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-17 09:30:17 +0900

    golf task068

--- logic/task068.py
+++ logic/task068.py
@@ -1,6 +1,5 @@
 def p(g):# surround unique cell with 2s
-    a=sum(g,[]);v=min(a,key=a.count);y,x=divmod(a.index(v),10);o=create(10,10)
-    for Y in y-1,y,y+1:
-        for X in x-1,x,x+1:
-            if-1<Y<10 and-1<X<10:o[Y][X]=2
+    a=sum(g,[]);v=min(a,key=a.count);y,x=divmod(a.index(v),10);r=range(10)
+    o=[[2*(y-i<2>x-j>-2<y-i)for j in r]for i in r]
     o[y][x]=v;return o
+
