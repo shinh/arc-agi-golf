2b70c229ef8b8a82d9e63fdf5b13590fa808531a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-17 09:51:57 +0900

    golf task020

--- logic/task020.py
+++ logic/task020.py
@@ -2,13 +2,10 @@ def p(g):
     # rotate points around their box center
     P=[(y,x,v)for y,r in enumerate(g)for x,v in enumerate(r)if v]
     if not P:return g
-    ys,xs,_=zip(*P);b,d=min(ys),max(ys);a,c=min(xs),max(xs);cx=(a+c)/2;cy=(b+d)/2
+    ys,xs,_=zip(*P);cx=(min(xs)+max(xs))/2;cy=(min(ys)+max(ys))/2
     o=[[0]*10 for _ in g]
     for y,x,v in P:
         x-=cx;y-=cy
-        o[int(round(y+cy))][int(round(x+cx))]=v;x,y=y,-x
-        o[int(round(y+cy))][int(round(x+cx))]=v;x,y=y,-x
-        o[int(round(y+cy))][int(round(x+cx))]=v;x,y=y,-x
-        o[int(round(y+cy))][int(round(x+cx))]=v;x,y=y,-x
+        for _ in'r'*4:o[round(y+cy)][round(x+cx)]=v;x,y=y,-x
     return o
 
