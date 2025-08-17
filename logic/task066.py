cdce2451c2a3514422121d6c708d2cdc32360256
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-18 00:56:20 +0900

    golf task066 more

--- logic/task066.py
+++ logic/task066.py
@@ -1,43 +1,24 @@
 # connect 3 to 2 around blocks
 def p(g):
-    for y in range(len(g)):
-        for x in range(len(g[0])):
-            if g[y][x]==2:
-                ry=y
-                rx=x
-    for y in range(len(g)):
-        for x in range(len(g[0])):
-            dy=dx=0
-            if g[y][x]==3 and g[y-1][x]==3:
-                dy=1
-            elif g[y][x]==3 and g[y][x-1]==3:
-                dx=1
-            else:
-                continue
-
-            cands=[]
-            for dy,dx in(dy,dx),(-dy,-dx):
-                ny=y
-                nx=x
-                o=[r*1for r in g]
-
-                q=0
-                while 0<=ny<len(g)and 0<=nx<len(g[0])and q<9:
-                    if g[ny][nx]==2:
-                        cands.append((q,o))
-                        break
-                    if g[ny][nx]==8:
-                        q+=1
-                        ny-=dy
-                        nx-=dx
-                        if dy:
-                            dy=0
-                            dx=[-1,1][rx>nx]
-                        else:
-                            dy=[-1,1][ry>ny]
-                            dx=0
-                    else:
-                        o[ny][nx]=3
-                        ny+=dy
-                        nx+=dx
-            return min(cands,key=lambda t:t[0])[1]
+    h=len(g);w=len(g[0]);dy=dx=0
+    for y in range(h):
+        for x in range(w):
+            t=g[y][x]
+            if t==2:ry=y;rx=x
+            elif t==3:
+                if y and g[y-1][x]==3:dy=1;sy=y;sx=x
+                elif x and g[y][x-1]==3:dx=1;sy=y;sx=x
+    b=9;B=g
+    for dy,dx in(dy,dx),(-dy,-dx):
+        ny,nx=sy,sx;o=[r*1for r in g];q=0
+        while q<9 and h>ny>=0<=nx<w:
+            t=g[ny][nx]
+            if t==2:
+                if q<b:b=q;B=o
+                break
+            if t==8:
+                q+=1;ny-=dy;nx-=dx
+                if dy:dy=0;dx=[-1,1][rx>nx]
+                else:dy=[-1,1][ry>ny];dx=0
+            else:o[ny][nx]=3;ny+=dy;nx+=dx
+    return B
