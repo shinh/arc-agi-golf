87aae6a6f30acf34db925938363e3e3b5f097007
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-17 09:49:56 +0900

    golf task074

--- logic/task074.py
+++ logic/task074.py
@@ -1,9 +1,7 @@
-def p(g):
-    # mirror non9
-    e=enumerate
-    for k in range(4):
-        for i,j,v in[(i,j,v)for i,r in e(g)for j,v in e(r)if v-9]:
-            x=(j,i^31,i^31,i)[k]
-            y=(i,j^31,j,j^31)[k]
-            if x<30>y:g[x][y]=v
-    return g
+e=enumerate
+def p(g):# mirror non9
+ for k in 0,1,2,3:
+  for i,r in e(g):
+   for j,v in e(r):
+    if v^9 and (x:=(j,i^31,i^31,i)[k])<30>(y:=(i,j^31,j,j^31)[k]):g[x][y]=v
+ return g
