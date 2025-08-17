b75e619eafdecfae7e583a76b756d16dcaa5266d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-17 14:26:24 +0900

    Golf 165 a bit more

--- logic/task165.py
+++ logic/task165.py
@@ -1,19 +1,17 @@
 def p(g):
-    # rotate to match shape then drop rare color
-    s={18,9,15,3,11,21,27,16,19,10}
+    s={0,6,8,9,11,12,16,17,18,24}
     for pc in range(10):
         P={(y,x)for y,r in enumerate(g)for x,v in enumerate(r)if v==pc}
         if P:
-            my,mx=map(min,zip(*P))
-            if {(y-my)*7+x-mx for y,x in P}==s:break
-    g=[*map(list,zip(*g))]
-    for r in g:
-        if pc in r:
-            x=r.index(pc)
-            while r[x]==pc:
-                x+=1
-            nc=set(r[x:])-{0}
-            if nc:
-                nc=nc.pop()
-                r[x:]=[nc]*(len(r)-x)
-    return[*map(list,zip(*g))]
+            my,mx=map(max,zip(*P))
+            if {(my-y)*7+mx-x for y,x in P}==s:break
+    for dx in range(7):
+        x=mx-dx
+        z=abs(3-dx)
+        sy=my+[-1,0,0,1][z]
+        for y in range(sy,20):
+            c=g[y][x]
+            if c:
+                for y in range(sy,20):
+                    g[y][x]=c
+    return g
