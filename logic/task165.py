fa2b2d93e49220024a280c21b49b3ea646dee317
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-17 13:43:33 +0900

    Remove code for non-existent case

--- logic/task165.py
+++ logic/task165.py
@@ -1,20 +1,14 @@
 def p(g):
  # rotate to match shape then drop rare color
  s={18,9,15,3,11,21,27,16,19,10}
- R=lambda a:[list(x)for x in zip(*a[::-1])]
- G=g
- for un in 4,3,2,1:
-  for pc in range(10):
-   P={(i,j)for i,r in enumerate(G)for j,v in enumerate(r)if v==pc}
+ for pc in range(10):
+   P={(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==pc}
    if P:
     mi,mj=map(min,zip(*P))
     if {(i-mi)*7+j-mj for i,j in P}==s:break
-  else:G=R(G);continue
-  break
- l=sum(G,[])
+ l=sum(g,[])
  m=min(set(l)-{pc},key=l.count)
- for j in{j for i,r in enumerate(G)for j,v in enumerate(r) if v==m and any((k,j)in P for k in range(i))}:
+ for j in{j for i,r in enumerate(g)for j,v in enumerate(r) if v==m and any((k,j)in P for k in range(i))}:
   b=max(i for i,k in P if k==j)+1
-  for r in G[b:]:r[j]=m
- for _ in(0,)*un:G=R(G)
- return G
+  for r in g[b:]:r[j]=m
+ return g
