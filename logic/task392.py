738513f39944980ab9cbcf2cd6e5a7b0af67e5aa
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-17 21:40:33 +0900

    Solve 392

--- logic/task392.py
+++ logic/task392.py
@@ -1,24 +1,16 @@
 def p(g):
- # expand the smallest blob at constant offsets
- c=max(map(max,g));o=[[5]*10for _ in g]
- s={(y,x)for y,r in enumerate(g)for x,v in enumerate(r)if v};O=[]
- while s:
-  q=[s.pop()]
-  for y,x in q:
-   o[y][x]=c
-   for Y,X in(y+1,x),(y-1,x),(y,x+1),(y,x-1):
-    if(Y,X)in s:s.remove((Y,X));q+=[(Y,X)]
-  O+=q,
- a=min(O,key=lambda R:(lambda Y,X:max(max(Y)-min(Y),max(X)-min(X)))(*zip(*R)));O.remove(a)
- b=min(O,key=lambda R:min(abs(i-x)+abs(j-y)for i,j in R for x,y in a))
- Y,X=zip(*a);U,D,L,R=min(Y),max(Y),min(X),max(X);Y,X=zip(*b)
- H=max(max(X)-R,L-min(X));V=max(max(Y)-D,U-min(Y))
- for _ in[0]*15:
-  U-=V;L-=H;D+=V;R+=H
-  for x in range(L,R+1):
-   for y in U,D:
-    if-1<y<10 and-1<x<10:o[y][x]=c
-  for y in range(U,D+1):
-   for x in L,R:
-    if-1<y<10 and-1<x<10:o[y][x]=c
- return o
+    for o in range(4):
+        for y in range(6):
+            for x in range(6):
+                c=g[y+1][x]
+                if g[y][x]<1and c and g[y][x+1]and c in g[y][x+2:]:
+                    l=g[y][x+2:].index(c)
+
+                    for ny in range(10):
+                        for nx in range(10):
+                            #d=max(abs(ny-y),abs(nx-x))
+                            d=max([y-ny-l+1,ny-y][ny>y],[x-nx-l+1,nx-x][nx>x])
+                            g[ny][nx]=[c,5][d%(l+1)!=1]
+
+        g=[*map(list,zip(*g[::-1]))]
+    return g
