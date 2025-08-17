870ba1bd2336356d9df3f6ab892bff7a35e61993
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-17 09:37:13 +0900

    golf task104

--- logic/task104.py
+++ logic/task104.py
@@ -1,4 +1,4 @@
 def p(g):#two4x4s
-    a=g[0][1]<3;b=g[1][0]<3;c=5*b+a*(4-8*b);o=[[0]*9 for _ in[0]*9];d=c+4-8*(a^b)
-    for i in 0,1,2,3:o[a+i][c:c+4]=o[a+4+i][d:d+4]=[3]*4
+    a=g[0][1]<3;b=g[1][0]<3;c=4*(a^b)+b;o=[9*[0]for _ in[0]*9];d=c^4
+    for i in 0,1,2,3:o[a+i][c:c+4]=o[a+4+i][d:d+4]=4*[3]
     return o
