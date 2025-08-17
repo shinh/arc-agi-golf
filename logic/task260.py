b2c176b7b1e435323b79efb7ff7f061c51fe15e9
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-17 09:35:50 +0900

    golf task260

--- logic/task260.py
+++ logic/task260.py
@@ -1,11 +1,11 @@
 def p(g):
     # draw diagonal line when 5 has empty cross arms
-    for y in range(9):
-        for x in range(9):
-            if (c:=g[y][x])==5:
+    for y,r in enumerate(g):
+        for x,c in enumerate(r):
+            if c==5:
                 for d in 1,-1:
                     a=y-d; b=x+d
-                    if g[y][b]+g[a][x]<1:
+                    if r[b]+g[a][x]<1:
                         for i in range(10-abs(a-b)):
                             g[a-min(a,b)+i][b-min(a,b)+i]=f
             elif c:
