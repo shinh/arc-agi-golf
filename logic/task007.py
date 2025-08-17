e70eda2a68e81a35d4fa28a8b654c3e7b778723e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-17 09:29:59 +0900

    Golf task007

--- logic/task007.py
+++ logic/task007.py
@@ -2,6 +2,5 @@ def p(g):
     # fill diagonals with last seen color
     c=[0]*3
     for i,v in enumerate(sum(g,[])):
-        if v:c[(i%7+i//7)%3]=v
-    r=range(7)
-    return[[c[(y+x)%3]for x in r]for y in r]
+        if v:c[i%3]=v
+    return[(c*3)[i%3:i%3+7]for i in range(7)]
