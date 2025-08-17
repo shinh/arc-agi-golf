9812fb2d52fb17d6606edc4681b7d820d3919815
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-17 14:11:18 +0900

    Golf 165 a bit

--- logic/task165.py
+++ logic/task165.py
@@ -1,14 +1,19 @@
 def p(g):
- # rotate to match shape then drop rare color
- s={18,9,15,3,11,21,27,16,19,10}
- for pc in range(10):
-   P={(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==pc}
-   if P:
-    mi,mj=map(min,zip(*P))
-    if {(i-mi)*7+j-mj for i,j in P}==s:break
- l=sum(g,[])
- m=min(set(l)-{pc},key=l.count)
- for j in{j for i,r in enumerate(g)for j,v in enumerate(r) if v==m and any((k,j)in P for k in range(i))}:
-  b=max(i for i,k in P if k==j)+1
-  for r in g[b:]:r[j]=m
- return g
+    # rotate to match shape then drop rare color
+    s={18,9,15,3,11,21,27,16,19,10}
+    for pc in range(10):
+        P={(y,x)for y,r in enumerate(g)for x,v in enumerate(r)if v==pc}
+        if P:
+            my,mx=map(min,zip(*P))
+            if {(y-my)*7+x-mx for y,x in P}==s:break
+    g=[*map(list,zip(*g))]
+    for r in g:
+        if pc in r:
+            x=r.index(pc)
+            while r[x]==pc:
+                x+=1
+            nc=set(r[x:])-{0}
+            if nc:
+                nc=nc.pop()
+                r[x:]=[nc]*(len(r)-x)
+    return[*map(list,zip(*g))]
