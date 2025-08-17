b289067e755cad2cee8565d10e1d283da5cf96ab
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-18 00:48:48 +0900

    Solve 233

--- logic/task233.py
+++ logic/task233.py
@@ -1,29 +1,33 @@
 def p(g):
- # find rectangles and fit them into main block
- h=len(g);w=len(g[0]);m=max(range(1,10),key=sum(g,[]).count);G=[*map(list,g)];C=[]
- for i in range(h):
-  for j in range(w):
-   if g[i][j]:
-    q=[(i,j)];g[i][j]=0;t=b=i;l=r=j
-    for x,y in q:
-     t=min(t,x);b=max(b,x);l=min(l,y);r=max(r,y)
-     for nx,ny in((x+1,y),(x-1,y),(x,y+1),(x,y-1)):
-      if h>nx>=0<=ny<w and g[nx][ny]:g[nx][ny]=0;q+=(nx,ny),
-    C+=(len(q),t,b,l,r),
- C.sort();n,t,b,l,r=C.pop()
- W=r-l+1;H=b-t+1;o=[z[l:r+1]for z in G[t:b+1]];R=[[m]*W for _ in range(H)]
- for n,t,b,l,r in C:
-  if (b-t+1)*(r-l+1)==n and m in (s:=sum(p:=[z[l:r+1]for z in G[t:b+1]],[])) and len({*s})>1:
-   r=[p]
-   for _ in'123':r+=[p:=[*zip(*p[::-1])]]
-   # try rotations and mirrors to fit
-   for t in r+[[s[::-1]for s in q]for q in r]:
-    h=len(t);w=len(t[0])
-    S=[(i,j)for i in range(H-h+1)for j in range(W-w+1)if all((t[a][b]==m)==(o[i+a][j+b]==0)for a in range(h)for b in range(w))]
-    if S:
-     i,j=S[-1]
-     if i*j*(i+h-H)*(j+w-W)==0:i,j=S[0]
-     for a in range(h):R[i+a][j:j+w]=t[a];o[i+a][j:j+w]=[m]*w
-     break
- return R
+    objs=[]
+    for x in range(len(g[0])-2):
+        for y in range(len(g)-2):
+            o=[[g[oy][ox]for ox in range(x,x+3)]for oy in range(y,y+3)]
+            s=set(sum(o,[]))
+            if len(s)>1 and len(s&{0})<1:
+                objs+=o,
+                for oy in range(y,y+3):
+                    for ox in range(x,x+3):
+                        g[oy][ox]=0
 
+    g=[*map(list,eval('zip(*filter(any,'*2+'g))))'))]
+    #show(g, "crop")
+    #print(len(objs))
+
+    for o in objs:
+        #print(o)
+        yet=1
+        for t in range(4):
+            if yet:
+                for y in range(len(g)-2):
+                    for x in range(len(g[0])-2):
+                        if all((o[oy][ox]!=2)==(g[y+oy][x+ox]>0)for oy in range(3)for ox in range(3))and yet:
+                            for oy in range(3):
+                                for ox in range(3):
+                                    g[y+oy][x+ox]=o[oy][ox]
+                            yet=0
+
+            # Probably better to rotate o instead of g, though
+            o=[*map(list,zip(*o[::-1]))]
+
+    return g
