c9753a8d9bcd85df5e3bb54431270d709037b703
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-18 01:41:16 +0900

    Solve 93

--- logic/task093.py
+++ logic/task093.py
@@ -1,11 +1,12 @@
-# expand 5 block based on surrounding colors
 def p(g):
-    # find bounding box and extend edges by neighbor counts
-    f=sum(g,[]);a=f.index(5);d=195-f[::-1].index(5);t,l=divmod(a,14);b,r=divmod(d,14);o=create(14,14)
-    for y in range(t,b+1):
-        a=sum(v%5>0 for v in g[y][:l]);d=sum(v%5>0 for v in g[y][r+1:])
-        o[y][l-a:r+d+1]=[5]*(r-l+1+a+d)
-    for x in range(l,r+1):
-        for i in range(sum(r[x]%5>0 for r in g[:t])):o[t-1-i][x]=5
-        for i in range(sum(r[x]%5>0 for r in g[b+1:])):o[b+1+i][x]=5
-    return o
+    #show(g, "input")
+    for t in range(4):
+        if 5 in g[0]:
+            for r in g:
+                sx=r.index(5)
+                s=len([c for c in r[:sx]if c])
+                r[:sx]=[0]*(sx-s)+[5]*s
+            #show(ng, "crop")
+
+        g=[*map(list,zip(*g[::-1]))]
+    return g
