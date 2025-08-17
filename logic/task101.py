de8d92267cf4862be65ca255f5a75f3c81f6ac44
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-17 19:07:51 +0900

    Golf 101

--- logic/task101.py
+++ logic/task101.py
@@ -1,13 +1,14 @@
+# 396
 def p(g):
     sx=sy=99
     ex=ey=-1
     q=g
     for o in range(80):
-        q=[[[c,1][c>0and p==1]for c,p in zip(r,(0,*r))]for r in zip(*q[::-1])]
+        q=[[[c,1][c>0and p&1]for c,p in zip(r,(0,*r))]for r in zip(*q[::-1])]
 
     for y in range(len(g)):
         for x in range(len(g[0])):
-            if q[y][x]==1:
+            if q[y][x]&1:
                 sx=min(sx,x)
                 sy=min(sy,y)
                 ex=max(ex,x+1)
@@ -23,19 +24,7 @@ def p(g):
     for r in range(3,0,-1):
         for y in range(-7,len(g)-ly*r+1):
             for x in range(-7,len(g[0])-lx*r+1):
-                if sy<=y<ey and sx<=x<ex:continue
-                if x>1and g[y][x-1]:continue
-                if y>1and g[y-1][x]:continue
-                ok=True
-                for dy in range(ly*r):
-                    for dx in range(lx*r):
-                        if y+dy<0 or x+dx<0:d=0
-                        else:d=g[y+dy][x+dx]
-                        c=g[sy+dy//r][sx+dx//r]
-                        #if y==7 and x==1:print("kk",dy,dx,d,c,ly,lx,r)
-                        if d!=[0,0,2][c]:
-                            ok=False
-                if ok:
+                if not((sy<=y<ey and sx<=x<ex)|(x>1and g[y][x-1])|(y>1and g[y-1][x]))and all([g[y+dy][x+dx],0][(y+dy<0)|(x+dx<0)]==[0,0,2][g[sy+dy//r][sx+dx//r]]for dy in range(ly*r)for dx in range(lx*r)):
                     #print(f"OK {r=} {y=} {x=} {sy=} {sx=}")
                     for dy in range(ly*r):
                         for dx in range(lx*r):
