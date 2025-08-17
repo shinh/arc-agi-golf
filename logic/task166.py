7132660c18041f3de2a828d9e13a7fb933d7f3af
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-18 02:15:45 +0900

    Golf 166

--- logic/task166.py
+++ logic/task166.py
@@ -1,3 +1,8 @@
 # fill rectangle enclosed by 8s with 2
-p=lambda g:[[(v or((t:=[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v>7])and min(i for i,j in t)<=i<=max(i for i,j in t)and min(j for i,j in t)<=j<=max(j for i,j in t))*2)for j,v in enumerate(r)]for i,r in enumerate(g)]
-
+# Far from the best. hmm.
+def p(g):
+    mask=[0]*len(g[0])
+    for r in g:
+        mask=[c|m for c,m in zip(r,mask)]
+    g=[[[c,2][8 in r and c<1 and m>0]for c,m in zip(r,mask)]for r in g]
+    return g
