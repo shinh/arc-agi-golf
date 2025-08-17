096cd82bfba7a528dd916ec506e37b6c0e7d2182
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-17 21:57:53 +0900

    Golf 299

--- logic/task299.py
+++ logic/task299.py
@@ -1,3 +1,3 @@
-def p(g):# cross row of 2 and column of 8
- i=sum(g,[]).index;s=i(2)//6;c=i(8)%6;g=range(6)
- return[[y==s and 2<<(x==c) or 8*(x==c) for x in g]for y in g]
+# cross row of 2 and column of 8
+# There should be a better formula
+p=lambda g:[[[0,2,8,4][(g[0][x]>0)*2+(2 in r)] for x in range(6)]for r in g]
