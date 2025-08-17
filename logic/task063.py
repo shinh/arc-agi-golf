f83e807e40f4fdcaeeeb30c338d1713510639ef5
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-17 09:48:57 +0900

    golf task063

--- logic/task063.py
+++ logic/task063.py
@@ -1,8 +1,8 @@
 # fill rows/cols of zeros with 3
 def p(g):
- q=range;r=[*map(list,g)];t=q(1,len(g)-1)
- for i in t:
-  for j in t:
-   if sum(g[i][1:-1])*sum(g[k][j]for k in t)<1:r[i][j]=3
+ r=[*map(list,g)];q=range(1,len(g)-1)
+ for i in q:
+  for j in q:
+   if sum(g[i][1:-1])*sum(g[k][j]for k in q)<1:r[i][j]=3
  return r
 
