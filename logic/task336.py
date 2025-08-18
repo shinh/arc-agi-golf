bc45ffad46d44966a6e212383034161926e48781
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-19 07:15:28 +0900

    golf task336

--- logic/task336.py
+++ logic/task336.py
@@ -1,12 +1,7 @@
-def p(g):
-    for o in range(4):
+def p(g):#fill gaps after 5s in any direction
+    for _ in[0]*4:
         for r,nr in zip(g,g[1:]):
-            n=r.count(5)
-            if n:
-                s=r.index(5)+1
-                e=[9-r[::-1].index(5),10][n<2 and nr.index(5)+1==s]
-                if n<3:
-                    r[s:e]=[8]*(e-s)
-
+            if 0<(n:=r.count(5))<3:
+                a=r.index(5);e=(9-r[::-1].index(5),10)[n<2and nr.index(5)==a];r[a+1:e]=[8]*(e-a-1)
         g=[*map(list,zip(*g[::-1]))]
     return g
