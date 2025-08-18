23909c949e2c39fffb9085a15b7f86f07d686c2a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-18 23:50:15 +0900

    Golf 102

--- logic/task102.py
+++ logic/task102.py
@@ -3,6 +3,7 @@ def p(g):
     for s in range(2,12):
         for y in range(12-s):
             for x in range(12-s):
-                 if all(g[y][x+i]==g[y+s][x+i]==g[y+i][x]==g[y+i][x+s]==5 for i in range(s+1))*all(sum(g[y+i][x+1:x+s])<1 for i in range(1,s)):
+                #if all([g[y][x+i]==g[y+s][x+i]==g[y+i][x]==g[y+i][x+s]==5 for i in range(s+1)]+[sum(g[y+i][x+1:x+s])<1 for i in range(1,s)]):
+                if all((g[y][x+i]==g[y+s][x+i]==g[y+i][x]==g[y+i][x+s]==5)*(i%s<1or sum(g[y+i][x+1:x+s])<1)for i in range(s+1)):
                     for r in g[y+1:y+s]:r[x+1:x+s]=[2]*(s-1)
     return g
