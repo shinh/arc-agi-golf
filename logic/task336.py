1ed7d84308d7fcfc8219ae3ed5a747a53058014a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-19 00:16:01 +0900

    Golf 336

--- logic/task336.py
+++ logic/task336.py
@@ -1,20 +1,12 @@
 def p(g):
     for o in range(4):
-        for y in range(10):
-            for x in range(10):
-                if g[y][x]==5:
-                    ey,ex=y,x
-                    while ey<10and g[ey][x]==5:ey+=1
-                    while ex<10and g[y][ex]==5:ex+=1
-                    ey-=1
-                    for fy in range(y+1,ey):
-                        for fx in range(x+1,ex-1):
-                            g[fy][fx]=8
-                    if g[ey][x:ex].count(0)==1:
-                        #print('zzz',g[ey][x:ex])
-                        x=g[ey].index(0,x)
-                        for fy in range(ey,10):
-                            g[fy][x]=8
+        for r,nr in zip(g,g[1:]):
+            n=r.count(5)
+            if n:
+                s=r.index(5)+1
+                e=[9-r[::-1].index(5),10][n<2 and nr.index(5)+1==s]
+                if n<3:
+                    r[s:e]=[8]*(e-s)
 
         g=[*map(list,zip(*g[::-1]))]
     return g
