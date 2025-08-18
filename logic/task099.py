9afa415a20f4d211777d6d498a16dc0a174c238b
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-19 07:12:42 +0900

    refactor task099 solution

--- logic/task099.py
+++ logic/task099.py
@@ -1,15 +1,19 @@
 def p(g):
- # flood-fill rectangles framed by 1's with inner color
+ # expand color cells to their surrounding frame of 1's
  for y in range(10):
   for x in range(10):
-   if g[y][x]==1:
-    q=[(y,x)];g[y][x]=-1;u=d=y;l=r=x
-    for i,j in q:
-     for n,m in((i+1,j),(i-1,j),(i,j+1),(i,j-1)):
-      if 0<=n<10>m>-1 and g[n][m]==1:
-       g[n][m]=-1;q+=[(n,m)];u=min(u,n);d=max(d,n);l=min(l,m);r=max(r,m)
-    c=max(max(R[l:r+1])for R in g[u:d+1])
+   c=g[y][x]
+   if c>1:
+    l=x
+    while g[y][l]!=1:l-=1
+    r=x
+    while g[y][r]!=1:r+=1
+    u=y
+    while u and 1 in g[u-1][l:r+1]:u-=1
+    d=y
+    while d<9 and 1 in g[d+1][l:r+1]:d+=1
     for i in range(u and u-1,d+1):
-     R=g[i];R[l:r+1]=[v<0 or v and v or c for v in R[l:r+1]]
+     R=g[i]
+     for j in range(l,r+1):
+      if R[j]!=1:R[j]=c
  return g
-
