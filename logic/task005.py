ed6839fd30c4a34e3ff67ab5736e7f67494805b7
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-18 22:15:27 +0900

    Refactor task005 solution with 3x3 patch search

--- logic/task005.py
+++ logic/task005.py
@@ -1,25 +1,25 @@
 def p(g):
- # repeat largest blob toward hint
- n=21;D=-1,0,1;E=[(a,b)for a in D for b in D if a|b];e=max(g[0]);t=[r[:]for r in g];B=[];R=range(n)
- def f(y,x):
-  if-1<y<n>-1<x and t[y][x]==c:
-   t[y][x]=e;S.append((y,x))
-   for a,b in E:f(y+a,x+b)
- for y in R:
-  for x in R:
-   c=t[y][x]
-   if c-e:S=[];f(y,x);len(S)>len(B)and(B:=S)
- Y,X=zip(*B);h=max(Y)+1-min(Y);w=max(X)+1-min(X)
- for dy,dx in E:
-  sy=(h+1)*dy;sx=(w+1)*dx;C=e
-  for y,x in B:
-   u=y+sy;v=x+sx
-   if-1<u<n and-1<v<n and g[u][v]-e:C=g[u][v];break
-  k=0
-  while C-e:
-   k+=1;f=0
-   for y,x in B:
-    u=y+sy*k;v=x+sx*k
-    if-1<u<n and-1<v<n:g[u][v]=C;f=1
-   if f<1:break
+ # repeat biggest 3x3 block indicated by hints
+ e=max(g[0]);B=[];m=0
+ for y in range(19):
+  for x in range(19):
+   t=[]
+   for i in range(3):
+    for j in range(3):
+     if g[y+i][x+j]!=e:t.append((y+i,x+j))
+   if len(t)>m:m=len(t);B=t
+ for dy in -1,0,1:
+  for dx in -1,0,1:
+   if dy|dx:
+    sy=4*dy;sx=4*dx;c=e
+    for y,x in B:
+     u=y+sy;v=x+sx
+     if 0<=u<21 and 0<=v<21 and g[u][v]!=e:c=g[u][v];break
+    k=0
+    while c!=e:
+     k+=1;f=0
+     for y,x in B:
+      u=y+sy*k;v=x+sx*k
+      if 0<=u<21 and 0<=v<21:g[u][v]=c;f=1
+     if f<1:break
  return g
