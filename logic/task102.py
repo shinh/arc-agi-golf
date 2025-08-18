019fadff0ee3f6eddd88e608e6dc6319fa4efdb0
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-18 22:16:14 +0900

    Rewrite task102 solution

--- logic/task102.py
+++ logic/task102.py
@@ -1,13 +1,8 @@
 def p(g):
  # fill square holes
- for y in range(12):
-  for x in range(12):
-   if g[y][x]<1:
-    q=[(y,x)];g[y][x]=1
-    for y1,x1 in q:
-     for y2,x2 in((y1-1,x1),(y1+1,x1),(y1,x1-1),(y1,x1+1)):
-      if-1<y2<12 and-1<x2<12 and g[y2][x2]<1:g[y2][x2]=1;q+=[(y2,x2)]
-    ys,xs=zip(*q);a=min(ys);b=max(ys);c=min(xs);d=max(xs)
-    c=2 if b-a==d-c==len(q)**.5-1 and all(g[a-1][i]==g[b+1][i]==5 for i in range(c,d+1)) and all(g[i][c-1]==g[i][d+1]==5 for i in range(a,b+1)) else 0
-    for y1,x1 in q:g[y1][x1]=c
+ for s in range(2,12):
+  for y in range(12-s):
+   for x in range(12-s):
+    if all(g[y][x+i]==g[y+s][x+i]==g[y+i][x]==g[y+i][x+s]==5 for i in range(s+1)) and all(sum(g[y+i][x+1:x+s])<1 for i in range(1,s)):
+     for r in g[y+1:y+s]:r[x+1:x+s]=[2]*(s-1)
  return g
