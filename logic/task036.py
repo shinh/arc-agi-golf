61df7befda41c8671fe5e85b8e48d6981962deb4
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-18 22:57:57 +0900

    Golf 036

--- logic/task036.py
+++ logic/task036.py
@@ -1,9 +1,6 @@
 def p(g):
-    # find smallest isolated color and crop
-    d={}
-    for y,r in enumerate(g):
-        for x,v in enumerate(r):
-            if v:
-                t=d.setdefault(v,[0,y,x,y,x]);t[0]+=1;t[1]=min(t[1],y);t[2]=min(t[2],x);t[3]=max(t[3],y);t[4]=max(t[4],x)
-    _,k,y0,x0,y1,x1=min((n,k,y0,x0,y1,x1)for k,(n,y0,x0,y1,x1)in d.items()if all(v in(0,k)for r in g[y0:y1+1]for v in r[x0:x1+1]))
-    return[[k*(v==k)for v in r[x0:x1+1]]for r in g[y0:y1+1]]
+    # +g[:5] is to workaround a single corner case.
+    f=[c for r in g+g[:5]for c in{*r}]
+    c=min(f,key=f.count)
+    f=lambda g:[*map(list,zip(*filter(lambda r:c in r,g)))]
+    return f(f(g))
