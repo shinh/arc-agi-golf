ce08caa1b3942f9095ca337d9030acf51bef471b
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-20 07:13:15 +0900

    shorten task394 period detection

--- logic/task394.py
+++ logic/task394.py
@@ -3,5 +3,5 @@ def p(g,p=1):
     # detect repeat and fill zero box
     for r in[*zip(*g)]+g:
         if 0not in r:
-            while(r[:p]*9)[:len(r)]!=r:p+=1
+            while r[:-p]!=r[p:]:p+=1
     return[[g[y%p][x%p]or g[y%p][x%p+p]for x,c in enumerate(r)if c<1]for y,r in enumerate(g)if 0in r]
