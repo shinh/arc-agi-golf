232c8ee43ee2dd5e9555d38ac714578dd12b22d2
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-20 04:00:36 +0900

    Golf 394

--- logic/task394.py
+++ logic/task394.py
@@ -1,9 +1,7 @@
-# 210
-def p(g):
+# 186
+def p(g,p=1):
     # detect repeat and fill zero box
-    p=1
-    for r in g+[*zip(*g)]:
-        if len({*r})>1>r.count(0):
+    for r in[*zip(*g)]+g:
+        if 0not in r:
             while(r[:p]*9)[:len(r)]!=r:p+=1
-            break
     return[[g[y%p][x%p]or g[y%p][x%p+p]for x,c in enumerate(r)if c<1]for y,r in enumerate(g)if 0in r]
