208cca5ec3b8225b6367ec700a9c6b359a6eec97
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-20 08:55:48 +0900

    Golf 098

--- logic/task098.py
+++ logic/task098.py
@@ -1,2 +1,3 @@
+# 121
 # boundary via neighbor xor
-p=lambda g,z=zip:(h:=[0]*len(g[0]),[[a*((a^b|a^c|a^d|a^e)>0)for a,b,c,d,e in z(r,r[1:]+[0],[0]+r,u,d)]for r,u,d in z(g,g[1:]+[h],[h]+g)])[1]
+p=lambda g,h=[[0]*99]:[[a*(a^b|a^c|a^d|a^e>0)for a,b,c,d,e in zip(r,r[1:]+[0],[0]+r,u,d)]for r,u,d in zip(g,g[1:]+h,h+g)]
