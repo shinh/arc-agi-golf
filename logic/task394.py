93986f44d966c2939c5f2969438155919ba3c76f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-20 03:58:25 +0900

    golf task394 solution

--- logic/task394.py
+++ logic/task394.py
@@ -2,12 +2,8 @@
 def p(g):
     # detect repeat and fill zero box
     p=1
-    for o in"00":
-        for r in g:
-            if len({*r})>1 and 0 not in r:
-                for p in range(1,9):
-                    if all(c==r[x%p]for x,c in enumerate(r)):
-                        break
-                break
-        g=[*map(list,zip(*g))]
+    for r in g+[*zip(*g)]:
+        if len({*r})>1>r.count(0):
+            while(r[:p]*9)[:len(r)]!=r:p+=1
+            break
     return[[g[y%p][x%p]or g[y%p+p][x%p+p]for x,c in enumerate(r)if c<1]for y,r in enumerate(g)if 0in r]
