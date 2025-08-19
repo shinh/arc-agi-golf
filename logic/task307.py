feea75eedf1f07906c54bf7345df692db5986aef
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-19 09:17:32 +0900

    Golf 307

--- logic/task307.py
+++ logic/task307.py
@@ -1,2 +1,3 @@
-p=lambda g:eval("[[g "+"for g in g for _ in[0,1]]"*2)
+# duplicate rows and columns
+p=lambda g:eval("[[g "+"for g in g for _ in'00']"*2)
 #p=lambda g:[[g for g in g for _ in[0,1]]for g in g for _ in[0,1]]
