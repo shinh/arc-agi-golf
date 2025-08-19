c3730c87bb8ff1b3b3ecb81a21a6b58580a92866
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-19 09:48:48 +0900

    Golf 008

--- logic/task008.py
+++ logic/task008.py
@@ -1,9 +1,2 @@
 import re
-def p(g):
-    for o in range(40):
-        #print(g,"2, 8"not in str(g),re.(r"2, 0[,0 ]+8",str(g)))
-        if re.search(r"2, 0[,0 ]+8",str(g))and"2, 8"not in str(g):
-            g=[[[0,8,2,8][(c>7)+(p==2)*2]for c,p in zip(r,[0]+r)]for r in g]
-
-        g=[*map(list,zip(*g[::-1]))]
-    return g
+p=lambda g,n=39:-n*g or p([*map(list,zip(*[[[[0,8,2,8][(c>7)+(p==2)*2],c][not re.search(r"2, 0[,0 ]+8",s:=str(g))or"2, 8"in s]for c,p in zip(r,[0]+r)]for r in g][::-1]))],n-1)
