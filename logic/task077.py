38da47a7c7b6283c8170ca6fc3fba18d13ac58b3
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-20 22:36:55 +0900

    Golf 77

--- logic/task077.py
+++ logic/task077.py
@@ -1,8 +1,6 @@
 def p(g):
-    # color of rarest pixel
-    f=sum(g,[]);c=min(f,key=f.count)
-    # positions of that color
-    s={(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==c};o={*s}
+    # positions of the red
+    s={(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v==2};o={*s}
     # fill bounding boxes of clusters
     while s:
         q=[s.pop()]
@@ -10,9 +8,9 @@ def p(g):
             for Y in range(y-2,y+3):
                 for X in range(x-2,x+3):
                     p=(Y,X)
-                    if p in s:s.remove(p);q+=p,
+                    if p in s:s-={p};q+=p,
         A,B=zip(*q);a=min(A);b=max(A)+1;d=min(B);e=max(B)+1
         for Y in range(a,b):g[Y][d:e]=[4]*(e-d)
-    for y,x in o:g[y][x]=c
+    for y,x in o:g[y][x]=2
     return g
 
