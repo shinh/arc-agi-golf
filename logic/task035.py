34df9722074018e985c5b28ef25b0cafcbfa91f8
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-21 00:35:51 +0900

    Golf 35

--- logic/task035.py
+++ logic/task035.py
@@ -1,11 +1,7 @@
 def p(g):
-    # replace 8 with closest color along cross
-    # scan column and row slices sequentially
-    z=[*zip(*g)];o=[*map(list,g)]
-    for y,r in enumerate(g):
-        for x,v in enumerate(r):
-            if v==8:
-                for t in z[x][y+1:],z[x][:y][::-1],r[x+1:],r[:x][::-1]:
-                    for v in t:
-                        if v:o[y][x]=v-8 and v or o[y][x];break
-    return o
+    for o in[0]*4:
+        for r in g:
+            if r[0]:
+                r[r.index(8)]=r[0]
+        g=[*map(list,zip(*g[::-1]))]
+    return g
