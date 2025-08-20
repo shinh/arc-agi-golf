2888b70a1b50a29e61ae4e1b22ff0a50cbf69e6b
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-20 13:20:09 +0900

    Golf task100

--- logic/task100.py
+++ logic/task100.py
@@ -1,2 +1,7 @@
-# pick most frequent nonzero
-p=lambda g:[[max(range(1,10),key=sum(g,[]).count)]*2]*2#ignore0
+#max bbox color
+def p(g):
+ s=''.join(map(str,sum(g,[])));w=len(g[0]);m=k=0
+ for c in'123456789':
+  a=s.find(c);b=s.rfind(c);a+1 and(t:=(b%w-a%w+1)*(b//w-a//w+1))>m and(m:=t,k:=int(c))
+ return [[k]*2]*2
+
