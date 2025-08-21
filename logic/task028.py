673d91d447715e7ad2300aa62c3d76c450ded345
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 08:03:54 +0900

    golf task028

--- logic/task028.py
+++ logic/task028.py
@@ -1,1 +1,1 @@
-def p(g,m=max,R=range(10)):e,a=m(map(m,g[:5])),m(map(m,g[5:]));return[[(c%9<1)|645>>r&1 and[e,a][r>4]for c in R]for r in R]# draw cross using max colors from halves
+def p(g,R=range(10)):f=sum(g,[]);return[[(c%9<1)|645>>r&1 and[max(f[:50]),max(f[50:])][r>4]for c in R]for r in R]# draw cross using max colors from halves
