406d144cafb5d2e1878436c1876854cec7dd4923
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 07:54:26 +0900

    golf task055

--- logic/task055.py
+++ logic/task055.py
@@ -1,6 +1,6 @@
-def p(g,d=0,b='020463010'):
+def p(g,d=0,b=b'020463010'):
  # fill empty cells from template using counts of dividers
  for r in g:
   d+=r[0]>0;x=0
-  r[:]=[(x:=x+(v>0))and v or int(b[d*3+x])for v in r]
+  r[:]=[(x:=x+(v>0))and v or b[d*3+x]^48for v in r]
  return g
