abf3358e631668d632f600af4c88f1c5a82d4e73
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 07:54:54 +0900

    Golf task046 using tuple-friendly approach

--- logic/task046.py
+++ logic/task046.py
@@ -1,17 +1,11 @@
+# glue blocks together by matching gray pins
 def p(g):
-    # glue blocks together by matching gray pins
-    o=[[]for _ in g];v=0;s=[]
-    for col in (*zip(*g),0):
-        if col==(0,0,0) or col==0:
-            if s:
-                b=list(zip(*s))
-                c=max(n for r in b for n in r if n%5)
-                t=[r[0]for r in b];l=5 in t and t.index(5)
-                t=[r[-1]for r in b];r=5 in t and t.index(5)
-                d=l-v;w=len(b[0])
-                for y in range(3):
-                    f=y+d
-                    o[y]+=[c if n==5 else n for n in b[f]] if 0<=f<3 else [0]*w
-                v+=r-l;s=[]
-        else:s.append(col)
+    o=[[],[],[]];v=0;s=[]
+    for c in(*zip(*g),0):
+        if c and any(c):s+=c,
+        elif s:
+            c=max(n for t in s for n in t if n%5);l=5 in s[0] and s[0].index(5);r=5 in s[-1] and s[-1].index(5);d=l-v;w=len(s)
+            for y in 0,1,2:
+                f=y+d;o[y]+=0<=f<3 and [t[f]==5 and c or t[f]for t in s] or [0]*w
+            v+=r-l;s=[]
     return o
