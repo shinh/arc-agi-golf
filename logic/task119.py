973a94e83e32fbfc69e0df8db7dc192d88508da6
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-21 13:07:04 +0900

    Golf 119

--- logic/task119.py
+++ logic/task119.py
@@ -1,17 +1,15 @@
 # reflect diagonal from 8s off 2s
 def p(g):
- z=lambda y,x:-1<x<12>y>-1 and g[y][x]
- for a in 1,-1:
-  for b in 1,-1:
-   f=0;n=[*map(list,g)]
-   for i in range(12):
-    for x in range(12):
-     y=i
-     if g[y][x]&z(y-a,x-b)&8:
-      y+=a;x+=b
-      while-1<x<12>y>-1:
-       if g[y][x]==2:f=1;y-=a;x-=b;z(y+a-1,x+b)&z(y+a+1,x+b)&2and(b:=-b)or(a:=-a)
-       elif g[y][x]:break
-       else:n[y][x]=3;y+=a;x+=b
-   if f:g=n
- return g
+    for o in range(8):
+        for m in range(2):
+            for y in range(10):
+                for x in range(10):
+                    if g[y][x]in(3,8)and g[y+1][x+1]in(3,8):
+                        c=g[y+2][x+2]
+                        if c==2 and g[y+2][x+1]==2 and g[y][x+2]==0:
+                            g[y][x+2]=3
+                        if c==0:
+                            g[y+2][x+2]=3
+            g=[*map(list,zip(*g))]
+        g=[*map(list,zip(*g[::-1]))]
+    return g
