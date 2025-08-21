ecb08d8c9739d3aa21b7b1d2293433e37d46fe9a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 08:04:31 +0900

    golf task022

--- logic/task022.py
+++ logic/task022.py
@@ -1,3 +1,3 @@
-def p(g):
-    #3x3 union around 5s
-    a=-1,0,1;e=enumerate;return[[max(g[y+i][x+j]for y,r in e(g)for x,v in e(r)if v==5)for j in a]for i in a]
+#3x3 5-union
+p=lambda g,a=(-1,0,1),e=enumerate:[[max(g[y+i][x+j]for y,r in e(g)for x,v in e(r)if v==5)for j in a]for i in a]
+
