a059cf86b3ed8f568241285d4d086ac1955ba651
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 01:01:36 +0900

    golf task036

--- logic/task036.py
+++ logic/task036.py
@@ -5,4 +5,4 @@
 #     return(f:=lambda x:[*map(list,zip(*filter(lambda r:min(f:=[c for r in g+g[:5]for c in{*r}],key=f.count)in r,x)))])(f(g))
 #     #return[*map(list,eval('zip(*filter(lambda r:min(f:=[c for r in g+g[:5]for c in{*r}],key=f.count)in r,'*2+'g))))'))]
 
-p=lambda g:(f:=lambda x:[*map(list,zip(*filter(lambda r:min(f:=[c for r in g+g[:5]for c in{*r}],key=f.count)in r,x)))])(f(g))
+p=lambda g:(f:=lambda x:[*zip(*[r for r in x if min(f:=[c for r in g+g[:5]for c in{*r}],key=f.count)in r])])(f(g))
