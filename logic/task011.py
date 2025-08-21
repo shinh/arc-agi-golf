09895b9faf901d6c3c4ecb91bd3c2d9dc11cec38
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 08:08:07 +0900

    golf task011

--- logic/task011.py
+++ logic/task011.py
@@ -1,7 +1,7 @@
 def p(j):
- A=range;t=0,4,8
+ B=range(11);t=0,4,8
  for r in t:
   for s in t:
    # expand 3x3 block & add lines
-   if sum(j[r+W][s+l]<1for W in A(3)for l in A(3))>4:
-    return[[5*(3in(i%4,c%4))or j[r+i//4][s+c//4]for c in A(11)]for i in A(11)]
+   if sum(j[r+w//3][s+w%3]<1for w in range(9))>4:
+    return[[5*(3in(i%4,c%4))or j[r+i//4][s+c//4]for c in B]for i in B]
