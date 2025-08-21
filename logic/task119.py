ed5bbcad7b963cc3043fcf16c5ce57bcfc61519d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 00:36:08 +0900

    Golf task119

--- logic/task119.py
+++ logic/task119.py
@@ -3,12 +3,9 @@ def p(g):
     for o in range(16):
         for y in range(10):
             for x in range(10):
-                if g[y][x]in(3,8)and g[y+1][x+1]in(3,8):
+                if g[y][x]>2<g[y+1][x+1]:
                     c=g[y+2][x+2]
-                    if c==2 and g[y+2][x+1]==2 and g[y][x+2]==0:
-                        g[y][x+2]=3
-                    if c==0:
-                        g[y+2][x+2]=3
-        if o%2:g=zip(*g[::-1])
-        g=[*map(list,zip(*g))]
+                    if c==g[y+2][x+1]==2>g[y][x+2]:g[y][x+2]=3
+                    if c<1:g[y+2][x+2]=3
+        g=g[::-1]if o%2 else[*map(list,zip(*g))]
     return g
