153d19b9832e2df98d1cf189c3d4ad8c1e6d3d33
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 07:53:55 +0900

    Golf task059

--- logic/task059.py
+++ logic/task059.py
@@ -3,11 +3,9 @@ def p(g):
     r=0,1,2;s=0,4,8;m=0
     for y in s:
         for x in s:
-            t=[a for i in r for j in r if (a:=g[y+i][x+j])%5]
-            m=max(m,len(t))
+            t=[a for i in r for j in r if (a:=g[y+i][x+j])%5];m=max(m,len(t))
     for y in s:
         for x in s:
-            t=[a for i in r for j in r if (a:=g[y+i][x+j])%5]
-            k=(len(t)==m)*sum(t[:1])
-            for i in r:g[y+i][x:x+3]=[k]*3
+            t=[a for i in r for j in r if (a:=g[y+i][x+j])%5];k=(len(t)==m)*sum(t[:1])
+            for i in r:g[y+i][x:x+3]=k,k,k
     return g
