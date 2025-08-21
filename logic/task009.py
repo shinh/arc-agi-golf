dc074874f170374cf7641efe3f1e2fbe8aaf146b
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 08:08:32 +0900

    golf task009

--- logic/task009.py
+++ logic/task009.py
@@ -1,12 +1,10 @@
-def p(g,R=range,E=enumerate):
+def p(g,E=enumerate):
  # connect same-colored dots
- C=g[0][2];o=[[0]*len(g[0])for _ in g]
+ C=g[0][2];o=[[0]*len(r)for r in g]
  for _ in 0,1:
   for y,r in E(g):
    for x,v in E(r):
     if v==C:o[y][x]=v
-    elif v:
-     try:X=r.index(v,x+1);o[y][x:X+1]=[v]*(X+1-x)
-     except:0
-  g=[*zip(*g)];o=[list(r)for r in zip(*o)]
+    elif v in r[x+1:]and v:X=r.index(v,x+1)+1;o[y][x:X]=[v]*(X-x)
+  g=[*zip(*g)];o=[*map(list,zip(*o))]
  return o
