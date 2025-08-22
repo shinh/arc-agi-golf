597e307301f57e2106162a47edce8bac8396f8aa
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 08:49:29 +0900

    golf task356

--- logic/task356.py
+++ logic/task356.py
@@ -1,14 +1,10 @@
-def p(g,r=range,E=enumerate):
+def p(g):
  # link
- d=[]
- for y,R in E(g):
-  for x,v in E(R):
-   if v:
-    for Y,X in d:
-     if y==Y:
-      for u in r(min(x,X),max(x,X)+1):R[u]=8
-     elif x==X:
-      for u in r(min(y,Y),max(y,Y)+1):g[u][x]=8
-    d+=[(y,x)]
- return g
-
+ h=[*map(list,g)]
+ for _ in 0,1:
+  for r,H in zip(g,h):
+   t=[i for i,v in enumerate(r)if v]
+   if t[1:]:b=t[-1];H[t[0]:b+1]=[8]*-~(b-t[0])
+  g=[*zip(*g)]
+  h=[*map(list,zip(*h))]
+ return h
