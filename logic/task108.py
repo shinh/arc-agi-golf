3c6853054213001953bcfedcc63e0d69f0ede595
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 17:55:58 +0900

    golf task108

--- logic/task108.py
+++ logic/task108.py
@@ -1,3 +1,2 @@
-def p(g,t=(0,2,4,6,8)):
-    # get max of each 2x2 block then upscale by 4
-    return sum([[sum([[max(g[i][j:j+2]+g[i+1][j:j+2])]*4 for j in t],[])]*4 for i in t],[])
+# get max of each 2x2 block then upscale by 4
+p=lambda g,t=(0,2,4,6,8):sum(((sum(((max(g[i][j:j+2]+g[i+1][j:j+2]),)*4 for j in t),()),)*4 for i in t),())
