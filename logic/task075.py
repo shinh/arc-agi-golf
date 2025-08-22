1378c304ca5c28680dcab6bddcc6dbf8f0cf1381
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 18:34:58 +0900

    golf task075

--- logic/task075.py
+++ logic/task075.py
@@ -1,8 +1,9 @@
 def p(g):
  # stamp top-left 3x3 block around each 1
- b=[r[:3]for r in g[:3]]
+ b=g[:3]
  for y,r in enumerate(g):
   for x,v in enumerate(r):
    if v==1:
-    for dy in 0,1,2:g[y+dy-1][x-1:x+2]=b[dy]
+    for dy in 0,1,2:g[y+dy-1][x-1:x+2]=b[dy][:3]
  return g
+
