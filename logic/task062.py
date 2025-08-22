c043ac961001fecab0b62767c671ff2bb4d50e2f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 17:48:12 +0900

    Golf task062: 2242->2245

--- logic/task062.py
+++ logic/task062.py
@@ -1,12 +1,12 @@
 def p(g):
     # mirror shape across line of 2s
     o=[[3]*10 for _ in g]
-    Y=[];X=[];t=[]
+    Y=X=()
     for y,r in enumerate(g):
         for x,v in enumerate(r):
-            if v==2:t+=x,;z=y
+            if v==2:t=x;z=y
             elif v:Y+=y,;X+=x,;c=v
     v=min(Y)<=z<=max(Y)
-    a=(t[0]+(max,min)[t[0]<min(X)](X),z+(max,min)[z<min(Y)](Y))[not v]
+    a=(t+(max,min)[t<min(X)](X),z+(max,min)[z<min(Y)](Y))[not v]
     for y,x in zip(Y,X):o[y][x]=o[[y,a-y][not v]][[x,a-x][v]]=c
     return o
