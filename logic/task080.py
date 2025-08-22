48b412d9a2e15e56fa2328c05e855a500f2de2ec
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 18:33:20 +0900

    golf task080

--- logic/task080.py
+++ logic/task080.py
@@ -1,15 +1,14 @@
 def p(g):
  m=len(g);n=len(g[0]);R=range
- a=next(i+1 for i in R(1,m)if len(set(g[i]))==1)
- b=next(j+1 for j in R(1,n)if len({r[j]for r in g})==1)
- B=[r[::b]for r in g[::a]];h=len(B);w=len(B[0]);d=1,0,-1,0,1
+ a=next(i+1 for i in R(1,m)if len({*g[i]})<2)
+ b=next(j+1 for j in R(1,n)if len({r[j]for r in g})<2)
+ B=[r[::b]for r in g[::a]];h=len(B);w=len(B[0])
  for y in R(h):
   for x in R(w):
    if B[y][x]:
     q=[(y,x)];l=B[y][x],
     for y,x in q:
-     for k in R(4):
-      Y,X=y+d[k],x+d[k+1]
+     for Y,X in((y+1,x),(y-1,x),(y,x+1),(y,x-1)):
       if h>Y>-1<X<w and B[Y][X]and(Y,X)not in q:q+=[(Y,X)];l+=B[Y][X],
     if len({*l})>1:break
   else:continue
@@ -23,5 +22,5 @@ def p(g):
     for k,dy,dx in P:
      Y,X=y+dy,x+dx
      if h>Y>-1<X<w:B[Y][X]=k
- return[[B[i//a][j//b]if(-~i%a)*(-~j%b) else g[i][j]for j in R(n)]for i in R(m)]
+ return[[B[i//a][j//b]if(~i%a)*(~j%b)else g[i][j]for j in R(n)]for i in R(m)]
 
