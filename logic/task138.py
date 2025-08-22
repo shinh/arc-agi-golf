7521487ec34818a178418699f6181548e446fcf3
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 19:11:22 +0900

    golf task138

--- logic/task138.py
+++ logic/task138.py
@@ -1,2 +1,3 @@
-# 118
-p=lambda g,t=79:-t*g or p([[([*r[:r.index(r[-1])]]+[r[-1]]*99)[:len(r)],r][t>4]for r in zip(*g[0in g[0]:][::-1])],t-1)
+# 114
+# rotate crop and fill from borders
+p=lambda g,t=79:-t*g or p([((r[:r.index(r[-1])]+r[-1:]*99)[:len(r)],r)[t>4]for r in zip(*g[0in g[0]:][::-1])],t-1)
