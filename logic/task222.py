e212b8015f00bb4cf49dd08f7a99ef33dfecea34
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 20:54:38 +0900

    golf task222

--- logic/task222.py
+++ logic/task222.py
@@ -6,7 +6,7 @@ def p(g):
             if c:=g[y0][x0]:
                 for y1 in range(y0+1,17):
                     for x1 in range(x0+1,17):
-                        if all(r[x0:x1]==[c]*(w:=x1-x0)for r in g[y0:y1]) and (a:=(y1-y0)*w)>B:
-                            B=a;o=create(16,16)
-                            for y in range(y0,y1):o[y][x0:x1]=[c]*w
+                        if all(r[x0:x1]==[c]*(w:=x1-x0)for r in g[y0:y1])and B<(y1-y0)*w:
+                            B=(y1-y0)*w;o=create(16,16)
+                            for y in range(y0,y1):o[y][x0:x1]=(c,)*w
     return o
