4d8479791fb01d4ebdb46b45c743b372613c18ec
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 20:46:14 +0900

    golf task184

--- logic/task184.py
+++ logic/task184.py
@@ -1,12 +1,12 @@
 def p(g):
  # merge blocks separated by empty rows/cols, keep first colors
- a=sum;R=[];k=m=0
- for r in g:R+=k,;k+=a(r)<1;m=k+1 if a(r)else m
- C=[];k=n=0
- for c in zip(*g):C+=k,;k+=a(c)<1;n=k+1 if a(c)else n
- o=[[0]*n for _ in[0]*m]
- for i,r in enumerate(g):
-  for j,v in enumerate(r):
-   if v and not o[R[i]][C[j]]:o[R[i]][C[j]]=v
+ a=sum;C=[];k=n=0
+ for c in zip(*g):C+=k,;k+=a(c)<1;a(c)and(n:=k+1)
+ o=[];k=0
+ for r in g:
+  if a(r):
+   o+=[[0]*n]*(k==len(o))
+   for c,v in zip(C,r):o[k][c]+=v*(o[k][c]<1)
+  else:k+=1
  return o
 
