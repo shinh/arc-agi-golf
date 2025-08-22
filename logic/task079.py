ca71806101201b902d84852b8a6149be8c9e7680
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 18:31:36 +0900

    golf task079, score 2273

--- logic/task079.py
+++ logic/task079.py
@@ -1,4 +1,4 @@
 def p(g):#mode of 3x3 centers with color confined in 5x5
-    g=[[0]*16]+[[0]+r+[0]for r in g]+[[0]*16]
-    m=[a for y in range(12)for x in range(12)if(w:=[r[x:x+5]for r in g[y:y+5]])and(a:=[r[1:4]for r in w[1:4]])and len(s:=set(sum(a,[]))-{0})==1 and sum(w,[]).count(c:=s.pop())==sum(a,[]).count(c)]
+    g=[[0]*16,*[[0,*r,0]for r in g],[0]*16]
+    m=[a for y in range(12)for x in range(12)if(c:=max(t:=sum(a:=[r[x+1:x+4]for r in g[y+1:y+4]],[])))and t.count(c)+t.count(0)>8 and sum(r[x:x+5].count(c)for r in g[y:y+5])==t.count(c)]
     return max(m,key=m.count)
