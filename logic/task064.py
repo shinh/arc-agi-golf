ef770a0f78db3a3dc809eeb5ccec5df3c8c35ea1
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 18:30:39 +0900

    golf task064

--- logic/task064.py
+++ logic/task064.py
@@ -1,11 +1,11 @@
 def p(g):
  # extend segments via rotations
- b=max(g[0],key=g[0].count)
+ b=max(r:=g[0],key=r.count)
  for _ in[0]*4:
   for r in g:
    for x,c in enumerate(r):
     if(c-b)*(x<1or r[x-1]==b)*(b in r[x+1:x+3]):
      for j,u in enumerate(r[x:-1],x):
-      if u==r[j+1]!=c!=b!=u:r[x:j]=[c]*(j-x);break
+      if b!=u==r[j+1]!=c:r[x:j]=[c]*(j-x);break
   g=[*map(list,zip(*g[::-1]))]
  return g
