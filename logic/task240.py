54ecdc0e309dd5072cb852f95c343948cbcede22
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 20:49:51 +0900

    golf task240 by unrolling loops

--- logic/task240.py
+++ logic/task240.py
@@ -3,10 +3,24 @@ def p(g,E=enumerate):
     for y,r in E(g):
         for x,c in E(r):
             if c:r[x]=g[18-y][x]=r[18-x]=g[18-y][18-x]=c
-    for _ in range(4):
-        for y,r in E(g):
-            for x,c in E(r):
-                if c and y+1<x<17-y and x%2:
-                    for j in range(x,17-y,2):r[j]=c
-        g=[*map(list,zip(*g[::-1]))]
+    for y,r in E(g):
+        for x,c in E(r):
+            if c and y+1<x<17-y and x%2:
+                for j in range(x,17-y,2):r[j]=c
+    g=[*map(list,zip(*g[::-1]))]
+    for y,r in E(g):
+        for x,c in E(r):
+            if c and y+1<x<17-y and x%2:
+                for j in range(x,17-y,2):r[j]=c
+    g=[*map(list,zip(*g[::-1]))]
+    for y,r in E(g):
+        for x,c in E(r):
+            if c and y+1<x<17-y and x%2:
+                for j in range(x,17-y,2):r[j]=c
+    g=[*map(list,zip(*g[::-1]))]
+    for y,r in E(g):
+        for x,c in E(r):
+            if c and y+1<x<17-y and x%2:
+                for j in range(x,17-y,2):r[j]=c
+    g=[*map(list,zip(*g[::-1]))]
     return g
