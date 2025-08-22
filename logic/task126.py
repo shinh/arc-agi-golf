4153b53fc236f136feb47c53b3646614678c94be
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 19:07:34 +0900

    golf task126 solution

--- logic/task126.py
+++ logic/task126.py
@@ -1,5 +1,4 @@
 def p(g):#arch->4
  for r,b in zip(g,g[1:]):
-  for j in range(1,len(r)-1):
-   if b[j-1]==b[j+1]==r[j]>b[j]<1:g[-1][j]=4
+  for j in range(1,len(r)-1):g[-1][j]|=4*(b[j-1]==b[j+1]==r[j]>b[j]<1)
  return g
