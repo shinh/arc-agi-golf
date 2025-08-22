7a8e1b2f1992d01677adaa884e71699ff794715f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 17:44:00 +0900

    refactor task120

--- logic/task120.py
+++ logic/task120.py
@@ -1,2 +1,2 @@
-# 117
-p=lambda g,z=[0]:[[[c,8][c and{c}==set(C)]for c,*C in zip(r,z+r,r[1:]+z,*R)]for r,*R in zip(g,[z*99]+g,g[1:]+[z*99])]
+# 114
+p=lambda g,z=[0]:[[[c,8][c and[c]*4==C]for c,*C in zip(r,z+r,r[1:]+z,*R)]for r,*R in zip(g,[z*99]+g,g[1:]+[z*99])]
