4156a74a8acee682bf9d3364077d5c7423a074cf
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 17:53:15 +0900

    refine task 098 solution

--- logic/task098.py
+++ logic/task098.py
@@ -1,3 +1,3 @@
-# 121
-# boundary via neighbor xor
-p=lambda g,h=[[0]*99]:[[a*(a^b|a^c|a^d|a^e>0)for a,b,c,d,e in zip(r,r[1:]+[0],[0]+r,u,d)]for r,u,d in zip(g,g[1:]+h,h+g)]
+# 118
+# boundary via neighbor set
+p=lambda g,h=[[0]*30]:[[a*({b,c,d,e}!={a})for a,b,c,d,e in zip(r,r[1:]+[0],[0]+r,u,d)]for r,u,d in zip(g,g[1:]+h,h+g)]
