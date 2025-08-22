c0cbff0e6f4a4f28ad62cf9abf183a1a6265d5b6
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 18:35:13 +0900

    golf task078

--- logic/task078.py
+++ logic/task078.py
@@ -1,4 +1,3 @@
 def p(g):
 # stack nonzero cells up
- h=len(g)
- return[list(r)for r in zip(*[((*filter(None,c),)+(0,)*h)[:h]for c in zip(*g)])]
+ return[*zip(*(sorted(c,key=0 .__eq__)for c in zip(*g)))]
