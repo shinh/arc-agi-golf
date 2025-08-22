f560833d3c97c47da609aa1c083612e4bcfd7d8d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 17:57:43 +0900

    golf task100

--- logic/task100.py
+++ logic/task100.py
@@ -1,6 +1,5 @@
 #bbox
 def p(g):
  s=str(sum(g,[]))[1::3];w=len(g[0])
- k=max(s,key=lambda c:c>'0'and~(a:=s.find(c))and((l:=s.rfind(c)-a)%w+1)*(l//w+1))
- return [[int(k)]*2]*2
+ return[[int(max(s,key=lambda c:(c>'0')*-~((l:=s.rfind(c)-s.find(c))%w)*-~(l//w)))]*2]*2
 
