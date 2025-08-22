25080aad8bd61ec70b85a54cc06ff7e1c2b95cab
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 19:06:22 +0900

    Golf task125

--- logic/task125.py
+++ logic/task125.py
@@ -1,12 +1,12 @@
 #paint8snear6
 def p(g):
-    for y in range(15):
-        for x in range(15):
-            if g[y][x]==6:
-                ey,ex=y,x
-                while g[ey][x]==6:ey+=1
-                while g[y][ex]==6:ex+=1
-                for fy in range(y-1,ey+1):
-                    for fx in range(x-1,ex+1):
-                        if g[fy][fx]==8:g[fy][fx]=3+(y<fy<ey)*(x<fx<ex)
-    return g
+ for y in range(15):
+  for x in range(15):
+   if g[y][x]==6:
+    Y,X=y,x
+    while g[Y][x]==6:Y+=1
+    while g[y][X]==6:X+=1
+    for i in range(y-1,Y+1):
+     for j in range(x-1,X+1):
+      if g[i][j]>7:g[i][j]=3+(y<i<Y)*(x<j<X)
+ return g
