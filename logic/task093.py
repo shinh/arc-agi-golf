cad4422f2bda2b522adf80dfce605daab512ce61
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 17:54:43 +0900

    golf: compress task093

--- logic/task093.py
+++ logic/task093.py
@@ -1,7 +1,8 @@
 def p(g):
     #show(g, "input")
     for _ in' '*4:
-        for r in g*(5in g[0]):sx=r.index(5);s=r[:sx].count(0);r[:sx]=[0]*s+[5]*(sx-s)
+        for r in g*(5in g[0]):j=r.index(5);r[:j]=sorted(5*(x>0)for x in r[:j])
         #show(ng, "crop")
         g=[*map(list,zip(*g[::-1]))]
     return g
+
