163c6df96adb525429a212eae8424979f9c34fa9
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 19:22:45 +0900

    golf task137

--- logic/task137.py
+++ logic/task137.py
@@ -1,7 +1,5 @@
 def p(g):
     # expand rare color rectangle outward
-    w=len(g[0]);a=sum(g,[]);c=min({*a},key=a.count)
-    y,x=zip(*(divmod(i,w)for i,v in enumerate(a)if v==c))
-    y=sorted(y);x=sorted(x);Y=y[1];X=x[1];s=Y-y[0]
-    for k in range(len(g)*w):i,j=divmod(k,w);g[i][j]=[g[i][j],c][max(abs(i-Y),abs(j-X))%s<1]
-    return g
+    w=len(g[0]);c=min(a:=sum(g,[]),key=a.count)
+    p=a.index(c);q=a.index(c,p+1);Y=q//w
+    return[[[r[j],c][max(i-Y,Y-i,j-q%w,q%w-j)%(Y-p//w)<1]for j in range(w)]for i,r in enumerate(g)]
