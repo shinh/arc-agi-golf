9c4f8b93d556e172ac8a724b500fa42006fea24b
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 08:51:26 +0900

    golf task348

--- logic/task348.py
+++ logic/task348.py
@@ -1,5 +1,2 @@
 # alternate columns expanding from marker
-def p(g):
- x=g[0].index(7);h=sum(r[x]>0 for r in g)
- for y in range(h):[len(g[0])>x+j>=0 and g[y].__setitem__(x+j,7+j%2)for j in range(y-h+1,h-y)]
- return g
+p=lambda g:(x:=g[0].index(7),h:=sum(r[x]>0 for r in g),[len(g[0])>x+j>=0 and g[y].__setitem__(x+j,7+j%2)for y in range(h)for j in range(y-h+1,h-y)])and g
