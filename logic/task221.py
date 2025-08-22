ac16e4f31ec9a9ea7d65ddf6c13fe7a0aae03e96
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 05:01:31 +0900

    golf task221

--- logic/task221.py
+++ logic/task221.py
@@ -1,4 +1,4 @@
 def p(g):
     # tile g where color repeats; zeros set grid size
-    f=sum(g,[]);z=f.count(0);r=range(z*3)
-    return [[g[y%3][x%3]*(z*(y//3)+x//3<9-z)for x in r]for y in r]
+    z=sum(g,[]).count(0);r=range(z*3)
+    return [[g[y%3][x%3]*(y//3*z+x//3<9-z)for x in r]for y in r]
