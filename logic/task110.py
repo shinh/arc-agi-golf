4c352bc97202f1fcd2eef6df9e3dba91c69a8a83
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 17:55:39 +0900

    Golf task110

--- logic/task110.py
+++ logic/task110.py
@@ -2,6 +2,6 @@ def p(g):
     # brute-force tile size using dict
     for q in range(1,30):
         for p in range(1,30):
-            d={}
-            if all(v<1 or d.setdefault((y%q,x%p),v)==v for y,r in enumerate(g)for x,v in enumerate(r)):
+            d={(y%q,x%p):v for y,r in enumerate(g)for x,v in enumerate(r)if v}
+            if all(d.get((y%q,x%p))==v for y,r in enumerate(g)for x,v in enumerate(r)if v):
                 return[[d.get((y%q,x%p),0)for x in range(29)]for y in range(29)]
