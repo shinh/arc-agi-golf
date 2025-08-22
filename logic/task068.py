4927cc4170b40397c4cc8f1f39dbef8cb5e83775
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 17:47:35 +0900

    golf: shorten unique cell surround

--- logic/task068.py
+++ logic/task068.py
@@ -1,5 +1,5 @@
 def p(g):# surround unique cell with 2s
     a=sum(g,[]);v=min(a,key=a.count);y,x=divmod(a.index(v),10);r=range(10)
-    o=[[2*(y-i<2>x-j>-2<y-i)for j in r]for i in r]
+    o=[[2*(2>i-y>-2<j-x<2)for j in r]for i in r]
     o[y][x]=v;return o
 
