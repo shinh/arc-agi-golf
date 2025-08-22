3cc7aede4120ae48643b977a7d1dca58dac83534
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 19:07:49 +0900

    golf task124

--- logic/task124.py
+++ logic/task124.py
@@ -1,10 +1,9 @@
 def p(g):
     # find shift and repeat
-    w=len(g[0])
-    fg=[(i,j,v)for i,r in enumerate(g)for j,v in enumerate(r)if v]
-    dy,dx=max((m,dy,dx)for dy in range(1,6)for dx in range(-w,w)if(m:=sum((i+dy,j+dx,v)in fg for i,j,v in fg)))[1:]
+    # pick dy,dx maximizing overlap then tile non-zero cells along that vector
+    w=len(g[0]);fg=[(i,j,v)for i,r in enumerate(g)for j,v in enumerate(r)if v]
+    dy,dx=max((sum((y+dy,x+dx,v)in fg for y,x,v in fg),dy,dx)for dy in range(1,6)for dx in range(-w,w))[1:]
     out=[[0]*w for _ in range(10)]
-    for i,j,v in fg:
-        y=i;x=j
+    for y,x,v in fg:
         while 10>y>-1<x<w:out[y][x]=v;y+=dy;x+=dx
     return out
