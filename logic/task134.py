8035f53508a29d2af19b92b93cd3a279ef62e643
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 19:07:01 +0900

    Golf task134

--- logic/task134.py
+++ logic/task134.py
@@ -1,9 +1,7 @@
 def p(g):
     # find 2x2 color, rotate-crop, downscale
-    for r,pr in zip(g,g[1:]):
-        for x in range(len(r)-1):
-            if (v:=r[x])==pr[x]==r[x+1]==pr[x+1]!=0:c=v
-    ac=({*sum(g,[])}-{0,c}).pop()
+    c=[a for r,s in zip(g,g[1:]) for a,b,d,e in zip(r,r[1:],s,s[1:]) if a==b==d==e>0][-1]
+    ac=sum({*sum(g,[])}-{c})
     for _ in[0]*96:g=[*zip(*g[-2+(c in g[-1])::-1])]
     r=len(g)//3
-    return[[ac*(g[y*r][x*r]==c)for x in range(3)]for y in range(3)]
+    return[[ac*(v==c)for v in row[::r]]for row in g[::r]]
