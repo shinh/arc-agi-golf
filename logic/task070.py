1f5f130c288eeb00b5439cf5aa7414e93c7e2043
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 18:31:08 +0900

    Golf task070

--- logic/task070.py
+++ logic/task070.py
@@ -1,8 +1,7 @@
 def p(g):
     # grow 3s over 1s with >=2 big neighbors
-    for y,r in enumerate(g):
-        for x in range(17):
-            if r[x]==1<sum((y and 2<g[y-1][x],y<16 and 2<g[y+1][x],x and 2<r[x-1],x<16 and 2<r[x+1])):
-                r[x]=3
-                return p(g)
+    for n in range(289):
+        if (r:=g[y:=n//17])[x:=n%17]==1<sum((y and g[y-1][x]>2,y<16 and g[y+1][x]>2,x and r[x-1]>2,x<16 and r[x+1]>2)):
+            r[x]=3
+            return p(g)
     return g
