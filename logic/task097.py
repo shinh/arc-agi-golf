183ba393abc7d5167ab011e4f12f486f84e0b1cc
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 18:00:01 +0900

    golf task097

--- logic/task097.py
+++ logic/task097.py
@@ -1,5 +1,3 @@
-def p(g):# zero isolated tiles
- for y,r in enumerate(g):
-  for x,c in enumerate(r):
-   if c and sum(R[x-(x>0):x+2].count(c)for R in g[y-(y>0):y+2])<2:r[x]=0
- return g
+# zero singles
+p=lambda g:[[c*(sum(r[x-(x>0):x+2].count(c)for r in g[y-(y>0):y+2])>1)for x,c in enumerate(r)]for y,r in enumerate(g)]
+
