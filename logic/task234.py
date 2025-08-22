3028086fe5745387461cb7204915e64c6ec9b298
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 04:59:09 +0900

    golf: shrink task234

--- logic/task234.py
+++ logic/task234.py
@@ -2,8 +2,7 @@ def p(g):
  # We remove only a single pixel every 4 iterations.
  for _ in[0]*44:
   for y,r in enumerate(g):
-   L=len(R:=g[y-1])
-   for x in range(L-1):
-    if r[x-1]+r[x+1]<1<R[x]*r[x]!=r[x]*r[x]:g+=[0]*L,;del g[y]
-  g=[*map(list,zip(*g[::-1]))]
+   for x in range((L:=len(R:=g[y-1]))-1):
+    if r[x-1]+r[x+1]<1<(v:=r[x])*R[x]!=v*v:g+=[0]*L,;del g[y]
+  g=[*zip(*g[::-1])]
  return g
