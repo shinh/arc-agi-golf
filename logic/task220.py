82c74c3b6890ec8a6bb35424931a8ebddcf39b6e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 20:54:02 +0900

    Golf task220

--- logic/task220.py
+++ logic/task220.py
@@ -1,11 +1,8 @@
-def p(g,E=enumerate):
+def p(g,d={8:4,2:1,3:6},E=enumerate):
 # color neighbors
- d={8:4,2:1,3:6}
  for r,R in E(g):
   for c,C in E(R):
-   for i in-1,0,1:
-    for j in-1,0,1:
-     if C and(i|j):
-      try:g[r+i][c+j]=d[C]
-      except:0
+   for i in-1,1:
+    try:H=g[r+i];H[c]=R[c+i]=H[c+i]=H[c-i]=d[C]
+    except:0
  return g
