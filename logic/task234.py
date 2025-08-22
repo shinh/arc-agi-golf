3a793a22c10dc0e35aa5f6dfaa41b2acfedf2a5d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 04:58:14 +0900

    golf task234

--- logic/task234.py
+++ logic/task234.py
@@ -4,6 +4,6 @@ def p(g):
   for y,r in enumerate(g):
    L=len(R:=g[y-1])
    for x in range(L-1):
-    if r[x-1]+r[x+1]<1<R[x]*r[x]!=r[x]*r[x]:g+=[0]*L,;del g[y]
-  g=[*map(list,zip(*g[::-1]))]
+    if(r[x-1]+r[x+1]<1)*(0<R[x]!=r[x]>0):g+=[0]*L,;del g[y]
+  g=[*zip(*g[::-1])]
  return g
