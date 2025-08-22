b6fd459869d73e520f4f02a61917a10014848745
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 19:09:38 +0900

    golf task154

--- logic/task154.py
+++ logic/task154.py
@@ -3,7 +3,7 @@
 # copy left chunk to right then wipe it, rotating to cover all dirs
 def p(g):
  for _ in[0]*4:
+  g=[*map(list,zip(*g))][::-1]
   for r in g:
-   if{2,5}<=set(r):s=r.index(2);r[s+2:s+4]=r[s-3:s-1][::-1];r[s-3:s-1]=0,0
-  g=[*map(list,zip(*g[::-1]))]
+   if{2,5}<=set(r):s=r.index(2);r[s+2:s+4]=r[s-2],r[s-3];r[s-3:s-1]=0,0
  return g
