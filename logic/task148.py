40673e6a823a35a262a2c7ea914385fae301c205
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 19:13:55 +0900

    golf task148

--- logic/task148.py
+++ logic/task148.py
@@ -1,10 +1,10 @@
 def p(g):
     # link every 8 to the side wall then duplicate its row
-    W=len(g[0]);s=sum(g,[]);P=[divmod(i,W)for i,v in enumerate(s)if v==2];E=[divmod(i,W)for i,v in enumerate(s)if v==8]
-    lc,st=min((x,y)for y,x in P);rc,ot=max((x,-y)for y,x in P);ot=-ot
-    if all(g[y][lc]-2 and x-lc for y,x in E):lc,st,ot=rc,ot,st
-    for y,x in E:
-        a,b=sorted((lc,x));g[y][a+1:b]=[8]*(b-a-1);g[y][x]=4;r=y+ot-st
-        if 0<=r<len(g):g[r]=[8]*W
-    for y,x in P:g[y][x]=2
+    w=len(g[0]);s=sum(g,[]);p=[divmod(i,w)for i,v in enumerate(s)if v&6==2];e=[divmod(i,w)for i,v in enumerate(s)if v&8]
+    l,a=min((x,y)for y,x in p);r,b=max((x,-y)for y,x in p);o=-b-a
+    if all((g[y][l]-2)*(x-l)for y,x in e):l=r;o=-o
+    for y,x in e:
+        a,b=sorted((l,x));g[y][a:b]=[8]*(b-a);g[y][x]=4;y+=o
+        if 0<=y<len(g):g[y]=[8]*w
+    for y,x in p:g[y][x]=2
     return g
