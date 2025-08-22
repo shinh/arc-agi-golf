e925e27c669c03f004e13410a92f0b2c83c6d089
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 18:35:40 +0900

    golf task073

--- logic/task073.py
+++ logic/task073.py
@@ -1,6 +1,5 @@
 def p(g):
-    for i,v in enumerate(g[2]):
-        if v:
-            g[2][i]=0
-            g[-1][i]=1
+    # drop third-row 1s to bottom
+    for i in 0,1,2,3,4:
+        if g[2][i]:g[-1][i],g[2][i]=1,0
     return g
