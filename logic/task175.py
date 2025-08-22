db8d9d63f83155572102be1eec35ed8520d3734b
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-22 20:23:12 +0900

    Golf task175

--- logic/task175.py
+++ logic/task175.py
@@ -1,8 +1,4 @@
 def p(g):
  # mirror diag & fill zeros with most common color
- n=range(21);a=sum(g,[]);m=max(set(a)-{0},key=a.count);k=a[0]
- for i in n:
-  for j in n:g[i][j]=g[j][i]=g[i][j] or g[j][i] or m
-  g[i][i]=k
- return g
+ a=sum(g,[]);m=max({*a}-{0},key=a.count);r=range(21);return[[a[0]*(i==j)or g[i][j]or g[j][i]or m for j in r]for i in r]
 
