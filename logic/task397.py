74558708f0352a6e84a3befc27ca35d3ad4b54f8
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 09:59:42 +0900

    Golf task397

--- logic/task397.py
+++ logic/task397.py
@@ -1,9 +1,7 @@
 # fill below nonzero 2x2 blocks with 3
-R=range(9)
 
 def p(g):
- o=eval(str(g))
- for y in R:
-  for x in R:
-   for r in o[y+2:][:all(a:=g[y][x:x+2]+g[y+1][x:x+2])*len({*a})]:r[x:x+2]=3,3
- return o
+ for i in range(81):
+  y=8-i//9;x=i%9
+  for r in g[y+2:][:all(a:=g[y][x:x+2]+g[y+1][x:x+2])*len({*a})]:r[x:x+2]=3,3
+ return g
