fe355f3654a95c478e30e9a39a090f01b0d63fbd
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 10:22:01 +0900

    comment task364

--- logic/task364.py
+++ logic/task364.py
@@ -1,17 +1,18 @@
 def p(g):
     # recolor 3s by shape
-    h=len(g);w=len(g[0]);v=set();D=(1,0,-1,0,0,1,0,-1)
+    h=len(g);w=len(g[0]);D=1,0,-1,0,0,1,0,-1
     for y in range(h):
         for x in range(w):
-            if g[y][x]-3 or (y,x)in v:continue
-            q=[(y,x)];C=[]
-            while q:
-                y1,x1=q.pop();v.add((y1,x1));C+=[(y1,x1)]
+            if g[y][x]-3:continue
+            C=[(y,x)];g[y][x]=0
+            # flood fill component
+            for y,x in C:
                 for i in 0,2,4,6:
-                    ny=y1+D[i];nx=x1+D[i+1]
-                    if 0<=ny<h and 0<=nx<w and g[ny][nx]==3 and (ny,nx)not in v:q+=[(ny,nx)]
-            n=sum(sum(((p[0]+D[i],p[1]+D[i+1])in C)for i in (0,2,4,6))==1 for p in C)
-            t=sum(((y-1,x)in C or (y+1,x)in C)and((y,x-1)in C or (y,x+1)in C)for y,x in C)
-            c=2 if n>2 else [6,1][t<2]
-            for y1,x1 in C:g[y1][x1]=c
+                    ny=y+D[i];nx=x+D[i+1]
+                    if h>ny>=0<=nx<w and g[ny][nx]==3:g[ny][nx]=0;C+=[(ny,nx)]
+            n=t=0
+            # count endpoints and crossings
+            for y,x in C:a=(y+1,x)in C;b=(y-1,x)in C;c=(y,x+1)in C;d=(y,x-1)in C;n+=a+b+c+d==1;t+=(a|b)and(c|d)
+            c=(2,[6,1][t<2])[n<3]
+            for y,x in C:g[y][x]=c
     return g
