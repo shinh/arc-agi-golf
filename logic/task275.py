3c7afff23166b3ba7057100fe69d2e9fe3ae7d53
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 16:50:50 +0900

    golf task275

--- logic/task275.py
+++ logic/task275.py
@@ -1,4 +1,5 @@
 def p(g):
- n=min(len(g),len(g[0]));p,q=[r[:n]for r in g[:n]],[r[-n:]for r in g[-n:]]
- if any(max(r)==8 for r in p):p,q=q,p
- return[[p[y//n][x//n]*q[y%n][x%n]//8 for x in range(n*n)]for y in range(n*n)]
+ # combine quadrants
+ n=min(len(g),len(g[0]));p=[r[:n]for r in g[:n]];g=[r[-n:]for r in g[-n:]]
+ if'8'in str(p):p,g=g,p
+ return[[a[x//n]*b[x%n]//8 for x in range(n*n)]for a in p for b in g]
