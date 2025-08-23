bcf6d4bcea88831bb9b3a99c445369786d5a7045
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 10:28:23 +0900

    Golf task328

--- logic/task328.py
+++ logic/task328.py
@@ -1,8 +1,8 @@
 def p(g):
  # nearest unique even-step color
- b=[r[:]for r in g]
- for y,r in enumerate(g):
-  for x in range(len(r)):
-   (m,k,v),(n,*_),*_=sorted((abs(y-i)+abs(x-j),~max(abs(y-i),abs(x-j))&1,v)for i,r in enumerate(b)for j,v in enumerate(r)if v)+[(99,0,0)]
-   if n>m and k:r[x]=v
+ e=enumerate;a=abs;b=eval(str(g))
+ for y,r in e(g):
+  for x,_ in e(r):
+   (m,k,v),(n,*_),*_=sorted((a(y-i)+a(x-j),~max(a(y-i),a(x-j))&1,v)for i,r in e(b)for j,v in e(r)if v)+[(99,0,0)]
+   if k*n>m:r[x]=v
  return g
