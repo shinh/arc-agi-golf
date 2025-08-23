8579f36591923a69ecec2c91663027001d56b644
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-24 01:24:20 +0900

    Golf 061

--- logic/task061.py
+++ logic/task061.py
@@ -1,2 +1,12 @@
-# infer tile with setdefault
-p=lambda g:next([[d.get((y%k,x%k),0)for x in range(18)]for y in range(18)]for k in range(1,19)if(d:={}) or all(d.setdefault((y%k,x%k),v)==v for y,r in enumerate(g) for x,v in enumerate(r) if v))
+# infer tile by using period.
+def p(g,p=1):
+    # Find the period.
+    for r in g:
+        while all(r)*(r[:-p]!=r[p:]):p+=1
+    # Complement the first square.
+    for y,r in enumerate(g):
+        for x,c in enumerate(r):
+            if c:
+                g[y%p][x%p]=c
+    # Complement the rest.
+    return[[g[y%p][x%p]for x,c in enumerate(r)]for y,r in enumerate(g)]
