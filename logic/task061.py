9a30558319989f9db206ef4e868b05c5dfa06290
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-24 01:56:23 +0900

    Golf 061

--- logic/task061.py
+++ logic/task061.py
@@ -4,8 +4,9 @@ def p(g,p=1):
     for r in g:
         while all(r)*(r[:-p]!=r[p:]):p+=1
     # Complement the first square.
-    for y,r in enumerate(g):
-        for x,c in enumerate(r):
-            g[y%p][x%p]|=c
+    for y in range(18):
+        for x in range(18):
+            g[y%p][x%p]|=g[y][x]
     # Complement the rest.
-    return[[g[y%p][x%p]for x,c in enumerate(r)]for y,r in enumerate(g)]
+    #return[(g[y%p][:p]*9)[:18]for y in range(18)]
+    return([(r[:p]*9)[:18]for r in g[:p]]*9)[:18]
