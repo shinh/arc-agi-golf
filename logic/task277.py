8e3a8420ca3d5d15a05977e8d35f7d218a4b84f8
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 16:17:37 +0900

    golf task277

--- logic/task277.py
+++ logic/task277.py
@@ -1,20 +1,18 @@
 def p(g):# flood fill shapes, count patterns, recolor odd one
- m={}
+ g=sum(g,[]);m={}
  for k in range(100):
-  i,j=divmod(k,10)
-  if g[i][j]:
-   o=[(i,j)];g[i][j]=0
-   for x,y in o:
-    for d in(-1,0,1):
-     for e in(-1,0,1):
-      u,v=x+d,y+e
-      if d|e and 0<=u<10>v>=0 and g[u][v]:g[u][v]=0;o+=[(u,v)]
-   s=frozenset((x-min(x for x,_ in o),y-min(y for _,y in o))for x,y in o)
-   m[s]=m.get(s,[])+[o]
+  if g[k]:
+   q=[k];g[k]=0
+   for i in q:
+    for n in-11,-10,-9,-1,1,9,10,11:
+     j=i+n
+     if 0<=j<100>-2<j%10-i%10<2 and g[j]:g[j]=0;q+=j,
+   s=tuple(j-q[0]for j in q)
+   m[s]=m.get(s,[])+[q]
  t=min(m,key=lambda k:len(m[k]))
  for k in m:
   c=1+(k==t)
-  for o in m[k]:
-   for i,j in o:g[i][j]=c
+  for q in m[k]:
+   for j in q:g[j]=c
    c=1
- return g
+ return[g[i:i+10]for i in range(0,91,10)]
