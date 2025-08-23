5f118d34707c108e3e28bd505f7d235ef9c3e259
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 10:24:36 +0900

    golf task313

--- logic/task313.py
+++ logic/task313.py
@@ -1,2 +1,3 @@
-def p(g,l=len):# repeat blocks then map colors via first row
- a=l({r[0]for r in g})-1;r=g[0];s=range(l(g));return[[r[r.index(g[i%a][j%(l({*r})-1)])+1]for j in s]for i in s]
+def p(g,l=len):#tile&map row0
+ r=g[0];s=range(l(g));return[[r[r.index(g[i%~-l({r[0]for r in g})][j%~-l({*r})])+1]for j in s]for i in s]
+
