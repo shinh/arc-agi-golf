c88ecfcf7067615bc9de1889279b5dcda40a0894
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 16:42:49 +0900

    golf task286

--- logic/task286.py
+++ logic/task286.py
@@ -1,6 +1,5 @@
 # rotate & fill alternating colors
 
 def p(g):
-    t=sum({*sum(g,[])}-{0,8})
-    for _ in[0]*272:g=[[a or b%8 and t-b for a,b in zip(r,r[1:]+(8,))]for r in zip(*g[::-1])]
+    for t in[sum({*sum(g,[])})]*272:g=[[a or b%8 and t-b-8 for a,b in zip(r,r[1:]+(8,))]for r in zip(*g[::-1])]
     return g
