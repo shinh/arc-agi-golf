d4db9f0769d9e22619695cbf37e67e3f787fe224
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 16:17:18 +0900

    Golf task281 solution

--- logic/task281.py
+++ logic/task281.py
@@ -1,8 +1,8 @@
 def p(g):
         # surround non-unique colors with a frame including the lone cell
-        h=len(g);w=len(g[0]);f=sum(g,[])
-        i=[i for i,v in enumerate(f) if v and f.count(v)<2][0];u=f[i];uy,ux=divmod(i,w)
-        Y,X=zip(*[divmod(i,w)for i,v in enumerate(f) if v and v!=u])
+        w=len(g[0]);f=sum(g,[])
+        i=[i for i,v in enumerate(f)if f.count(v)<2<v+2][0];u=f[i]
+        Y,X=zip(*[(i//w,i%w)for i,v in enumerate(f)if 0<v!=u])
         t=min(Y);b=max(Y);l=min(X);r=max(X);B,C=g[t][l],g[t+1][l+1]
-        t=min(t,uy);b=max(b,uy);l=min(l,ux);r=max(r,ux)
-        return [[0 if y<t or y>b or x<l or x>r else B if y in(t,b) or x in(l,r) else C for x in range(w)]for y in range(h)]
+        t=min(t,i//w);b=max(b,i//w);l=min(l,i%w);r=max(r,i%w)
+        return [[(l<=x<=r)*(t<=y<=b)*(B*(y in(t,b) or x in(l,r)) or C) for x in range(w)]for y in range(len(g))]
