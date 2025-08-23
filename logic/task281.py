2e855db3d684b94972a1cecea0c6a5d52d527b33
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 23:55:31 +0900

    golf task281

--- logic/task281.py
+++ logic/task281.py
@@ -1,20 +1,12 @@
 def p(g):
-    for o in range(4):
-        e=[r.index(8)for r in g if 8 in r]
-        if e:
-            e,=e
+    # spread color
+    for o in[0]*4:
+        if (e:=[r.index(8)for r in g if 8 in r]):
+            e=e[0]
             for r in g:
                 f=n=0
                 for x,c in enumerate(r):
-                    if c:
-                        if f<1:
-                            f=c
-                        elif n<1:
-                            n=c
-                    elif n and x<e:
-                        r[x-1:e]=[n]*(e-x+1)
-                        r[e]=f
-                        n=0
-
+                    if c:f<1and(f:=c)or n<1and(n:=c)
+                    elif n and x<e:r[x-1:e+1]=[n]*(e-x+1)+[f];n=0
         g=[*map(list,zip(*g[::-1]))]
     return g
