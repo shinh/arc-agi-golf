4b3c183bbf872116801514df46d3e46c526b915e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 16:18:15 +0900

    golf: optimize task271

--- logic/task271.py
+++ logic/task271.py
@@ -1,2 +1,3 @@
-def p(g):# pick 3x3 with most 1s then 8s
-    R=range(7);m,b,n=max((sum(9*(v==1)+v//8for r in g[y:y+3]for v in r[x:x+3]),y,x)for y in R for x in R);return[r[n:n+3]for r in g[b:b+3]]
+R=range(7)
+# pick 3x3 with most 1s then 8s
+p=lambda g:max((sum((v==1)*9+v//8for r in B for v in r),B)for B in([r[x:x+3]for r in g[y:y+3]]for y in R for x in R))[1]
