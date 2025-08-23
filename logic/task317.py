b8f19cf4c798d5f56cd956c533b510b7f3f9fbcf
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 12:08:21 +0900

    chore: golf task317

--- logic/task317.py
+++ logic/task317.py
@@ -1,2 +1,2 @@
 def p(E):#1 near 5
- r=range(len(E));return[[any(5 in R[j and j-1:j+2] for R in E[i and i-1:i+2])for j in r]for i in r]
+ r=range(len(E));return[[any(5 in R[j-(j>0):j+2]for R in E[i-(i>0):i+2])for j in r]for i in r]
