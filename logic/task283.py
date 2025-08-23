5e7e2e58c943a9e3d2e50f332744d7bd07e0f829
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 16:47:53 +0900

    golf task283

--- logic/task283.py
+++ logic/task283.py
@@ -1,4 +1,4 @@
 def p(g):#frame 5-rect via set
- s={(x,y)for y,r in enumerate(g)for x,v in enumerate(r)if v>4}
- for x,y in s:g[y][x]=[1,4,2][len(s&{(x+1,y),(x-1,y),(x,y+1),(x,y-1)})-2]
+ s={(i%10,i//10)for i,v in enumerate(sum(g,[]))if v>4}
+ for x,y in s:g[y][x]=len(s&{(x+1,y),(x-1,y),(x,y+1),(x,y-1)})*3%5
  return g
