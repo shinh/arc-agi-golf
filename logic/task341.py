58276cd5eda2400c4a25f3f5d05143a41ba25b66
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 23:48:36 +0900

    Golf 341 a bit

--- logic/task341.py
+++ logic/task341.py
@@ -1,9 +1,9 @@
 def p(g):
- # bridge shapes with 8
- f=sum(g,[]);w=len(g[0])
- a,b=[(i:=f.index(c),j:=len(f)-1-f[::-1].index(c))and(i//w,j//w,i%w,j%w)for c in set(f)-{0}]
- c=a[3]<b[2]or b[3]<a[2]
- if a[c*2]>b[c*2]:a,b=b,a
- s,t,u,v=[(a[1]+1,b[0],max(a[2],b[2])+1,min(a[3],b[3])),(max(a[0],b[0])+1,min(a[1],b[1]),a[3]+1,b[2])][c]
- for r in g[s:t]:r[u:v]=[8]*(v-u)
- return g
+    # bridge shapes with 8
+    f=sum(g,[])
+    a,b=[(i:=f.index(c),j:=len(f)-1-f[::-1].index(c))and(i//10,j//10,i%10,j%10)for c in{*f}-{0}]
+    c=a[3]<b[2]or b[3]<a[2]
+    if a[c*2]>b[c*2]:a,b=b,a
+    s,t,u,v=[(a[1]+1,b[0],max(a[2],b[2])+1,min(a[3],b[3])),(max(a[0],b[0])+1,min(a[1],b[1]),a[3]+1,b[2])][c]
+    for r in g[s:t]:r[u:v]=[8]*(v-u)
+    return g
