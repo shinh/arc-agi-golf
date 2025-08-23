0304cd972e6165d56c36f8e1bcc467cc46b7262d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 16:38:22 +0900

    golf task263

--- logic/task263.py
+++ logic/task263.py
@@ -1,4 +1,4 @@
 # find unique 3x3 block by zero pattern
 def p(g):
- b,m=zip(*((t:=[r[x:x+3]for r in g[y:y+3]],[c<1for c in sum(t,[])])for y in range(0,len(g),3)for x in range(0,len(g[0]),3)));return b[m.index(min(m,key=m.count))]
-
+ # group rows every 3 then find the unique zero mask
+ b,m=zip(*((t:=[r[x:x+3]for r in R],[c<1for c in sum(t,[])])for R in zip(*[iter(g)]*3)for x in range(len(R[0]))[::3]));return b[m.index(min(m,key=m.count))]
