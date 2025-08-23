27af1d5e0961c91d065ddf88b9079d8f64fe8fbe
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 16:42:16 +0900

    golf task290

--- logic/task290.py
+++ logic/task290.py
@@ -1,3 +1,3 @@
 # 101
-def p(g):a,b={c for r in g for c in r if c};return[r for r in[[c^a^b for c in r if c]for r in g]if r]
+def p(g):a,b={*sum(g,[])}-{0};return[t for r in g if(t:=[c^a^b for c in r if c])]
 
