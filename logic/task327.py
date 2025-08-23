4f55434123228d5eab12c073627022b25b20587a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 10:06:16 +0900

    Golf task327 diagonal propagation

--- logic/task327.py
+++ logic/task327.py
@@ -1,1 +1,2 @@
-def p(g,e=enumerate):X=[[0]*6 for _ in g*2];[X[r+i].__setitem__(c+i,v)for r,R in e(g)for c,v in e(R)if v for i in range(6-max(r,c))];return X
+# extend diagonals
+def p(g,e=enumerate):X=[[0]*6for _ in g+g];[X[r+i].__setitem__(c+i,v)for r,R in e(g)for c,v in e(R)for i in range(6-max(r,c))if v];return X
