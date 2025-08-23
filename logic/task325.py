49728e5995664069e3224309d13c9a167e86b008
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 10:05:19 +0900

    golf task325

--- logic/task325.py
+++ logic/task325.py
@@ -1,11 +1,5 @@
-def p(m,K=range):
- r,c=len(m),len(m[0]);n=0
+def p(m,K=range):# floodfill n*n diag8
+ r,c=len(m),len(m[0])
  def f(y,x):
-  m[y][x]=0
-  for b,a in(1,0),(-1,0),(0,1),(0,-1):
-   Y,X=y+b,x+a
-   if r>Y>-1<X<c and m[Y][X]:f(Y,X)
- for i in K(r):
-  for j in K(c):
-   if m[i][j]:n+=1;f(i,j)
- return[[8*(i==j)for j in K(n)]for i in K(n)]
+  if m[y][x]:m[y][x]=0;[r>Y>-1<X<c and f(Y,X)for Y,X in((y+1,x),(y-1,x),(y,x+1),(y,x-1))];return 1
+ n=sum(f(i,j)or 0 for i in K(r)for j in K(c));return[[8*(i==j)for j in K(n)]for i in K(n)]
