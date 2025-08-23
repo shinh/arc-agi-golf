ddf246ce36c30f6e142878367bb2c5554a374c22
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 16:44:32 +0900

    golf task278

--- logic/task278.py
+++ logic/task278.py
@@ -2,11 +2,11 @@
 def p(g):
  for _ in 0,1:
   for y,r in enumerate(g):
-   for x in range(len(r)-1):
-    if r[x]==r[x+1]==2:
+   for x in range(len(r)):
+    if r[x:x+2]==[2]*2:
      for Y in range(y-1,y+2):
       for X in range(x-1,x+3):
-       if 0<=Y<len(g)and 0<=X<len(r)and g[Y][X]!=2:g[Y][X]=3
+       if len(g)>Y>=0<=X<len(r)and g[Y][X]-2:g[Y][X]=3
   g=[*map(list,zip(*g))]
  return g
 
