4e670378dd5aee6ce4d87f0892dcb398df94bd33
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 16:38:41 +0900

    golf task259

--- logic/task259.py
+++ logic/task259.py
@@ -1,3 +1,3 @@
-# 90
+# 89
 # rotate off 1 borders, then clear remaining 1s
-p=lambda g,t=63:-t*[[g*(g>1)for g in g]for g in g]or p([*zip(*g[2>max(g[0]):][::-1])],t-1)
+p=lambda g,t=63:-t*eval(str(g).replace('1','0'))or p([*zip(*g[2>max(g[0]):][::-1])],t-1)
