58f426d9e3ba23810516677de223ddeb0bca81a0
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 10:20:24 +0900

    golf task384

--- logic/task384.py
+++ logic/task384.py
@@ -1,2 +1,2 @@
 # crop bbox of nonzeros and double size
-def p(g):y,x=zip(*((i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v));return[[v for v in r[min(x):max(x)+1]for _ in(0,0)]for r in g[min(y):max(y)+1]for _ in(0,0)]
+p=lambda g:[[v for v in r for _ in(0,0)]for r in zip(*filter(any,zip(*g)))if any(r)for _ in(0,0)]
