7b0a6b6e778828c6cf4f7d4c8b2882988cad1156
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 10:03:03 +0900

    golf task382

--- logic/task382.py
+++ logic/task382.py
@@ -1,21 +1,20 @@
 def p(g):
     # draw a right-moving trail of 8s that climbs once for each 2 in the bottom row
-    r=lambda t:[*map(list,zip(*t[::-1]))]
-    h=lambda t:[row[::-1]for row in t]
+    R=lambda g:[*zip(*g[::-1])];F=lambda g:[r[::-1]for r in g]
     for k in range(4):
-        if 2 in g[-1] and any(8 in (row[0],row[-1])for row in g):
-            f=all(row[0]-8 for row in g)
-            if f:g=h(g)
-            o=[[2*(c==2)for c in row]for row in g]
-            for y,row in enumerate(g):
-                if row[0]==8:
+        if 2 in g[-1]and any(r[0]>7or r[-1]>7for r in g):
+            f=max(r[0]for r in g)<8
+            if f:g=F(g)
+            o=[[c&2for c in r]for r in g]
+            for y,r in enumerate(g):
+                if r[0]>7:
                     z=y
                     for x,c in enumerate(g[-1]):
                         z-=c==2
                         if z<0:break
                         o[z][x]=8
-            if f:o=h(o)
-            for _ in range(4-k):o=r(o)
+            if f:o=F(o)
+            for _ in range(4-k):o=R(o)
             return o
-        g=r(g)
+        g=R(g)
 
