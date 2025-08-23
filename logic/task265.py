254f893a5cc9c0d556de4d2459357dea8d59cfe6
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-23 16:18:41 +0900

    Golf task265 with in-place rectangle fill

--- logic/task265.py
+++ logic/task265.py
@@ -1,12 +1,13 @@
 def p(g):
     # fill rectangles of zeros with 2
     o=[r[:]for r in g]
-    for y in range(17):
-        for x in range(17):
+    R=range(17)
+    for y in R:
+        for x in R:
             if sum(g[y][x:x+2]+g[y+1][x:x+2])<1 and (y<1 or sum(g[y-1][x:x+2])) and (x<1 or g[y][x-1]+g[y+1][x-1]):
-                X=x+2;Y=y+2
-                while X<18 and g[y][X]+g[y+1][X]<1:X+=1
-                while Y<18 and sum(g[Y][x:X])<1:Y+=1
-                for r in o[y:Y]:r[x:X]=[2]*(X-x)
+                X=x+2
+                while X<18>g[y][X]+g[y+1][X]==0:X+=1
+                Y=y;w=[2]*(X-x)
+                while Y<18>sum(g[Y][x:X])==0:o[Y][x:X]=w;Y+=1
     return o
 
