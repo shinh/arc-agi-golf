7d39f37af1b3ba5f7f92fe7cb2cb43ae647099c9
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-25 09:08:07 +0900

    Add a failing test

--- myminifier_test.py
+++ myminifier_test.py
@@ -128,6 +128,16 @@ def test_find_expandable_variables_skips_reassignments():
     assert myminifier.find_expandable_variables(code) == {"c": 0}
 
 
+def test_find_expandable_variables_skips_assignment_in_for():
+    code = r"""
+for i in range(3):
+    b=i
+return fn2(b)
+"""
+    # Assignments in loop must be skipped.
+    assert myminifier.find_expandable_variables(code) == {}
+
+
 def test_expand_variable_replaces_usage():
     code = "r=range;r(3);r(4)"
     assert myminifier.expand_variable(code, "r") == "range(3);range(4)"
