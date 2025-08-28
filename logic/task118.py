053d7ce60ff70a89767a44f419639e3a4b932daa
Darren Smith <darrenks@golfscript.com>
2025-08-27 20:14:01 -0600

    new alg for 118 (not golfed)`

--- logic/task118.py
+++ logic/task118.py
@@ -1,39 +1,30 @@
 def p(g):
- # expand rare color into cross
- # fill small gaps, find longest line, extend to cross
- h=len(g);w=len(g[0])
- t=sum(g,[]);c=min(t,key=t.count)
- S={divmod(i,w)for i,v in enumerate(t)if v==c}
- p=0
- while p-len(S):
-  p=len(S)
-  for _ in 0,1:
-   for y in range(h):
-    q=-9
-    for x in range(w):
-     if(y,x)in S:
-      if x-q<5:S|={(y,t)for t in range(q,x)}
-      q=x
-   h,w=w,h;S={(x,y)for y,x in S}
- L=0;M=set()
- for _ in 0,1:
-  for y in range(h):
-   x=0
-   while x<w:
-    if(y,x)in S:
-     z=x
-     while z<w and(y,z)in S:z+=1
-     d=z-x
-     if d>L:L=d;M=set()
-     if d==L and all((y-1,t)not in S and(y+1,t)not in S for t in range(x,z)):M.add((y,x+d//2))
-     x=z
-    x+=1
-  h,w=w,h;S={(x,y)for y,x in S};M={(x,y)for y,x in M}
- m=L//2
- M|={(y,x)for y,x in S if{(y-1,x),(y+1,x)}&S and{(y,x-1),(y,x+1)}&S}
- for y,x in M:
-  for d in range(-m,m+1):
-   0<=y+d<h and S.add((y+d,x))
-   0<=x+d<w and S.add((y,x+d))
- for y,x in S:g[y][x]=(8,c)[g[y][x]==c]
- return g
+    ds=3,4
+    z=0
+    been=set()
+    while 1: # can also do while 1
+        z+=1
+        a=[]
+        for y,row in enumerate(g):
+            for x,_ in enumerate(row):
+                for l in ds:
+                    points=set()
+                    for d in range(-l+1,l):
+                        yy=y+d
+                        if 0<=yy<len(g):
+                            points.add((yy,x))
+                        xx=x+d
+                        if 0<=xx<len(g[0]):
+                            points.add((y,xx))
+                    points-=been
+                    colors = [g[yy][xx] for yy,xx in points]
+                    if colors.count(0) == 0:
+                        count = colors.count(2)
+                        # prioritize covering reds, then reducing diameter (when sorted)
+                        a+=[(-count,l,points)]
+        count,d,points=min(a)
+        ds=[d] # set diameter for all next crosses
+        if count==0: break
+        for y,x in points: g[y][x]+=g[y][x]-2 # 5 -> 8, 2 -> 2
+        been|=points
+    return g
