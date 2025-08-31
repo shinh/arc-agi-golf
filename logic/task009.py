8bd0d0f1df6b0f899cd82031f5acfc49b5b97e0e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-31 21:31:49 +0900

    Golf task009 solution

--- logic/task009.py
+++ logic/task009.py
@@ -1,6 +1,5 @@
 def p(g,n=0):
-    # connect same-colored dots
+    # connect same dots
     for t in 0,1:
-        g=[[[n:=[n,[0,-v][v in r[x+1:]]][v and v!=g[0][2]],v][v!=0]for x,v in enumerate(r)]for r in zip(*g)]
-    g=[[abs(c)for c in r]for r in g]
-    return g
+        g=[[[n:=[n,-v*(v in r[x+1:])][0<v!=g[0][2]],v][v!=0]for x,v in enumerate(r)]for r in zip(*g)]
+    return[list(map(abs,r))for r in g]
