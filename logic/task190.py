f7a88d7f1d0360e316f4e9b411d1c9d8729fbcc9
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-01 00:16:01 +0900

    Solve 190

--- logic/task190.py
+++ logic/task190.py
@@ -1,12 +1,6 @@
-def p(g):#ray casting from 2x2 block
- c=max(sum(g,[]))
- for y in range(9):
-  for x in range(9):
-   if g[y][x:x+2]==g[y+1][x:x+2]==[c]*2:a=y;b=x
- for d in-1,1:
-  for e in-1,1:
-   i,j=a+d+(d>0),b+e+(e>0)
-   if-1<i<10>j>-1<g[i][j]==c:
-    while-1<(i:=i+d)<10>(j:=j+e)>-1<g[i][j]<1:g[i][j]=c
- return g
-
+# ray casting from 2x2 block
+def p(g,o=4):
+    for z in range(49):
+        if(c:=g[y:=z//7][x:=z%7])*g[y+2][x+2]*(g[y+2][x+1]<1):
+            g[y+3][x+3]=c
+    return-o*g or p([*map(list,zip(*g[::-1]))],o-1)
