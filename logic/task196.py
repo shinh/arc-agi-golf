24fb4c6ef2ef242c567671c4c26b777716a24167
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-31 23:55:11 +0900

    Golf 196

--- logic/task196.py
+++ logic/task196.py
@@ -1,5 +1,5 @@
 # recolor rectangular loops
 def p(g):
     for t in range(144):
-        g=[[[c,[2,3,0][p:=t//48]][c==p and(n%3==[2,0,0][p] or p==2)]for c,n in zip(r,[*r[1:],2])]for r in zip(*g[::-1])]
+        g=[[[c,[2,3,0][p:=t//48]][c==p and n%(5-p*2)==(p<1)*2]for c,n in zip(r,[*r[1:],2])]for r in zip(*g[::-1])]
     return g
