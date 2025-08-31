1b1a84cf115f7d58d5e1341b909b0a9c6e7867f8
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-08-31 23:46:04 +0900

    Solve 196

--- logic/task196.py
+++ logic/task196.py
@@ -1,12 +1,5 @@
-def p(g):# recolor rectangular loops
- R=range;h=len(g);w=len(g[0])
- for y in R(h):
-  for x in R(w):
-   if g[y][x]>0:
-    X=x;Y=y
-    while X<w and g[y][X]>0:X+=1
-    while Y<h and g[Y][x]>0:Y+=1
-    if X-x>1<Y-y and all(g[Y-1][i]>0 for i in R(x,X))and all(g[i][X-1]>0 for i in R(y,Y))and any(0in g[i][x:X]for i in R(y,Y)):
-     for i in R(x,X):g[y][i]=g[Y-1][i]=3
-     for i in R(y,Y):g[i][x]=g[i][X-1]=3
- return g
+# recolor rectangular loops
+def p(g):
+    for t in range(144):
+        g=[[[c,[2,3,0][p:=t//48]][c==p and(n%3==[2,0,0][p] or p==2)]for c,n in zip(r,[*r[1:],2])]for r in zip(*g[::-1])]
+    return g
