1822b3b7c98adf3efd07bfe2a70d02789daf1797
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-01 00:44:02 +0900

    refactor task196 with recursive lambda

--- logic/task196.py
+++ logic/task196.py
@@ -1,5 +1,2 @@
 # recolor rectangular loops
-def p(g):
-    for t in range(144):
-        g=[[[c,[2,3,0][p:=t//48]][c==p and n%(5-p*2)==(p<1)*2]for c,n in zip(r,[*r[1:],2])]for r in zip(*g[::-1])]
-    return g
+p=lambda g,t=0:t<144and p([[[c,[2,3,0][q:=t//48]][c==q and n%(5-q*2)==(q<1)*2]for c,n in zip(r,[*r[1:],2])]for r in zip(*g[::-1])],t+1)or g
