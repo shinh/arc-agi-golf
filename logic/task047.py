620c748f519bc6f7f4055641cb4d1ca9fea6316c
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-01 00:42:59 +0900

    Golf 47

--- logic/task047.py
+++ logic/task047.py
@@ -1,6 +1,2 @@
 # draw lines
-def p(g):
- r=range(9)
- (a,b,c),(d,e,f)=[(i,j,v)for i in r for j in r if(v:=g[i][j])]
- for i in r:g[a][i]=g[i][b]=c;g[d][i]=g[i][e]=f;g[a][e]=g[d][b]=2
- return g
+p=lambda g:[[[c,7,8,2][(7 in(rt:=r+[*t]))+(8 in rt)*2]for c,t in zip(r,zip(*g))]for r in g]
