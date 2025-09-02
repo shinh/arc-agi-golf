cef2f79551976516249f9363fdfb9c50e2100003
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-03 00:28:00 +0900

    golf task213

--- logic/task213.py
+++ logic/task213.py
@@ -1,2 +1,3 @@
-p=lambda g:len(o:=[[*s]for r in g if len(s:={*r}-{0,5})])==len(g)and[*zip(*p([*zip(*g)]))]or[r*len(o)for r in o]
+# stack unique non-zero colors; rotate if every row has one
+p=lambda g:len(o:=[[*s]for r in g if(s:={*r}-{0,5})])^len(g)and[r*len(o)for r in o]or[*zip(*p([*zip(*g)]))]
 
