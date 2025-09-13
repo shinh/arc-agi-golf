b37c92a56ad3e84fb8cdf333eb499ddf898eaa9f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-13 20:25:28 +0900

    golf task319

--- logic/task319.py
+++ logic/task319.py
@@ -1,21 +1,18 @@
 def p(g):
     # compare border mini to shapes
-    F=sum(g,[]);b=max(F,key=F.count)
-    for c in range(10):
-        f=lambda g:[*zip(*([[b,c][q==c]for q in r]for r in g if c in r))]
+    F=sum(g,[]);b=max(F,key=F.count);R=range;Y=len(g);X=len(g[0]);C=F.count
+    for c in R(10):
+        f=lambda t:[*zip(*([[b,c][q==c]for q in r]for r in t if c in r))]
         o=f(f(g))
-        if o and c!=b:
-            for y in range(-len(o)*2,len(g)-3):
-                for x in range(-len(o[0])*2,len(g[0])-3):
-                    for q in range(10):
-                        ok=1;cnt=0
-                        for dy in range(len(o)*2):
-                            for dx in range(len(o[0])*2):
-                                ny=y+dy;nx=x+dx
-                                if 0<=ny<len(g)and 0<=nx<len(g[0]):
-                                    if(o[dy//2][dx//2]==c)!=(g[ny][nx]==q):ok=0
-                                    if g[ny][nx]==q:
-                                        cnt+=1
-                        if ok and cnt==F.count(q)>0and q!=c:
-                            return o
+        if c^b:
+            H=len(o)*2;W=len(o and o[0])*2
+            for y in R(-H,Y):
+                for x in R(-W,X):
+                    for q in R(10):
+                        ok=cnt=0
+                        for i in R(H):
+                            for j in R(W):
+                                if Y>y+i>=0<=x+j<X:
+                                    v=g[y+i][x+j];ok+=(o[i//2][j//2]==c)^(v==q);cnt+=v==q
+                        if ok<1 and cnt==C(q)>0 and q-c:return o
     return g
