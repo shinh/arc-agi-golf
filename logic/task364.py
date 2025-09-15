369d63ce8e5730e7758194ceb5e90ed2201ff577
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-15 16:37:48 +0900

    Solve 364

--- logic/task364.py
+++ logic/task364.py
@@ -1,18 +1,27 @@
 def p(g):
+    #show(g,"input")
     # recolor 3s by shape
-    h=len(g);w=len(g[0]);D=1,0,-1,0,0,1,0,-1
-    for y in range(h):
-        for x in range(w):
-            if g[y][x]-3:continue
-            C=[(y,x)];g[y][x]=0
-            # flood fill component
-            for y,x in C:
-                for i in 0,2,4,6:
-                    ny=y+D[i];nx=x+D[i+1]
-                    if h>ny>=0<=nx<w and g[ny][nx]==3:g[ny][nx]=0;C+=[(ny,nx)]
-            n=t=0
-            # count endpoints and crossings
-            for y,x in C:a=(y+1,x)in C;b=(y-1,x)in C;c=(y,x+1)in C;d=(y,x-1)in C;n+=a+b+c+d==1;t+=(a|b)and(c|d)
-            c=(2,[6,1][t<2])[n<3]
-            for y,x in C:g[y][x]=c
+    for o in range(80):
+        for y,(r,pr,nr) in enumerate(zip(g,[[0]*99]+g,g[1:]+[[0]*99])):
+            for x,(c,p,n,t,u) in enumerate(zip(r,[0]+r,r[1:]+[0],pr,nr)):
+                if c==p==t==3:
+                    g[y][x]=1+(n>0)+(u>0)
+                elif c==n==t==3:
+                    g[y][x]=1+(p>0)+(u>0)
+                if c==3 and p==n==1:
+                    g[y][x]=6
+                elif p and c:
+                    if 2 in (c,p):
+                        g[y][x]=2
+                    elif 6 in (c,p):
+                        g[y][x]=6
+                    elif 1 in (c,p) and o>8:
+                        g[y][x]=1
+        #if o%4==0:
+        #    show(g,f"step{o:02}")
+        g=[*map(list,zip(*g[::-1]))]
+    #for t in range(80):
+    #    g=[[[c,[max(c,p)+(c==p==1),p,c,c][(c==3)+(p==3)*2]][p>0 and c>0]for c,p in zip(r,(0,)+r)]for r in zip(*g[::-1])]
+        #g=[[[c,max(c,p)+(c==p==1)][p>0 and c>0]for c,p in zip(r,(0,)+r)]for r in zip(*g[::-1])]
+    #show(g,"output")
     return g
