cc8192605d93a5cc855314d597d816dacb3b179d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-16 09:26:56 +0900

    Golf 364 a bit more

--- logic/task364.py
+++ logic/task364.py
@@ -1,5 +1,2 @@
-def p(g):
-    # recolor 3s by shape
-    for o in range(40):
-        g=[[[[[c,[u for u in[2,6,1,c]if u in(c,p)][0]][p*c and o>8],6][c==3 and p==n==1],(p>0)+(n>0)+(u>0)][c==t==3 and p+n>0]for c,p,n,t,u in zip(r,[0]+r,r[1:]+[0],pr,nr)]for r,pr,nr in zip(g,[[0]*99]+g,g[1:]+[[0]*99])];g=[*map(list,zip(*g[::-1]))]
-    return g
+# recolor 3s by shape
+p=lambda g,o=39:-o*g or p([*zip(*[[[[[c,[u for u in[2,6,1,c]if u in(c,p)][0]][p*c and o<32],6][c==3 and p==n==1],(p>0)+(n>0)+(u>0)][p+n and c==t==3]for c,p,n,t,u in zip(r,[0,*r],[*r[1:],0],pr,nr)]for r,pr,nr in zip(g,[[0]*99]+g,g[1:]+[[0]*99])][::-1])],o-1)
