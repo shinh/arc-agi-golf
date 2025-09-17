c04fbec226f84beb3419b87509d7697826e12b84
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-18 07:34:48 +0900

    Golf task051

--- logic/task051.py
+++ logic/task051.py
@@ -1,6 +1,5 @@
 def p(g):
     # extend the lone colored cell toward its neighbor
-    w=len(g[0]);f=sum(g,[]);y,x=divmod(f.index(c:=min(f,key=f.count)),w);a,b=(g[y+1][x]>0)-(g[y-1][x]>0),(g[y][x+1]>0)-(g[y][x-1]>0)
-    while len(g)>y+a>-1<x+b<w:
-        y+=a;x+=b;g[y][x]=g[y][x]or c
+    f=sum(g,[]);w=len(g[0]);i=f.index(c:=min(f,key=f.count));y=i//w;x=i%w;a,b=[(f[i+o]>0)-(f[i-o]>0)for o in(w,1)]
+    while-1<(y:=y+a)<len(g)>-1<(x:=x+b)<w:g[y][x]=g[y][x]or c
     return g
