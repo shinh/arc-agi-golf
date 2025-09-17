99872bd62fdf2ab57425596eef3bc16415e1dc95
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-18 08:06:38 +0900

    Improve task025 compression score

--- logic/task025.py
+++ logic/task025.py
@@ -1,14 +1,18 @@
 def p(g):
-    h=len(g);w=len(g[0])
-    for c in range(1,10):# move tiles toward full row/column else erase
-        try:r=g.index([c]*w)
-        except:r=-1
-        try:k=(*zip(*g),).index((c,)*h)
-        except:k=-1
-        for y in range(h):
-            for x in range(w):
+    h=len(g);w=len(g[0]);R=range
+    for c in R(1,10):# move to row/col else erase
+        r=-1
+        for y in R(h):
+            if g[y]==[c]*w:r=y;break
+        k=-1
+        for x in R(w):
+            for y in R(h):
+                if g[y][x]!=c:break
+            else:k=x;break
+        for y in R(h):
+            for x in R(w):
                 if g[y][x]==c:
-                    if ~r and y-r:g[y][x]=0;g[r+(y>r)-(y<r)][x]=c
-                    elif ~k and x-k:g[y][x]=0;g[y][k+(x>k)-(x<k)]=c
+                    if ~r*(y-r):g[y][x]=0;g[r+(y>r)-(y<r)][x]=c
+                    elif ~k*(x-k):g[y][x]=0;g[y][k+(x>k)-(x<k)]=c
                     elif r<0>k:g[y][x]=0
     return g
