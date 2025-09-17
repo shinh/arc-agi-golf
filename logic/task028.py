79e5fa221d84b06405fb0e1f022a7e73ea7615b9
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-18 08:06:23 +0900

    Golf task028 solution

--- logic/task028.py
+++ logic/task028.py
@@ -1,1 +1,1 @@
-def p(g,R=range(10)):f=sum(g,[]);return[[(c%9<1)|645>>r&1 and[max(f[:50]),max(f[50:])][r>4]for c in R]for r in R]# draw cross using max colors from halves
+def p(g,R=range(10)):f=sum(g,[]);return[[((c%9<1)|645>>r&1)*max(f[r//5*50:][:50])for c in R]for r in R]# half max cross
