a29cf64df60d7005c3bcc37ba2efa4d0fd9e5019
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-18 08:04:45 +0900

    Golf task048 logic

--- logic/task048.py
+++ logic/task048.py
@@ -6,10 +6,10 @@ def p(g):
  for r in g:s+=0,*r,0
  s+=[0]*w
  q=[i:=s.index(2),i+1,i+w,i+w+1]
- for d in q:s[d]=1
+ for j in q:s[j]=1
  for i in q:
-  for d in-1,1,-w,w:
-   if s[i+d]&2:return(8,),
-   if s[i+d]&8:s[i+d]=1;q+=i+d,
+  for j in i-1,i+1,i-w,i+w:
+   if 2&s[j]:return(8,),
+   if 8&s[j]:s[j]=1;q+=j,
  return(0,),
 
