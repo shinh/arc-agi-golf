74918ef611dfe637fae44a753deaa908ea7ecfac
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-18 07:36:34 +0900

    Golf task033 solution

--- logic/task033.py
+++ logic/task033.py
@@ -1,4 +1,1 @@
-def p(g):
- # copy pattern blocks
- r=range(len(g))
- return[[g[i][j]or g[5][0]*(g[i%6][j%6]>0)for j in r]for i in r]
+p=lambda g,e=enumerate:[[v or g[i%6][j%6]and g[5][0]for j,v in e(r)]for i,r in e(g)]#copypatternblocks
