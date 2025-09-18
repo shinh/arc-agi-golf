dc0f1ab0f78830b9bba32d9e91560e130075a439
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-18 22:17:44 +0900

    Golf task013 stripes solver

--- logic/task013.py
+++ logic/task013.py
@@ -1,7 +1,9 @@
 def p(g):
  # stripes
  (y1,x1,c1),(y2,x2,c2)=sorted((y,x,c)for y,r in enumerate(g)for x,c in enumerate(r)if c)
- b=x1*x2<1 or x1==x2 or y1*y2>0>=abs(x1-x2)-abs(y1-y2)
- if b<1:g=[*zip(*g)];(y1,c1),(y2,c2)=sorted(((x1,c1),(x2,c2)))
- w=len(g[0]);d=y2-y1;g[y1::2*d]=[[c1]*w]*len(g[y1::2*d]);g[y2::2*d]=[[c2]*w]*len(g[y2::2*d])
+ d=y2-y1
+ b=y1>0<d
+ if b<1:g=[*zip(*g)];(y1,c1),(y2,c2)=sorted(((x1,c1),(x2,c2)));d=y2-y1
+ w=len(g[0]);c=c1,c2
+ for k,y in enumerate(range(y1,len(g),d)):g[y]=[c[k&1]]*w
  return((*zip(*g),),g)[b]
