3551df4ad9d94e5248251d01407f655611ad40eb
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-19 00:38:29 +0900

    Golf 356 a bit

--- logic/task356.py
+++ logic/task356.py
@@ -1,10 +1,10 @@
 def p(g):
  # link
- h=[*map(list,g)]
+ h=map(list,g)
  for _ in 0,1:
-  for r,H in zip(g,h):
+  for r,H in zip(h,g):
    t=[i for i,v in enumerate(r)if v]
    if t[1:]:b=t[-1];H[t[0]:b+1]=[8]*-~(b-t[0])
-  g=[*zip(*g)]
-  h=[*map(list,zip(*h))]
- return h
+  h=zip(*h)
+  g=[*map(list,zip(*g))]
+ return g
