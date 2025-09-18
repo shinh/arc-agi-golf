62ed878b7d8c34d310c60080e5ca8ab2bb01c013
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-18 12:40:00 +0900

    Golf task062

--- logic/task062.py
+++ logic/task062.py
@@ -6,7 +6,7 @@ def p(g):
         for x,v in enumerate(r):
             if v==2:t=x;z=y
             elif v:Y+=y,;X+=x,;c=v
-    v=min(Y)<=z<=max(Y)
-    a=(t+(max,min)[t<min(X)](X),z+(max,min)[z<min(Y)](Y))[not v]
-    for y,x in zip(Y,X):o[y][x]=o[[y,a-y][not v]][[x,a-x][v]]=c
+    v=z in Y
+    a=(t+(max,min)[t<min(X)](X),z+(max,min)[z<min(Y)](Y))[v^1]
+    for y,x in zip(Y,X):o[y][x]=o[[y,a-y][v^1]][[x,a-x][v]]=c
     return o
