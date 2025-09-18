7c7b0a82d32dcb32f0602e7daf44c6c0fafb8e92
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-18 13:51:18 +0900

    Golf task 069 output buffer

--- logic/task069.py
+++ logic/task069.py
@@ -4,7 +4,7 @@ def p(g):
     for y in R(10):
         for x in R(10):
             if g[y][x]%8:a=min(a,x);b=min(b,y);c=max(c,x);d=max(d,y)
-    h=d-b+1;w=c-a+1;p=[r[a:c+1]for r in g[b:d+1]];o=create(10,10)
+    h=d-b+1;w=c-a+1;p=[r[a:c+1]for r in g[b:d+1]];o=[[0]*10 for _ in R(10)]
     for y in R(11-h):
         for x in R(11-w):
             if all((g[y+i][x+j]==8)==(p[i][j]>0)for i in R(h)for j in R(w)):
