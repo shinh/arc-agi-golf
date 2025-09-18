c756c53edb5915fd3edec1c7c58de11282f6c09d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-18 12:40:40 +0900

    Golf task065 unique quadrant slicing

--- logic/task065.py
+++ logic/task065.py
@@ -1,3 +1,3 @@
 def p(g):# uniq q
  s=sum(g,[]);m=len(g);n=m>>1;d=s.index(min(s,key=s.count))
- return[[r[n+1:],r[:n]][d%m<n]for r in[g[n+1:],g[:n]][d<m*n]]
+ return[[r[-n:],r[:n]][d%m<n]for r in[g[-n:],g[:n]][d<m*n]]
