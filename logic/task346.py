1368608d25ab5a8af4d679467c21d289ff112bc0
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-19 00:30:59 +0900

    Use regexp for 346

--- logic/task346.py
+++ logic/task346.py
@@ -1,1 +1,2 @@
-p=lambda g,e=enumerate:next([[v]]for i,r in e(g)for j,v in e(r)if 7<sum(g[i+d][j-1:j+2].count(x)for d in(-1,0,1)if(x:=r[j-1])))# find intruder's center color by counting left color in 3x3
+import re
+p=lambda g:[[int(re.search(r"([1-9]), \1, \1,.*\1, ((?!\1)[1-9]), \1",str(g)).group(2))]]
