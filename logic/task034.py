16c330cdcbe8700556eaaa01e845a060715ff55f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-18 19:22:27 +0900

    Golf task034

--- logic/task034.py
+++ logic/task034.py
@@ -1,14 +1,14 @@
 def p(g):
     # extend from 2s
-    r=range(9)
-    z=[(y,x)for y in r for x in r if g[y][x]]
-    a=[(y,x)for y,x in z if g[y][x]==2]
-    c=next(g[y][x]for y,x in z if g[y][x]-2)
-    my,mx=map(min,zip(*z))
-    for y,x in z:g[y][x]=c
-    for y,x in a:
-        dy=1-2*(y==my);dx=1-2*(x==mx);t=z
-        while(t:=[(Y+dy,X+dx)for Y,X in t if 9>Y+dy>-1<X+dx<9]):
-            for Y,X in t:g[Y][X]=c
+    z=[(y,x,g[y][x])for y in range(9) for x in range(9) if g[y][x]]
+    c=next(v for y,x,v in z if v-2)
+    my,mx,_=map(min,zip(*z))
+    for y,x,v in z:
+        if v==2:
+            dy=1-2*(y==my);dx=1-2*(x==mx)
+            for Y,X,_ in z:
+                while-1<(Y:=Y+dy)<9>-1<(X:=X+dx)<9:
+                    g[Y][X]=c
+        g[y][x]=c
     return g
 
