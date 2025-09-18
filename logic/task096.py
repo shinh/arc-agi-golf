70e7a5c60655cd78cafc973c9d63303c453307cd
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-18 12:37:17 +0900

    Golf task096 solution

--- logic/task096.py
+++ logic/task096.py
@@ -1,20 +1,16 @@
 # 660
 # longest runs -> cross arms
 def p(g):
- bg=max(g[0],key=g[0].count);m=[];L=0
- for c in range(10):
+ R=range;bg=max(g[0],key=g[0].count);t=g+[*zip(*g)];m=[];L=0
+ for c in R(10):
   mx=0,
-  for r in g+[*zip(*g)]:
-   if c in r:
-    a=r.index(c);b=len(r)-r[::-1].index(c);l=r[a:b];k=len(l)
-    n1=next((i for i in range(k)if l[i]-c),k);n2=next((i for i in range(k)if l[~i]-c),k)
-    q=max(n1,n2);n=q*2+k-n1-n2
-    mx=max(mx,(n,q))
-  mx[0]and c-bg and(m.append([c,*mx])or(L:=max(L,mx[0])))
- o=[[bg]*L for _ in[0]*L]
+  for r in t:
+   if c in r:a=r.index(c);b=len(r)-r[::-1].index(c);k=b-a;n1=next((i for i in R(k)if r[a+i]-c),k);n2=next((i for i in R(k)if r[b-1-i]-c),k);mx=max(mx,(k+abs(n1-n2),max(n1,n2)))
+  if mx[0]and c-bg:L=max(L,mx[0]);m+=[(c,*mx)]
+ o=[[bg]*L for _ in R(L)]
  for c,n,q in m:
-  n+=n==2;y=(L-n)//2
-  for _ in[0]*4:
-   for i in range(q):o[y][y+i]=o[y+i][y]=c
+  n+=n==2;y=L-n>>1
+  for _ in R(4):
+   for i in R(q):o[y][y+i]=o[y+i][y]=c
    o=[*map(list,zip(*o[::-1]))]
  return o
