9eea65ed7f86d025b3d4b12ce8d0fc7fd101b251
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-18 12:39:46 +0900

    Golf task061 solution

--- logic/task061.py
+++ logic/task061.py
@@ -4,9 +4,9 @@ def p(g,p=1):
     for r in g:
         while all(r)*(r[:-p]!=r[p:]):p+=1
     # Complement the first square.
-    for y in range(18):
-        for x in range(18):
-            g[y%p][x%p]|=g[y][x]
+    R=range(18)
+    for y in R:
+        for x in R:g[y%p][x%p]|=g[y][x]
     # Complement the rest.
-    #return[(g[y%p][:p]*9)[:18]for y in range(18)]
-    return([(r[:p]*9)[:18]for r in g[:p]]*9)[:18]
+    #return[(g[y%p][:p]*9)[:18]for y in R]
+    return[(g[y%p][:p]*9)[:18]for y in R]
