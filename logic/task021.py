d0e58c4896bdbd54e1389d73daa4325e11878687
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-18 19:37:10 +0900

    Golf task021 solution

--- logic/task021.py
+++ logic/task021.py
@@ -1,2 +1,2 @@
 def p(g):
-    a=g[0][0];f=lambda t:-~sum(a not in r for r in t);return[[a]*f(zip(*g))]*f(g) # fill top-left block
+    a=g[0][0];return[[a]*-~sum(c!=a for c in g[0])]*-~sum(r[0]!=a for r in g) # fill top-left block
