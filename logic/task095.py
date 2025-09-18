927d60ff752179c65033f64c03b2939f2fdc3cda
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-18 13:47:28 +0900

    Golf task095 solution

--- logic/task095.py
+++ logic/task095.py
@@ -1,6 +1,5 @@
-def p(g,E=enumerate):# ring5
+def p(g,E=enumerate):#ring
  for r,R in E(g):
   for c,C in E(R):
-   if C>4:
-    for L in g[r-1:r+2]:L[c-1:c+2]=[1]*3;R[c]=5
+   for L in g[r-1:r+2]*(C>4):L[c-1:c+2]=1,1,1;R[c]=5
  return g
