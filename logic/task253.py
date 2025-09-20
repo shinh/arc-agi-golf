b7fd53697a6e041c2ee6a315b63497cfd42ff796
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-20 21:04:33 +0900

    Golf task253 logic

--- logic/task253.py
+++ logic/task253.py
@@ -1,10 +1,9 @@
 # rotate grid to reuse L pattern detection
 
 def p(j):
- c=[[0]*4for _ in[0]*4]
+ k=[]
  for _ in[0]*4:
-  for r,s in zip(j,j[1:]):
-   for a,b,d in zip(r,r[1:],s):
-    if a==b==d>0:c[0][0]=c[1][0]=c[0][1]=a
-  j=[*zip(*j[::-1])];c=[*map(list,zip(*c[::-1]))]
- return c
+  k+=[next((a for r,s in zip(j,j[1:])for a,b,d in zip(r,r[1:],s)if a==b==d>0),0)]
+  j=[*zip(*j[::-1])]
+ a,d,c,b=k
+ return[[a,a,b,b],[a,0,0,b],[d,0,0,c],[d,d,c,c]]
