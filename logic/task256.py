6d2f1c1594f868b43478812308cd05f27ef840e7
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-20 21:18:48 +0900

    Golf task256 solution

--- logic/task256.py
+++ logic/task256.py
@@ -1,6 +1,6 @@
 # triangles at 2
 def p(g):
- i=g.index(max(g));a=g[i].index(0)+i;t=a
- while t:y=a-t;g[y][:t]=[2+(i>y)-(y>i)]*t;t-=1
+ i=g.index(max(g));d=g[i].index(0);t=i+d
+ while t:g[i+d-t][:t]=[2+(t>d)-(t<d)]*t;t-=1
  return g
 
