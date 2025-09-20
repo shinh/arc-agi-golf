b9670a98cb2dc70c2848b09709de8d8e8a7b65d7
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-20 18:57:32 +0900

    Golf task343 solution

--- logic/task343.py
+++ logic/task343.py
@@ -1,7 +1,7 @@
 def p(g):# extend by prefix
  for r in g:
-  i=15
-  while i>0==r[i-1]:i-=1;p=1
-  while r[p:i]!=r[:i-p]:p+=1
+  while r[-1]<1<sum(r):r.pop()
+  p=1
+  while r[p:]!=r[:-p]:p+=1
   r[:]=(r[:p]*15)[:15]
  return g
