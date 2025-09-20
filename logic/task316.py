097db3422a9c0148545a2f36d0e2cbcba68a8cb2
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-20 21:02:01 +0900

    Golf task316 solution

--- logic/task316.py
+++ logic/task316.py
@@ -1,3 +1,2 @@
-# first nonzero per nonempty column, zigzag
-p=lambda m:((v:=[s for c in zip(*m)if(s:=sum(c))]+[0]*9)[:3],v[5:2:-1],v[6:9])
-
+# first nonzero per column zigzag
+p=lambda m:((v:=list(map(sum,filter(sum,zip(*m))))+[0]*9)[:3],v[5:2:-1],v[6:9])
