e1fc12ad3848a7d9e8f2c8ebcf46ff506ddddd02
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-20 18:28:13 +0900

    Golf task348 solution

--- logic/task348.py
+++ logic/task348.py
@@ -1,2 +1,2 @@
-# alternate columns expanding from marker
-p=lambda g:(x:=g[0].index(7),h:=sum(r[x]>0 for r in g),[len(g[0])>x+j>=0 and g[y].__setitem__(x+j,7+j%2)for y in range(h)for j in range(y-h+1,h-y)])and g
+# alt cols
+p=lambda g:(x:=g[0].index(7),h:=sum(r[x]>0for r in g),[[(abs(j-x)+y<h)*(7+(j+x&1))for j in range(len(r))]for y,r in enumerate(g)])[-1]
