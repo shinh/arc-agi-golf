fc624d8c4b47b995fcc3e0a372c82106f597420b
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-20 21:15:26 +0900

    Golf task269 solution

--- logic/task269.py
+++ logic/task269.py
@@ -1,2 +1,2 @@
-# scale grid by count of colored cells
-def p(g):s=sum;n=s(map(bool,s(g,[])));return s([[s(([x]*n for x in r),[])]*n for r in g],[])
+#scale color count
+def p(g):s=sum;n=s(map(bool,s(g,[])));return s((n*[s((n*[x]for x in r),[])]for r in g),[])
