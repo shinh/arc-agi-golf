684b47df6f187726895ca5fe8d70aa04d44b6f31
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-20 21:22:00 +0900

    Golf task386

--- logic/task386.py
+++ logic/task386.py
@@ -1,2 +1,2 @@
-# set 3 unless 7 or 5 at offset
-p=lambda g:[[3*(r[x]^7>0<r[x+4]^5)for x in(0,1,2)]for r in g]
+# 3 unless 7 or 5 at offset
+p=lambda g:[[3*(r[x]<7>r[x+4]<5)for x in(0,1,2)]for r in g]
