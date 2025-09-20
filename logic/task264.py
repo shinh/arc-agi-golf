902e501c7cbedf7675db852cb3392e58d2d8bc44
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-20 21:04:18 +0900

    Golf task 264 solution

--- logic/task264.py
+++ logic/task264.py
@@ -1,5 +1,5 @@
 def p(g):# copy tiles depending on non-5 positions
- o=[[0]*9for _ in[0]*9]
+ o=[[0]*9for _ in range(9)]
  k=10,15,14,21,0,27,22,33,26
  for y in range(len(g)-2):
   for x in range(len(g[0])-2):
@@ -7,7 +7,7 @@ def p(g):# copy tiles depending on non-5 positions
    for d in range(9):
     c=g[y+d//3][x+d%3]
     if c<1:break
-    v+=c%5 and d+2
+    if c%5:v+=d+2
    else:
     c=k.index(v)
     for d in range(9):
