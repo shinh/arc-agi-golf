6a3dd5885259c95364f78e1cc65bbc638a56be12
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-20 21:05:05 +0900

    Golf task230 solution

--- logic/task230.py
+++ logic/task230.py
@@ -1,7 +1,6 @@
 def p(j):
- # mark diagonals 1..4 around 2x2 block of 5s
+ # mark 1..4 diagonals around 2x2 5s
  for y in range(len(j)-1):
   for x in range(len(j[0])-1):
-   if 5==j[y][x]==j[y][x+1]==j[y+1][x]==j[y+1][x+1]:
-    j[y-1][x-1]=1;j[y-1][x+2]=2;j[y+2][x-1]=3;j[y+2][x+2]=4
+   if j[y][x:x+2]==j[y+1][x:x+2]==[5]*2:j[y-1][x-1:x+3:3]=1,2;j[y+2][x-1:x+3:3]=3,4
  return j
