d4b2d6270f60b8dd2b2f665302174c3a6af70fec
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-20 20:21:51 +0900

    Golf task308 solution

--- logic/task308.py
+++ logic/task308.py
@@ -1,15 +1,13 @@
 def p(g):# place shapes touching edges, else near center
-    d={}
-    for y,r in enumerate(g):
-        for x,v in enumerate(r):d[v]=d.get(v,[])+[(y,x)]
-    bg=max(d,key=lambda k:len(d[k]));del d[bg]
-    if not d:return[[bg]]
-    H=W=0
-    for c,v in d.items():
-        xs,ys=zip(*v);mi=min(xs);mj=min(ys);s=[(x-mi,y-mj)for x,y in v];a=max(xs)-mi+1;b=max(ys)-mj+1;d[c]=s,a,b;H=max(H,a);W=max(W,b)
+    f=sum(g,[]);bg=max(f,key=f.count);q={*f}-{bg}
+    if not q:return[[bg]]
+    H=W=0;S=[]
+    for c in q:
+        v=[(y,x)for y,r in enumerate(g)for x,t in enumerate(r)if t==c]
+        ys,xs=zip(*v);a=min(ys);b=min(xs);s=[(y-a,x-b)for y,x in v];h=max(ys)-a+1;w=max(xs)-b+1
+        S+=[(c,s,h,w)];H=max(H,h);W=max(W,w)
     O=[[bg]*W for _ in[0]*H]
-    for c,(s,a,b) in d.items():
-        *_,y,x=max((sum(y+i in(0,H-1)or x+j in(0,W-1)for i,j in s),-abs(H-a-2*y)-abs(W-b-2*x),y,x)for y in range(H-a+1)for x in range(W-b+1))
+    for c,s,h,w in S:
+        y,x=max((sum(y+i in(0,H-1)or x+j in(0,W-1)for i,j in s),-abs(H-h-2*y)-abs(W-w-2*x),y,x)for y in range(H-h+1)for x in range(W-w+1))[2:]
         for i,j in s:O[y+i][x+j]=c
     return O
-
