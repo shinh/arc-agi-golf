2db3cbe0c1642f3565fd0e82ad238e587a30b5a2
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-20 21:20:53 +0900

    Golf task250 solution

--- logic/task250.py
+++ logic/task250.py
@@ -1,5 +1,5 @@
 def p(g):#move 5s toward 2 cluster
- t=sum(g,[]);w=len(g[0]);y,x=divmod(t.index(2)+~w,w);o=[[2*(v==2)for v in r]for r in g]
- for i,v in enumerate(t):
-  if v>4:o[max(y,min(y+3,i//w))][max(x,min(x+3,i%w))]=5
+ t=sum(g,[]);y,x=divmod(t.index(2)-11,10);o=[[v&2 for v in r]for r in g]
+ for i in range(100):
+  if t[i]>4:o[min(y+3,max(y,i//10))][min(x+3,max(x,i%10))]=5
  return o
