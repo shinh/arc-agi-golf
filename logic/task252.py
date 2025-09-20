151b2166eb3fcb8b87aea6f64230896dcc1c448d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-20 21:07:07 +0900

    Golf task252 solution

--- logic/task252.py
+++ logic/task252.py
@@ -1,8 +1,7 @@
 def p(g):
- # use first row/col to draw diagonals
- r=range(len(g))
- for x in r[1::2]:
-  for y in r[x:]:
-   if g[0][y-x]:g[x][y]=4
-   if g[y-x][0]:g[y][x]=4
- return g
+	#use 1st row/col diag
+	for y in range(len(g)):
+		for x in range(1,y+1,2):
+			if g[0][y-x]:g[x][y]=4
+			if g[y-x][0]:g[y][x]=4
+	return g
