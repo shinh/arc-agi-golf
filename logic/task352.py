85098e489b9982609deb9e6d57d7eb5314d7ecec
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-20 16:40:40 +0900

    Golf task352

--- logic/task352.py
+++ logic/task352.py
@@ -1,1 +1,1 @@
-p=lambda g,e=enumerate:[v-2or[R.__setitem__(slice(c:=x-(x>0),x+2),[q or 1 for q in R[c:x+2]])for R in g[y-(y>0):y+2]]for y,r in e(g)for x,v in e(r)]and g# 1 near 2
+p=lambda g,e=enumerate:[v or r.__setitem__(x,any(2 in q[x-(x>0):x+2]for q in g[y-(y>0):y+2]))for y,r in e(g)for x,v in e(r)]and g# 1 near 2
