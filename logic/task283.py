d232d13b25299dd1a7a1f6b25647efd5cbab9dd1
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-20 21:17:05 +0900

    Golf task283 comprehension

--- logic/task283.py
+++ logic/task283.py
@@ -1,4 +1,4 @@
 def p(g):#frame 5-rect via set
- s={(i%10,i//10)for i,v in enumerate(sum(g,[]))if v>4}
+ s={(i%10,i//10)for i in range(100)if sum(g,[])[i]>4}
  for x,y in s:g[y][x]=len(s&{(x+1,y),(x-1,y),(x,y+1),(x,y-1)})*3%5
  return g
