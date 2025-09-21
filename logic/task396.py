66413f4156745294c140a7937b9fe334a334a83e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-21 09:46:33 +0900

    Refine task396 border detection

--- logic/task396.py
+++ logic/task396.py
@@ -1,5 +1,5 @@
 # crop largest border of the most common color and recolor to rarest
 def p(g):
  s=[v for v in sum(g,[])if v];a=max(s,key=s.count);b=min(s,key=s.count);H=len(g);W=len(g[0])
- _,y,x,h,w=max((h*w,y,x,h,w)for y in range(H)for x in range(W)for h in range(2,-~H-y)for w in range(2,-~W-x)if all(i*j*(h+~i)*(w+~j)or g[y+i][x+j]==a for i in range(h)for j in range(w)))
+ _,y,x,h,w=max((h*w,y,x,h,w)for y in range(H)for x in range(W)for h in range(2,-~H-y)for w in range(2,-~W-x)if g[y][x:x+w]==[a]*w==g[y+h-1][x:x+w]and all(r[x]==a==r[x+w-1]for r in g[y:y+h]))
  return[[[b,v][v!=a]for v in r[x:x+w]]for r in g[y:y+h]]
