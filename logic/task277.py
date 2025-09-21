cd5c370c5fed478f9952e8d1e9bfac2c61464aa3
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-21 09:49:22 +0900

    Golf task277

--- logic/task277.py
+++ logic/task277.py
@@ -6,13 +6,11 @@ def p(g):# flood fill shapes, count patterns, recolor odd one
    for i in q:
     for n in-11,-10,-9,-1,1,9,10,11:
      j=i+n
-     if 0<=j<100>-2<j%10-i%10<2 and g[j]:g[j]=0;q+=j,
+     if 0<=j<100 and g[j] and-2<j%10-i%10<2:g[j]=0;q+=j,
    s=tuple(j-q[0]for j in q)
    m[s]=m.get(s,[])+[q]
- t=min(m,key=lambda k:len(m[k]))
+ u=m[min(m,key=lambda k:len(m[k]))][0]
  for k in m:
-  c=1+(k==t)
   for q in m[k]:
-   for j in q:g[j]=c
-   c=1
+   for j in q:g[j]=1+(q is u)
  return[g[i:i+10]for i in range(0,91,10)]
