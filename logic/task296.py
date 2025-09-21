2ff55337fdc0acdafc09d5b1bf4c1e1d485f442b
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-21 09:48:03 +0900

    Golf task296 solution

--- logic/task296.py
+++ logic/task296.py
@@ -1,3 +1,2 @@
 def p(g):
- #max per region
- return[(r[0],max(r[1:6]),r[6])for r in(g[0],[*map(max,*g[1:4])],g[4])]
+ return[(a,max(b),c)for a,*b,c in(g[0],[*map(max,*g[1:4])],g[4])]#max per region
