e2b4ba73b88ac6d72b56c9f27c5c8f490d35f8eb
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-21 20:31:40 +0900

    Golf task369 solution

--- logic/task369.py
+++ logic/task369.py
@@ -1,8 +1,9 @@
 def p(g):
  # fill holes w/|size-4|
  def d(i,j):
-  if not(-1<i<10>j>-1)or g[i][j]:return[]
-  g[i][j]=1;return[(i,j)]+d(i+1,j)+d(i-1,j)+d(i,j+1)+d(i,j-1)
+  if 0<=i<10>j>=0==g[i][j]:
+   g[i][j]=1;return[(i,j),*d(i+1,j),*d(i-1,j),*d(i,j+1),*d(i,j-1)]
+  return[]
  for k in range(100):
-  for i,j in (s:=d(k//10,k%10)):g[i][j]=abs(len(s)-4)
+  for i,j in(s:=d(k//10,k%10)):g[i][j]=abs(len(s)-4)
  return g
