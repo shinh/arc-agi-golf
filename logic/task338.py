03faad3f4e53bbe11715a3ccbd09f0e535d00f10
Darren Smith <darrenks@golfscript.com>
2025-09-22 11:01:14 -0600

    ideas for 338

--- logic/task338.py
+++ logic/task338.py
@@ -1,5 +1,3 @@
-# Just solved.
-#
 # Based on 029
 # fill holes
 def p(g):
@@ -9,3 +7,30 @@ def p(g):
   except:0
  f(0,0);f(0,-1)
  return[[3*(v<1)for v in r]for r in g]
+
+# idea
+# make all blacks green then flood fill black+red (don't add neighbors if red)
+
+# idea
+# # test cases are all rectangles so just replace red black* red with green*
+# doesn't handle solid lines and multiple per line
+# def p(g):
+#     return [for r in g:
+#         try:
+#           a=r.index(2)
+#           b=r.index(2,a+1)
+#           r[a]=r[b]=0
+#           r[a+2:b+1]=[3]*(b-a-1)
+#         except:0
+#     return g
+
+# idea
+# def p(g):
+#     c=0
+#     return [[3 if (c:=c+v)==2 and not v else 0 for v in r]for r in g]
+
+
+# idea regex
+# import re
+# def p(g):
+#     return [for r in g]
--- scripts/scores.rb
+++ scripts/scores.rb
@@ -1,10 +1,10 @@
 # these scores are from https://docs.google.com/spreadsheets/d/e/2PACX-1vQ7RUqwrtwRD2EJbgMRrccAHkwUQZgFe2fsROCR1WV5LA1naxL0pU2grjQpcWC2HU3chdGwIOUpeuoK/pubhtml#gid=1427788625
 # a bit awkard to copy from that sheet since read online mode
-a=File.read("scores.txt").lines.to_a
+a=File.read("scripts/scores.txt").lines.to_a
 best= 400.times.map{|i|(a[i*2].split("\t").map(&:to_i)[2..-1]-[0]*100).min}
 
 ours=400.times.map{|i|
-  File.size("../submissions/task%03d.py"%(i+1))
+  File.size("submissions/task%03d.py"%(i+1))
 }
 z=1.upto(400).zip(best, ours)
 z.sort_by{|n,b,o|1.0*o/b}.each{|a|p a}
