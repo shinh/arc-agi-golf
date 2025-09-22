c53062b066b61a361130ea9547888ea5a4f877b8
Darren Smith <darrenks@golfscript.com>
2025-09-22 11:15:08 -0600

    golf 359

--- logic/task359.py
+++ logic/task359.py
@@ -1,2 +1,1 @@
-# row/col majority colors
-p=lambda g:(z:=[*zip(*g)],i:=sum(len({*r})for r in z)<sum(len({*r})for r in g),(o:=[[max(r,key=r.count)]*len(r)for r in(g,z)[i]]))and(o,[*zip(*o)])[i]
+p=lambda g,e=enumerate:[[max(a:=g[y]+[*[*zip(*g)][x]],key=a.count) for x,v in e(r)] for y,r in e(g)]
