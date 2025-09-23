b471ffa231951fc4ddf43374bc57bec03e96c803
Darren Smith <darrenks@golfscript.com>
2025-09-23 09:27:41 -0600

    golf 338

--- logic/task338.py
+++ logic/task338.py
@@ -1,12 +1,74 @@
-# Based on 029
-# fill holes
-def p(g):
- def f(x,y):
-  try:
-   if g[y][x]<2:g[y][x]=4;f(x,y+1);f(x,y-1);f(x+1,y);f(x-1,y)
-  except:0
- f(0,0);f(0,-1)
- return[[3*(v<1)for v in r]for r in g]
+# 155
+# def p(g):
+#  def f(x,y):
+#   try:
+#    if g[y][x]<2:g[y][x]=2;f(x,y+1);f(x,y-1);f(x+1,y);f(x-1,y)
+#   except:0
+#  f(0,0);f(0,-1)
+#  return [[0**v*3for v in r]for r in g]
+
+# 133
+# def p(g):
+#     g2=[]
+#     for r in g:
+#         r2 = []
+#         p = 0
+#         c = 0
+#         for v in r:
+#             c ^= v == 2
+#             if p and v: c = 0
+#             r2+=[(not v and c)*3]
+#             p=v
+#         g2+=[r2]
+#     return g2
+
+# c v p  new c
+# 0 0 0  0
+# 0 0 1  0
+# 0 1 0  1
+# 0 1 1  0
+# 1 0 0  1
+# 1 0 1  1
+# 1 1 0  0
+# 1 1 1  0
+
+# # 109
+# def p(g):
+#     g2=[]
+#     for r in g:
+#         r2 = []
+#         p = 0
+#         c = 0
+#         for v in r:
+#             c=(c^v)*0**(p*v)
+#             r2+=[(c>v)*3]
+#             p=v
+#         g2+=[r2]
+#     return g2
+
+
+# 93
+# def p(g):
+#     g2=[]
+#     for r in g:
+#         p = 0
+#         c = 0
+#         g2+=[[((c:=(c^v)*0**(p*v))>(p:=v))*3 for v in r]]
+#     return g2
+
+# 86 (more improvement with magic formulas still possible probably)
+p=lambda g,q=0,c=0:[[((c:=(c^v)*0**(q*v))>(q:=v))*3for v in g[0]]]+(g[1:]and p(g[1:]))
+
+
+# other (not working ideas):...
+
+# def p(g):
+#     og=[r*1 for r in g]
+#     for y in range(1,len(g)):
+#         for x in range(1,len(g)):
+#             if og[y-1][x] and og[y][x-1] and not g[y][x]: og[y][x]=g[y][x]=3
+#             else: g[y][x]=0
+#     return g
 
 # idea
 # make all blacks green then flood fill black+red (don't add neighbors if red)
@@ -15,13 +77,15 @@ def p(g):
 # # test cases are all rectangles so just replace red black* red with green*
 # doesn't handle solid lines and multiple per line
 # def p(g):
-#     return [for r in g:
-#         try:
-#           a=r.index(2)
-#           b=r.index(2,a+1)
-#           r[a]=r[b]=0
-#           r[a+2:b+1]=[3]*(b-a-1)
-#         except:0
+#     for r in g:
+#         p=1
+#         f=0
+#         for x,v in enumerate(r):
+#            if f%2 and v and p: f+=1
+#            if p==0 and v: f+=1
+#            if f%2 and not v: r[x]=3
+#            else: r[x]=0
+#            p=v
 #     return g
 
 # idea
@@ -29,8 +93,8 @@ def p(g):
 #     c=0
 #     return [[3 if (c:=c+v)==2 and not v else 0 for v in r]for r in g]
 
-
 # idea regex
 # import re
 # def p(g):
 #     return [for r in g]
+
