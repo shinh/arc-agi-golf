25b28da8d75422772a346e40f207bda68cec1ec6
Darren Smith <darrenks@golfscript.com>
2025-09-23 14:55:48 -0600

    126

--- logic/task126.py
+++ logic/task126.py
@@ -1,4 +1,28 @@
-def p(g):#arch->4
- for r,b in zip(g,g[1:]):
-  for j in range(1,len(r)-1):g[-1][j]|=4*(b[j-1]==b[j+1]==r[j]>b[j]<1)
- return g
+# def p(g):#arch->4
+#  for r,b in zip(g,g[1:]):
+#   for j in range(1,len(r)-1):g[-1][j]|=4*(b[j-1]==b[j+1]==r[j]>b[j]<1)
+#  return g
+
+# def p(g):
+#     for x in range(len(g[0])):
+#         g[-1][x] = 4*(len(g)-1==[*zip(*g)][x].count(0))
+#     return g
+
+
+# def p(g):
+#     g[-1]=[4*(len(g)-1==[*zip(*g)][x].count(0)) for x in range(len(g[0]))]
+#     return g
+
+# 83
+# p=lambda g:g[:-1]+[[4*(len(g)-1==[*zip(*g)][x].count(0)) for x in range(len(g[0]))]]
+
+# 62
+p=lambda g:g[:-1]+[[4*(len(g)-1==x.count(0))for x in zip(*g)]]
+
+#
+# def p(g):
+#     t=[*zip(*g)]
+#     for x in range(1,len(g[0])-1):
+#         g[-1][x] = 4*(t[x-1]==t[x+1])
+#     return g
+
