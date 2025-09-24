91cd73e52c38d9d734ba42a9336cc6e0530d4e42
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-24 09:25:35 +0900

    Golf task204 solution

--- logic/task204.py
+++ logic/task204.py
@@ -1,9 +1,7 @@
 # fill square interiors by size parity
 def p(g):
- for i in range(len(g)-1):
-  a,b=g[i:i+2]
+ for i,(a,b) in enumerate(zip(g,g[1:])):
   for j in range(len(a)-1):
-   if a[j]==b[j]==a[j+1]==1>b[j+1]:
-    s=[(n:=(a+[0])[j:].index(0))%2*5+2]*(n-2)
-    for m in range(n-2):g[i+m+1][j+1:j+n-1]=s
+   if b[j+1]<1==a[j]==a[j+1]==b[j]:
+    for r in g[i+1:i+(n:=(a+[0])[j:].index(0))-1]:r[j+1:j+n-1]=[(n&1)*5+2]*(n-2)
  return g
