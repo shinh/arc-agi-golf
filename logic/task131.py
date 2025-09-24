5c439985ce468269dbf6e5f9f95181ca9984fe0f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-24 09:46:43 +0900

    Golf task131

--- logic/task131.py
+++ logic/task131.py
@@ -1,9 +1,7 @@
 def p(g):
  #drop3sunder2add8
- w=len(g[0])
- if w>len(g):return [*zip(*p([*zip(*g)]))]
+ if (w:=len(g[0]))>(h:=len(g)):return [*zip(*p([*zip(*g)]))]
  s=sum(map(list,g),[])
- a=s.index(2)//w;b=s.index(3)//w;s=len(g)-s[::-1].index(3)//w
- if b<a:return p(g[::-1])[::-1]
- return g[:a+1]+g[b:s]+[(8,)*w]+[(0,)*w]*(len(g)-a+b-s-2)
+ a=s.index(2)//w;b=s.index(3)//w;s=h-s[::-1].index(3)//w
+ return p(g[::-1])[::-1]if b<a else g[:a+1]+g[b:s]+[(8,)*w]+[(0,)*w]*(h-a+b-s-2)
 
