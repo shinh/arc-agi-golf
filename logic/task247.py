6cc10e67ebf3ad5edb8c7df7fe49b15531fb51b6
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-24 09:07:47 +0900

    Golf task247 solution

--- logic/task247.py
+++ logic/task247.py
@@ -1,4 +1,2 @@
-def p(g):# flatten and count
-    s=sum(g,[])
-    t=sorted((s.count(i),s.index(i)%len(g[0]),i)for i in {*s}-{0})
-    m=t[-1][0];return[[i for a,b,i in t if a==m]]*m
+def p(g):#
+    s=sum(g,[]);t=sorted((s.count(i),s.index(i)%10,i)for i in{*s}-{0});m=t[-1][0];return[[c for a,_,c in t if a==m]]*m
