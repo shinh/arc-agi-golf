dad1fae05551d41f30d1f7963cef4af713b5458b
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-24 09:26:42 +0900

    Golf task198

--- logic/task198.py
+++ logic/task198.py
@@ -1,3 +1,3 @@
 # rotate grid and paint
-# 151
-p=lambda g,n=79:-n*g or p([[[a,3+(b>0)*(b!=3)*(b in c+[4])][a in(0,3)]for a,b,*c in zip(r,[*r[1:],3],r[2:]+(3,3),[3,*r])]for r in zip(*g[::-1])],n-1)
+# 147
+p=lambda g,n=23:-n*g or p([[[a,3+(b>0<3!=b)*(b in c+[4])][a in(0,3)]for a,b,*c in zip(r,[*r[1:],3],r[2:]+(3,3),[3,*r])]for r in zip(*g[::-1])],n-1)
