e416b4d0b385134c3f1b7c2a51a2ce5708704c64
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-24 09:05:23 +0900

    Golf task266

--- logic/task266.py
+++ logic/task266.py
@@ -1,7 +1,5 @@
-def p(g):
- # fill diagonals around 2
- i=sum(g,[]).index(2);g[x:=i//5][y:=i%5]=0
+def p(g):# fill diagonals around 2
+ i=sum(g,[]).index(2);g[i//5][i%5]=0
  for v in 3,8,6,7:
-  if 3>(X:=x-1+2*(v>6))>-1<(Y:=y-1+2*(5<v<8))<5:g[X][Y]=v
+  if 3>(X:=i//5+(v>6)*2-1)>-1<(Y:=i%5+(v&4)//2-1)<5:g[X][Y]=v
  return g
-
