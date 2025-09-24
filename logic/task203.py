62811a55aa7790721c5b760a026a9b6773c19de3
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-24 09:25:43 +0900

    Golf task203 solution

--- logic/task203.py
+++ logic/task203.py
@@ -1,6 +1,5 @@
-def p(g,r=range,i=0):# fill concentric frames using reversed diagonal
-    n=len(g)
-    for c in[g[i][i]for i in r(n//2)][::-1]:
-        for j in r(i,n-i):g[i][j]=g[~i][j]=g[j][i]=g[j][~i]=c
-        i+=1
+def p(g,r=range):# fill concentric frames using reversed diagonal
+    n=len(g);h=[g[i][i]for i in r(n//2)]
+    for i in r(n//2):
+        for j in r(i,n-i):g[i][j]=g[~i][j]=g[j][i]=g[j][~i]=h[~i]
     return g
