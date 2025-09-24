10fd5ff48125e83d623d2879f35a653f640399a4
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-24 09:35:09 +0900

    Golf task121 solution

--- logic/task121.py
+++ logic/task121.py
@@ -1,2 +1,2 @@
-def p(g):#3x3 around 8->center=2nd max
-    y,x=divmod(sum(g,[]).index(8),len(g[0]));g=[t[x-1:x+2]for t in g[y-1:y+2]];g[1][1]=sorted(sum(g,[]))[7];return g
+def p(g):#crop 3x3 around 8; center=2nd max
+	s=sum(g,[]);g=[s[s.index(8)+o:][:3]for o in(-14,-1,12)];g[1][1]=sorted(sum(g,[]))[7];return g
