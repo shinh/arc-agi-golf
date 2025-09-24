b67899bd5f133f2ea270fdc60076c15ed5530c31
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-24 09:32:00 +0900

    Golf task169 recursion fill

--- logic/task169.py
+++ logic/task169.py
@@ -1,12 +1,12 @@
 def p(g):
     # recolor 5-blocks by size
+    def f(y,x):
+        if 9>=y>=0<=x<=9 and g[y][x]==5:
+            g[y][x]=0;q.append((y,x))
+            f(y+1,x);f(y-1,x);f(y,x+1);f(y,x-1)
     for y in range(10):
         for x in range(10):
             if g[y][x]==5:
-                q=[(y,x)];g[y][x]=0
-                for i,j in q:
-                    for A,B in(i+1,j),(i-1,j),(i,j+1),(i,j-1):
-                        if 0<=A<10 and 0<=B<10 and g[A][B]==5:
-                            g[A][B]=0;q+=(A,B),
+                q=[];f(y,x)
                 for i,j in q:g[i][j]=5-len(q)
     return g
