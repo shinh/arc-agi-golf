3e1f0d53a9ad7945830781e5258236f88b0d7e04
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-24 09:19:23 +0900

    Golf task224 solution

--- logic/task224.py
+++ logic/task224.py
@@ -1,5 +1,6 @@
 def p(g):# draw inner color rectangle just inside 5s
-    y,x=zip(*[(y,x)for y,r in enumerate(g)for x,c in enumerate(r)if c==5]);t=min(y)+1;b=max(y);l=min(x)+1;r=max(x);c=next(g[y][x]for y in range(t,b)for x in range(l,r)if g[y][x]%5)
-    for x in range(l,r):g[t][x]=g[b-1][x]=c
-    for y in range(t,b):g[y][l]=g[y][r-1]=c
+    y,x=zip(*((y,x)for y,r in enumerate(g)for x,c in enumerate(r)if c==5));t=min(y)+1;b=max(y);l=min(x)+1;r=max(x);h=g[t:b]
+    c=max(max(w[l:r])for w in h)
+    h[0][l:r]=h[-1][l:r]=[c]*(r-l)
+    for w in h:w[l]=w[r-1]=c
     return g
