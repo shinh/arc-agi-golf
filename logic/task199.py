04eeb50544da9f80095d7849f689f7a224ab440e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-24 09:26:32 +0900

    Golf task199 solution

--- logic/task199.py
+++ logic/task199.py
@@ -1,6 +1,6 @@
-def p(g,A=enumerate):#stripe
+def p(g,A=enumerate):#bar
  for y,r in A(g):
   for x,v in A(r):
-   if 0<v!=4:
+   if~4&v:
     for R in g[:y+1]:R[x&1::2]=[4]*len(R[x&1::2])
     g[y+1][x]=v;return g
