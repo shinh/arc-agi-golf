a6319636b2f34540c078af4f174eece5b0f53e92
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-24 09:28:37 +0900

    Golf task190

--- logic/task190.py
+++ logic/task190.py
@@ -1,5 +1,4 @@
 # ray casting from 2x2 block
 def p(g,o=3):
-    for z in range(49):
-        if(c:=g[y:=z//7][x:=z%7])>g[y+2][x+1]==0<g[y+2][x+2]:g[y+3][x+3]=c
+    for z in range(49):g[(y:=z//7)+3][(x:=z%7)+3]|=g[y][x]*(g[y+2][x+1]<1<=g[y+2][x+2])
     return-o*g or p([*map(list,zip(*g[::-1]))],o-1)
