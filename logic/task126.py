550ad489938589362d0181f5741fddc46782e40b
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-24 09:47:04 +0900

    Golf task126 arch detection

--- logic/task126.py
+++ logic/task126.py
@@ -1,4 +1,4 @@
 def p(g):#arch->4
  for r,b in zip(g,g[1:]):
-  for j in range(1,len(r)-1):g[-1][j]|=4*(b[j-1]==b[j+1]==r[j]>b[j]<1)
+  for j in range(1,len(r)-1):g[-1][j]|=4*(b[j-1]==b[j+1]==r[j]>b[j])
  return g
