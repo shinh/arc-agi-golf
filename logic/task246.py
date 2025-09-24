d6bfe91c9af0241b52d51364ce23dab7ddfd66c0
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-09-24 09:07:59 +0900

    Golf task 246

--- logic/task246.py
+++ logic/task246.py
@@ -1,5 +1,5 @@
 def p(g):
     # connect2-3 w8s
-    f=sum(g,[]);w=len(g[0]);(y,x),(Y,X)=[divmod(f.index(i),w)for i in(2,3)]
-    while y-Y or x-X:x+=(X>x)-(X<x);g[y][x]=8;y+=(x==X)*((Y>y)-(Y<y))
+    f=sum(g,[]);w=len(g[0]);a=f.index;y,x=divmod(a(2),w);Y,X=divmod(a(3),w)
+    while(y-Y)|(x-X):x+=X>x;x-=X<x;g[y][x]=8;y+=x==X and((Y>y)-(Y<y))
     return g
