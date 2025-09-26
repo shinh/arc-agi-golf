f9444f293c34b290d01cdeffd95dcbb0ffc5f0d1
Darren Smith <darrenks@golfscript.com>
2025-09-25 20:52:34 -0600

    first pass at 367

--- logic/task367.py
+++ logic/task367.py
@@ -1,14 +1,13 @@
-def p(g):
-    # flood-fill 0 rectangles
-    h=len(g);w=len(g[0])
-    for i,r in enumerate(g):
-        for j,v in enumerate(r):
-            if v:continue
-            q=[(i,j)];r[j]=1
-            for x,y in q:
-                for X,Y in((x+1,y),(x-1,y),(x,y+1),(x,y-1)):
-                    if-1<X<h and-1<Y<w and g[X][Y]<1:g[X][Y]=1;q+=[(X,Y)]
-            x,y=zip(*q);a,A=min(x),max(x);b,B=min(y),max(y)
-            k=len(q)==(A-a+1)*(B-b+1)and all(g[x][y]-5 for x,y in zip((a-2,a-1)*2+(A+2,A+1)*2,(b-1,b-2,B+1,B+2)*2)if-1<x<h and-1<y<w)and 4
-            for x,y in q:g[x][y]=k
+# if in a black inside corner that doesn't extend make yellow
+# if black and yellow neighbor make yellow
+# rotate 4x to only check rule in 1 direction
+def p(g,R=range(20)):
+    for _ in R:
+        for y in R:
+            for x in R:
+                try:
+                    if g[y][x]==0 and g[y+1][x]==4 or g[y][x]==g[y+2][x+1]==g[y+1][x+2]==0 and g[y+1][x]==g[y][x+1]==5:
+                        g[y][x]=4
+                except:0
+        g = [*map(list,zip(*g[::-1]))]
     return g
