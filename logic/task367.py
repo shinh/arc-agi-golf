503ef69216babf87e56ca40b3a6e5511076f1559
Darren Smith <darrenks@golfscript.com>
2025-09-26 09:46:25 -0600

    optimize 367 a bit

--- logic/task367.py
+++ logic/task367.py
@@ -1,13 +1,25 @@
 # if in a black inside corner that doesn't extend make yellow
 # if black and yellow neighbor make yellow
 # rotate 4x to only check rule in 1 direction
+# def p(g,R=range(20)):
+#     for _ in R:
+#         for y in R:
+#             for x in R:
+#                 try:
+#                     if g[y][x]==0 and g[y+1][x]==4 or g[y][x]==g[y+2][x+1]==g[y+1][x+2]==0 and g[y+1][x]==g[y][x+1]==5:
+#                         g[y][x]=4
+#                 except:0
+#         g = [*map(list,zip(*g[::-1]))]
+#     return g
+
 def p(g,R=range(20)):
     for _ in R:
+        t=g[::-1]
+        g = [*map(list,zip(*t))]
         for y in R:
             for x in R:
                 try:
-                    if g[y][x]==0 and g[y+1][x]==4 or g[y][x]==g[y+2][x+1]==g[y+1][x+2]==0 and g[y+1][x]==g[y][x+1]==5:
-                        g[y][x]=4
+                    1/(g[y][x:x+2]==[0,4] or t[x+1][y:y+3]== g[y+1][x:x+3]==[5,5,0])
+                    g[y][x]=4
                 except:0
-        g = [*map(list,zip(*g[::-1]))]
     return g
