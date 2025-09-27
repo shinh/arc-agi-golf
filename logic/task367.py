e56e5ef42baac2fe6ea149cf24b43cc5cad163d9
Darren Smith <darrenks@golfscript.com>
2025-09-27 09:21:13 -0600

    golf 367

--- logic/task367.py
+++ logic/task367.py
@@ -12,14 +12,41 @@
 #         g = [*map(list,zip(*g[::-1]))]
 #     return g
 
-def p(g,R=range(20)):
-    for _ in R:
+# 202
+# def p(g,R=range(20)):
+#     for _ in R:
+#         t=g[::-1]
+#         g = [*map(list,zip(*t))]
+#         for y in R:
+#             for x in R:
+#                 try:
+#                     1/(g[y][x:x+2]==[0,4] or t[x+1][y:y+3]== g[y+1][x:x+3]==[5,5,0])
+#                     g[y][x]=4
+#                 except:0
+#     return g
+
+# 181
+# def p(g):
+#     for i in range(8000):
+#         t=g[::-1]
+#         g = [*map(list,zip(*t))]
+#         x = i % 23
+#         y = i % 19
+#         try:
+#             1/(g[y][x:x+2]==[0,4] or t[x+1][y:y+3]==g[y+1][x:x+3]==[5,5,0])
+#             g[y][x]=4
+#         except:0
+#     return g
+
+# 179
+def p(g,i=8000):
+    while i:
+        i-=1
         t=g[::-1]
         g = [*map(list,zip(*t))]
-        for y in R:
-            for x in R:
-                try:
-                    1/(g[y][x:x+2]==[0,4] or t[x+1][y:y+3]== g[y+1][x:x+3]==[5,5,0])
-                    g[y][x]=4
-                except:0
+        x = i % 23
+        y = i % 19
+        try:
+            g[y][x//(g[y][x:x+2]==[0,4] or t[x+1][y:y+3]==g[y+1][x:x+3]==[5,5,0])]=4
+        except:0
     return g
