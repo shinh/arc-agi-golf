3beb3e1a7e1bd6367c4e0ba21d3e8b10cb4e7792
Darren Smith <darrenks@golfscript.com>
2025-09-29 09:36:16 -0600

    failed to improve 126

--- logic/task126.py
+++ logic/task126.py
@@ -2,7 +2,7 @@
 #  for r,b in zip(g,g[1:]):
 #   for j in range(1,len(r)-1):g[-1][j]|=4*(b[j-1]==b[j+1]==r[j]>b[j]<1)
 #  return g
-
+#
 # def p(g):
 #     for x in range(len(g[0])):
 #         g[-1][x] = 4*(len(g)-1==[*zip(*g)][x].count(0))
@@ -24,8 +24,23 @@
 # p=lambda g:g[:-1]+[[x.count(max(1,*x))%2*4for x in zip(*g)]]
 
 # 58
-p=lambda g:g[:-1]+[[sum(x)/max(1,*x)%2*4for x in zip(*g)]]
+# p=lambda g:g[:-1]+[[sum(x)/max(1,*x)%2*4for x in zip(*g)]]
+#
+# p=lambda g:g[:-1]+[[sum(map(bool,x))%2*4for x in zip(*g)]]
+# p=lambda g:g[:-1]+[[( {*x[1::2]}!={*x[::2]})*4for x in zip(*g)]]
+# p=lambda g:g[:-1]+[[len(g)%x.count(0)%2*4for x in zip(*g)]]
+# p=lambda g,v=7:g[:-1]+[[(v==2*(v:=sum(x)))*4for x in zip(*g)]]
+# p=lambda g:g[:-1]+[[x.count(max(x))%2*4for x in zip(*g)]]
 
+# def p(g):
+#     for a,b in zip(g,g[1:]):
+#         for c,d,z in zip(a,b[1:],range(19)):
+#             if c==d>0:
+#                 g[-1][z]=4
+#     return g
+
+# 58
+p=lambda g:g[:-1]+[[(sum(x)==max(x)>0)*4for x in zip(*g)]]
 
 #
 # def p(g):
