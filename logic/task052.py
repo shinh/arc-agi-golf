0da71f7f76c3705bc0f8fee915f34c398fa5d429
Darren Smith <darrenks@golfscript.com>
2025-10-02 11:04:08 -0600

    52

--- logic/task052.py
+++ logic/task052.py
@@ -1,1 +1,12 @@
-p=lambda g:[[(r==r[:1]*3)*5]*3 for r in g]# uniform row -> 5 else 0
+# p=lambda g:[[(r==r[:1]*3)*5]*3for r in g]# uniform row -> 5 else 0
+# p=lambda g:[[(len({*r})<2)*5]*3for r in g]# uniform row -> 5 else 0
+# p=lambda g:[[1//len({*r})*5]*3for r in g]# uniform row -> 5 else 0
+# idea
+# p=lambda g:[[len({*r})*3%7]*3for r in g]# uniform row -> 5 else 0
+
+
+# p=lambda g:[[(a==b==c)*5]*3for a,b,c in g]# uniform row -> 5 else 0
+# p=lambda g:[[(a,a==b)*5]*3for a,*b in g]# uniform row -> 5 else 0
+
+# there's always a max of 2 different colors so this works since len never 3
+p=lambda g:[[len({*r})%2*5]*3for r in g]# uniform row -> 5 else 0
