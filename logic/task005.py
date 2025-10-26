c55d3ed15b7180140a2f85e8ddced69866da1e73
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 17:57:44 +0900

    Golf task005 solution

--- logic/task005.py
+++ logic/task005.py
@@ -1,10 +1,9 @@
 def p(g):
- _,u,v=max((sum(g[u+t//3][v+t%3]>0for t in range(9)),u,v)for u in range(19)for v in range(19))
+ _,u,v=max((sum(0<g[u+t//3][v+t%3]for t in range(9)),u,v)for u in range(19)for v in range(19))
  for a in-4,0,4:
   for b in-4,0,4:
-   if a|b and(c:=max(g[u+t//3+a][v+t%3+b]for t in range(9))):
+   if c:=a|b and max(g[u+t//3+a][v+t%3+b]for t in range(9)):
     for t in range(9):
-     if g[u+t//3][v+t%3]:
-      r=u+t//3+a;w=v+t%3+b
-      while-1<r<21>w>-1:g[r][w]=c;r+=a;w+=b
+     if g[(r:=u+t//3)][(w:=v+t%3)]:
+      while-1<(r:=r+a)<21 and-1<(w:=w+b)<21:g[r][w]=c
  return g
