6bc3d5aada1a1dae153a4103beaea4ccbf77bfc4
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 07:50:19 +0900

    Golf task170

--- logic/task170.py
+++ logic/task170.py
@@ -1,4 +1,4 @@
 def p(g):
-  b=bytes(map(any,g));r=b.rfind(1);B=bytes(map(bool,g[r]));a=B.find(1);B=B.rfind(1)-a+1;d=g[r-B+1:r+1];x=[u[a:a+B]for u in d];b=[0]*B
-  for u in d:u[a:a+B]=b
-  b=bytes(map(any,g));C=b.find(1);r=b.rfind(1);a=bytes(map(any,zip(*g))).find(1);s=(r-C+1)//B;return[[g[C+i*s][a+j*s]and x[i][j]for j in range(B)]for i in range(B)]
+  b=bytes(map(any,g));r=b.rfind(1);b=bytes(map(bool,g[r]));a=b.find(1);B=b.rfind(1)-a+1;y=r-B+1;R=range(B);x=[g[y+i][a:a+B]for i in R];b=[0]*B
+  for i in R:g[y+i][a:a+B]=b
+  b=bytes(map(any,g));C=b.find(1);r=b.rfind(1);a=bytes(map(any,zip(*g))).find(1);s=(r-C+1)//B;return[[g[C+i*s][a+j*s]and x[i][j]for j in R]for i in R]
