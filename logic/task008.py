46976c5b041ae3bf368e63deacd49d51a568f3b7
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 17:51:18 +0900

    Golf task008 solution

--- logic/task008.py
+++ logic/task008.py
@@ -1,5 +1,3 @@
 def p(g):
- for _ in'1111':
-  x=bytes(map(max,g));a=x.find(2);b=x.rfind(2)+1;c=x.find(8)
-  g=[*zip(*((g,g[:a]+g[b:c]+g[a:b]+g[c:])[c>b]))][::-1]
+ for _ in'<'*4:x=bytes(map(max,g));f=x.find;a=f(2);b=x.rfind(2)+1;c=f(8);g=[*zip(*(g,g[:a]+g[b:c]+g[a:b]+g[c:])[c>b])][::-1]
  return g
