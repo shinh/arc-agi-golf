a28200f40b47d08de1d71f796b48aff19cc25cc0
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 07:42:58 +0900

    Golf task368 solution

--- logic/task368.py
+++ logic/task368.py
@@ -1,12 +1,13 @@
-def p(g):# copy first non-05 block onto 5
- for y in range(10):
-  for x in range(10):
+def p(g):# copy block to 5
+ r=range
+ for y in r(10):
+  for x in r(10):
    if g[y][x]%5:
     h=w=1
     while y+h<10 and g[y+h][x]%5:h+=1
     while x+w<10 and g[y][x+w]%5:w+=1
-    for Y in range(11-h):
-     for X in range(11-w):
+    for Y in r(11-h):
+     for X in r(11-w):
       if g[Y][X]==5:
-       for L in range(h):g[Y+L][X:X+w]=g[y+L][x:x+w]
+       for L in r(h):g[Y+L][X:X+w]=g[y+L][x:x+w]
     return g
