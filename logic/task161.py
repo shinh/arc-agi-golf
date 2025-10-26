e5e8362e3efb9a16f996fc52f3f222173bca786d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 18:22:04 +0900

    Golf task 161 solution

--- logic/task161.py
+++ logic/task161.py
@@ -1,2 +1,1 @@
-def p(g):
- k=max(set(sum(g,[]))-{x for r in g[1:-1]for x in r[1:-1]});return[[k*(k in(r[0],r[~0],a,b))for a,b in zip(g[0],g[~0])]for r in g]
+def p(g):k=max({*sum(g,[])}-{*sum((r[1:-1]for r in g[1:-1]),[])});return[[k*(k in(r[0],r[~0],*z))for z in zip(g[0],g[~0])]for r in g]
