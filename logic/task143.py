987c30c6f41d6eb63254ae1fb459aa9e73d9c5d0
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 07:41:06 +0900

    Golf task143 logic

--- logic/task143.py
+++ logic/task143.py
@@ -1,5 +1,5 @@
 def p(g):
- t=sum(g,[]);P=[i for i in range(23)if t[i]*(i%10<3)];b=1
- while len({t[b+j]for j in P})^1|t.count(t[b+P[0]])-len(P):b+=1
- for j in P:t[b+j]=5
- return[*zip(*[iter(t)]*10)]
+ t=sum(g,[]);P=[i for i in range(23)if t[i]>0>i%10-3];b=1
+ while len({t[b+j]for j in P})^1|len(P)^t.count(t[b+P[0]]):b+=1
+ for j in P:g[(b+j)//10][(b+j)%10]=5
+ return g
