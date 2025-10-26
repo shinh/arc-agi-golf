17aba3c98fff044668eb85e3cffcc952ea24fb33
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 17:20:14 +0900

    Golf task153 solution

--- logic/task153.py
+++ logic/task153.py
@@ -1,6 +1,6 @@
 def p(g):
  s=sum(g,[]);a=max({*s}-{0},key=s.count);b=sum({*s})-a
- h=[*zip(*filter(any,zip(*filter(any,[[x*(x==a)for x in r]for r in g]))))]
+ h=[*zip(*[c for c in zip(*[r for r in g if a in r])if a in c])]
  for _ in 0,1:
-  k=3-len(h);z=(0,)*len(h[0]);h=[z]*k*(0 in h[0])+h+[z]*k*(1-(0 in h[0]));h=[*zip(*h)]
+  k=3-len(h);z=(0,)*len(h[0]);t=0 in h[0];h=[z]*k*t+h+[z]*k*(1-t);h=[*zip(*h)]
  return[[x or b for x in r]for r in h]
