236f498709bbe96ed530518dc29ee9b4ee90faa2
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 07:45:43 +0900

    Golf task101 pattern scaling

--- logic/task101.py
+++ logic/task101.py
@@ -1,3 +1,4 @@
+# flood fill pattern then stamp scaled copies
 def p(A):
  n=len(A);m=len(A[0]);M=[];G=[]
  def o(i,j):
@@ -14,18 +15,15 @@ def p(A):
  for i in range(n):
   for j in range(m):
    if A[i][j]==2:
-    a=1
-    while i+a<n and A[i+a][j]==2:a+=1
-    b=1
-    while j+b<m and A[i][j+b]==2:b+=1
-    s=min(a,b)
+    s=1
+    while i+s<n and j+s<m and A[i+s][j]==2 and A[i][j+s]==2:s+=1
     for r,t in M:
      for x in range(s):
       for y in range(s):
        u=i+(r-P)*s+x;v=j+(t-Q)*s+y
-       if 0<=u<n and 0<=v<m:
+       if-1<u<n and-1<v<m:
         A[u][v]==2 and G.append((u,v))
         A[u][v]=1
- for i,j in M: A[i][j]=1
- for i,j in G: A[i][j]=2
+ for i,j in M:A[i][j]=1
+ for i,j in G:A[i][j]=2
  return A
