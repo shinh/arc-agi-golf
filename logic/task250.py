71d6457e7536daf60df74b160bb4e46f3859ea42
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 18:02:41 +0900

    Golf task250 solution

--- logic/task250.py
+++ logic/task250.py
@@ -1,5 +1,4 @@
 def p(g):
  s=sum(g,[]);a,b=divmod(s.index(2),10)
- for i in range(100):
-  if s[i]&1:g[i//10][i%10]=0;g[min(a+2,max(a-1,i//10))][min(b+2,max(b-1,i%10))]=5
+ while 5 in s:g[(i:=s.index(5))//10][i%10]=s[i]=0;g[min(a+2,max(a-1,i//10))][min(b+2,max(b-1,i%10))]=5
  return g
