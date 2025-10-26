1f3686107947d4a0aa71c330cb7e1766f0a3aa46
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 17:42:29 +0900

    Golf task143 solution

--- logic/task143.py
+++ logic/task143.py
@@ -1,5 +1,5 @@
 def p(g):
- t=sum(g,[]);P=[i for i in range(23)if i%10<3 and t[i]]
- b=next(i for i in range(1,78)if len({t[i+j]for j in P})<2 and t.count(t[i+P[0]])==len(P))
+ t=sum(g,[]);P=[i for i in range(23)if t[i]*(i%10<3)];b=1
+ while len({t[b+j]for j in P})^1|t.count(t[b+P[0]])-len(P):b+=1
  for j in P:t[b+j]=5
  return[*zip(*[iter(t)]*10)]
