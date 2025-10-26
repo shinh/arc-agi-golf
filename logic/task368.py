6076b3b4226052d42a3929907c53f56d2472f032
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 17:33:47 +0900

    Golf task368 with fixed grid dimensions

--- logic/task368.py
+++ logic/task368.py
@@ -1,13 +1,12 @@
 def p(g):# copy first non-05 block onto 5
- c=len(g)
- for y in range(c):
-  for x in range(c):
+ for y in range(10):
+  for x in range(10):
    if g[y][x]%5:
     h=w=1
-    while y+h<c and g[y+h][x]%5:h+=1
-    while x+w<c and g[y][x+w]%5:w+=1
-    for Y in range(c-h+1):
-     for X in range(c-w+1):
+    while y+h<10 and g[y+h][x]%5:h+=1
+    while x+w<10 and g[y][x+w]%5:w+=1
+    for Y in range(11-h):
+     for X in range(11-w):
       if g[Y][X]==5:
        for L in range(h):g[Y+L][X:X+w]=g[y+L][x:x+w]
     return g
