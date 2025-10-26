165928b42acb0884c70d5a126cb54b5e101a4963
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 17:18:26 +0900

    Golf task184

--- logic/task184.py
+++ logic/task184.py
@@ -1,1 +1,1 @@
-p=lambda g:(R:=[-1,*[i for i,r in enumerate(g)if max(r)<1],len(g)],C:=[-1,*[i for i,c in enumerate(zip(*g))if max(c)<1],len(g[0])],[[max(max(r[x+1:y])for r in g[a+1:b])for x,y in zip(C,C[1:])]for a,b in zip(R,R[1:])])[-1]
+p=lambda g:(f:=lambda a:[~0,*[i for i,r in enumerate(a)if max(r)<1],len(a)],C:=f([*zip(*g)]),[[max(max(r[x+1:y])for r in g[a+1:b])for x,y in zip(C,C[1:])]for a,b in zip(f(g),f(g)[1:])])[-1]
