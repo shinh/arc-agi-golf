d3c5313e1369a245fc75f3234c43dd0cc177788c
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 17:49:41 +0900

    Optimize task099 solution

--- logic/task099.py
+++ logic/task099.py
@@ -1,7 +1,7 @@
 def p(g):
-  for l in 0,1,4:
-    k=[i for i,r in enumerate(g)if r[l]*r[l+4]]
-    if k:
-      for r in g[k[0]-1:k[-1]+1]:
-        r[l:l+5]=[v or max(g[i][l+2]for i in k)for v in r[l:l+5]]
-  return g
+ for l in 0,1,4:
+  k=[(r[l+2],i)for i,r in enumerate(g)if r[l]*r[l+4]]
+  if k:
+   for r in g[k[0][1]-1:k[-1][1]+1]:
+    r[l:l+5]=[v or max(k)[0]for v in r[l:l+5]]
+ return g
