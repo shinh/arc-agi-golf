d37d64e6d927fdea8c44e775a16750f270992376
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 07:51:10 +0900

    Golf task216 solution

--- logic/task216.py
+++ logic/task216.py
@@ -1,4 +1,2 @@
-def p(a):
- # densest block
- return max((sum(sum(r)-w for r in b),w*len(b),b)for y in range(20)for c in range(20)for w in[(a[y][c:]+[0]).index(0)]for b in[[r[c:c+w]for r in a[y:y+([r[c]for r in a[y:]]+[0]).index(0)]]])[2]
-
+# densest block
+p=lambda a:max((sum(sum(b,[]))-w*h,w*h,b)for y in range(20)for c in range(20)for w,h in[((a[y][c:]+[0]).index(0),([r[c]for r in a[y:]]+[0]).index(0))]for b in[[r[c:c+w]for r in a[y:y+h]]])[2]
