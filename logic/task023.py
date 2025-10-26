89b7c8755b4a23dbd3ef3169cac53e994731814f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 18:02:13 +0900

    Golf task023 solver

--- logic/task023.py
+++ logic/task023.py
@@ -1,11 +1,11 @@
 def p(g):
- w=len(g[0]);t=sum(g,[]);s={i for i,x in enumerate(t)if x>4}
+ w=len(g[0]);s={i for i,x in enumerate(sum(g,[]))if x>4}
  def f():
-  if not s:return 1
+  if not s:return g
   n=min(s)
   for e,c in((0,w,2*w),2),((0,1,2),2),((0,1,w,w+1),8):
-   if all(n+o in s for o in e):
+   if s>={n+o for o in e}:
     for o in e:s.remove(n+o);g[(n+o)//w][(n+o)%w]=c
-    if f():return 1
+    if f():return g
     for o in e:s.add(n+o);g[(n+o)//w][(n+o)%w]=5
- f();return g
+ return f()
