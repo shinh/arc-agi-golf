c604212781fb3896af4046b17c66ccd4cc3f5463
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 07:39:19 +0900

    Improve task278 golf score

--- logic/task278.py
+++ logic/task278.py
@@ -1,5 +1,5 @@
 def p(a):
  for i,j in(s:={(i,j)for i,r in enumerate(a)for j,v in enumerate(r)if v}):
-  if(i+1,j)in s or(i-1,j)in s or(i,j+1)in s or(i,j-1)in s:
+  if s&{(i+1,j),(i-1,j),(i,j+1),(i,j-1)}:
    for t in a[i-(i>0):i+2]:t[j-(j>0):j+2]=[x or 3 for x in t[j-(j>0):j+2]]
  return a
