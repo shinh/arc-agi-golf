413e3ab6426c059d7b7b6a8afa11d2e8b7c443e7
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 17:55:56 +0900

    Golf task012 diamond expansion

--- logic/task012.py
+++ logic/task012.py
@@ -1,3 +1,3 @@
 # expand cross pattern into larger diamond with cross arms
 def p(g):
-    r,E=range(-2,3),enumerate;[a*b*(a*a-b*b)or g[i+a].__setitem__(j+b,(R[j-1],x)[a*a==b*b])for i,R in E(eval(str(g)))for j,x in E(R)if x>0<R[j-1]*R[j+1]for a in r for b in r];return g
+    r,E=range(-2,3),enumerate;[a*b*(a*a-b*b)or g[i+a].__setitem__(j+b,R[j-1+(a*a==b*b)])for i,R in E(eval(str(g)))for j,x in E(R)if x>0<R[j-1]*R[j+1]for a in r for b in r];return g
