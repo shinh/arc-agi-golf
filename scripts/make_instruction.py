0f885e469f5acf96d3fb3fe5136e4a96e57818eb
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 14:14:54 +0900

    Update ours

--- scripts/make_instruction.py
+++ scripts/make_instruction.py
@@ -10,8 +10,8 @@ import sota
 
 def read_ours():
     ours = []
-    for code, sub in zip(sorted(glob.glob("logic/*.py")), sorted(glob.glob("submissions/*.py"))):
-        ours.append((os.path.getsize(sub), open(code).read()))
+    for code, o in zip(sorted(glob.glob("logic/*.py")), sorted(glob.glob("ours/*.txt"))):
+        ours.append((2500 - int(open(o).read()), open(code).read()))
     return ours
 
 
