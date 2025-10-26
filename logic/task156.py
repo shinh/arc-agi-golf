ef2889c4a1245ab74c143df3c69b5aeee600f285
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 18:22:32 +0900

    Golf task156 solution

--- logic/task156.py
+++ logic/task156.py
@@ -1,9 +1,9 @@
 def p(g):
  d=t=0
  for i in range(1,9):
-  t^=max(g[i])<1
-  r=g[i]
+  r=g[i];t^=max(r)<1
   for j in range(1,9):
    if r[j-1]*r[j+1]*g[i-1][j]*g[i+1][j]:
     r[j]=1+t;d+=1-2*t
- return eval(str(g).translate({49:50,50:49}))*(d>0) or g
+ return(eval(str(g).translate({49:50,50:49})),g)[d<1]
+
