12dc3ef6fa77c2f6a15d18b0821b731dfc8bc7b3
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 17:23:31 +0900

    Golf task379 solution

--- logic/task379.py
+++ logic/task379.py
@@ -6,10 +6,7 @@ def p(g):
             s=0
             for x,v in enumerate(r):
                 if v==2:s=x+1
-                if v==8 and r[0]-8 and x>s>0:
-                    r[s-1:x+1]=[2]*(x+2-s)
-                    for Y in y-1,y,y+1:g[Y][x-1:x+2:2]=8,8
-                    s=0
+                if v==8!=r[0]and x>s>0:r[s-1:x+1]=[2]*(x+2-s);g[y-1][x-1]=g[y][x-1]=g[y+1][x-1]=g[y-1][x+1]=g[y][x+1]=g[y+1][x+1]=8;s=0
         #show(g,"o")
-        g=[[*r]for r in zip(*g[::-1])]
+        g=[*map(list,zip(*g[::-1]))]
     return g
