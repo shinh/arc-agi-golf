8f64547bd8039e04b78dc61f3f19d37f31e1d81b
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 18:17:46 +0900

    Golf task355 solution

--- logic/task355.py
+++ logic/task355.py
@@ -1,4 +1,1 @@
-def p(g):
- A=sum(g,[]);b=min(A,key=A.count)
- d=[sum({*r}&{x[i]for x in g})-b for r in g for i,v in enumerate(r)if v==b]
- return [[max(d,key=d.count)]]
+def p(g):b=min(s:=sum(g,[]),key=s.count);return[[max(d:=[sum({*r}&{*c})-b for c in zip(*g) for r,v in zip(g,c)if v==b],key=d.count)]]
