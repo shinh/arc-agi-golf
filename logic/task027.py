e676477dac24821a4d8da442fdd222ae83d6a645
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 17:46:02 +0900

    Golf task027 solution

--- logic/task027.py
+++ logic/task027.py
@@ -1,4 +1,4 @@
-def p(g):
- s={(i,j)for i in range(10)for j in range(10)if g[i][j]};o=s|{(9-j,i)for i,j in s}!=s|{(j,9-i)for i,j in s}
+def p(g):# rot
+ r=range(10);s={(i,j)for i in r for j in r if g[i][j]};a,b=zip(*s);o=min(a)+max(b)>9
  for i,j in s:g[j][9-i+o]=g[j][9-i+o]or 2
  return g
