da622da96d7426a71b63082b3497aa9aa522d108
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 07:32:20 +0900

    Golf task281 solution

--- logic/task281.py
+++ logic/task281.py
@@ -1,11 +1,11 @@
 def p(g):
-    #spread
-    for o in[0]*4:
-        for e in{r.index(8)for r in g if 8in r}:
-            for r in g:
-                f=n=0
-                for x in range(e):
-                    if(c:=r[x]):n or f and(n:=c)or(f:=c)
-                    elif n:r[x-1:e+1]=[n]*(e-x)+[n,f];n=0
-        g[:]=map(list,zip(*g[::-1]))
-    return g
+ #spread
+ for _ in[0]*4:
+  for e in[r.index(8)for r in g if 8in r]:
+   for r in g:
+    f=n=0
+    for x in range(e):
+     if(c:=r[x]):n or f and(n:=c)or(f:=c)
+     elif n:r[x-1:e+1]=[n]*(e-x)+[n,f];n=0
+  g[:]=map(list,zip(*g[::-1]))
+ return g
