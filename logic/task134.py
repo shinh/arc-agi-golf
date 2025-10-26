6a9fe386c21ec8694f85d5c3ca4f3fb1dba826da
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 07:48:39 +0900

    Golf task134 solution

--- logic/task134.py
+++ logic/task134.py
@@ -1,5 +1,4 @@
-def p(g):#rotate&scale
-    c=[a for r,s in zip(g,g[1:]) for a,b,d,e in zip(r,r[1:],s,s[1:])if a==b==d==e>0][-1]
-    A=sum({*sum(g,[])})-c
-    for _ in[0]*96:g=[*zip(*g[(c in g[-1])-2::-1])]
-    return[[A*(v==c)for v in R[::len(g)//3]]for R in g[::len(g)//3]]
+def p(g):#crop
+    c=[a for r,s in zip(g,g[1:]) for a,b,d,e in zip(r,r[1:],s,s[1:])if a==b==d==e>0][-1];A=sum({*sum(g,[])})-c
+    g=[*zip(*[r for r in zip(*[r for r in g if c in r])if c in r])];s=len(g)//3
+    return[[A*(v==c)for v in R[::s]]for R in g[::s]]
