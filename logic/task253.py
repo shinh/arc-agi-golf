c7a2c56d61ad1fdbf59ca196d400ab72e5d5d216
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 17:52:13 +0900

    Golf task253 solution

--- logic/task253.py
+++ logic/task253.py
@@ -1,6 +1,2 @@
-def p(j):
- a=b=c=d=0
- for r,s in zip(j,j[1:]):
-  for x,y,u,v in zip(r,r[1:],s,s[1:]):
-   a|=x*(x==y==u);b|=x*(x==u==v);c|=x*(x==y==v);d|=v*(v==y==u)
- return (a,a,c,c),(a,0,0,c),(b,0,0,d),(b,b,d,d)
+def p(j):# collect corner colors
+ a,b,c,d=map(max,zip(*((x*(x==y==u),x*(x==u==v),x*(x==y==v),v*(v==y==u))for r,s in zip(j,j[1:])for x,y,u,v in zip(r,r[1:],s,s[1:]))));return(a,a,c,c),(a,0,0,c),(b,0,0,d),(b,b,d,d)
