dc429c043f3297ab69e90371e45fbb451b3b3507
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 17:44:00 +0900

    Golf task017 with set-based tile detection

--- logic/task017.py
+++ logic/task017.py
@@ -1,3 +1,5 @@
+# tile period fill
+R=range(21)
 def p(g,a=1):
- A=range(21);m={}
- return all((v:=g[i][j])<1 or m.setdefault((i%a,j%a),v)==v for i in A for j in A)and[[g[i][j]or m[i%a,j%a]for j in A]for i in A]or p(g,a+1)
+ t={((i%a,j%a),v)for i in R for j in R if(v:=g[i][j])}
+ return(len((d:=dict(t)))^len(t)and p(g,a+1)or[[g[i][j]or d[i%a,j%a]for j in R]for i in R])
