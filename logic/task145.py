88f7a354c0e4dd6a6df3b6a30484c21c1dd92b3f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 18:15:18 +0900

    Golf task145 solution

--- logic/task145.py
+++ logic/task145.py
@@ -1,17 +1,15 @@
-def p(g):
- # paint max area 1 and min area 8
- h=len(g);w=len(g[0]);R=range;s=[]
+def p(g):# paint max area 1 and min area 8
+ L=len;e=enumerate;s=[]
  def f(y,x):
-  if-1<y<h and-1<x<w and g[y][x]<1:
-   g[y][x]=10+len(s);return 1+f(y+1,x)+f(y-1,x)+f(y,x+1)+f(y,x-1)
+  if L(g)>y>-1<x<L(g[0]) and g[y][x]<1:
+   g[y][x]=10+L(s);return 1+f(y+1,x)+f(y-1,x)+f(y,x+1)+f(y,x-1)
   return 0
- for y in R(h):
-  for x in R(w):
-   if g[y][x]<1:
-    s+=f(y,x),
+ for y,r in e(g):
+  for x,v in e(r):
+   if v<1:s+=f(y,x),
  m=max(s);n=min(s)
- for y in R(h):
-  for x in R(w):
-   if(v:=g[y][x])>9:g[y][x]=s[v-10]==m or(s[v-10]==n)*8
+ for y,r in e(g):
+  for x,v in e(r):
+   if v>9:r[x]=(v:=s[v-10])==m or v==n and 8
  return g
 
