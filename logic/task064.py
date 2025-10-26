2f566eba66fbde711c2cd64664e46694562712bb
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 07:41:23 +0900

    Golf task064 solution

--- logic/task064.py
+++ logic/task064.py
@@ -2,7 +2,7 @@ def p(g):#fill
  a=sum(g,[]);c=a.count;s=min(a,key=c);m=min({*a}-{s},key=c)
  for _ in'0'*4:
   for r in g:
-   try:i,j=r.index(s),r.index(m);r[i+1:j]=[s]*(j-i-1)
+   try:i,j=map(r.index,(s,m));r[i+1:j]=[s]*~(i-j)
    except:0
-  g=[*map(list,zip(*g[::-1]))]
+  g=[*map(list,zip(*g))][::-1]
  return g
