1915946cc8b37b2dfe1ca9422ec5a09fb726d9d9
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 17:40:53 +0900

    Golf task192 solution

--- logic/task192.py
+++ logic/task192.py
@@ -1,4 +1,4 @@
 def p(g):
-    # keep cells touching b both axes
-    f=sum(g,[]);b=max({*f}-{0},key=f.count);c=[*zip(*g)]
-    return[[b*(b in c[x][y-(y>0):y+2])*(b in r[x-(x>0):x+2])if v*(v-b)else v for x,v in enumerate(r)]for y,r in enumerate(g)]
+    # keep cells near b
+    f=sum(g,[]);b=max({*f}-{0},key=f.count);c=[*zip(*g)];e=enumerate
+    return[[v if v in(0,b)else b*(b in c[x][y-(y>0):y+2]and b in r[x-(x>0):x+2]) for x,v in e(r)]for y,r in e(g)]
