6069d3ba745b8dcc3269943f5138464b9a411412
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 07:37:52 +0900

    Golf task062 solution

--- logic/task062.py
+++ logic/task062.py
@@ -4,8 +4,7 @@ def p(g):
  for _ in[0]*4:
   g=[*zip(*g)][::-1]
   for r in range(10):
-   if sum(g[r])==d==g[r].count(2)*2:
+   if sum(w:=g[r])==d==w.count(2)*2:
     for y in range(min(r,8-r)+1):
-     t=g[r+1+y]
-     if sum(t)>t.count(2)*2:g[r-y]=t
+     if sum(t:=g[r+1+y])>t.count(2)*2:g[r-y]=t
  return[[c or 3for c in r]for r in g]
