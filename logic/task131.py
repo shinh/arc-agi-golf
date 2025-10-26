78d7748374afef7bd2ae3776622339ce8968d0a4
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 07:49:46 +0900

    Golf task131 solution to 188 chars

--- logic/task131.py
+++ logic/task131.py
@@ -1,4 +1,1 @@
-def p(g):
- m=len(g[0]);n=len(g)
- if m>n:return[*zip(*p([*zip(*g)]))]
- i=([2in r for r in g]).index(1);return p(g[::-1])[::-1]if'3'in str(g[:i])else(g[:i+1]+[r for r in g if 3in r]+[m*[8]]+n*[m*[0]])[:n]
+p=lambda g:(m:=len(g[0]))>(n:=len(g))and[*zip(*p([*zip(*g)]))]or'3'in str(g[:(i:=[2in r for r in g].index(1))])and p(g[::-1])[::-1]or(g[:i+1]+[r for r in g if 3in r]+[m*[8]]+n*[m*[0]])[:n]
