98f7e820179b7a84158628ad13cb09353af0135c
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 18:21:18 +0900

    Golf task199 solution

--- logic/task199.py
+++ logic/task199.py
@@ -1,4 +1,4 @@
 def p(g):
- s=sum(g,[]);v=max(s);i,j=divmod(s.index(v),len(g));g[i+1][j]=v
+ s=sum(g,[]);i,j=divmod(s.index(v:=max(s)),len(g));g[i+1][j]=v
  for r in g[:i+1]:r[j&1::2]=[4]*len(r[j&1::2])
  return g
