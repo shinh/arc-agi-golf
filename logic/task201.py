2648e06ae06bf99d285ba2511194afbc2a8a0889
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 17:59:04 +0900

    Golf task201 logic

--- logic/task201.py
+++ logic/task201.py
@@ -1,9 +1,8 @@
 def p(g):
- A=[i for i,r in enumerate(g)if 4 in r];t=A[0];v=A[-1]
- B=[i for i,c in enumerate(zip(*g))if 4 in c];s=B[0];u=B[-1]
+ A=[i for i,r in enumerate(g)if 4in r];t=A[0];v=A[-1]
+ B=[i for i,c in enumerate(zip(*g))if 4in c];s=B[0];u=B[-1]
  o=[r[s:u+1]for r in g[t:v+1]]
  for r in g[t:v+1]:r[s:u+1]=[0]*(u-s+1)
- h=[*zip(*filter(any,zip(*filter(any,g))))]
- m=any(a[0]==b[0]for a,b in zip(h,o[1:]))
- for i,r in enumerate(h):o[i+1][1:-1]=(r,r[::-1])[not m]
+ h=[*zip(*filter(any,zip(*filter(any,g))))];m=any(a[0]==b[0]for a,b in zip(h,o[1:]))
+ for a,b in zip(h,o[1:]):b[1:-1]=a[::2*m-1]
  return o
