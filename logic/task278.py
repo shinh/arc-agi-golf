0b7aa360da589db51f93ddd43dd15b48f87a8549
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 17:23:13 +0900

    Golf task278 solution

--- logic/task278.py
+++ logic/task278.py
@@ -1,7 +1,5 @@
 def p(a):
- s={(i,j)for i,r in enumerate(a)for j,v in enumerate(r)if v}
- for i,j in s:
-  if{(i+1,j),(i-1,j),(i,j+1),(i,j-1)}&s:
-   for t in a[i and i-1:i+2]:
-    k=j and j-1;t[k:j+2]=[x or 3 for x in t[k:j+2]]
+ for i,j in(s:={(i,j)for i,r in enumerate(a)for j,v in enumerate(r)if v}):
+  if(i+1,j)in s or(i-1,j)in s or(i,j+1)in s or(i,j-1)in s:
+   for t in a[i-(i>0):i+2]:t[j-(j>0):j+2]=[x or 3 for x in t[j-(j>0):j+2]]
  return a
