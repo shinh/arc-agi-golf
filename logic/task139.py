15cf93f14fe0c875912d4459a1dc86be09a108bf
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 07:44:41 +0900

    Golf task139 solution

--- logic/task139.py
+++ logic/task139.py
@@ -1,6 +1,6 @@
-def p(g):
+def p(g):# fill 3x3 windows with 7
  for k in range(49):
-  a=g[k//7:][:3];w=[r[(j:=k%7):j+3]for r in a]
+  a=g[k//7:][:3];j=k%7;w=[r[j:j+3]for r in a]
   if min(map(sum,w+[*zip(*w)])):
    for r in a:r[j:j+3]=[x or 7for x in r[j:j+3]]
  return g
