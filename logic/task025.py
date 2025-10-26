8a562725c316b92b89d1f418e2fef4601ac50691
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 07:46:17 +0900

    Golf task025 solution

--- logic/task025.py
+++ logic/task025.py
@@ -1,7 +1,8 @@
+# fuse
 def p(g):
  t=[*zip(*g)];h=0
  for j,c in enumerate(t):
-  if 0<min(c)==max(c):
-   k=c[0];h=h or [[0]*len(t)for _ in g]
+  if(k:=min(c))==max(c)>0:
+   h=h or [[0]*len(t)for _ in g]
    for R,a in zip(h,g):R[j]=R[j-(k in a[:j])+(k in a[j+1:])]=k
- return h or [*zip(*p(t))]
+ return h or[*zip(*p(t))]
