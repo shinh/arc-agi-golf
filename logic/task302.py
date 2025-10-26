244d6f01d8aec06470282d02a5f3aaef675c22c6
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 17:31:48 +0900

    Golf task302 solution

--- logic/task302.py
+++ logic/task302.py
@@ -1,9 +1,7 @@
+R=range(1,12)
 def p(a):
- R=range(1,12)
  for i in R:
-  r=a[i]
   for j in R:
-   if r[j]<1<(g:=a[i-1][j])==r[j-1]:
-    s=r[j:].index(g)
-    for q in a[i:i+s]:q[j:j+s]=[g+s]*s
+   if(g:=a[i-1][j])==(r:=a[i])[j-1]>1>r[j]:
+    for q in a[i:i+(s:=r[j:].index(g))]:q[j:j+s]=[g+s]*s
  return a
