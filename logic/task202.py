3b1b9d3f0b7c826b16ce4ba59388409dae109f9a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 18:31:11 +0900

    Golf task202 transformation

--- logic/task202.py
+++ logic/task202.py
@@ -1,4 +1,4 @@
 # zero hole lines
 def p(g):
-    for _ in[0]*4:t=next((r for r in g if max(r)>min(r)>0),());g=(*zip(*([v*all(b|v^a for a,b in zip(t,r))for v in r]for r in g[::-1])),)
+    for _ in[0]*4:g=(*zip(*([v*all(b|v^a for a,b in zip(next((r for r in g if 0<min(r)<max(r)),()),r))for v in r]for r in g[::-1])),)
     return g
