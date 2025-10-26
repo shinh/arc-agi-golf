cb1fdd9b2d065b35b3e4fc38c42ac2960ee918e9
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 17:41:26 +0900

    Golf task270 solution

--- logic/task270.py
+++ logic/task270.py
@@ -1,5 +1,6 @@
 def p(m):
- n=len(m);l=sum(m,[]);i=l.index;a=i(1);b=i(2);o=l[:]
+ # slide tiles to anchors
+ n=len(m);l=sum(m,[]);i=l.index;s=i(2),i(1);o=l[:]
  for k,v in enumerate(l):
-  if v>2:c=(b,a)[v>3];t=k-c;e=abs(t);o[c+t//(e//n or e)]=v;o[k]^=o[k]*(o[k]==v)
+  if v>2:e=abs(t:=k-s[v>3]);o[k-t+t//(e//n or e)]=v;o[k]*=o[k]!=v
  return[*zip(*[iter(o)]*n)]
