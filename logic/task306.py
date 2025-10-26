4cdf4e5454e283527e667b785de1ca0e0c0165ec
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 18:04:47 +0900

    Golf task306 solution

--- logic/task306.py
+++ logic/task306.py
@@ -1,3 +1,3 @@
 def p(g):
- for a,b in zip(g,g[10:]):s=max(a,b);a[:]=b[:]=((max(s[:9],s[10:19],s[20:29])+[4])*(len(s)//9))[:-1]
+ for a,b in zip(g,g[10:]):d=max((s:=max(a,b))[:9],s[10:19],s[~8:]);b[:]=a[:]=d+([4]+d)*s.count(4)
  return g
