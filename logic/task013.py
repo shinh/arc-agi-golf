8e4954e6b62e46d9f63f5ce93df15543a781ecbe
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 07:47:49 +0900

    Golf task013 solution

--- logic/task013.py
+++ logic/task013.py
@@ -1,5 +1,4 @@
 def p(g):
- t=1>max(g[0]+g[-1]);g=[*map(list,zip(*g))]*t or g
- s,c=min(a:=[(j,v)for r in g for j,v in enumerate(r)if v]);u,d=max(a)
+ g=[g,[*map(list,zip(*g))]][t:=1>max(g[0]+g[-1])];s,c=min(a:=[(j,v)for r in g for j,v in enumerate(r)if v]);u,d=max(a)
  for r in g:r[s::u-s]=((c,d)*7)[:len(r[s::u-s])]
- return [*zip(*g)]*t or g
+ return[g,[*zip(*g)]][t]
