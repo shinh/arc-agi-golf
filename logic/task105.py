98b31b5b66dfc8f595aebba31c38a6a81ce5dadc
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-26 17:16:07 +0900

    Golf task105 solution

--- logic/task105.py
+++ logic/task105.py
@@ -1,8 +1,8 @@
 def p(a):
- u=[i for i,r in enumerate(a)if any(r)];t,b=u[0],u[-1]
- c=max(i for i in range(13)if any(r[i]for r in a))
- L=sum(any(a[i][3:c])for i in range(t+1,b))
+ t,*_,b=[i for i,r in enumerate(a)if any(r)]
+ c=max(i for i,r in enumerate(zip(*a))if any(r))
+ v=a[t+1:b];L=sum(any(r[3:c])for r in v)
  for i in range(t,b+1):
   for j in range(2,c+1):
-   if i in(t,b)or j in(2,c)or any(a[i][3:c])==L==1 or any(a[k][j]for k in range(t+1,b))and L-1:a[i][j]=a[i][j]or 2
+   if i in(t,b)or j in(2,c)or L*any(a[i][3:c])==1 or any(r[j]for r in v)*(L-1):a[i][j]=a[i][j]or 2
  return a
