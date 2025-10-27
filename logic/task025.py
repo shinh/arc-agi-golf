ecf6503075dd754ea9b03d7a47f57e623b50383f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 16:35:03 +0900

    Golf task025 solution

--- logic/task025.py
+++ logic/task025.py
@@ -3,6 +3,5 @@ def p(g):
  t=[*zip(*g)];h=0
  for j,c in enumerate(t):
   if(k:=min(c))==max(c)>0:
-   h=h or [[0]*len(t)for _ in g]
-   for R,a in zip(h,g):R[j]=R[j-(k in a[:j])+(k in a[j+1:])]=k
+   for R,a in zip(h:=h or[[0]*len(t)for _ in g],g):R[j]=R[j-(k in a[:j])+(k in a[j+1:])]=k
  return h or[*zip(*p(t))]
