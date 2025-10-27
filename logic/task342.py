f850e77470c1618d3ca60f21622e7e64e8ea8bf0
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 20:08:06 +0900

    Golf task342 quadrant mapping

--- logic/task342.py
+++ logic/task342.py
@@ -1,5 +1,5 @@
 def p(g):
+ # map quadrants
  s=sum(g,[]);b=s.index(8);A=[0]*100
- for i,x in enumerate(s):
-  if x&7:A[b+(i-b>9)*10+(i%10>b%10)]=x
+ for i,x in enumerate(s):A[b+(i-b>9)*10+(i%10>b%10)]+=x&7and x
  return[*zip(*[iter(A)]*10)]
