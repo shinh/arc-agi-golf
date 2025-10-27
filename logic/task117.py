651aff3abee5536d5411f5a1c3eba83f39e4ec21
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 20:10:19 +0900

    Golf task117 solution

--- logic/task117.py
+++ logic/task117.py
@@ -2,14 +2,8 @@ def p(g):
  n=len(g)
  for i in range(1,n-1):
   for j in range(1,n-1):
-   if g[i][j] and g[i][j]==g[i-1][j-1]==g[i-1][j+1]==g[i+1][j-1]==g[i+1][j+1]:
-    y,x=i,j
+   if 0<g[i][j]==g[i-1][j-1]==g[i-1][j+1]==g[i+1][j-1]==g[i+1][j+1]:y,x=i,j
  for i in range(n):
   for j in range(n):
-   if g[i][j]:
-    g[i][2*x-j]=g[i][j]
- for i in range(n):
-  for j in range(n):
-   if g[i][j]:
-    g[2*y-i][j]=g[i][j]
+   if(v:=g[i][j]):g[i][2*x-j]=g[2*y-i][j]=g[2*y-i][2*x-j]=v
  return g
