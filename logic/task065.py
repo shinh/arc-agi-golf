d846a5b31447a9d869fd4e118302c9802084f826
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 20:21:40 +0900

    Golf task065 solution

--- logic/task065.py
+++ logic/task065.py
@@ -1,3 +1,3 @@
 def p(g):# uniq q
  s=sum(g,[]);m=len(g);n=m>>1;d=s.index(min(s,key=s.count))
- return[[r[-n:],r[:n]][d%m<n]for r in[g[-n:],g[:n]][d<m*n]]
+ return[r[-n*(d%m>=n):][:n]for r in g[-n*(d>=m*n):][:n]]
