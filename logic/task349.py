be5d6c2b1c315ec5e975943478d4bd5a1ed52b82
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 16:54:36 +0900

    Golf task349 logic

--- logic/task349.py
+++ logic/task349.py
@@ -1,12 +1,11 @@
 def p(g):
  for y,r in enumerate(g):
   for x,v in enumerate(r):
-   if v-9 or y and g[y-1][x]==9 or x and r[x-1]==9:continue
-   a=b=1
-   while x+a<len(r)and r[x+a]==9:a+=1
-   while y+b<len(g)and g[y+b][x]==9:b+=1
-   for R in g[y+b:]:R[x:x+a]=[t or 1 for t in R[x:x+a]]
-   q=a//2
-   s=max(0,y-q);t=max(0,x-q)
-   for R in g[s:y+b+q]:R[t:x+a+q]=[max(k,3)for k in R[t:x+a+q]]
+   if v>8 and(y<1 or g[y-1][x]-9)and(x<1 or r[x-1]-9):
+    a=b=1
+    while r[x+a:x+a+1]==[9]:a+=1
+    while g[y+b:y+b+1]and g[y+b][x]==9:b+=1
+    for R in g[y+b:]:R[x:x+a]=[t or 1 for t in R[x:x+a]]
+    q=a>>1
+    for R in g[max(y-q,0):y+b+q]:U=max(x-q,0);R[U:x+a+q]=[max(k,3)for k in R[U:x+a+q]]
  return g
