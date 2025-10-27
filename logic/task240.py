27ef2db2fd1ec9d1458e15aec877ba9729281135
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 16:35:34 +0900

    Golf task240 loops

--- logic/task240.py
+++ logic/task240.py
@@ -1,5 +1,5 @@
 def p(g):
- for i in 1,3,5,7:
-  for j in 1,3,5,7:g[i][j]=g[i][~j]=g[~i][j]=g[~i][~j]=g[~i][j]or g[~i][~j]or g[i][j]or g[i][~j]
+ for i in range(1,9,2):
+  for j in range(1,9,2):g[i][j]=g[i][~j]=g[~i][j]=g[~i][~j]=g[~i][j]or g[~i][~j]or g[i][j]or g[i][~j]
   for j in range(i+2,17-i,2):g[i][j]=g[~i][j]=g[j][i]=g[j][~i]=g[i][i+2]
  return g
