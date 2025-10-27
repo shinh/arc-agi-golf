e2bbb09423e42fae7d75989376360c108ce4eded
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 15:20:15 +0900

    Golf task277 solution

--- logic/task277.py
+++ logic/task277.py
@@ -1,8 +1,9 @@
 def p(g):
- s={x+y*1j for x,r in enumerate(g)for y,v in enumerate(r)if v};m=s|{0}
+ e=enumerate
+ s={x+y*1j for x,r in e(g)for y,v in e(r)if v};m={*s}
  while s:
   c={s.pop()}
-  while(t:={q for q in s for p in c if 0<abs(q-p)<2}):
+  while(t:={q for q in s for p in c if abs(q-p)<2}):
    c|=t;s-=t
   if len(c)<len(m):m=c
- return[[v and 1+(x+y*1j in m)for y,v in enumerate(r)]for x,r in enumerate(g)]
+ return[[v and 1+(x+y*1j in m)for y,v in e(r)]for x,r in e(g)]
