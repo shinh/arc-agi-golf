d082bc5bed1b04a30d2b76af5d96d4925c30f1ea
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 20:21:17 +0900

    Golf task224 by inlining slice bounds

--- logic/task224.py
+++ logic/task224.py
@@ -1,5 +1,5 @@
 def p(g):# draw inner color rectangle just inside 5s
-    y,x=zip(*((y,x)for y,r in enumerate(g)for x,c in enumerate(r)if c==5));t=min(y)+1;b=max(y);l=min(x)+1;r=max(x);h=g[t:b]
+    y,x=zip(*((y,x)for y,r in enumerate(g)for x,c in enumerate(r)if c==5));h=g[min(y)+1:max(y)];l=min(x)+1;r=max(x)
     c=max(max(w[l:r])for w in h)
     h[0][l:r]=h[-1][l:r]=[c]*(r-l)
     for w in h:w[l]=w[r-1]=c
