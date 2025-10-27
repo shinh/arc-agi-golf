f4db19267a687c9fafd0c4b29cb4a1f75b691f4b
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 17:30:23 +0900

    Golf task220 solution

--- logic/task220.py
+++ logic/task220.py
@@ -1,5 +1,5 @@
-def p(g,E=enumerate):# color nbrs
+def p(g,E=enumerate):#paint nb
  for r,R in E(g):
   for c,C in E(R):
-   for i in(-1,1)*(268>>C&1):H=g[r+i];H[c]=R[c+i]=H[c+i]=H[c-i]=C*5%9
+   for i in(-1,1)*(1<C%6<4):R[c+i]=k=C*5%9;g[r+i][c-1:c+2]=[k]*3
  return g
