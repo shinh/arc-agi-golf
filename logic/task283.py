132ea5e1c74db97b34478b492accc369489a83c0
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 17:29:43 +0900

    Golf task283

--- logic/task283.py
+++ logic/task283.py
@@ -1,4 +1,3 @@
 def p(g):#frame 5-rect via set
- s={(i%10,i//10)for i in range(100)if sum(g,[])[i]>4}
- for x,y in s:g[y][x]=len(s&{(x+1,y),(x-1,y),(x,y+1),(x,y-1)})*3%5
+ for i in (s:={i+i//10 for i in range(100)if sum(g,[])[i]>4}):g[i//11][i%11]=len({i-1,i+1,i-11,i+11}&s)*3%5
  return g
