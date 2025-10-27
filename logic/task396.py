888264d6012eb8902ffb82d4315c74b50e796a09
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 17:16:05 +0900

    Golf task396 solution

--- logic/task396.py
+++ logic/task396.py
@@ -1,6 +1,3 @@
-def p(a):
- # locate the biggest solid rectangle and recolor it after cropping
- h=len(a)
- m,c,y,x,d,e=max((((D:=next((j for j in range(Y,h)if a[j][X]^f),h))-Y)*((E:=next((i for i in range(X,len(r))if r[i]^f),len(r)))-X),f,Y,X,D,E)for Y,r in enumerate(a)for X,f in enumerate(r)if f)
- g=sum({*sum(a,[])})-c
- return[[[g,v][v!=c]for v in r[x:e]]for r in a[y:d]]
+def p(a):# crop&paint
+ h=len(a);m,c,y,x,d,e=max(((D:=next((j for j in range(Y,h)if a[j][X]^f),h)-Y)*(E:=next((i for i in range(X,len(r))if r[i]^f),len(r))-X),f,Y,X,D,E)for Y,r in enumerate(a)for X,f in enumerate(r)if f)
+ return[[((sum({*sum(a,[])})-c,v)[v!=c])for v in r[x:x+e]]for r in a[y:y+d]]
