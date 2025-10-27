729e418f322e5a3939bdd78cee0010c16375d7a1
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 17:30:24 +0900

    Improve task089 boundary check golf

--- logic/task089.py
+++ logic/task089.py
@@ -9,7 +9,7 @@ def p(g):
      for x in-1,0,1:
       for y in-1,0,1:
        r=i+x;q=j+y
-       if n>r>-1<q<n and g[r][q]and(r,q)not in l:l+=(r,q),
+       if-1<r<n>q>-1 and g[r][q]and(r,q)not in l:l+=(r,q),
     if l[1:]:p[c]=[(i-a,j-b,g[i][j])for i,j in l]
     else:s+=[(a,b,c)]
  for a,b,c in s:
