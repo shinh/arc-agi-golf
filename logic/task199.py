ebdfc63c23a6738aa8ce2e6143d0b7404e87429d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 15:54:12 +0900

    Golf task199 by deriving row via max

--- logic/task199.py
+++ logic/task199.py
@@ -1,4 +1,4 @@
 def p(g):
- s=sum(g,[]);i,j=divmod(s.index(v:=max(s)),len(g));g[i+1][j]=v
+ i=g.index(r:=max(g));j=r.index(v:=max(r));g[i+1][j]=v
  for r in g[:i+1]:r[j&1::2]=[4]*len(r[j&1::2])
  return g
