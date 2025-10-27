5eb8ccd1860b83561f277affaf329a8cc31d8a48
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 20:22:30 +0900

    Golf task200 stripe solution

--- logic/task200.py
+++ logic/task200.py
@@ -1,7 +1,7 @@
 def p(g):#stripe
- r=g[9];v=sum(r);j=r.index(v);t=9
- while j<10:
-  for r in g:r[j]=v
-  if j-9:t^=9;g[t][j+1]=5
-  j+=2
- return g
+	j=g[9].index(v:=sum(g[9]));t=9
+	while j<10:
+		for r in g:r[j]=v
+		if j<9:g[t:=t^9][j+1]=5
+		j+=2
+	return g
