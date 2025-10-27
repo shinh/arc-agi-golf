b8eb1af29d0c1ddd7e7f1e3ec42c9567adc13c37
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 20:04:09 +0900

    Golf task182 with flattened pattern check

--- logic/task182.py
+++ logic/task182.py
@@ -2,10 +2,10 @@ def p(g):
     # find the top-left of frame and recolor matching shapes
     for y in range(16):
         for x in range(16):
-            if g[y][x]==g[y+1][x]==g[y][x+1]==5:sx,sy=x,y
+            if g[y][x]==5==g[y][x+1]==g[y+1][x]:sx,sy=x,y
     for y in range(16):
         for x in range(16):
-            if all((g[sy+dy+1][sx+dx+1]>0)==(g[y+dy][x+dx]>0)for dy in range(5)for dx in range(5)):
+            if[g[sy+dy+1][sx+dx+1]>0for dy in range(5)for dx in range(5)]==[g[y+dy][x+dx]>0for dy in range(5)for dx in range(5)]:
                 for dy in range(5):
                     for dx in range(5):
                         g[y+dy][x+dx]=g[sy+dy+1][sx+dx+1]
