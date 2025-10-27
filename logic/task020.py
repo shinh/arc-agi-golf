ea9a7358b911ff0ae6cb5b22fa2cb259b45e63f2
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 16:56:02 +0900

    Optimize task020 looping

--- logic/task020.py
+++ logic/task020.py
@@ -1,6 +1,11 @@
+# mirror around axes
 def p(g):
  a,b=[bytes(map(any,x)).find(1)+2for x in(g,zip(*g))]
- for c,r in enumerate(g,-a):
-  for d,v in enumerate(r,-b):
+ c=-a
+ for r in g:
+  d=-b
+  for v in r:
    if v:g[a+c][b-d]=g[a-c][b-d]=g[a-d][b+c]=g[a+d][b-c]=v
+   d+=1
+  c+=1
  return g
