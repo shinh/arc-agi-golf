477d40af1f4f0b8af58e9ae571891908dc3ea3e6
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 16:35:48 +0900

    Improve task268 arm filling loop

--- logic/task268.py
+++ logic/task268.py
@@ -4,7 +4,8 @@ def p(g):
  t,*_,B=[i for i,x in enumerate(g)if any(x)];l,*_,r=[i for i,x in enumerate(zip(*g))if any(x)]
  if all(g[t][l:r+1]):return [*zip(*p([*zip(*g[::-1])]))][::-1]
  for i in range(B):
-  s=i<=t;L=l+1+s;h=g[i];h[L:r-s]=[4]*(r-s-L)
+  h=g[i]
+  for c in range(l+1+(i<=t),r-(i<=t)):h[c]=4
   if i<t:
    for c in i+l-t+2,r-2+t-i:
     if-1<c<len(g):h[c]=4
