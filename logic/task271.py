186038176694bac066010e26df05e24da48b2fa3
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 20:05:17 +0900

    Golf task 271 solution

--- logic/task271.py
+++ logic/task271.py
@@ -1,1 +1,1 @@
-_=range(7);p=lambda g:max([[r[x:x+3]for r in g[y:y+3]]for y in _ for x in _],key=lambda B:sum((v==1)*9+v//8for v in sum(B,[])))#3x31>8
+p=lambda g:max(([r[i%7:][:3]for r in g[i//7:][:3]]for i in range(49)),key=lambda B:sum((v==1)*9+v//8for v in sum(B,[])))#3x31>8
