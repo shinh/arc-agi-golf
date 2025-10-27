30176a1f6f5b56a39ac7944fca202181309fb874
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 16:00:32 +0900

    Golf task336 solution

--- logic/task336.py
+++ logic/task336.py
@@ -1,2 +1,2 @@
-# fill interior via spins
-p=lambda g,t=4:t and p([(f:=0)or[(c+(f==1>c)*8,f:=f+(c==5)*(d<4 or f))[0]for d,c in enumerate(r)]for r in zip(*g[::-1])],t-1)or g
+# spin fill
+p=lambda g,t=4:t and p([(f:=0)or[c+8*(f==1>c)+(f:=f+(c==5)*(f|(4>d)))*0 for d,c in enumerate(r)]for r in zip(*g[::-1])],t-1)or g
