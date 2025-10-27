fb079fcd59d7465a68ac423dd89aca599b12b5fe
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 16:36:22 +0900

    Simplify task302 fill logic

--- logic/task302.py
+++ logic/task302.py
@@ -3,6 +3,6 @@ R=range(1,12)
 def p(a):
  for i in R:
   for j in R:
-   if(g:=a[i-1][j])==(r:=a[i])[j-1]>r[j]:
-    for q in a[i:i+(s:=r[j:].index(g))]:q[j:j+s]=[g+s]*s
+   if a[i-1][j]==(r:=a[i])[j-1]==5>r[j]:
+    for q in a[i:i+(s:=r[j:].index(5))]:q[j:j+s]=[5+s]*s
  return a
