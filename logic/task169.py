d0ec6bcee611f3793e14b7def53fcd01293a0762
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 15:19:25 +0900

    Golf task169 solution

--- logic/task169.py
+++ logic/task169.py
@@ -1,11 +1,7 @@
 def p(g):
-    # recolor 5-blocks by size
+    #recolor5s
     def f(y,x,t,u):
         if 9>=y>=0<=x<=9>=g[y][x]==t:
-            g[y][x]=u
-            return 1+f(y+1,x,t,u)+f(y-1,x,t,u)+f(y,x+1,t,u)+f(y,x-1,t,u)
+            g[y][x]=u;return 1+f(y+1,x,t,u)+f(y-1,x,t,u)+f(y,x+1,t,u)+f(y,x-1,t,u)
         return 0
-    for y in range(10):
-        for x in range(10):
-            g[y][x]==5 and f(y,x,-99,5-f(y,x,5,-99))
-    return g
+    return[g[y][x]==5and f(y,x,-99,5-f(y,x,5,-99))for y in range(10)for x in range(10)]and g
