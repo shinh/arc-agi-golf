681771596a44c783393c73d8f656fa2f4fa529b7
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 15:24:18 +0900

    Optimize task 134 golf solution

--- logic/task134.py
+++ logic/task134.py
@@ -1,4 +1,4 @@
 def p(g):#crop
     c=[a for r,s in zip(g,g[1:]) for a,b,d,e in zip(r,r[1:],s,s[1:])if a==b==d==e>0][-1];A=sum({*sum(g,[])})-c
-    g=[*zip(*[r for r in zip(*[r for r in g if c in r])if c in r])];s=len(g)//3
+    g=[r for r in g if c in r];g=[*zip(*g)];g=[r for r in g if c in r];g=[*zip(*g)];s=len(g)//3
     return[[A*(v==c)for v in R[::s]]for R in g[::s]]
