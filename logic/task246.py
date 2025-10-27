f2868fbcec33ee49ca329224b85ea9c4d28a0f65
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 15:56:22 +0900

    Golf task246 path builder

--- logic/task246.py
+++ logic/task246.py
@@ -1,5 +1,5 @@
 def p(g):
     # join23
-    a=sum(g,[]).index;y,x,Y,X=divmod(a(2),w:=len(g[0]))+divmod(a(3),w)
-    while y-Y|x-X:x+=X>x;x-=X<x;g[y][x]=8;y+=x==X and(1,-1)[Y<y]
+    a=sum(g,[]);w=len(g[0]);i=a.index(2);j=a.index(3)
+    while (i:=i+((j%w>i%w)-(j%w<i%w) or w*((j>i)-(j<i))))-j:g[i//w][i%w]=8
     return g
