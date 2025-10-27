a965decd80dd9e8c4b3fcfbc9c370732922bf83e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 16:57:29 +0900

    Golf task112 solution

--- logic/task112.py
+++ logic/task112.py
@@ -1,5 +1,5 @@
 def p(a):
  w=len(a[0]);s=sum(a,[]);t=s.index(3);r=t//w*2+1;t=t%w*2+1
  for k,v in enumerate(s):
-  if v:p=a[r-k//w];p[k%w]=p[t-k%w]=a[k//w][t-k%w]=v
+  if v:b=a[r-k//w];b[k%w]=b[d:=t-k%w]=a[k//w][d]=v
  return a
