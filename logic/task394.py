59eef4c8c28fe536d6c3bbf462dcaad1e22e8413
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 20:05:30 +0900

    Improve task394 golf expression

--- logic/task394.py
+++ logic/task394.py
@@ -1,2 +1,2 @@
 def p(g):
- n=len(g);e=3-(n<7);y,x=divmod(sum(g,[]).index(0),n);b=g[y].count(0);return[g[y+a+e-(y+a>=n-e)*2*e][x:x+b]for a in range(b)]
+ n=len(g);e=3-(n<7);y,x=divmod(sum(g,[]).index(0),n);b=g[y].count(0);return[g[y+a-e+(y+a<n-e)*2*e][x:x+b]for a in range(b)]
