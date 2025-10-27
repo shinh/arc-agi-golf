974a3c24713b9647aec7fd830ae33d3833661f21
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 16:34:47 +0900

    Golf task 074 orbit minima

--- logic/task074.py
+++ logic/task074.py
@@ -1,4 +1,2 @@
-t={30,31}
-r=range(30)
 # orbit mins
-p=lambda a:[[min(a[x][y]for X,Y in((i,j),(j,i))for x in{X,31-X}-t for y in{Y,31-Y}-t)for j in r]for i in r]
+p=lambda a,r=range(30):[[min(a[x][y]for X in(j,i)for x in(X,31-X)for y in(i+j-X,31-i-j+X)if x<30>y)for j in r]for i in r]
