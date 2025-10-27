f973f924a59052ee1c085f3776f54ee059be3022
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 16:34:08 +0900

    Golf task046 anchor adjustments

--- logic/task046.py
+++ logic/task046.py
@@ -1,11 +1,11 @@
 def p(g):
  # align anchors
  r=[];v=0;s=[];z=0,0,0
- for c in (*zip(*g),z):
+ for c in*zip(*g),z:
   if c>z:s+=c,
   elif s:
-   l=5 in s[0]and s[0].index(5)
-   c=max(n-5*(n==5)for t in s for n in t)
-   r+=[[(n==5)*c or n for n in(z+t+z)[l-v+3:l-v+6]]for t in s]
-   v-=l-(5 in s[-1]and s[-1].index(5));s=[]
+   l=(s[0]+(5,)).index(5)%3
+   c=max(n for t in s for n in t if n-5)
+   r+=[[(n==5)*c or n for n in(z+t+z)[l-v+3:][:3]]for t in s]
+   v+=(s[-1]+(5,)).index(5)%3-l;s=[]
  return[*zip(*r)]
