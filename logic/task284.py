7c3f612bb5fde5c49b5214d842769e16c95a0127
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 15:25:28 +0900

    Golf task 284 solution

--- logic/task284.py
+++ logic/task284.py
@@ -1,11 +1,11 @@
 def p(g):
- a=[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v]
- q=a[0][0]!=a[1][0]
+ a,b=[(i,j)for i,r in enumerate(g)for j,v in enumerate(r)if v]
+ q=a[0]!=b[0]
  if q:g=[*map(list,zip(*g))]
- m=(a[1][1-q]-a[0][1-q]-1)>>1
- for d in 1,-1:
-  y=a[d<0];x=y[q];j=y[1-q];w=j+d*m;c=g[x][j]
-  g[x][j:w:d]=[c]*m
+ m=(b[1-q]-a[1-q]-1)>>1
+ for d,u in((1,a),(-1,b)):
+  x=u[q];y=u[1-q];w=y+d*m;c=g[x][y]
+  g[x][y:w:d]=[c]*m
   for r in g[x-2:x+3]:r[w-d]=c
   g[x-2][w]=g[x+2][w]=c
- return (g,[*zip(*g)])[q]
+ return(g,[*zip(*g)])[q]
