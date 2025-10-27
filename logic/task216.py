935ee8ed83ec510a7967fc081954e9d521ea2cc8
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 17:07:51 +0900

    Golf task216 walrus

--- logic/task216.py
+++ logic/task216.py
@@ -1,2 +1,2 @@
 # densest block
-p=lambda a:max((sum(sum(b,[]))-w*h,w*h,b)for y in range(20)for c in range(20)for w,h in[((a[y][c:]+[0]).index(0),([r[c]for r in a[y:]]+[0]).index(0))]for b in[[r[c:c+w]for r in a[y:y+h]]])[2]
+p=lambda a:max((sum(sum((b:=[r[c:c+w]for r in a[y:y+h]]),[]))-w*h,w*h,b)for y in range(20)for c in range(20)for w,h in[((a[y][c:]+[0]).index(0),([r[c]for r in a[y:]]+[0]).index(0))])[2]
