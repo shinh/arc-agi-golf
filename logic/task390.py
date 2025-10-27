46d7ea6bc2930fe110f548ea99c7caab1b5c85d1
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 16:53:18 +0900

    Golf task390 by reusing slice anchor

--- logic/task390.py
+++ logic/task390.py
@@ -3,5 +3,5 @@
 def p(g):
  for _ in"r"*4:
   for r in(g:=[*map(list,zip(*g[::-1]))]):
-   if 2in r:c=r.index;s=c(2);m=c(2,s+1)-s-3>>1;r[s+~m:~-s],r[s+2:s+m+2]=r[s+2:s+m+2][::-1],[0]*m
+   if 2in r:c=r.index;s=c(2);k=s+2;m=c(2,s+1)-s-3>>1;r[s+~m:~-s],r[k:k+m]=r[k:k+m][::-1],[0]*m
  return g
