9eb0db0da3a373b31d2349b71288484d3f224bdf
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 16:15:59 +0900

    Simplify task382 row check for better compression

--- logic/task382.py
+++ logic/task382.py
@@ -3,7 +3,7 @@ def p(g,s=1):#rot+drop
  for _ in[0]*4:
   if s*(2in(g:=[*map(list,zip(*g[::-1]))])[0]+g[-1]):
    for r,R in e(g):
-    if R[0]>7:
+    if R[0]:
      for c,a in e(g[0]):
       r+=a==2;r-=g[-1][c]==2
       if-1<r<len(g):g[r][c]=8
