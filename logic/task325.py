1d3b0d1b5e71f2c55e16fea0ca98113abb1d6001
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 16:38:06 +0900

    Golf task325 solution

--- logic/task325.py
+++ logic/task325.py
@@ -1,8 +1,8 @@
-def p(g):
- a,b=len(g),len(g[0])
- def f(i,j):
-  if 0<=i<a and 0<=j<b and g[i][j]:
-   g[i][j]=0;f(i+1,j);f(i-1,j);f(i,j+1);f(i,j-1);return 1
+def p(a):
+ i,d=len(a),len(a[0])
+ def r(n,e):
+  if 0<=n<i and 0<=e<d and a[n][e]:
+   a[n][e]=0;r(n+1,e);r(n-1,e);r(n,e+1);r(n,e-1);return 1
   return 0
- r=range(sum(f(i,j)for i in range(a)for j in range(b)))
- return[[8*(i==j)for j in r]for i in r]
+ o=range(sum(r(n,e)for e in range(d)for n in range(i)))
+ return[[8*(n==e)for e in o]for n in o]
