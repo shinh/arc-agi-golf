c8aae6118c53fe9ace475c79829afa91a19c828a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 15:16:42 +0900

    Golf task099 solution

--- logic/task099.py
+++ logic/task099.py
@@ -1,7 +1,5 @@
 def p(g):
  for l in 0,1,4:
-  k=[(r[l+2],i)for i,r in enumerate(g)if r[l]*r[l+4]]
-  if k:
-   for r in g[k[0][1]-1:k[-1][1]+1]:
-    r[l:l+5]=[v or max(k)[0]for v in r[l:l+5]]
+  if k:=[(r[l+2],i)for i,r in enumerate(g)if r[l]*r[l+4]]:
+   for r in g[k[0][1]-1:k[-1][1]+1]:r[l:l+5]=[v or max(k)[0]for v in r[l:l+5]]
  return g
