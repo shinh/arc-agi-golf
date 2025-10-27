b098b100516daebd2bb17df435d86f49226893d6
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-27 16:36:58 +0900

    Golf task225 solution

--- logic/task225.py
+++ logic/task225.py
@@ -1,4 +1,4 @@
 def p(g):
  r=c=0;R=range(6)
  while g[r][c]<1:c=-~c%6;r+=c<1
- return[[g[i][j]if(i-r)%4<2or(j-c)%4<2else g[r+(i<r)][c+(j<c)]for j in R]for i in R]
+ return[[(g[r+(i<r)][c+(j<c)],g[i][j])[i-r&j-c&2<1]for j in R]for i in R]
