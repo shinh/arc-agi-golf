f0cdcb491020a35b1b1145d5b47272608e6e3a61
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 15:54:07 +0900

    Golf task013 index lookup

--- logic/task013.py
+++ logic/task013.py
@@ -1,4 +1,4 @@
 def p(g):
- g=[g,[*map(list,zip(*g))]][t:=max(g[0]+g[-1])<1];(s,c),(u,d)=sorted((j,v)for r in g for j,v in enumerate(r)if v)
+ g=[g,[*map(list,zip(*g))]][t:=max(g[0]+g[-1])<1];(s,c),(u,d)=sorted((r.index(v),v)for r in g for v in r if v)
  for r in g:r[s::u-s]=([c,d]*7)[:len(r[s::u-s])]
  return(g,[*zip(*g)])[t]
