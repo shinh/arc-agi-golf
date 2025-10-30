df7978ebb2cba265c7bfa0b3ae1f931ca8c6a3e0
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 15:44:15 +0900

    Golf task107 implementation

--- logic/task107.py
+++ logic/task107.py
@@ -1,10 +1,8 @@
 def p(g):#scale+diag
- n=len({*sum(g,[])})-1
- z=range(n)
- sy,sx=n*(g[0][1]<1),n*(g[1][0]<1)
+ n=len({*sum(g,[])})-1;z=range(n);sy,sx=(g[0][1]<1)*n,(g[1][0]<1)*n
  g=[[c for c in r for _ in z]for r in g for _ in z]
  for i in z:
   for y in sy+~i,sy+n*2+i:
    for x in sx+~i,sx+n*2+i:
-    if y|x>-1and g[y][x]<1:g[y][x]=2
+    if(1>g[y][x])*(y|x>-1):g[y][x]=2
  return g
