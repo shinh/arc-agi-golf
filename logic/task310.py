87aa70942314b13c3f14e70f31ce2584a6a5d364
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 17:03:55 +0900

    Golf task310 solution

--- logic/task310.py
+++ logic/task310.py
@@ -1,1 +1,1 @@
-def p(m):f=sum(m,[]);g=f.count;i=f.index(min(f,key=g));n=len(m);l=g(f[i])+4>>2;return[x[i%n:][:l]for x in m[i//n:][:l]]
+def p(m):f=sum(m,[]);g,n=f.count,len(m);l=g(f[i:=f.index(min(f,key=g))])+4>>2;return[x[i%n:][:l]for x in m[i//n:][:l]]
