58be564d8c2571ac14f88affb45927b575abd7a0
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 19:23:57 +0900

    Improve task260 compression

--- logic/task260.py
+++ logic/task260.py
@@ -1,2 +1,2 @@
 # extend diagonal endpoints while clearing intruders
-p=lambda g,e=enumerate:(lambda a,b,c,d:[[(x%5 and x,c)[(a<d)*(i-j==a-2)+(b>d)*(i-j==b+2)]for j,x in e(r)]for i,r in e(g)])(min(i-j for i,r in e(g)for j,x in e(r)if x==5),max(i-j for i,r in e(g)for j,x in e(r)if x==5),*next((v,i-j)for i,r in e(g)for j,v in e(r)if v%5))
+p=lambda g,e=enumerate:(lambda a,b,c,d:[[(x%5 and x,c)[(a<d)*(i-j==a-2)+(b>d)*(i-j==b+2)]for j,x in e(r)]for i,r in e(g)])(min(i-j for i,r in e(g)for j,x in e(r)if x==5),max(i-j for i,r in e(g)for j,x in e(r)if x==5),*next((x,i-j)for i,r in e(g)for j,x in e(r)if x%5))
