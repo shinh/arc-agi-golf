cb20b07be8b053026f28e993d8146a60ce57af36
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 17:10:53 +0900

    Golf task027 using flattened indices

--- logic/task027.py
+++ logic/task027.py
@@ -1,4 +1,4 @@
 def p(g):# rot
- r=range(10);s={(j,9-i)for i in r for j in r if g[i][j]};m,n=map(max,zip(*s));o=m>n
+ s={(k%10,9-k//10)for k,v in enumerate(sum(g,[]))if v};m,n=map(max,zip(*s));o=m>n
  for j,k in s:g[j][k+o]=g[j][k+o]or 2
  return g
