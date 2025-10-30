c87eff9ffac6b5fba5c3ffb398ce7b53a89ff928
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 17:11:48 +0900

    Golf task147 solution

--- logic/task147.py
+++ logic/task147.py
@@ -1,1 +1,1 @@
-p=lambda g:[[v and[3,8][sum(r[j-(j>0):j+2]+[x[j]for x in g[i-(i>0):i+2]])>6]for j,v in enumerate(r)]for i,r in enumerate(g)]
+E=enumerate;p=lambda g:[[v and 8-5*(sum(r[j-(j>0):j+2]+[x[j]for x in g[i-(i>0):i+2]])<7)for j,v in E(r)]for i,r in E(g)]
