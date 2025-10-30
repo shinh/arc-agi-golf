a960309e59e40ef5959fe5a87f2849d501deb43a
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 15:17:17 +0900

    Golf task044 solution

--- logic/task044.py
+++ logic/task044.py
@@ -1,13 +1,13 @@
 def p(g):
- s=sum(g,[]);L=[(),()]
+ s=sum(g,[]);l=[(),()]
  for y,r in enumerate(g):
   if 5in r:
    for x in range(r.index(5)+1,9-r[::-1].index(5)):
-    if not s[i:=y*10+x]:L[y>4]+=i,
- t=[sum(1<<(x-f[0])for x in f)for f in L]
+    if not s[i:=y*10+x]:l[y>4]+=i,
+ d={tuple(x-f[0]for x in f):f for f in l}
  for k in{*s}-{0,5}:
-  q=[i for i,a in enumerate(s)if a==k];x=sum(1<<(i-q[0])for i in q)
-  if x in t:
+  q=[i for i,a in enumerate(s)if a==k]
+  if f:=d.get(tuple(i-q[0]for i in q)):
    for i in q:g[i//10][i%10]=0
-   for i in L[t.index(x)]:g[i//10][i%10]=k
+   for i in f:g[i//10][i%10]=k
  return g
