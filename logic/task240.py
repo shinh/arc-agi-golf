f1938147edca2d82932bbea4c880895ef4ca7f4e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 15:53:11 +0900

    Revert "Merge pull request #1893 from shinh/codex/run-make_instruction.py-with-select-important"
    This reverts commit a9318eee38b70ad16642883e538be56ea3dc2a7e, reversing
    changes made to f13f93076b468b984d82f22cfb0102368d5b0e5a.
    
    broken??

--- logic/task240.py
+++ logic/task240.py
@@ -1,6 +1,5 @@
 def p(g):
-# mirrored quadrants never mix colours, so bitwise or keeps the hue
  for i in range(1,9,2):
-  for j in range(1,9,2):g[i][j]=g[i][~j]=g[~i][j]=g[~i][~j]=g[~i][j]|g[~i][~j]|g[i][j]|g[i][~j]
+  for j in range(1,9,2):g[i][j]=g[i][~j]=g[~i][j]=g[~i][~j]=g[~i][j]or g[~i][~j]or g[i][j]or g[i][~j]
   for j in range(i+2,17-i,2):g[i][j]=g[~i][j]=g[j][i]=g[j][~i]=g[i][i+2]
  return g
