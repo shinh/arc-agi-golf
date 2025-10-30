4203ac9ce94e750f111e5164cf102c78db66a250
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 19:21:01 +0900

    Refine task323 golf path

--- logic/task323.py
+++ logic/task323.py
@@ -1,7 +1,7 @@
 def p(g):
  for d in-1,1:
   r,c=divmod(sum(g,[]).index(8),13)#s
-  for t in range(169):
-   b=t&2>0;r+=d-d*b;c-=d*b
+  for b in[0,0,1,1]*6:
+   r+=d-d*b;c-=d*b
    if-1<r<13>c>-1:g[r][c]=5
  return g
