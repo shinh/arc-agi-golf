04713294b8d972b09253e02768fa1436bfb3915f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 15:55:15 +0900

    Golf task368 logic

--- logic/task368.py
+++ logic/task368.py
@@ -5,8 +5,9 @@ def p(g):
     h=w=1
     while i+h<10 and g[i+h][j]%5:h+=1
     while j+w<10 and g[i][j+w]%5:w+=1
-    for y in range(10):
-     for x in range(10):
-      if g[y][x]==5:
-       for k in range(h):g[y+k][x:x+w]=g[i+k][j:j+w]
+    b=[g[i+k][j:j+w]for k in range(h)]
+    for i in range(10):
+     for j in range(10):
+      if g[i][j]==5:
+       for k in range(h):g[i+k][j:j+w]=b[k]
     return g
