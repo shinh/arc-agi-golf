f83cadf8e3de7e5720a7e193620b590c3bf15037
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 19:20:53 +0900

    Golf task004 solution

--- logic/task004.py
+++ logic/task004.py
@@ -1,6 +1,3 @@
 def p(g,m=-1):
- for r in g[::-1]:
-  p,m=m,-1
-  for x,v in enumerate(r):v and(m:=x)
-  r[:0]=0,;r[p]=r.pop(p+1)or r[p]
+ for r in g[::-1]:p=m;m=-1;[m:=x for x,v in enumerate(r)if v];r[:0]=0,;r[p]+=r.pop(p+1)
  return g
