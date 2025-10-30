b932f12d1e23590c3d3266512b93a17cbd52880f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 19:21:47 +0900

    Refine task250 golf solution

--- logic/task250.py
+++ logic/task250.py
@@ -1,4 +1,4 @@
 def p(g):
  s=sum(g,[]);x=s.index;a,b=divmod(x(2),10)
- while 5 in s:i=x(5);j=i%10;g[(k:=i//10)][j]=s[i]=0;g[min(a+2,max(a-1,k))][min(b+2,max(b-1,j))]=5
+ while 5 in s:s[i:=x(5)]=g[k:=i//10][i%10]=0;g[min(a+2,max(a-1,k))][min(b+2,max(b-1,i%10))]=5
  return g
