2066da76dc6be17ee28b7a6bdb2dc1f67c89606f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 19:25:19 +0900

    Golf task019 solution

--- logic/task019.py
+++ logic/task019.py
@@ -1,2 +1,2 @@
 def p(g):#tile diag8
- g=[r*2 for r in g]*2;t=-1,1;e=enumerate;return[[c or 8*any(g[y+d][x+e]for d in t for e in t if len(g)>y+d>-1<x+e<len(r))for x,c in e(r)]for y,r in e(g)]
+ g=[r*2 for r in g]*2;E=enumerate;return[[c or 8*any(g[y+d][x+e]for d in(-1,1)for e in(-1,1)if len(g)>y+d>-1<x+e<len(r))for x,c in E(r)]for y,r in E(g)]
