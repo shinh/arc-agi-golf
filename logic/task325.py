ccd6b65c12a1f5a36ae78b6b07868bb58c4ed6cf
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 17:10:17 +0900

    Golf task325 flood fill

--- logic/task325.py
+++ logic/task325.py
@@ -1,8 +1,8 @@
 def p(g):
  a,b=len(g),len(g[0])
  def f(i,j):
-  if i in range(a)and j in range(b)and g[i][j]:
+  if a>i>=0<=j<b and g[i][j]:
    g[i][j]=0;f(i+1,j);f(i-1,j);f(i,j+1);f(i,j-1);return 1
   return 0
- k=sum(f(i,j)for i in range(a)for j in range(b))
- return[[8*(i==j)for j in range(k)]for i in range(k)]
+ r=range(sum(f(i,j)for i in range(a)for j in range(b)))
+ return[[8*(i==j)for j in r]for i in r]
