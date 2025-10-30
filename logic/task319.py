62de1074591e32ec3d24fb0602d8c1f0d70a146f
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 15:16:43 +0900

    Golf task319 overlay search

--- logic/task319.py
+++ logic/task319.py
@@ -5,8 +5,5 @@ def p(g):
         o=g
         for _ in 0,0:o=[*zip(*([[b,d][q==d]for q in e]for e in o if d in e))]
         H=len(o)*2;W=len(o[0])*2
-        for y in r(-H,Y):
-            for x in r(-W,X):
-                for q in s-{d}:
-                    if c(q)==sum(0<=y+i<Y and 0<=x+j<X and o[i//2][j//2]==d and g[y+i][x+j]==q for i in r(H) for j in r(W))==sum(0<=y+i<Y and 0<=x+j<X and o[i//2][j//2]==d for i in r(H) for j in r(W)):return o
+        if any(c(q)==sum(0<=y+i<Y and 0<=x+j<X and o[i//2][j//2]==d and g[y+i][x+j]==q for i in r(H) for j in r(W))==sum(0<=y+i<Y and 0<=x+j<X and o[i//2][j//2]==d for i in r(H) for j in r(W))for y in r(-H,Y)for x in r(-W,X)for q in s-{d}):return o
     return g
