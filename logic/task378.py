ed860adf002a80e47dd801e82c66f5556d44323b
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 15:41:58 +0900

    Golf task378 solution

--- logic/task378.py
+++ logic/task378.py
@@ -1,15 +1,1 @@
-import re
-
-p = lambda g, k=3: -k * g or (
-    p(
-        eval(
-            re.sub(
-                r"(0)(?=.{%d}(?:.{%d}){0,9}([1-9]).{2}\2.{%d}\2.{2}0.{%d}([1-9]))"
-                % (len(g) * 3 + 4, len(g) * 3 + 5, len(g) * 3 - 2, len(g) * 3 + 4),
-                r"\3",
-                str([*zip(*g[::-1])]),
-            )
-        ),
-        k - 1,
-    )
-)
+from re import sub;p=lambda g,k=3:k<0 and g or p(eval(sub(r'(0)(?=.{%d}(?:.{%d}){0,9}([1-9]).{2}\2.{%d}\2.{2}0.{%d}([1-9]))'%((n:=len(g)*3+4),n+1,n-6,n),r'\3',str([*zip(*g[::-1])]))),k-1)
