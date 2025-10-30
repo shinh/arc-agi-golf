499990b99d2566c306c895562c06fe378b4ed233
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 15:47:36 +0900

    Golf task192 neighbor checks

--- logic/task192.py
+++ logic/task192.py
@@ -1,2 +1,2 @@
 def p(g):# near b
-    f=sum(g,[]);b=max({*f}-{0},key=f.count);*c,=zip(*g);e=enumerate;return[[b*(v>0)*(b in c[x][y-(y>0):y+2])*(b in r[x-(x>0):x+2])for x,v in e(r)]for y,r in e(g)]
+    f=sum(g,[]);b=max({*f}-{0},key=f.count);*c,=zip(*g);e=enumerate;return[[b*(v>0)*(b in c[x][:y+2][-3:])*(b in r[:x+2][-3:])for x,v in e(r)]for y,r in e(g)]
