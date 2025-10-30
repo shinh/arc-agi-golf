a5187fedf2826905f6540175c4d602ba8e9e98cf
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 17:14:12 +0900

    Golf task246 solution

--- logic/task246.py
+++ logic/task246.py
@@ -1,5 +1,4 @@
-def p(g):
-    # join23
-    a=sum(g,[]);w=len(g[0]);i=a.index(2);j=a.index(3)
-    while (i:=i+((j%w>i%w)-(j%w<i%w) or w*((j>i)-(j<i))))-j:g[i//w][i%w]=8
+def p(g):# join23
+    w=len(g[0]);i,j=map(sum(g,[]).index,(2,3))
+    while(i:=i+((j%w>i%w)-(j%w<i%w)or w*(1,-1)[i>j]))-j:g[i//w][i%w]=8
     return g
