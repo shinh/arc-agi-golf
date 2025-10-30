596f3b88930757e56032c26848481d1799e936a9
Darren Smith <darrenks@golfscript.com>
2025-10-30 16:29:10 -0600

    golf 222 a bit

--- logic/task132.py
+++ logic/task132.py
@@ -1,6 +1,16 @@
+# 162
 def p(g):
  w,b=len(g[0]),sum(g,[])
  for v in{*b}-{0}:
   i=b.index;a=i(v);d=i(v,a+1);y,l=sorted((a%w,d%w))
   for r in g[a//w:d//w+1]:r[y:l+1]=[v]*(l-y+1)
  return g
+
+# def p(g):
+#     # for each color
+#         # find minx,miny,maxx,maxy
+#         # if present, fill
+
+# def p(g):
+#     # for each cell and each color, if there is said color before and after it x and y, then make that color
+#     return [[max( for c in range(10)) for x in range(len(g[0]))]for y in range(len(g))]
--- logic/task222.py
+++ logic/task222.py
@@ -1,11 +1,57 @@
+# # 198 (want 103)
+# def p(g):
+#  t=[]
+#  for r in range(15):
+#   for c in range(15):
+#    if g[r][c]==g[r+1][c]==g[r][c+1]==g[r+1][c+1]>0:t+=g[r][c],
+#  v=max(t,key=t.count);o=[[0]*16for _ in g]
+#  for r in range(15):
+#   for c in range(15):
+#    if g[r][c]==g[r+1][c]==g[r][c+1]==g[r+1][c+1]==v:
+#     o[r][c]=o[r][c+1]=o[r+1][c]=o[r+1][c+1]=v
+#  return o
+
+# doesn't work, there are some with a 2x2 square of another color
+# def p(g):
+#     h=[[0]*16for _ in g]
+#     for r in range(15):
+#         for c in range(15):
+#             if g[r][c]==g[r+1][c]==g[r][c+1]==g[r+1][c+1]:
+#                 h[r][c]=h[r+1][c]=h[r][c+1]=h[r+1][c+1]=g[r][c]
+#     return h
+
+# def p(g):
+#     for xs in range(15,1,-1):
+#         for ys in range(15,1,-1):
+#             for y in range(13):
+#                 for x in range(13):
+#                     o=[[0]*16for _ in g]
+#                     for dy in range(y,min(y+ys,15)):
+#                         o[dy][x:x+xs]=g[dy][x:x+xs]
+#                     s={*sum(o,[])}
+# #                     s={*sum(r[x:x+xs] for r in g[y:y+ys],[])}
+#                     if len(s)==2:
+#                         return o
+#
+#
+# def p(g):
+#     h=[[0]*16for _ in g]
+#     gt=[*zip(*g)]
+#     for r in range(15):
+#         for c in range(15):
+#             z=g[r][c]
+#             if g[r][c:].index(z)+g[r][:c:-1].index(z)+gt[c][r:].index(z)+gt[c][:r:-1].index(z)>5:
+#                 h[r][c]=z
+#     return h
+
 def p(g):
- t=[]
- for r in range(15):
-  for c in range(15):
-   if g[r][c]==g[r+1][c]==g[r][c+1]==g[r+1][c+1]>0:t+=g[r][c],
- v=max(t,key=t.count);o=[[0]*16for _ in g]
- for r in range(15):
-  for c in range(15):
-   if g[r][c]==g[r+1][c]==g[r][c+1]==g[r+1][c+1]==v:
-    o[r][c]=o[r][c+1]=o[r+1][c]=o[r+1][c+1]=v
- return o
+    h=[[0]*16for _ in g]
+    for b in [2,3]:
+        b3=5-b
+        for y in range(15):
+            for x in range(15):
+#                 if z and [r[x:x+b] for r in g[y:y+b3]]==[[z]*b]*b3:
+#                 if z and [*zip(*g[y:y+b3])][x:x+b]==[(z,)*b3]*b:
+                for dy in range(b3*([r[x:x+b] for r in g[y:y+b3]]==[[g[y][x]]*b]*b3)):
+                    h[y+dy][x:x+b]=[g[y][x]]*b
+    return h
