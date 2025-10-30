3b3038e68a5844cdaf07ab0d74b3b243045d7b4d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 19:19:24 +0900

    Golf task355 solution

--- logic/task355.py
+++ logic/task355.py
@@ -1,1 +1,2 @@
-def p(g):b=min(s:=sum(g,[]),key=s.count);return[[max(d:=[sum({*r}&{*c}) for c in zip(*g) for r,v in zip(g,c)if v==b],key=d.count)-b]]
+# prefer the color most aligned with the rare value
+p=lambda g:[[max(d:=[sum({*r}&{*c}) for c in zip(*g) for r,v in zip(g,c)if v==(b:=min(s:=sum(g,[]),key=s.count))],key=d.count)-b]]
