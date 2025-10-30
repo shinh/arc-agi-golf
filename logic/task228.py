cc986aa76f3076a09ecc1c5a259c969e99184f61
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 15:44:38 +0900

    Golf task228 assignments

--- logic/task228.py
+++ logic/task228.py
@@ -1,7 +1,6 @@
 def p(a):
  r=next(filter(any,a));b=a.index(r);c=~a[::-1].index(r);e=~r[::-1].index(max(r));d=r.index(max(r))
  #
- a[b-1][e+1]=a[c-1][d+1];a[b-1][d-1]=a[c-1][e-1]
- a[c+1][e+1]=a[b+1][d+1];a[c+1][d-1]=a[b+1][e-1]
+ a[b-1][e+1]=a[c-1][d+1];a[c+1][e+1]=a[b+1][d+1];a[b-1][d-1]=a[c-1][e-1];a[c+1][d-1]=a[b+1][e-1]
  a[b+1][d+1]=a[b+1][e-1]=a[c-1][d+1]=a[c-1][e-1]=0;return a
 
