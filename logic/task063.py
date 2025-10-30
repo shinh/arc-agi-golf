d70dc2242446f5028cdfe1388417ccd340133336
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 19:21:55 +0900

    Golf task063 implementation

--- logic/task063.py
+++ logic/task063.py
@@ -1,4 +1,5 @@
+# row/col empty ->3.
 def p(g):
- t=g[1:-1];b=[*map(any,zip(*t))][1:-1]
- for r in t:a=r[1:-1];r[1:-1]=(x or(1^(any(a)&y))*3for x,y in zip(a,b))
+ _,*t,_=g;_,*b,_=map(any,zip(*t))
+ for r in t:_,*a,_=r;r[1:-1]=(x or-3*any(a)*y+3 for x,y in zip(a,b))
  return g
