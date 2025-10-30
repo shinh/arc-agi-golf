68cffbd801f9f9d89706add40f70a1731e3e9a1e
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 19:22:20 +0900

    Optimize task340 by duplicated edge updates

--- logic/task340.py
+++ logic/task340.py
@@ -4,7 +4,12 @@ def p(g):
   for x in range(1,len(k)-1):
    v,k[x]=k[x],0
    if v==g[1][0]:k[1]=v
+   if v==g[1][0]:k[1]=v
+   if v==g[1][0]:k[1]=v
+   if v==g[1][-1]:k[-2]=v
    if v==g[1][-1]:k[-2]=v
    if v==g[0][1]:g[1][x]=v
+   if v==g[0][1]:g[1][x]=v
+   if v==g[-1][1]:g[-2][x]=v
    if v==g[-1][1]:g[-2][x]=v
  return g
