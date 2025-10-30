4b9e30d42e5deeb067fad4cb53edbbefd72d632d
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 15:54:26 +0900

    Golf task222 solution

--- logic/task222.py
+++ logic/task222.py
@@ -1,11 +1,11 @@
 def p(g):
- R=range(15);t=[]
- for r in R:
-  for c in R:
+ t=[]
+ for r in range(15):
+  for c in range(15):
    if g[r][c]==g[r+1][c]==g[r][c+1]==g[r+1][c+1]>0:t+=g[r][c],
  v=max(t,key=t.count);o=[[0]*16for _ in g]
- for r in R:
-  for c in R:
+ for r in range(15):
+  for c in range(15):
    if g[r][c]==g[r+1][c]==g[r][c+1]==g[r+1][c+1]==v:
     o[r][c]=o[r][c+1]=o[r+1][c]=o[r+1][c+1]=v
  return o
