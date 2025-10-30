4a6e1d146a1d53ef9f4a8d5b96c8402025c521f8
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2025-10-30 15:47:01 +0900

    Golf task046 solution

--- logic/task046.py
+++ logic/task046.py
@@ -1,11 +1,7 @@
 def p(g):
- # align anchors
- r=[];v=0;s=[];z=0,0,0
+ #align anchors
+ r=[];v=0;s=();z=0,0,0
  for c in*zip(*g),z:
   if c>z:s+=c,
-  elif s:
-   l=(s[0]+(5,)).index(5)%3
-   c=max(n for t in s for n in t if n-5)
-   r+=[[(n==5)*c or n for n in(z+t+z)[l-v+3:][:3]]for t in s]
-   v+=(s[-1]+(5,)).index(5)%3-l;s=[]
+  elif s:l=(s[0]+(5,)).index(5)%3;u=sum(s,());c=sum({*u})-5;r+=[[c*(n==5)or n for n in(z+t+z)[l-v+3:][:3]]for t in s];v+=(s[-1]+(5,)).index(5)%3-l;s=()
  return[*zip(*r)]
