74c72e9414378256c24485f5457aac8b1bb1d430
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2026-04-16 20:03:58 +0900

    Shorten task283 comment spacing

--- logic/task283.py
+++ logic/task283.py
@@ -1,3 +1,3 @@
-def p(g):#frame 5-rect via set
- for i in (s:={i+i//10 for i in range(100)if sum(g,[])[i]>4}):g[i//11][i%11]=len({i-1,i+1,i-11,i+11}&s)*3%5
+def p(g):#frame via set
+ for i in(s:={i+i//10 for i in range(100)if sum(g,[])[i]>4}):g[i//11][i%11]=len({i-1,i+1,i-11,i+11}&s)*3%5
  return g
