6d7bb570f13d0309ae68502ee2edda09f757d3a3
Shinichiro Hamaji <shinichiro.hamaji@gmail.com>
2026-04-16 20:04:04 +0900

    Optimize task 220 transformation

--- logic/task220.py
+++ logic/task220.py
@@ -1,5 +1,5 @@
 def p(g,E=enumerate):#paint nb
  for r,R in E(g):
   for c,C in E(R):
-   for i in(-1,1)*(1<C%6<4):R[c+i]=k=C*5%9;g[r+i][c-1:c+2]=[k]*3
+   for i in(-1,1)*(1<C%6<4):R[c+i]=k=5*C%9;g[r+i][c-1:c+2]=[k]*3
  return g
